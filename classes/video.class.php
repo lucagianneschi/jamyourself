c3584499a70fa229bd53727c8501b96b409e7a2d
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-05-24 20:11:51 +0200

    Allineamento della classe secondo standard e secondo docuwiki; aggiunto metodi per il loveCounter

--- classes/video.class.php
+++ classes/video.class.php
@@ -1,203 +1,193 @@
 <?php
 
+//descrizione della classe:http://www.socialmusicdiscovering.com/dokuwiki/doku.php?id=definizioni:properties_classi:video
+//api: DA FARE!!
+
 class Video{
 
-	private $objectId;
-	private $createdAt;
-	private $updatedAt;
-	private $active;
-	private $URL;	
-	private $uploader;
-	private $author;
-	private $title;
-	private $description;
-	private $thumbnail;
-	private $tags;
-	private $duration;
-	private $counter;
-	private $loveCounter;
-	private $featuring;
-	private $ACL;
+	private $objectId;  		//string: objectId su Parse  								#1
+	private $active;    		//BOOL: segnala se l'istanza della classe è attiva o no 	#17
+	private $author;			//string: Utente che ha girato il video 					#4
+    private $counter;			//number: Contatore per il gradimento 						#11
+	private $description;		//string: Descrizione del video data dall'utente  			#7
+	private $duration;			//number: Durata del video									#10
+	private $fromUser;   		//User: Punta allo user che effettua l'embed del video  	#3
+	private $featuring;			//array: per segnalare la presenza di altri utenti 			#12 
+	private $loveCounter;		//number: Contatore per il numero di azioni love 			#16
+	private $tags;				//array: stringhe per la categorizzazione del video 		#9
+	private $thumbnail;  		//string: Percorso immagine del thumbnail del video 		#8
+	private $title;				//string:Titolo del video  									#5
+	private $URL;    			//string: URL del video  									#2
+	private $createdAt;			//DataTime: data di creazione del video						#13
+	private $updatedAt;			//DataTime: data di update del video						#14
+	private $ACL;				//Access Control List										#15
 
 
 	public function __construct(){
 	}
 
 	/** FUNZIONI GET */
-
-	public function getCounter(){
-
-		return $this->counter;
-
-	}
-
+	//string: objectId su Parse  								#1
 	public function getObjectId(){
-
 		return $this->objectId;
-
 	}
 
-	
+	//BOOL: segnala se l'istanza della classe è attiva o no 	#17
 	public function getActive(){
 		return $this->active;
 	}
 	
-	public function getURL(){
-
-		return $this->URL;
-
-	}
-
-	public function getUploader(){
-
-		return $this->uploader;
-
-	}
-	
+	//string: Utente che ha girato il video 					#4
 	public function getAuthor(){
-
 		return $this->author;
-
 	}
 
-	public function getTitle(){
-
-		return $this->title;
-
+	//number: Contatore per il gradimento 						#11
+	public function getCounter(){
+		return $this->counter;
 	}
 
+	//string: Descrizione del video data dall'utente  			#7
 	public function getDescription(){
-
 		return $this->description;
-
 	}
 
-	public function getThumbnail(){
-
-		return $this->thumbnail;
-
-
-	}
-
-	public function getTags(){
-
-		return $this->tags;
-
-	}
-
+	//number: Durata del video									#10
 	public function getDuration(){
-
 		return $this->duration;
-
 	}
 
+	
+	//array: per segnalare la presenza di altri utenti 			#12
 	public function getFeaturing(){
-
 		return $this->featuring;
-
 	}
 
+	//User: Punta allo user che effettua l'embed del video  	#3
+	public function getFromUser(){
+		return $this->fromUser;
+	}
+
+	//number: Contatore per il numero di azioni love 			#16
+	public function getLoveCounter(){
+		return $this->loveCounter;
+	}
+
+	//array: stringhe per la categorizzazione del video 		#9
+	public function getTags(){
+		return $this->tags;
+	}
+
+	//string:Titolo del video  									#5
+	public function getTitle(){
+		return $this->title;
+	}
+
+	//string: Percorso immagine del thumbnail del video 		#8
+	public function getThumbnail(){
+		return $this->thumbnail;
+	}
+
+	//string: URL del video  									#2
+	public function getURL(){
+		return $this->URL;
+	}
+
+	//DataTime: data di creazione del video						#13
 	public function getCreatedAt(){
-
 		return $this->createdAt;
-
 	}
 
+	//DataTime: data di update del video						#14
 	public function getUpdatedAt(){
-
 		return $this->updatedAt;
+	}
 
+	//Access Control List										#15
+	public function getACL(){
+		return $this->ACL;
 	}
 
 	/** FUNZIONI SET */
 	
-	public function setURL($URL){
-	
-		$this->URL = $URL;
-	
-	}
-	
-	public function setACL($ACL){
-		$this->ACL = ACL;
+	//string: objectId su Parse  								#1
+	public function setObjectId($objecId){
+		$this->objectId = $objecId;
 	}
 
-	public function setUploader(User $uploader){
-
-		$this->uploader = $uploader;
-
-	}
-
-	public function setAuthor($author){
-
-		$this->author = $author;
-
-	}
-
-	public function setTitle($title){
-
-		$this->title = $title;
-	}
-
-	public function setDescription($description){
-
-		$this->description = $description;
-
-	}
-
-	public function setThumbnail($thubmnail){
-
-		$this->thumbnail = $thubmnail;
-	}
-
+	//BOOL: segnala se l'istanza della classe è attiva o no 	#17
 	public function setActive($active){
 		$this->active = $active;
 	}
-	
-	public function setTags($tags){
 
-		$this->tags = $tags;
-
+	//string: Utente che ha girato il video 					#4
+	public function setAuthor($author){
+		$this->author = $author;
 	}
 
+	//number: Contatore per il gradimento 						#11
+	public function setCounter($counter){
+		$this->counter = $counter;
+	}
+
+	//string: Descrizione del video data dall'utente  			#7
+	public function setDescription($description){
+		$this->description = $description;
+	}
+
+	//number: Durata del video									#10
 	public function setDuration($duration){
-
 		$this->duration = $duration;
-
 	}
 
+	//array: per segnalare la presenza di altri utenti 			#12
 	public function setFeaturing(array $featuring){
-
 		$this->featuring = $featuring;
 	}
 
-	public function setObjectId($objecId){
-
-		$this->objectId = $objecId;
-
+	//User: Punta allo user che effettua l'embed del video  	#3
+	public function setFromUser(User $fromUser){
+		$this->fromUser = $fromUser;
 	}
 
-	public function setCreatedAt(DateTime $createdAt){
-
-		$this->createdAt = $createdAt;
-
-	}
-
-	public function setCounter($counter){
-		
-		$this->counter = $counter;
-		
-	}
-	
+	//number: Contatore per il numero di azioni love 			#16
 	public function setLoveCounter($loveCounter){
-		
 		$this->loveCounter = $loveCounter;
-		
 	}
-	
+
+	//array: stringhe per la categorizzazione del video 		#9
+	public function setTags($tags){
+		$this->tags = $tags;
+	}
+
+	//string:Titolo del video  									#5
+	public function setTitle($title){
+		$this->title = $title;
+	}
+
+	//string: Percorso immagine del thumbnail del video 		#8
+	public function setThumbnail($thubmnail){
+		$this->thumbnail = $thubmnail;
+	}
+
+	//string: URL del video  									#2
+	public function setURL($URL){
+		$this->URL = $URL;
+	}
+
+	//DataTime: data di creazione del video						#13
+	public function setCreatedAt(DateTime $createdAt){
+		$this->createdAt = $createdAt;
+	}
+
+	//DataTime: data di update del video						#14
 	public function setUpdatedAt(DateTime $updatedAt){
-
 		$this->updatedAt = $updatedAt;
+	}
 
+	//Access Control List										#15
+	public function setACL($ACL){
+		$this->ACL = ACL;
 	}
 }
 
