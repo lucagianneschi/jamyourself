60521825e67d431c4b94d09e3d937b001c9fc887
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-05-29 22:57:24 +0200

    aggiunta comments e modifiche per raggiungimento standard

--- classes/song.class.php
+++ classes/song.class.php
@@ -4,95 +4,101 @@
 
 class Song{
 
-	private $objectId;		//string: objectId su Parse									    #1
-	private $active;		//BOOL: attiva/disattiva l'istanza della classe  				#2
-	private $counter;		//number: Contatore di gradimento 								#3
-	private $duration;		//number: Durata della Song										#5
-	private $featuring;		//array: presenza di altri Utenti								#6
-	private $filePath;		//string: Indirizzo del file									#7
-	private $fromUser;		//User: Utente che effettua la creazione della Song				#8
-	private $genre;			//string: Genere della Song										#9
+	private $objectId;		//string: objectId su Parse									   
+	private $active;		//BOOL: attiva/disattiva l'istanza della classe 
+	private $comments;      //relation: puntatori ad oggetti Comment
+	private $counter;		//number: Contatore di gradimento 								
+	private $duration;		//number: Durata della Song										
+	private $featuring;		//array: presenza di altri Utenti								
+	private $filePath;		//string: Indirizzo del file									
+	private $fromUser;		//User: Utente che effettua la creazione della Song				
+	private $genre;			//string: Genere della Song										
 	private $location;      //geoPoint: coordinate di localizzazione della canzone			#11  //DA RIMUOVERE(?)
-	private $loveCounter;   //number: per tenere conto del numero di azioni love			#17
-	private $record;		//Record (Parse Object): disco di appartenenza della song       #12
-	private $title;			//string: titolo della song  									#13		
-	private $createdAt;		//DateTime: data e tempo di upload								#14
-	private $updatedAt;		//DateTime: data e tempo di ultima modifica						#15
-	private $ACL;			//Access Control list										    #16
+	private $loveCounter;   //number: per tenere conto del numero di azioni love			
+	private $record;		//Record (Parse Object): disco di appartenenza della song       
+	private $title;			//string: titolo della song  											
+	private $createdAt;		//DateTime: data e tempo di upload								
+	private $updatedAt;		//DateTime: data e tempo di ultima modifica						
+	private $ACL;			//Access Control list										    
 
 	//SETTERS
 
-	//string: objectId su Parse									    #1
+	//string: objectId su Parse									    
 	public function setObjectId($objectId){
 		$this->objectId = $objectId;
 	}
 
-	//BOOL: attiva/disattiva l'istanza della classe  				#2
+	//BOOL: attiva/disattiva l'istanza della classe  				
 	public function setActive($active){
 		$this->active = $active;
 	}
 
-	//number: Contatore di gradimento 								#3
+	//relation: puntatori ad oggetti Comment
+	public function setComments($comments){
+		$this->comments = $comments;
+	}
+
+	//number: Contatore di gradimento 								
 	public function setCounter($counter){
 		$this->counter = $counter;
 	}
 
-	//number: Durata della Song										#5
+	//number: Durata della Song										
 	public function setDuration($duration){
 		$this->duration = $duration;
 	}
 
-	//array: presenza di altri Utenti								#6
+	//array: presenza di altri Utenti								
 	public function setFeaturing(array $featuring){
 		$this->featuring = $featuring;
 	}
 	
-	//string: Indirizzo del file									#7
+	//string: Indirizzo del file									
 	public function setFilePath($filePath){
 		$this->filePath = $filePath;
 	}
 
-	//User: Utente che effettua la creazione della Song				#8
+	//User: Utente che effettua la creazione della Song				
 	public function setFromUser(User $fromUser){
 		$this->fromUser = $fromUser;
 	}
 
-	//string: Genere della Song										#9
+	//string: Genere della Song									
 	public function setGenre($genre){
 		$this->genre = $genre;
 	}
 
-	//geoPoint: coordinate di localizzazione della canzone			#11
+	//geoPoint: coordinate di localizzazione della canzone			
 	public function setLocation(parseGeoPoint $location){
 		$this->location = $location;
 	}
 
-	//number: per tenere conto del numero di azioni love			#17
+	//number: per tenere conto del numero di azioni love			
 	public function setLoveCounter(array $loveCounter){
 		$this->loveCounter = $loveCounter;
 	}
 
-	//Record (Parse Object): disco di appartenenza della song       #12
+	//Record (Parse Object): disco di appartenenza della song       
 	public function setRecord(Record $record){
 		$this->album = $record;
 	}
 
-	//string: titolo della song  									#13
+	//string: titolo della song  									
 	public function setTitle($title){
 		$this->title = $title;
 	}
 
-	//DateTime: data e tempo di upload								#14
+	//DateTime: data e tempo di upload								
 	public function setCreatedAt(DateTime $createdAt){
 		$this->createdAt = $createdAt;
 	}
 
-	//DateTime: data e tempo di ultima modifica					    #15
+	//DateTime: data e tempo di ultima modifica					    
 	public function setUpdatedAt(DateTime $updatedAt){
 		$this->updatedAt = $updatedAt;
 	}
 
-	//Access Control list										    #16
+	//Access Control list										    
 	public function setACL(array $ACL){
 		$this->ACL = $ACL;
 	}
@@ -100,77 +106,82 @@ class Song{
 
 	//GETTERS
 
-	//string: objectId su Parse									    #1
+	//string: objectId su Parse									    
 	public function getObjectId(){
 		return $this->objectId;
 	}
 
-	//BOOL: attiva/disattiva l'istanza della classe  				#2
+	//BOOL: attiva/disattiva l'istanza della classe  			
 	public function getActive(){
 		return $this->active;
 	}
 
-	//number: Contatore di gradimento 								#3
+	//relation: puntatori ad oggetti Comment 								
+	public function getComments(){
+		return $this->comments;
+	}
+
+	//number: Contatore di gradimento 								
 	public function getCounter(){
 		return $this->counter;
 	}
 
-	//number: Durata della Song										#5
+	//number: Durata della Song										
 	public function getDuration(){
 		return $this->duration;
 	}
 
-	//array: presenza di altri Utenti								#6
+	//array: presenza di altri Utenti								
 	public function getFeaturing(){
 		return $this->featuring;
 	}
 
-	//string: Indirizzo del file									#7
+	//string: Indirizzo del file									
 	public function getFilePath(){
 		return $this->filePath;
 	}
 
-	//User: Utente che effettua la creazione della Song				#8
+	//User: Utente che effettua la creazione della Song				
 	public function getFromUser(){
 		return $this->fromUser;
 	}
 
-	//string: Genere della Song										#9
+	//string: Genere della Song										
 	public function getGenre(){
 		return $this->genre;
 	}
 
-	//geoPoint: coordinate di localizzazione della canzone			#11
+	//geoPoint: coordinate di localizzazione della canzone			
 	public function getLocation(){
 		return $this->location;
 	}
 	
-	//number: per tenere conto del numero di azioni love			#17
+	//number: per tenere conto del numero di azioni love			
 	public function getLoveCounter(){
 		return $this->loveCounter;
 	}	
 
-	//Record (Parse Object): disco di appartenenza della song       #12
+	//Record (Parse Object): disco di appartenenza della song       
 	public function getRecord(){
 		return $this->record;
 	}
 
-	//string: titolo della song  									#13
+	//string: titolo della song  									
 	public function getTitle(){
 		return $this->title;
 	}
 
-	//DateTime: data e tempo di upload								#14
+	//DateTime: data e tempo di upload								
 	public function getCreatedAt(){
 		return $this->createdAt;
 	}
 
-	//DateTime: data e tempo di ultima modifica					    #15
+	//DateTime: data e tempo di ultima modifica					    
 	public function getUpdatedAt(){
 		return $this->updatedAt;
 	}
 
-	//Access Control list										    #16
+	//Access Control list										    
 	public function getACL(){
 		return $this->ACL;
 	}
