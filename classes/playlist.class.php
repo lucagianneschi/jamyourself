369dede23a1d4075ef14a4314e086646d3b223e4
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-06-09 12:12:59 +0200

    aggiunta toString

--- classes/playlist.class.php
+++ classes/playlist.class.php
@@ -1,5 +1,6 @@
 <?php
-/*! \par Info Generali:
+
+/* ! \par Info Generali:
  *  \author    Stefano Muscas
  *  \version   1.0
  *  \date      2013
@@ -18,150 +19,140 @@
  *  <a href="http://www.socialmusicdiscovering.com/dokuwiki/doku.php?id=documentazione:api:playlist">API</a>
  */
 
-class Playlist{
-	
-	private $objectId;			//string: objectId Parse
-	private $active;  			//BOOL: classe attiva/non attiva
-	private $fromUser;			//Pointer to Parse User, utente a cui appartiene la playlist
-	private $name;				//string: nome della playlist
-	private $songs;     		//relation: lista delle canzoni inserite dall'utente nella playslit
-	private $unlimited; 		//BOOL: indica se il numero di canzoni da mettere nella playlist è illimitato o meno
-	private $createdAt; 		//DateTime: data e ora di creazione dell'istanza della playlist
-	private $updatedAt; 		//DateTime: data e ora di update dell'istanza della playlist
-	private $ACL;				//Access Control List
+class Playlist {
 
-	public function __construct(){
-		
-	}
-	
-	//getters
-	//string: objectId Parse
-	public function getObjectId(){
-		return $this->objectId;  
-	}
+    private $objectId;   //string: objectId Parse
+    private $active;     //BOOL: classe attiva/non attiva
+    private $fromUser;   //Pointer to Parse User, utente a cui appartiene la playlist
+    private $name;    //string: nome della playlist
+    private $songs;       //relation: lista delle canzoni inserite dall'utente nella playslit
+    private $unlimited;   //BOOL: indica se il numero di canzoni da mettere nella playlist è illimitato o meno
+    private $createdAt;   //DateTime: data e ora di creazione dell'istanza della playlist
+    private $updatedAt;   //DateTime: data e ora di update dell'istanza della playlist
+    private $ACL;    //Access Control List
 
-	//BOOL: classe attiva/non attiva
-	public function getActive(){
-		return $this->active;
-	}
-
-	//Pointer to Parse User, utente a cui appartiene la playlist
-	public function getFromUser(){
-		return $this->fromUser;
-	}
-
-	//string: nome della playlist
-	public function getName(){
-		return $this->name;
-	}
-		
-	//relation: lista delle canzoni inserite dall'utente nella playslit
-	public function getSongs(){
-		return $this->songs;
-	}
-
-	//BOOL: indica se il numero di canzoni da mettere nella playlist è illimitato o meno
-	public function getUnlimited(){
-		return $this->unlimited;
-	}
-
-	//DateTime: data e ora di creazione dell'istanza della playlist
-	public function getCreatedAt(){
-		return $this->createdAt; 
-	}
-	
-	//DateTime: data e ora di update dell'istanza della playlist
-	public function getUpdatedAt(){
-		return $this->updatedAt; 
-	}
-	
-	//Access Control List
-	public function getACL(){
-		return $this->ACL;  
-	}
-	
+    public function __construct() {
         
-	//setters
-	//string: objectId Parse
-	public function setObjectId($objectId){
-		$this->objectId = $objectId;  
-	}
+    }
 
-	//BOOL: classe attiva/non attiva
-	public function setActive($active){
-		$this->active = $active;
-	}
-	
-	//Pointer to Parse User, utente a cui appartiene la playlist
-	public function setFromUser(User $fromUser){
-		$this->fromUser = $fromUser;
-	}
+    //getters
+    //string: objectId Parse
+    public function getObjectId() {
+        return $this->objectId;
+    }
 
-	//string: nome della playlist
-	public function setName($name){
-		$this->name = $name;
-	}
+    //BOOL: classe attiva/non attiva
+    public function getActive() {
+        return $this->active;
+    }
 
-	//relation: lista delle canzoni inserite dall'utente nella playslit
-	public function setSongs(array $songs){
-		$this->songs = $songs;
-	}
+    //Pointer to Parse User, utente a cui appartiene la playlist
+    public function getFromUser() {
+        return $this->fromUser;
+    }
 
-	//BOOL: indica se il numero di canzoni da mettere nella playlist è illimitato o meno
-	public function setUnlimited($unlimited){
-		$this->unlimited = $unlimited;
-	}
-	
-	//DateTime: data e ora di creazione dell'istanza della playlist
-	public function setCreatedAt(DateTime $createdAt){
-		$this->createdAt = $createdAt;  
-	}
-	
-	//DateTime: data e ora di update dell'istanza della playlist
-	public function setUpdatedAt(DateTime $updatedAt)
-	{$this->updatedAt = $updatedAt;  
-	}
-	
-	//Access Control List
-	public function setACL($ACL){
-		$this->ACL = $ACL; 
-	}
-	
-	public function __toString(){
-		$string ="";
-                
-		$string.="objectId -> ".$this->objectId."<br>";
-		
+    //string: nome della playlist
+    public function getName() {
+        return $this->name;
+    }
 
-		$string.="fromUser -> ".$this->fromUser->getUsername()."<br>";
+    //relation: lista delle canzoni inserite dall'utente nella playslit
+    public function getSongs() {
+        return $this->songs;
+    }
 
-		$string.="name -> ".$this->getName()."<br>";
-		
-		$converted_res = ($this->unlimited) ? 'true' : 'false';
-		
-		$string.="unlimited -> ".$converted_res."<br>";
+    //BOOL: indica se il numero di canzoni da mettere nella playlist è illimitato o meno
+    public function getUnlimited() {
+        return $this->unlimited;
+    }
 
-		$i=0;
-		if($this->songs){
-			foreach($this->songs as $song){
-				$string.="songs[".$i."] -> ".$song->getTitle()." - ".$song->getTitle()."<br>";	//controllare il funzionamento di questa funzione dopo modifica del tipo!! da array a relation!!		
-				$i++;
-			}
-		}
-		
-		$converted_res = ($this->active) ? 'true' : 'false';
-                
-		$string.="active-> ".$converted_res."<br>";	
-			
-		if($this->getUpdatedAt())$string.="updatedAt -> ".$this->getUpdatedAt()->format('d/m/Y H:i:s')."<br>";
-                
-		if($this->getCreatedAt())$string.="createdAt -> ".$this->getCreatedAt()->format('d/m/Y H:i:s')."<br>";
-                
-		//$string.="-> ".$this->ACL;
-		
-		return $string;
-	}
-	
+    //DateTime: data e ora di creazione dell'istanza della playlist
+    public function getCreatedAt() {
+        return $this->createdAt;
+    }
+
+    //DateTime: data e ora di update dell'istanza della playlist
+    public function getUpdatedAt() {
+        return $this->updatedAt;
+    }
+
+    //Access Control List
+    public function getACL() {
+        return $this->ACL;
+    }
+
+    //setters
+    //string: objectId Parse
+    public function setObjectId($objectId) {
+        $this->objectId = $objectId;
+    }
+
+    //BOOL: classe attiva/non attiva
+    public function setActive($active) {
+        $this->active = $active;
+    }
+
+    //Pointer to Parse User, utente a cui appartiene la playlist
+    public function setFromUser(User $fromUser) {
+        $this->fromUser = $fromUser;
+    }
+
+    //string: nome della playlist
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    //relation: lista delle canzoni inserite dall'utente nella playslit
+    public function setSongs(array $songs) {
+        $this->songs = $songs;
+    }
+
+    //BOOL: indica se il numero di canzoni da mettere nella playlist è illimitato o meno
+    public function setUnlimited($unlimited) {
+        $this->unlimited = $unlimited;
+    }
+
+    //DateTime: data e ora di creazione dell'istanza della playlist
+    public function setCreatedAt(DateTime $createdAt) {
+        $this->createdAt = $createdAt;
+    }
+
+    //DateTime: data e ora di update dell'istanza della playlist
+    public function setUpdatedAt(DateTime $updatedAt) {
+        $this->updatedAt = $updatedAt;
+    }
+
+    //Access Control List
+    public function setACL($ACL) {
+        $this->ACL = $ACL;
+    }
+
+    public function __toString() {
+        $string = '';
+        $string .= '[objectId] => ' . $this->getObjectId() . '<br />';
+        $string .= '[active] => ' . $this->getActive() . '<br />';
+        $fromUser = $this->getFromUser();
+        $string.="[fromUser] => " . $fromUser->getObjectId() . "<br />";
+        $string.="[name] => " . $this->getName() . "<br />";
+        foreach ($this->getSongs() as $song) {
+            $string .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+            $string .= "[song] => " . $song->getObjectId() . "<br />";
+        }
+        $string.="[unlimited] => " . $this->getUnlimited() . "<br />";
+        $string .= '[createdAt] => ' . $this->getCreatedAt()->format('d-m-Y H:i:s') . '<br />';
+        $string .= '[updatedAt] => ' . $this->getUpdatedAt()->format('d-m-Y H:i:s') . '<br />';
+        foreach ($this->getACL()->acl as $key => $acl) {
+            $string .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+            $string .= '[key] => ' . $key . '<br />';
+            foreach ($acl as $access => $value) {
+                $string .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+                $string .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+                $string .= '[access] => ' . $access . ' -> ' . $value . '<br />';
+            }
+        }
+        return $string;
+    }
+
 }
 
 ?>
