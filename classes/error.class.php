67aa065e915fe7d0ba8766cd5fa898d89d4c4e0c
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-06-09 13:03:15 +0200

    aggiunta toString

--- classes/error.class.php
+++ classes/error.class.php
@@ -1,5 +1,6 @@
 <?php
-/*! \par Info Generali:
+
+/* ! \par Info Generali:
  *  \author    Daniele Caldelli
  *  \version   1.0
  *  \date      2013
@@ -19,95 +20,111 @@
  */
 
 class Error {
-	
-	private $objectId;
-	private $errorClass;
-	private $errorCode;
-	private $errorMessage;
-	private $errorFunction;
-	private $errorFunctionParameter;
-	private $createdAt;
-	private $updatedAt;
-	//private $ACL;
-	
-	public function getObjectId() {
-		return $this->objectId;
-	}
-	
-	public function getErrorClass() {
-		return $this->errorClass;
-	}
-	
-	public function getErrorCode() {
-		return $this->errorCode;
-	}
-	
-	public function getErrorMessage() {
-		return $this->errorMessage;
-	}
-	
-	public function getErrorFunction() {
-		return $this->errorFunction;
-	}
-	
-	public function getErrorFunctionParameter() {
-		return $this->errorFunctionParameter;
-	}
-	
-	public function getCreatedAt() {
-		return $this->createdAt;
-	}
-	
-	public function getUpdatedAt() {
-		return $this->updatedAt;
-	}
-	
-	public function printError() {
-		echo '[objectId] => ' . $this->getObjectId() . '<br />';
-		echo '[errorClass] => ' . $this->getErrorClass() . '<br />';
-		echo '[errorCode] => ' . $this->getErrorCode() . '<br />';
-		echo '[errorMessage] => ' . $this->getErrorMessage() . '<br />';
-		echo '[errorFunction] => ' . $this->getErrorFunction() . '<br />';
-		foreach ($this->getErrorFunctionParameter() as $parameter) {
-			echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-			echo '[errorFunctionParameter] => ' . $parameter . '<br />';
-		}
-		echo '[createdAt] => ' . $this->getCreatedAt() . '<br />';
-		echo '[updatedAt] => ' . $this->getUpdatedAt() . '<br />';
-	}
-	
-	public function setObjectId($objectId) {
-		$this->objectId = $objectId;
-	}
-	
-	public function setErrorClass($errorClass) {
-		$this->errorClass = $errorClass;
-	}
-	
-	public function setErrorCode($errorCode) {
-		$this->errorCode = $errorCode;
-	}
-	
-	public function setErrorMessage($errorMessage) {
-		$this->errorMessage = $errorMessage;
-	}
-	
-	public function setErrorFunction($errorFunction) {
-		$this->errorFunction = $errorFunction;
-	}
-	
-	public function setErrorFunctionParameter($errorFunctionParameter) {
-		$this->errorFunctionParameter = $errorFunctionParameter;
-	}
-	
-	public function setCreatedAt(DateTime $createdAt) {
-		$this->createdAt = $createdAt;
-	}
-	
-	public function setUpdatedAt(DateTime $updatedAt) {
-		return $this->updatedAt = $updatedAt;
-	}
-	
+
+    private $objectId;
+    private $errorClass;
+    private $errorCode;
+    private $errorMessage;
+    private $errorFunction;
+    private $errorFunctionParameter;
+    private $createdAt;
+    private $updatedAt;
+    private $ACL;
+
+    public function getObjectId() {
+        return $this->objectId;
+    }
+
+    public function getErrorClass() {
+        return $this->errorClass;
+    }
+
+    public function getErrorCode() {
+        return $this->errorCode;
+    }
+
+    public function getErrorFunction() {
+        return $this->errorFunction;
+    }
+
+    public function getErrorFunctionParameter() {
+        return $this->errorFunctionParameter;
+    }
+
+    public function getErrorMessage() {
+        return $this->errorMessage;
+    }
+
+    public function getCreatedAt() {
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt() {
+        return $this->updatedAt;
+    }
+
+    public function getACL() {
+        return $this->ACL;
+    }
+
+    public function setObjectId($objectId) {
+        $this->objectId = $objectId;
+    }
+
+    public function setErrorClass($errorClass) {
+        $this->errorClass = $errorClass;
+    }
+
+    public function setErrorCode($errorCode) {
+        $this->errorCode = $errorCode;
+    }
+
+    public function setErrorFunction($errorFunction) {
+        $this->errorFunction = $errorFunction;
+    }
+
+    public function setErrorFunctionParameter($errorFunctionParameter) {
+        $this->errorFunctionParameter = $errorFunctionParameter;
+    }
+
+    public function setErrorMessage($errorMessage) {
+        $this->errorMessage = $errorMessage;
+    }
+
+    public function setCreatedAt(DateTime $createdAt) {
+        $this->createdAt = $createdAt;
+    }
+
+    public function setUpdatedAt(DateTime $updatedAt) {
+        return $this->updatedAt = $updatedAt;
+    }
+
+    public function setACL($ACL) {
+        return $this->ACL = $ACL;
+    }
+
+    public function __toString() {
+        $string = '';
+        $string .= '[objectId] => ' . $this->getObjectId() . '<br />';
+        $string .= '[errorClass] => ' . $this->getErrorClass() . '<br />';
+        $string .= '[errorCode] => ' . $this->getErrorCode() . '<br />';
+        $string .= '[errorFunction] => ' . $this->getErrorFunction() . '<br />';
+        $string .= '[errorFunctionParameter] => ' . $this->getErrorFunctionParameter() . '<br />';
+        $string .= '[errorMessage] => ' . $this->getErrorMessage() . '<br />';
+        $string .= '[createdAt] => ' . $this->getCreatedAt()->format('d-m-Y H:i:s') . '<br />';
+        $string .= '[updatedAt] => ' . $this->getUpdatedAt()->format('d-m-Y H:i:s') . '<br />';
+        foreach ($this->getACL()->acl as $key => $acl) {
+            $string .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+            $string .= '[key] => ' . $key . '<br />';
+            foreach ($acl as $access => $value) {
+                $string .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+                $string .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+                $string .= '[access] => ' . $access . ' -> ' . $value . '<br />';
+            }
+        }
+        return $string;
+    }
+
 }
 
 ?>
