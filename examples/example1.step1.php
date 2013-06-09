c8b08c2e11de4ca102abe3bd79f0d6a018d7ccdc
danielecaldelli <danielecaldelli@gmail.com>
2013-06-09 15:53:36 +0200

    prima versione di esempio controller ricaricamento pagina

---
+++ examples/example1.step1.php
@@ -0,0 +1,15 @@
+<html>
+
+<form action="controllers/example1.step1.controller.php" method="post">
+	Seleziona il tipo utente:<br />
+	<input type="radio" name="type" value="j" />Jammer<br />
+	<input type="radio" name="type" value="s" />Spotter<br />
+	<input type="radio" name="type" value="v" />Venue<br />
+
+	<input type="submit" name="Step 1" value="step1">
+	<?php
+	if (isset($_SESSION['step1Error'])) {
+		echo $_SESSION['step1Error'];
+	}
+	?>
+</form>
