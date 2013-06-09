5d22a398f38299140a7374be5495ebfa4ee49702
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-06-09 12:47:44 +0200

    aggiunto metodo toString

--- classes/question.class.php
+++ classes/question.class.php
@@ -1,5 +1,6 @@
 <?php
-/*! \par Info Generali:
+
+/* ! \par Info Generali:
  *  \author    Daniele Caldelli
  *  \version   1.0
  *  \date      2013
@@ -19,115 +20,131 @@
  */
 
 class Question {
-	
-	private $objectId;
-	private $answer;
-	private $mailFrom;
-	private $mailTo;
-	private $name;
-	private $replied;
-	private $subject;
-	private $text;
-	private $createdAt;
-	private $updatedAt;
-	private $ACL; //perchè non si setta??
-	
-	public function getObjectId() {
-		return $this->objectId;
-	}
-	
-	public function getAnswer() {
-		return $this->answer;
-	}
-	
-	public function getMailFrom() {
-		return $this->mailFrom;
-	}
-	
-	public function getMailTo() {
-		return $this->mailTo;
-	}
-	
-	public function getName() {
-		return $this->name;
-	}
-	
-	public function getReplied() {
-		return $this->replied;
-	}
-	
-	public function getSubject() {
-		return $this->subject;
-	}
-	
-	public function getText() {
-		return $this->text;
-	}
-	
-	public function getCreatedAt() {
-		return $this->createdAt;
-	}
-	
-	public function getUpdatedAt() {
-		return $this->updatedAt;
-	}
-	
-	public function printQuestion() {
-		echo '[objectId] => ' . $this->getObjectId() . '<br />';
-		echo '[answer] => ' . $this->getAnswer() . '<br />';
-		echo '[mailFrom] => ' . $this->getMailFrom() . '<br />';
-		echo '[mailTo] => ' . $this->getMailTo() . '<br />';
-		echo '[name] => ' . $this->getName() . '<br />';
-		echo '[replied] => ' . $this->getReplied() . '<br />';
-		echo '[subject] => ' . $this->getSubject() . '<br />';
-		echo '[text] => ' . $this->getText() . '<br />';
-		echo '[createdAt] => ' . $this->getCreatedAt() . '<br />';
-		echo '[updatedAt] => ' . $this->getUpdatedAt() . '<br />';
-	}
-	
-	public function setObjectId($value) {
-		$this->objectId = $value;
-	}
-	
-	public function setAnswer($value) {
-		$this->answer = $value;
-	}
-	
-	public function setMailFrom($value) {
-		$this->mailFrom = $value;
-	}
-	
-	public function setMailTo($value) {
-		$this->mailTo = $value;
-	}
-	
-	public function setName($value) {
-		$this->name = $value;
-	}
-	
-	public function setReplied($value) {
-		$this->replied = $value;
-	}
-	
-	public function setSubject($value) {
-		$this->subject = $value;
-	}
-	
-	public function setText($value) {
-		$this->text = $value;
-	}
-	 
-	public function setCreatedAt(DateTime $value) {
-		$this->createdAt = $value;
-	}
-	
-	public function setUpdatedAt(DateTime $value) {
-		return $this->updatedAt = $value;
-	}
-        
-        public function setACL(parseACL $value) {
-		return $this->ACL = $value;
-	}
+
+    private $objectId;
+    private $answer;
+    private $mailFrom;
+    private $mailTo;
+    private $name;
+    private $replied;
+    private $subject;
+    private $text;
+    private $createdAt;
+    private $updatedAt;
+    private $ACL;
+
+    public function getObjectId() {
+        return $this->objectId;
+    }
+
+    public function getAnswer() {
+        return $this->answer;
+    }
+
+    public function getMailFrom() {
+        return $this->mailFrom;
+    }
+
+    public function getMailTo() {
+        return $this->mailTo;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getReplied() {
+        return $this->replied;
+    }
+
+    public function getSubject() {
+        return $this->subject;
+    }
+
+    public function getText() {
+        return $this->text;
+    }
+
+    public function getCreatedAt() {
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt() {
+        return $this->updatedAt;
+    }
+
+    public function getACL() {
+        return $this->ACL;
+    }
+
+    public function setObjectId($value) {
+        $this->objectId = $value;
+    }
+
+    public function setAnswer($value) {
+        $this->answer = $value;
+    }
+
+    public function setMailFrom($value) {
+        $this->mailFrom = $value;
+    }
+
+    public function setMailTo($value) {
+        $this->mailTo = $value;
+    }
+
+    public function setName($value) {
+        $this->name = $value;
+    }
+
+    public function setReplied($value) {
+        $this->replied = $value;
+    }
+
+    public function setSubject($value) {
+        $this->subject = $value;
+    }
+
+    public function setText($value) {
+        $this->text = $value;
+    }
+
+    public function setCreatedAt(DateTime $value) {
+        $this->createdAt = $value;
+    }
+
+    public function setUpdatedAt(DateTime $value) {
+        return $this->updatedAt = $value;
+    }
+
+    public function setACL($value) {
+        return $this->ACL = $value;
+    }
+
+    public function __toString() {
+        $string = '';
+        $string .= '[objectId] => ' . $this->getObjectId() . '<br />';
+        $string .= '[answer] => ' . $this->getAnswer() . '<br />';
+        $string .= '[mailFrom] => ' . $this->getMailFrom() . '<br />';
+        $string .= '[mailTo] => ' . $this->getMailTo() . '<br />';
+        $string .= '[name] => ' . $this->getName() . '<br />';
+        $string .= '[replied] => ' . $this->getReplied() . '<br />';
+        $string .= '[subject] => ' . $this->getSubject() . '<br />';
+        $string .= '[text] => ' . $this->getText() . '<br />';
+        $string .= '[createdAt] => ' . $this->getCreatedAt()->format('d-m-Y H:i:s') . '<br />';
+        $string .= '[updatedAt] => ' . $this->getUpdatedAt()->format('d-m-Y H:i:s') . '<br />';
+        foreach ($this->getACL()->acl as $key => $acl) {
+            $string .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+            $string .= '[key] => ' . $key . '<br />';
+            foreach ($acl as $access => $value) {
+                $string .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+                $string .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+                $string .= '[access] => ' . $access . ' -> ' . $value . '<br />';
+            }
+        }
+        return $string;
+    }
+
 }
 
 ?>
