ed48df1ebdd1a0f1757e19063801522ad1610327
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-06-12 01:05:35 +0200

    allineata classe; corretti errori

--- classes/statusParse.class.php
+++ classes/statusParse.class.php
@@ -9,7 +9,7 @@
  *  \par Info Classe:
  *  \brief     Stutus Class
  *  \details   Classe status dello User, raccoglie uno stato dell'utente, posso collegarci immagine o song
- *  
+ *
  *  \par Commenti:
  *  \warning
  *  \bug
@@ -28,293 +28,271 @@ class StatusParse {
         $this->parseQuery = new ParseQuery("Status");
     }
 
-    /**
-     * Ho deciso di unificare la save e l'update cos� da poter effettuare semplicemente il salvataggio
-     * dell'oggetto senza paranoie se esiste gi� o meno su Parse
-     * Il test viene fatto sull'objectId dello status, che naturalmente � nullo se
-     * l'oggetto � creato per la prima volta
-     *
-     * @param unknown $status
-     * @return NULL|unknown
-     */
+    public function getStatus() {
+        $status = null;
+        $result = $this->parseQuery->find();
+        if (is_array($result->results) && count($result->results) > 0) {
+            $ret = $result->results[0];
+            if ($ret) {
+                //recupero l'utente
+                $status = $this->parseToStatus($ret);
+            }
+        }
+        return $status;
+    }
+
+    public function getStatuses() {
+        $statuses = null;
+        $result = $this->parseQuery->find();
+        if (is_array($result->results) && count($result->results) > 0) {
+            $statuses = array();
+            foreach (($result->results) as $status) {
+                $statuses [] = $this->parseToStatus($status);
+            }
+        }
+        return $statuses;
+    }
+
     public function saveStatus(Status $status) {
 
         $parse = new parseObject("Status");
         $parse->active = $status->getActive();
-
         if ($status->getCommentators() != null && count($status->getCommentators()) > 0) {
             $arrayPointer = array();
             foreach ($status->getCommentators() as $user) {
                 $pointer = $parse->dataType("pointer", array("_User"), $user->getObjectId());
                 array_push($arrayPointer, $pointer);
             }
+            $parse->commentators = $user->dataType("relation", $arrayPointer);
         } else {
             $parse->commentators = null;
         }
-
         if ($status->getComments() != null && count($status->getComments()) > 0) {
             $arrayPointer = array();
             foreach ($status->getComments() as $comment) {
                 $pointer = $parse->dataType("pointer", array("Comment"), $comment->getObjectId());
                 array_push($arrayPointer, $pointer);
             }
+            $parse->comments = $comment->dataType("relation", $arrayPointer);
         } else {
             $parse->comments = null;
         }
-
         $parse->counter = $status->getCounter();
-
-        //puntatore ad un evento
-        if (($event = $status->getEvent())) {
-            $parse->event = array("__type" => "Pointer", "className" => "Event", "objectId" => $event->getObjectId());
-        };
-
-        $parse->fromUser = $status->getfromUser();
-        $parse->image = $status->getImage();
-        if (($geoPoint = $status->getLocation())) {
-            $parse->location = $geoPoint->location;
+        if (($event = $status->getEvent()) != null) {
+            $pointerParse = $parse->dataType("pointer", array("Event"), $event->getObjectId());
+            $parse->event = $pointerParse;
+        } else {
+            $parse->event = null;
         }
-
+        if (($fromUser = $parse->getFromUser()) != null) {
+            $pointerParse = $parse->dataType("pointer", array("_User"), $fromUser->getObjectId());
+            $parse->fromUser = $pointerParse;
+        } else {
+            $parse->fromUser = null;
+        }
+        if (($image = $parse->getImage()) != null) {
+            $pointerParse = $parse->dataType("pointer", array("Image"), $image->getObjectId());
+            $parse->image = $pointerParse;
+        } else {
+            $parse->image = null;
+        }
+        if (($geoPoint = $parse->getLocation()) != null) {
+            $parseLocation = $parse->dataType("geopoint",array($geoPoint->latitude, $geoPoint->longitude));
+            $parse->location = $parseLocation;
+        } else {
+            $parse->location = null;
+        }
         $parse->loveCounter = $status->getLoveCounter();
-
         if ($status->getLovers() != null && count($status->getLovers()) > 0) {
             $arrayPointer = array();
             foreach ($status->getLovers() as $user) {
                 $pointer = $parse->dataType("pointer", array("_User"), $user->getObjectId());
                 array_push($arrayPointer, $pointer);
             }
+            $parse->lovers = $user->dataType("relation", $arrayPointer);
         } else {
             $parse->lovers = null;
         }
-
-        if (($song = $status->getSong())) {
-            $parse->song = array("__type" => "Pointer", "className" => "Song", "objectId" => $song->getObjectId());
+        if (($song = $status->getSong()) != null) {
+            $pointerParse = $parse->dataType("pointer", array("Song"), $image->getObjectId());
+            $parse->song = $pointerParse;
+        } else {
+            $parse->song = null;
         }
-
         if ($status->getTaggedUsers() != null && count($status->getTaggedUsers()) > 0) {
             $arrayPointer = array();
             foreach ($status->getTaggedUsers() as $user) {
                 $pointer = $parse->dataType("pointer", array("_User"), $user->getObjectId());
                 array_push($arrayPointer, $pointer);
             }
+            $parse->taggedUsers = $user->dataType("relation", $arrayPointer);
         } else {
             $parse->taggedUsers = null;
         }
-
-
         $parse->text = $status->getText();
         $acl = new parseACL();
         $acl->setPublicReadAccess(true);
         $acl->setPublicWriteAccess(true);
         $parse->setACL($acl);
 
-		//se esiste l'objectId vuol dire che devo aggiornare
-		//se non esiste vuol dire che devo salvare
-        if(( $status->getObjectId())!=null ){
-			//update
-              try{
-				//update
-				$result = $parse->update($status->getObjectId());
-				
-				//aggiorno l'update
-				$status->setUpdatedAt(new DateTime($result->updatedAt, new DateTimeZone("America/Los_Angeles")));
-			}
-			catch(ParseLibraryException $e){
-				return false;
-			}
-		}else{
-				
-			try{
-				//salvo
-				$result = $parse->save();
+        //se esiste l'objectId vuol dire che devo aggiornare
+        //se non esiste vuol dire che devo salvare
+        if (( $status->getObjectId()) != null) {
+            //update
+            try {
+                //update
+                $result = $parse->update($status->getObjectId());
 
-				//aggiorno i dati per la creazione
-				$status->setObjectId($result->objectId);
-				$status->setCreatedAt(new DateTime($result->createdAt,new DateTimeZone("America/Los_Angeles")));
-				$status->setUpdatedAt(new DateTime($result->createdAt,new DateTimeZone("America/Los_Angeles")));
-			}
-			catch(ParseLibraryException $e){
+                //aggiorno l'update
+                $status->setUpdatedAt(new DateTime($result->updatedAt, new DateTimeZone("America/Los_Angeles")));
+            } catch (Exception $e) {
+                $error = new error();
+                $error->setErrorClass(__CLASS__);
+                $error->setErrorCode($e->getCode());
+                $error->setErrorMessage($e->getMessage());
+                $error->setErrorFunction(__FUNCTION__);
+                $error->setErrorFunctionParameter(func_get_args());
+                $errorParse = new errorParse();
+                $errorParse->saveError($error);
+            }
+        } else {
 
-				return false;
+            try {
+                //salvo
+                $result = $parse->save();
 
-			}
+                //aggiorno i dati per la creazione
+                $status->setObjectId($result->objectId);
+                $status->setCreatedAt(new DateTime($result->createdAt, new DateTimeZone("America/Los_Angeles")));
+                $status->setUpdatedAt(new DateTime($result->createdAt, new DateTimeZone("America/Los_Angeles")));
+            } catch (Exception $e) {
+                $error = new error();
+                $error->setErrorClass(__CLASS__);
+                $error->setErrorCode($e->getCode());
+                $error->setErrorMessage($e->getMessage());
+                $error->setErrorFunction(__FUNCTION__);
+                $error->setErrorFunctionParameter(func_get_args());
+                $errorParse = new errorParse();
+                $errorParse->saveError($error);
+            }
+        }
 
-		}
+        //restituisco status aggiornato
+        return $status;
+    }
 
-		//restituisco status aggiornato
-		return $status;
-	}
+    public function deleteStatus(Status $status) {
+        $status->setActive(false);
+        if ($this->save($status))
+            return true;
+        else
+            return false;
+    }
+    public function parseToStatus(stdClass $parseObj) {
 
-	public function deleteStatus(Status $status){
-
-		$status->setActive(false);
-
-		if($this->save($status)) return true;
-		else return false;
-			
-	}
-
-	/**
-	 * 
-	 * @param unknown $objectId
-	 * @return Status
-	 */
-	public function getStatus($statusId){
-		
-		$parseStatus = new parseObject("Status");
-
-		$res = $parseStatus->get($statusId);
-		
-		$status = $this->parseToStatus($res);
-
-		return $status;
-	}
-
-	/**
-	 * Restituisce tutti gli status dell'utente
-	 * (il limite delle query � impostato a 100 dalla parse.lib)
-	 * @param User $user
-	 * @return Ambigous <multitype:, NULL>
-	 */
-	public function getStatusByUser(User $user){
-
-		$list = null;
-			
-		$this->parseQuery->wherePointer('fromUser','_User', $user->getObjectId());
-
-		$return = $this->parseQuery->find();
-
-		if (is_array($return->results) && count($return->results)>0){
-
-			$list = array();
-
-			foreach ($return->results as $result) {
-									
-				array_push($list, $this->parseToStatus($result));
-				
-			}
-
-		}
-
-		return $list;
-	}
-
-	/**
-	 * Effettua il parsing di una riga di una tabella della classe Status
-	 * trasformandola in un oggetto Status della nostra libreria
-	 * @param stdClass $parseObj oggetto rappresentatne una riga della tabella Status del DB di Parse
-	 * @return NULL|Status NULL in caso di errore, un oggetto Status in caso di successo
-	 */
-	public function parseToStatus(stdClass $parseObj){
-			
-		if($parseObj == null) return null; 	//se non ho ricevuto niente...
-			
-		//lo status da restituire
-		$status = new status();
-		
-		//recupero objectId
-		if(isset( $parseObj->objectId ) )$status->setObjectId($parseObj->objectId);
-
-	 	//recupero Active
-	 	if(isset( $parseObj->active ) )$status->setActive($parseObj->active);
-	 	
-		//array di puntatori ad oggetti
-		if(isset($parseObj->commentators)){
-			$parseUser = new UserParse();
-			$commentators = $parseUser->getUserArrayById($parseObj->commentators);
-			$status->setCommentators($commentators) ;
-		}
-
-                //array di puntatori ad oggetti
-		if(isset($parseObj->comments)){
-			$parseComment = new Comment();
-			$comments = $parseComment->getObjectId($parseObj->comments);
-			$status->setComments($comments) ;
-		}
-
-	 	//recupero il counter
-	 	if(isset( $parseObj->counter ) )$status->setCounter($parseObj->counter);
-
-		if(isset( $parseObj->event ) ){
-	 		
-	 		//parse ha in ->event un puntatore 
-	 		
-	 		//creo un nuovo parseEvent per fare la query su Eventi
-	 		$parseEvent = new EventParse();
-	 		
-	 		//recupero il puntatore all'evento
-	 		$eventPointer = $parseObj->event;
-	 		
-	 		//recupero l'evento con una query su parse
-	 		$parseEvent->getEvent($eventPointer->objectId);	 		
-	 		
-	 		//setto l'evento in status
-	 		$status->setEvent($parseEvent);
-	 	}
+        if ($parseObj == null)
+            return null;  //se non ho ricevuto niente...
 
 
-	 	//recupero l'utente che ha pubblicato lo status che � un Pointer
-	 	if(isset( $parseObj->fromUser ) ){
-	 		
-	 		//userParse per fare la query sulla tabella Utenti
-	 		$parseUser = new userParse();
-	 		
-	 		//prelevo il puntatore
-	 		$parsePointer = $parseObj->fromUser;
-	 		
-	 		//request per reperire l'utente
-	 		$fromUser = $parseUser->getUserById($parsePointer->objectId);
-	 		
-	 		//setto su status l'utente
-	 		$status->setFromUser($fromUser);
-	 	}
-	 	
-		if(isset( $parseObj->image ) )$status->setImage($parseObj->image);
+//lo status da restituire
+        $status = new status();
 
-	 	//recupero il geoPoint
-	 	if(isset( $parseObj->location ) ){
-	 		
-	 		//recupero il GeoPoint
-	 		$geoParse = $parseObj->geoCoding;
-	 		
-	 		//geopontizzo
-	 		$geoPoint = new parseGeoPoint($geoParse->latitude, $geoParse->longitude);
-	 		
-	 		//salvo sullo status
-	 		$status->setLocation($geoPoint);
-	 	}
+        //recupero objectId
+        if (isset($parseObj->objectId))
+            $status->setObjectId($parseObj->objectId);
+        //recupero Active
+        if (isset($parseObj->active))
+            $status->setActive($parseObj->active);
+        //array di puntatori ad User
+        if (isset($parseObj->commentators)) {
+            $parseQueryUser = new UserParse();
+            $parseQueryUser->whereRelatedTo('commentators', 'Status', $parseObj->objectId);
+            $status->setCommentators($parseQueryUser->getUsers());
+        } else {
+            $status->commentators = null;
+        }
+        if (isset($parseObj->comments)) {
+            $parseQueryComments = new CommentParse();
+            $parseQueryComments->whereRelatedTo('comments', 'Status', $parseObj->objectId);
+            $status->setComments($parseQueryComments->getComments());
+        } else{
+            $status->comments = null;
+        }
+        if (isset($parseObj->counter)){
+            $status->setCounter($parseObj->counter);
+        } else {
+            $status->counter = 0;
+        }
+        if (isset($parseObj->event)) {
+            $parseQueryEvent = new ImageParse();
+            $parseQueryEvent->whereEqualTo("objectId", $parseObj->event);
+            $status->setEvent($parseQueryEvent);
+        } else {
+            $status->event = null;
+        }
+        if (isset($parseObj->fromUser)) {
+            $parseQueryUser = new UserParse();
+            $parseQueryUser->whereEqualTo("objectId", $parseObj->fromUser);
+            $status->setFromUser($parseQueryUser);
+        }  else {
+            $status->fromUser = null;
+        }
+        if (isset($parseObj->image)) {
+            $parseQueryImage = new ImageParse();
+            $parseQueryImage->whereEqualTo("objectId", $parseObj->image);
+            $status->setImage($parseQueryImage);
+        } else {
+            $status->image = null;
+        }
+        if (isset($parseObj->location)) {
+                $geoParse = $parseObj->location;
+                $tempObj = new parseObject("temp");
+                $location = $tempObj->dataType("geopoint", array($geoParse->latitude, $geoParse->longitude));
+                $status->setLocation($location);
+        } else {
+            $status->location = null;
+        }
+        //recupero il loveCounter
+        if (isset($parseObj->loveCounter)){
+        $status->setCounter($parseObj->loveCounter);
+        } else {
+            $status->loveCounter = 0;
+        }
+        if (isset($parseObj->lovers)) {
+            $parseQueryUser = new UserParse();
+            $parseQueryUser->whereRelatedTo('lovers', 'Status', $parseObj->objectId);
+            $status->setLovers($parseQueryUser->getUsers());
+        } else {
+            $status->lovers = null;
+        }
+        if (isset($parseObj->song)) {
+            $parseQuerySong = new ImageParse();
+            $parseQuerySong->whereEqualTo("objectId", $parseObj->song);
+            $status->setSong($parseQuerySong);
+        }else {
+            $status->song = null;
+        }
+        if (isset($parseObj->text))
+            $status->setText($parseObj->text);
+        if (isset($parseObj->taggedUsers)) {
+            $parseQueryUser = new UserParse();
+            $parseQueryUser->whereRelatedTo('taggedUsers', 'Status', $parseObj->objectId);
+            $status->setTaggedUsers($parseQueryUser->getUsers());
+        } else {
+            $status->taggedUsers = null;
+        }
+        //creo la data di tipo DateTime per createdAt e updatedAt
+        if (isset($parseObj->createdAt))
+            $status->setCreatedAt(new DateTime($parseObj->createdAt, new DateTimeZone("America/Los_Angeles")));
+        if (isset($parseObj->updatedAt))
+            $status->setUpdatedAt(new DateTime($parseObj->updatedAt, new DateTimeZone("America/Los_Angeles")));
+        $acl = new parseACL();
+        $acl->setPublicReadAccess(true);
+        $acl->setPublicWriteAccess(true);
+        $status->setACL($acl);
+        return $status;
+    }
 
-	 	//recupero il loveCounter
-	 	if(isset( $parseObj->loveCounter ) )$status->setCounter($parseObj->loveCounter);
-
-	 	//array di puntatori ad oggetti
-		if(isset($parseObj->lovers)){
-			$parseUser = new UserParse();
-			$lovers = $parseUser->getUserArrayById($parseObj->lovers);
-			$status->setLovers($lovers) ;
-		}
-
-		if(isset( $parseObj->song ) )$status->setSong($parseObj->song);
-	 	
-	 	if(isset( $parseObj->text ) )$status->setText($parseObj->text);
-	 	
-	 	//array di puntatori ad oggetti
-		if(isset($parseObj->taggedUsers)){
-			$parseUser = new UserParse();
-			$taggedUsers = $parseUser->getUserArrayById($parseObj->taggedUsers);
-			$status->setTaggedUsers($taggedUsers) ;
-		}
-	 	
-	 	//creo la data di tipo DateTime per createdAt e updatedAt
-		if( isset($parseObj->createdAt) ) $status->setCreatedAt(new DateTime($parseObj->createdAt,new DateTimeZone("America/Los_Angeles")));
-		if( isset($parseObj->updatedAt) ) $status->setUpdatedAt(new DateTime($parseObj->updatedAt,new DateTimeZone("America/Los_Angeles")));
-	 	
-	 	//recupero l'ACL
-	 	if(isset( $parseObj->ACL ) ){
-	 		$ACL = null;
-	 		$status->setACL($ACL);
-	 	}
-	 	return $status;
-	}
 }
-?> 
+
+?>
