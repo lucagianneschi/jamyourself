0ef21e5aa97f170362da47107de16604f71eb5b2
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-06-12 10:20:28 +0200

    aggiunta classe test per FAQ

---
+++ classes/test/testFaq.class.php
@@ -0,0 +1,37 @@
+<?php
+ini_set('display_errors', '1');
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/script/wp_daniele/root/config.php';
+require_once PARSE_DIR . 'parse.php';
+require_once CLASSES_DIR . 'faq.class.php';
+require_once CLASSES_DIR . 'faqParse.class.php';
+
+$faq = new Faq();
+$faq->setObjectId('aAbBcCdD');
+$faq->setAnswer('Questa è una answer');
+$faq->setArea('Area di interesse della Faq');
+$faq->setPosition('posizione 10');
+$faq->setQuestion('Questa è una question');
+$faq->setTags(array('tag1', 'tag2'));
+$dateTime = new DateTime();
+$faq->setCreatedAt($dateTime);
+$faq->setUpdatedAt($dateTime);
+$acl = new parseACL();
+$acl->setPublicReadAccess(true);
+$acl->setPublicWriteAccess(true);
+$faq->setACL($acl);
+
+echo 'STAMPO LA faq APPENA CREATO  <br>';
+echo $faq;
+
+echo '<br />-------------------------------------------------------------------------------<br />';
+
+echo 'INIZIO IL SALVATAGGIO DELLa faq APPENA CREATO<br />';
+$faqParse = new FaqParse();
+if (get_class($faqParse->saveFaq($faq))) {
+	echo 'ATTENZIONE: e\' stata generata un\'eccezione: ' . $faqParse->saveFaq($faq)->getErrorMessage() . '<br/>';
+}
+echo 'FINITO IL SALVATAGGIO DEL faq APPENA CREATO<br />';
+
+echo '<br />-------------------------------------------------------------------------------<br />';
+?>
