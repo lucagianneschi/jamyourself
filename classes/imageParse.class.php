d19a84da7cd2fb2672ac80bb96a7320bc4ed942c
Stefano <stefanom@jamyourself.com>
2013-06-16 00:26:12 +0200

    scritto secondo nuovo standard

--- classes/imageParse.class.php
+++ classes/imageParse.class.php
@@ -1,5 +1,6 @@
 <?php
-/*! \par Info Generali:
+
+/* ! \par Info Generali:
  *  \author    Maria Laura Fresu
  *  \version   1.0
  *  \date      2013
@@ -18,166 +19,268 @@
  *  <a href="http://www.socialmusicdiscovering.com/dokuwiki/doku.php?id=documentazione:api:image">API</a>
  */
 
-class ImageParse{
-	
-	private $parseQuery;
+class ImageParse {
 
-	function __construct(){
-		$this->parseQuery = new parseQuery("Image");
-	}
-	
-	function save(Image $image){
-		
-		//recupero le info dell'oggetto
-		$parse = new parseObject("Image");
-		
-		$parse->active = $image->getActive();
-		$parse->album = $image->getAlbum();
-		foreach($parse->getComments() as $comment){
-			$parse->data->comments->__op = "AddRelation";
-			$parse->data->comments->objects = array(array("__type" => "Pointer", "className" => "Comment", "objectId" => ($comment ->getObjectId())));
-		}
+    private $parseQuery;
 
-		$parse->counter = $image->getCounter();
-		$parse->description = $image->getDescription();
+    function __construct() {
+        $this->parseQuery = new parseQuery("Image");
+    }
 
-		//array di utenti
-		$parse->featuring = array();
-		foreach ($image->getFeaturing() as $user){
-			array_push($parse->featuring, $user->getObjectId());
-		}
+    function save(Image $image) {
 
-		$parse->filePath = $image->getFilePath();
-		
-		if( ( $fromUser = $image->getFromUser() ) != null ) {
-			$parse->fromUser = array("__type" => "Pointer", "className" => "_User", "objectId" => $author->getObjectId() );			
-		}
-		
-		$parse->loveCounter = $image->getLoveCounter(); 
-		if( ($geoPoint = $image->getLocation() ) != null ){
-			$parse->location = $geoPoint->location;			
-		}
-		
-		$parse->tag = $image->getTags();	
-
-		
-		//$parse->ACL = $image->getACL();
-		
-		//se esiste l'objectId vuol dire che devo aggiornare
-		//se non esiste vuol dire che devo salvare
-		if(( $image->getObjectId())!=null ){
-			//update
-			try{
-				//update
-				$result = $parse->update($image->getObjectId());
-		
-				//aggiorno l'update
-				$image->setUpdatedAt(new DateTime($result->updatedAt, new DateTimeZone("America/Los_Angeles")));
-					
-			}
-			catch(ParseLibraryException $e){
-		
-				return false;
-		
-			}
-		
-		
-		
-		}else{
-		
-			try{
-				//salvo
-				$result = $parse->save();
-		
-				//aggiorno i dati per la creazione
-				$image->setObjectId($result->objectId);
-				$image->setCreatedAt(new DateTime($result->createdAt,new DateTimeZone("America/Los_Angeles")));
-				$image->setUpdatedAt(new DateTime($result->createdAt,new DateTimeZone("America/Los_Angeles")));
-			}
-			catch(ParseLibraryException $e){
-		
-				return false;
-		
-			}
-		
-		}
-		
-		//restituisco image aggiornato
-		return $image;
-	}
-	
-	function delete(Image $image){
-		$image->setActive(false);
-		
-		if($this->save($image)) return true;
-		else return false;		
-	}
-	
-	function getImage($imageId){
-		
-		$image = new Image();
-			
-		$parseImage = new parseObject("Image");
-		
-		$res = $parseImage->get($imageId);
-	
-		$image = $this->parseToImage($res);
-		
-		return $image;		
-	}
-	
-	function parseToImage(stdClass $parseObj){
-		
-		$image = new Image();
-		//recupero objectId
-		if(isset( $parseObj->objectId ) )$image->setObjectId($parseObj->objectId);
-		if(isset($parseObj->album)){
-			$parseAlbum = new AlbumParse();
-			$pointer = $parseObj->album;
-			$image->setActive($parseAlbum->getAlbum($pointer->objectId));
-		}
-				
-		if(isset($parseObj->comments))  $image->setCounter($parseObj->comments);//DA MODIFICARE??
-		if(isset($parseObj->counter))  $image->setCounter($parseObj->counter);
-		if(isset($parseObj->description))  $image->setDescription($parseObj->description);
-
-		if(isset($parseObj->featuring)){
-			$parseUser = new UserParse();
-			$featuring = $parseUser->getUserArrayById($parseObj->featuring);
-			$image->setFeaturing($featuring);
-		}
-		if(isset($parseObj->filePath))  $image->setFilePath($parseObj->filePath);
-		
-		if(isset($parseObj->fromUser)){
-			$parseUser = new UserParse();
-			$pointer = $parseObj->fromUser;
-			$fromUser = $parseUser->getUserById($pointer->objectId);
-			$image->setActive($fromUser);
-		}
-
-		if(isset($parseObj->location)){
-			$geoParse = $parseObj->location;			
-			$geoPoint = new parseGeoPoint($geoParse->latitude, $geoParse->longitude);
-			$image->setLocation($geoPoint);
-
-		}
-		if(isset($parseObj->loveCounter))  $image->setLoveCounter($parseObj->loveCounter); //aggiunta per il contatore di love
+        //recupero le info dell'oggetto
+        $parse = new parseObject("Image");
 
 
-		if(isset($parseObj->tags))  $image->setTags($parseObj->tag);
+        if ($parse->active)
+            $parse->active = $image->getActive();
+        if ($parse->album)
+            $parse->album = toParsePointer($image->getAlbum());
+        if ($parse->commentators)
+            $parse->commentators = toParseRelation($image->getCommentator());
+        if ($parse->comments)
+            $parse->comments = toParseRelation($image->getComments());
+        if ($parse->counter)
+            $parse->counter = $image->getCounter();
+        if ($parse->description)
+            $parse->description = $image->getDescription();
+        if ($parse->featuring)
+            $parse->featuring = toParseRelation($image->getFeaturing());
+//        if ($parse->file)
+//            $parse->file = $image->getFile();
+        if ($parse->filePath)
+            $parse->filePath = $image->getFilePath();
+        if ($parse->fromUser)
+            $parse->fromUser = toParsePointer($image->getFromUser());
+        if ($parse->location)
+            $parse->location = toParseGeoPoint($image->getLocation());
+        if ($parse->loveCounter)
+            $parse->loveCounter = $image->getLoveCounter();
+        if ($parse->lovers)
+            $parse->lovers = toParseRelation($image->getLovers());
+        if ($parse->tags)
+            $parse->tags = $image->getTags();
+        if ($parse->ACL)
+            $parse->ACL = toParseACL($image->getACL());
 
-		//creo la data di tipo DateTime per createdAt e updatedAt
-		if( isset($parseObj->createdAt) ) $image->setCreatedAt(new DateTime($parseObj->createdAt,new DateTimeZone("America/Los_Angeles")));
-		if( isset($parseObj->updatedAt) ) $image->setUpdatedAt(new DateTime($parseObj->updatedAt,new DateTimeZone("America/Los_Angeles")));
+        if (( $image->getObjectId()) != null) {
+            //update
+            try {
+                //update
+                $result = $parse->update($image->getObjectId());
 
-		//ACL
-		if(isset( $parseObj->ACL ) ){
-				
-			$ACL = null;
-			$image->setACL($ACL);
-		
-		}
-		
-	}
+                //aggiorno l'update
+                $image->setUpdatedAt(new DateTime($result->updatedAt, new DateTimeZone("America/Los_Angeles")));
+            } catch (ParseLibraryException $e) {
+
+                return false;
+            }
+        } else {
+
+            try {
+                //salvo
+                $result = $parse->save();
+
+                //aggiorno i dati per la creazione
+                $image->setObjectId($result->objectId);
+                $image->setCreatedAt(new DateTime($result->createdAt, new DateTimeZone("America/Los_Angeles")));
+                $image->setUpdatedAt(new DateTime($result->createdAt, new DateTimeZone("America/Los_Angeles")));
+            } catch (ParseLibraryException $e) {
+
+                return false;
+            }
+        }
+
+        //restituisco image aggiornato
+        return $image;
+    }
+
+    function delete(Image $image) {
+        $image->setActive(false);
+
+        if ($this->save($image))
+            return true;
+        else
+            return false;
+    }
+
+    public function getImage($objectId) {
+        $result = parseObject::get($objectId);
+        return $this->parseToActivity($result);
+    }
+
+    public function getImages() {
+        $images = null;
+        $result = $this->parseQuery->find();
+        if (is_array($result->results) && count($result->results) > 0) {
+            $images = array();
+            foreach ($result->results as $image) {
+                if ($image) {
+                    //recupero l'utente
+                    $images[] = $this->parseToImage($image);
+                }
+            }
+        }
+        return $images;
+    }
+
+    function parseToImage(stdClass $parseObj) {
+
+        if ($parseObj == null)
+            return null;
+
+        $image = new Image();
+
+        if (isset($parseObj->objectId))
+            $image->setObjectId($parseObj->objectId);
+        else
+            return null;
+
+        if (isset($parseObj->active))
+            $image->setActive($parseObj->active);
+        if (isset($parseObj->album))
+            $image->setAlbum(AlbumParse::getAlbum($parseObj->album->objectId));
+
+        if (isset($parseObj->commentators)) {
+            $parse = new UserParse();
+            $parse->whereRelatedTo("commentators", "Image", $parseObj->objectId);
+            $image->setCommentator($parse->getUsers());
+        }
+
+        if (isset($parseObj->comments)) {
+            $parse = new CommentParse();
+            $parse->whereRelatedTo("comments", "Image", $parseObj->objectId);
+            $image->setComments($parse->getComments());
+        }
+
+        if (isset($parseObj->counter))
+            $image->setCounter($parseObj->counter);
+        if (isset($parseObj->description))
+            $image->setDescription($parseObj->description);
+        if (isset($parseObj->featuring)) {
+            $parse = new UserParse();
+            $parse->whereRelatedTo("featuring", "Image", $parseObj->objectId);
+            $image->setFeaturing($parse->getUsers());
+        }
+//        if (isset($parseObj->file)) $image->setFile ();
+        if (isset($parseObj->filePath))
+            $image->setFilePath($parseObj->filePath);
+        if (isset($parseObj->fromUser)) {
+            $parse = new UserParse();
+            $parse->whereRelatedTo("fromUser", "Image", $parseObj->objectId);
+            $image->setFromUser($parse->getUsers());
+        }
+        if (isset($parseObj->location)) {
+            $parseGeoPoint = new parseGeoPoint($res->location->latitude, $res->location->longitude);
+            $image->setLocation($parseGeoPoint->location);
+        }
+        if (isset($parseObj->loveCounter))
+            $image->setLoveCounter($parseObj->loveCounter);
+        if (isset($parseObj->lovers))
+            $image->setLovers($parseObj->lovers);
+        if (isset($parseObj->tags) && count($parseObj->tags) > 0)
+            $image->setTags($parseObj->tags);
+        if (isset($parseObj->createdAt))
+            $image->setCreatedAt(new DateTime($parseObj->createdAt));
+        if (isset($parseObj->updatedAt))
+            $image->setUpdatedAt(new DateTime($parseObj->updatedAt));
+        if (isset($parseObj->ACL))
+            $image->setACL($parseObj->ACL);
+    }
+
+    public function getCount() {
+        $this->parseQuery->getCount();
+    }
+
+    public function setLimit($int) {
+        $this->parseQuery->setLimit($int);
+    }
+
+    public function setSkip($int) {
+        $this->parseQuery->setSkip($int);
+    }
+
+    public function orderBy($field) {
+        $this->parseQuery->orderBy($field);
+    }
+
+    public function orderByAscending($value) {
+        $this->parseQuery->orderByAscending($value);
+    }
+
+    public function orderByDescending($value) {
+        $this->parseQuery->orderByDescending($value);
+    }
+
+    public function whereInclude($value) {
+        $this->parseQuery->whereInclude($value);
+    }
+
+    public function where($key, $value) {
+        $this->parseQuery->where($key, $value);
+    }
+
+    public function whereEqualTo($key, $value) {
+        $this->parseQuery->whereEqualTo($key, $value);
+    }
+
+    public function whereNotEqualTo($key, $value) {
+        $this->parseQuery->whereNotEqualTo($key, $value);
+    }
+
+    public function whereGreaterThan($key, $value) {
+        $this->parseQuery->whereGreaterThan($key, $value);
+    }
+
+    public function whereLessThan($key, $value) {
+        $this->parseQuery->whereLessThan($key, $value);
+    }
+
+    public function whereGreaterThanOrEqualTo($key, $value) {
+        $this->parseQuery->whereGreaterThanOrEqualTo($key, $value);
+    }
+
+    public function whereLessThanOrEqualTo($key, $value) {
+        $this->parseQuery->whereLessThanOrEqualTo($key, $value);
+    }
+
+    public function whereContainedIn($key, $value) {
+        $this->parseQuery->whereContainedIn($key, $value);
+    }
+
+    public function whereNotContainedIn($key, $value) {
+        $this->parseQuery->whereNotContainedIn($key, $value);
+    }
+
+    public function whereExists($key) {
+        $this->parseQuery->whereExists($key);
+    }
+
+    public function whereDoesNotExist($key) {
+        $this->parseQuery->whereDoesNotExist($key);
+    }
+
+    public function whereRegex($key, $value, $options = '') {
+        $this->parseQuery->whereRegex($key, $value, $options = '');
+    }
+
+    public function wherePointer($key, $className, $objectId) {
+        $this->parseQuery->wherePointer($key, $className, $objectId);
+    }
+
+    public function whereInQuery($key, $className, $inQuery) {
+        $this->parseQuery->whereInQuery($key, $className, $inQuery);
+    }
+
+    public function whereNotInQuery($key, $className, $inQuery) {
+        $this->parseQuery->whereNotInQuery($key, $className, $inQuery);
+    }
+
+    public function whereRelatedTo($key, $className, $objectId) {
+        $this->parseQuery->whereRelatedTo($key, $className, $objectId);
+    }
+
 }
+
 ?>
