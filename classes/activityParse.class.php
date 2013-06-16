f751793ed0e71bcff5d50e9111874b42a2799365
Stefano <stefanom@jamyourself.com>
2013-06-16 12:51:22 +0200

    correzioni con l'utilizzo dei metodi utils.class.php

--- classes/activityParse.class.php
+++ classes/activityParse.class.php
@@ -36,134 +36,40 @@ class ActivityParse {
 
         //creo un'istanza dell'oggetto della libreria ParseLib
         $parseObj = new parseObject("Activity");
-
-        //inizializzo le properties
-        //accepted BOOL: da definire																	
+																
         $parseObj->accepted = $activity->getAccepted();
+									
+        $parseObj->active = $activity->getActive();		
 
-        //BOOL:Indica se l'istanza della classe è attiva 									
-        $parseObj->active = $activity->getActive();
+        $parseObj->album = toParsePointer($activity->getAlbum());	
 
-        //puntatore alla album
-        //Album (Parse Object): Istanza della classe Album associata all'activity 			
-        if ($activity->getAlbum() != null) {
-            $album = $activity->getAlbum();
-            $parseObj->album = array("__type" => "Pointer", "className" => "Album", "objectId" => $album->getObjectId());
-        }
-        else
-            $parseObj->album = null;
+        $parseObj->comment = toParsePointer($activity->getComment());        
 
-        //puntatore alla comment
-        //Comment (Parse Object): Istanza della classe Comment associata all'activity		
+        $parseObj->event = toParsePointer($activity->getEvent());
 
-        if ($activity->getComment() != null) {
-            $comment = $activity->getComment();
-            $parseObj->comment = array("__type" => "Pointer", "className" => "Comment", "objectId" => $comment->getObjectId());
-        }
-        else
-            $parseObj->comment = null;
+        $parseObj->fromUser = toParsePointer($activity->getFromUser());
 
-//puntatore all'evento
-        //Event (Parse Object): Istanza della classe Event associata all'activity           
-        if ($activity->getEvent() != null) {
-            $event = $activity->getEvent();
-            $parseObj->event = array("__type" => "Pointer", "className" => "Event", "objectId" => $event->getObjectId());
-        }
-        else
-            $parseObj->event = null;
+        $parseObj->image = toParsePointer($activity->getImage());
 
-        //puntatore al fromUser
-        //User:Utente che effettua l'azione 												
-        if ($activity->getFromUser() != null) {
-            $user = $activity->getFromUser();
-            $parseObj->fromUser = array("__type" => "Pointer", "className" => "_User", "objectId" => $user->getObjectId());
-        }
-        else
-            $parseObj->fromUser = null;
+        $parseObj->playlist = toParsePointer($activity->getPlaylist());
 
-        //puntatore all'image
-        //Image (Parse Object): Istanza della classe Image associata all'activity           
-        if ($activity->getImage() != null) {
-            $image = $activity->getImage();
-            $parseObj->image = array("__type" => "Pointer", "className" => "Image", "objectId" => $image->getObjectId());
-        }
-        else
-            $parseObj->image = null;
-
-        //puntatore alla question
-        //Playlist (Parse Object): Istanza della classe Playlist associata all'activity     
-        if ($activity->getPlaylist() != null) {
-            $playlist = $activity->getPlaylist();
-            $parseObj->playlist = array("__type" => "Pointer", "className" => "Playlist", "objectId" => $playlist->getObjectId());
-        }
-        else
-            $parseObj->playlist = null;
-
-        //puntatore alla question
-        //Question (Parse Object): Istanza della classe Question associata all'activity     
-        if ($activity->getQuestion() != null) {
-            $question = $activity->getQuestion();
-            $parseObj->question = array("__type" => "Pointer", "className" => "Question", "objectId" => $question->getObjectId());
-        }
-        else
-            $parseObj->question = null;
-
-
-        //BOOL:Indica se l'istanza della classe è stata letta o meno 						
+        $parseObj->question = toParsePointer($activity->getQuestion());
+					
         $parseObj->read = $activity->getRead();
 
-        //puntatore alla song
-        //Record (Parse Object): Istanza della classe Record associata all'activity 		
-        if ($activity->getRecord() != null) {
-            $record = $activity->getRecord();
-            $parseObj->record = array("__type" => "Pointer", "className" => "Record", "objectId" => $record->getObjectId());
-        }
-        else
-            $parseObj->record = null;
+        $parseObj->record = toParsePointer($activity->getRecord());
 
-        //puntatore alla song
-        //Song (Parse Object): Istanza della classe Song associata all'activity 			
-        if ($activity->getSong() != null) {
-            $song = $activity->getSong();
-            $parseObj->song = array("__type" => "Pointer", "className" => "Song", "objectId" => $song->getObjectId());
-        }
-        else
-            $parseObj->song = null;
+        $parseObj->song = toParsePointer($activity->getSong());
 
-        //string:Indica lo status di un'attività del tipo richiesta-accettazione/rifiuto    
         $parseObj->status = $activity->getStatus();
 
-        //puntatore al toUser
-        //User:Utente che riceve l'azione 													
-        if ($activity->getToUser() != null) {
-            $user = $activity->getToUser();
-            $parseObj->toUser = array("__type" => "Pointer", "className" => "_User", "objectId" => $user->getObjectId());
-        }
-        else
-            $parseObj->toUser = null;
-
-        //Stringa type
-        //string:Indica la tipologia di attività 											
+        $parseObj->toUser = toParsePointer($activity->getToUser());
+										
         $parseObj->type = $activity->getType();
 
-        //ACL:access control list, determina le politiche di accesso alla classe 			
-        //$parseObj->ACL = $activity->getACL();   //perchè non la prendi?????
-        //puntatore allo stato
-        //Status(Parse Object): Istanza della classe Status associata all'activity 			
-        if ($activity->getUserStatus() != null) {
-            $status = $activity->getUserStatus();
-            $parseObj->userStatus = array("__type" => "Pointer", "className" => "Status", "objectId" => $status->getObjectId());
-        }
-        else
-            $parseObj->userStatus = null;
+        $parseObj->userStatus = toParsePointer($activity->getUserStatus());
 
-        //Video (Parse Object):Istanza della classe Video associata all'activity            
-        if ($activity->getVideo() != null) {
-            $video = $activity->getVideo();
-            $parseObj->video = array("__type" => "Pointer", "className" => "Video", "objectId" => $video->getObjectId());
-        }
-        else
-            $parseObj->video = null;
+        $parseObj->video = toParsePointer($activity->getVideo());
 
         //caso update
         if ($activity->getObjectId() != null) {
@@ -200,43 +106,22 @@ class ActivityParse {
      * @param string $activityId
      * @return boolean|Activity
      */
-    public function getActivity() {
-
-        $activity = false;
-
-        $result = $this->parseQuery->find();
-
-        if (is_array($result->results) && count($result->results) > 0) {
-
-            $ret = $result->results[0];
-
-            if ($ret) {
-
-                //recupero l'utente
-                $activity = $this->parseToActivity($ret);
-            }
-        }
-
-        return $activity;
+    public function getActivity($objectId){
+        $result = (new parseRestClient())->get($objectId);
+        return $this->parseToActivity($result);
     }
 
-    public function getActivities() {
-
-        $activities = false;
-
+    public function getActivities(){
+        $activities = null;
         $result = $this->parseQuery->find();
-
         if (is_array($result->results) && count($result->results) > 0) {
-
+            $activities = array();
             foreach ($result->results as $activity) {
                 if ($activity) {
-
-                    //recupero l'utente
                     $activities[] = $this->parseToActivity($activity);
                 }
             }
         }
-
         return $activities;
     }
 
@@ -410,226 +295,97 @@ class ActivityParse {
 //     
 //*************************************************************************/
     public function getCount() {
-        $this->_count = 1;
-        $this->_limit = 0;
-        return $this->find();
+        $this->parseQuery->getCount();
     }
 
     public function setLimit($int) {
-        if ($int >= 1 && $int <= 1000) {
-            $this->_limit = $int;
-        } else {
-            $this->throwError('parse requires the limit parameter be between 1 and 1000');
-        }
+        $this->parseQuery->setLimit($int);
     }
 
     public function setSkip($int) {
-        $this->_skip = $int;
+        $this->parseQuery->setSkip($int);
     }
 
     public function orderBy($field) {
-        if (!empty($field)) {
-            $this->_order[] = $field;
-        }
+        $this->parseQuery->orderBy($field);
     }
 
     public function orderByAscending($value) {
-        if (is_string($value)) {
-            $this->_order[] = $value;
-        } else {
-            $this->throwError('the order parameter on a query must be a string');
-        }
+        $this->parseQuery->orderByAscending($value);
     }
 
     public function orderByDescending($value) {
-        if (is_string($value)) {
-            $this->_order[] = '-' . $value;
-        } else {
-            $this->throwError('the order parameter on parseQuery must be a string');
-        }
+        $this->parseQuery->orderByDescending($value);
     }
 
     public function whereInclude($value) {
-        if (is_string($value)) {
-            $this->_include[] = $value;
-        } else {
-            $this->throwError('the include parameter on parseQuery must be a string');
-        }
+        $this->parseQuery->whereInclude($value);
     }
 
     public function where($key, $value) {
-        $this->whereEqualTo($key, $value);
+        $this->parseQuery->where($key, $value);
     }
 
     public function whereEqualTo($key, $value) {
-        if (isset($key) && isset($value)) {
-            $this->_query[$key] = $value;
-        } else {
-            $this->throwError('the $key and $value parameters must be set when setting a "where" query method');
-        }
+        $this->parseQuery->whereEqualTo($key, $value);
     }
 
     public function whereNotEqualTo($key, $value) {
-        if (isset($key) && isset($value)) {
-            $this->_query[$key] = array(
-                '$ne' => $value
-            );
-        } else {
-            $this->throwError('the $key and $value parameters must be set when setting a "where" query method');
-        }
+        $this->parseQuery->whereNotEqualTo($key, $value);
     }
 
     public function whereGreaterThan($key, $value) {
-        if (isset($key) && isset($value)) {
-            $this->_query[$key] = array(
-                '$gt' => $value
-            );
-        } else {
-            $this->throwError('the $key and $value parameters must be set when setting a "where" query method');
-        }
+        $this->parseQuery->whereGreaterThan($key, $value);
     }
 
     public function whereLessThan($key, $value) {
-        if (isset($key) && isset($value)) {
-            $this->_query[$key] = array(
-                '$lt' => $value
-            );
-        } else {
-            $this->throwError('the $key and $value parameters must be set when setting a "where" query method');
-        }
+        $this->parseQuery->whereLessThan($key, $value);
     }
 
     public function whereGreaterThanOrEqualTo($key, $value) {
-        if (isset($key) && isset($value)) {
-            $this->_query[$key] = array(
-                '$gte' => $value
-            );
-        } else {
-            $this->throwError('the $key and $value parameters must be set when setting a "where" query method');
-        }
+        $this->parseQuery->whereGreaterThanOrEqualTo($key, $value);
     }
 
     public function whereLessThanOrEqualTo($key, $value) {
-        if (isset($key) && isset($value)) {
-            $this->_query[$key] = array(
-                '$lte' => $value
-            );
-        } else {
-            $this->throwError('the $key and $value parameters must be set when setting a "where" query method');
-        }
+        $this->parseQuery->whereLessThanOrEqualTo($key, $value);
     }
 
     public function whereContainedIn($key, $value) {
-        if (isset($key) && isset($value)) {
-            if (is_array($value)) {
-                $this->_query[$key] = array(
-                    '$in' => $value
-                );
-            } else {
-                $this->throwError('$value must be an array to check through');
-            }
-        } else {
-            $this->throwError('the $key and $value parameters must be set when setting a "where" query method');
-        }
+        $this->parseQuery->whereContainedIn($key, $value);
     }
 
     public function whereNotContainedIn($key, $value) {
-        if (isset($key) && isset($value)) {
-            if (is_array($value)) {
-                $this->_query[$key] = array(
-                    '$nin' => $value
-                );
-            } else {
-                $this->throwError('$value must be an array to check through');
-            }
-        } else {
-            $this->throwError('the $key and $value parameters must be set when setting a "where" query method');
-        }
+        $this->parseQuery->whereNotContainedIn($key, $value);
     }
 
     public function whereExists($key) {
-        if (isset($key)) {
-            $this->_query[$key] = array(
-                '$exists' => true
-            );
-        }
+        $this->parseQuery->whereExists($key);
     }
 
     public function whereDoesNotExist($key) {
-        if (isset($key)) {
-            $this->_query[$key] = array(
-                '$exists' => false
-            );
-        }
+        $this->parseQuery->whereDoesNotExist($key);
     }
 
     public function whereRegex($key, $value, $options = '') {
-        if (isset($key) && isset($value)) {
-            $this->_query[$key] = array(
-                '$regex' => $value
-            );
-
-            if (!empty($options)) {
-                $this->_query[$key]['options'] = $options;
-            }
-        } else {
-            $this->throwError('the $key and $value parameters must be set when setting a "where" query method');
-        }
+        $this->parseQuery->whereRegex($key, $value, $options = '');
     }
 
     public function wherePointer($key, $className, $objectId) {
-        if (isset($key) && isset($className)) {
-            $this->_query[$key] = $this->dataType('pointer', array($className, $objectId));
-        } else {
-            $this->throwError('the $key and $className parameters must be set when setting a "where" pointer query method');
-        }
+        $this->parseQuery->wherePointer($key, $className, $objectId);
     }
 
     public function whereInQuery($key, $className, $inQuery) {
-        if (isset($key) && isset($className)) {
-            $this->_query[$key] = array(
-                '$inQuery' => $inQuery,
-                'className' => $className
-            );
-        } else {
-            $this->throwError('the $key and $value parameters must be set when setting a "where" query method');
-        }
+        $this->parseQuery->whereInQuery($key, $className, $inQuery);
     }
 
     public function whereNotInQuery($key, $className, $inQuery) {
-        if (isset($key) && isset($className)) {
-            $this->_query[$key] = array(
-                '$notInQuery' => $inQuery,
-                'className' => $className
-            );
-        } else {
-            $this->throwError('the $key and $value parameters must be set when setting a "where" query method');
-        }
+        $this->parseQuery->whereNotInQuery($key, $className, $inQuery);
     }
 
-    /**
-     * Example - to find users with a particular role id
-     * ES: per trovare gli Utenti in relazione con un Album, dove
-     * nella tabella album abbiamo una colonna "userRelation"
-     * la query si fa su : $query->parseQuery('_User);
-     * $query->whereRelatedTo('users', '_Role', $roleId);
-     * 
-     * @param type $key = nome colonna del tipo relazione
-     * @param type $className = classe di cui si cerca la relazione
-     * @param type $objectId = id dell'oggetto di cui si cercano le relazioni
-     */
     public function whereRelatedTo($key, $className, $objectId) {
-        if (isset($key) && isset($className) && isset($objectId)) {
-            if ($className === 'Role')
-                $className = '_Role';
-            if ($className === 'User')
-                $className = '_User';
-            $pointer = $this->dataType('pointer', array($className, $objectId));
-            $this->_query['$relatedTo'] = $this->dataType('relatedTo', array($pointer, $key));
-        } else {
-            $this->throwError('the $key and $classname and $objectId parameters must be set when setting a "whereRelatedTo" query method');
-        }
+        $this->parseQuery->whereRelatedTo($key, $className, $objectId);
     }
+
 
 }
 
