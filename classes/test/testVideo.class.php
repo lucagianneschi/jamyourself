dff28b72cc0860672d3391d066ccb6fa705fc17f
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-06-21 00:36:04 +0200

    modifiche secondo nuovo standard classi

--- classes/test/testVideo.class.php
+++ classes/test/testVideo.class.php
@@ -24,6 +24,7 @@ require_once ROOT_DIR . 'config.php';
 require_once PARSE_DIR . 'parse.php';
 require_once CLASSES_DIR . 'comment.class.php';
 require_once CLASSES_DIR . 'commentParse.class.php';
+require_once CLASSES_DIR . 'utils.class.php';
 
 $video = new Video();
 $video->setObjectId('aAbBcCdD');
