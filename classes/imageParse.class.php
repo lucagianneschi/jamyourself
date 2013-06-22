442e54663ed5a47206418ac0582b7e0d4f4dd8c2
Stefano <stefanom@jamyourself.com>
2013-06-22 15:59:20 +0200

    [TERMINATA] da revisionare dagli altri

--- classes/imageParse.class.php
+++ classes/imageParse.class.php
@@ -46,7 +46,7 @@ class ImageParse {
         $this->parseQuery = new parseQuery("Image");
     }
 
-    function save(Image $image) {
+    function saveImage(Image $image) {
 
         //recupero le info dell'oggetto
         $parse = new parseObject("Image");
@@ -64,7 +64,11 @@ class ImageParse {
         $parseObj->location = toParseGeoPoint($image->getLocation());
         $parseObj->loveCounter = $image->getLoveCounter();
         $parseObj->lovers = toParseRelation("_User", $image->getLovers());
-        $parseObj->tags = $image->getTags();
+        if ($image->getTags() != null && count($image->getTags()) > 0)
+            $parseObj->tags = $image->getTags();
+        else
+            $parseObj->tags = null;
+
         $parseObj->ACL = toParseACL($image->getACL());
 
         if (( $image->getObjectId()) != null) {
@@ -98,15 +102,15 @@ class ImageParse {
         return $image;
     }
 
-    function delete(Image $image) {
+    function deleteImage(Image $image) {
         $image->setActive(false);
         return $this->save($image);
     }
 
     public function getImage($objectId) {
         try {
-            $parseRestClient = new parseRestClient();
-            $result = $parseRestClient->get($objectId);
+            $query = new parseObject("Image");
+            $result = $query->get($objectId);
             return $this->parseToImage($result);
         } catch (Exception $exception) {
             return throwError($exception, __CLASS__, __FUNCTION__, func_get_args());
@@ -119,9 +123,10 @@ class ImageParse {
             $result = $this->parseQuery->find();
             if (is_array($result->results) && count($result->results) > 0) {
                 $images = array();
-                foreach ($result->results as $image) {
-                    if ($image) {
-                        $images[] = $this->parseToImage($image);
+                foreach ($result->results as $obj) {
+                    if ($obj) {
+                        $image = $this->parseToImage($obj);
+                        $images[$image->getObjectId()] = $image;
                     }
                 }
             }
