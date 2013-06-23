fd13febea9fcafa12b8d3b88d2aafaa4ab4da3c2
Stefano <stefanom@jamyourself.com>
2013-06-23 13:48:07 +0200

    [Test completato]

--- classes/test/testActivity.class.php
+++ classes/test/testActivity.class.php
@@ -11,58 +11,68 @@ require_once PARSE_DIR . 'parse.php';
 require_once CLASSES_DIR . 'activity.class.php';
 require_once CLASSES_DIR . 'activityParse.class.php';
 
-$objectId = null;
-$accepted = true;
-$active = true;
-
-$pAlbum = new AlbumParse();
-$album = $pAlbum->getAlbum("iH2oruHDLk");
-
-$pComment = new CommentParse();
-$comment = $pComment->getComment("JEMROjxGB7");
-
-
-$pEvent = new EventParse();
-$event = $pEvent->getEvent("19OuVtOhfJ");
-
-$pFromUser = new UserParse();
-$fromUser = $pFromUser->getUser("sDqka2c1WB");
-
-$pImage = new ImageParse();
-$image = $pImage->getImage("Sahxw6cA54");
-
-$pPlaylist = new PlaylistParse();
-$playlist = $pPlaylist->getPlaylist("s1eWGF8rkU");
-
-$pQuestion = new QuestionParse();
-$question = $pQuestion->getQuestion("YvGtWTXV0O");
-
-$read = false;
-
-$pRecord = new RecordParse();
-$record = $pRecord->getRecord("dHzayJzbKZ");
-
-$pSong = new SongParse();
-$song = $pSong->getSong("otkv0QlSOs");
-
-$status = "Test su Activity";
-
-$pUser = new UserParse();
-$toUser = $pUser->getUser("1l9es3F5WO");
-
-$type = "TEST_ACTIVITY_SAVE";
-
-$pStatus = new StatusParse();
-$userStatus = $pStatus->getStatus("1WZPRyq39Z");
-
-$pVideo = new VideoParse();
-$video = $pVideo->getVideo("ihcPvm6BIv");
-
 $ACL = new parseACL();
 $ACL->setPublicWriteAccess(true);
 
 $activity = new Activity();
 
+$activity->setAccepted(null);
+$activity->setActive(null);        
+$activity->setAlbum(null);
+$activity->setComment(null);
+$activity->setEvent(null);
+$activity->setFromUser(null);
+$activity->setImage(null);
+$activity->setPlaylist(null);
+$activity->setQuestion(null);
+$activity->setRead(null);
+$activity->setRecord(null);
+$activity->setSong(null);
+$activity->setStatus(null);
+$activity->setToUser(null);
+$activity->setType(null);
+$activity->setUserStatus(null);
+$activity->setVideo(null);
+$activity->setACL($ACL);
+echo '<br />-------------------------------------------------------------------------------<br />';
+echo "<br/><br/> Stampo Activity prima del salvataggio con valori NULL:  <br/><br/>";
+echo '<br />-------------------------------------------------------------------------------<br />';
+echo $activity;
+echo '<br />-------------------------------------------------------------------------------<br />';
+echo "<br/><br/> Stampo Activity restituita dal salvataggio con valori NULL:  <br/><br/>";
+echo '<br />-------------------------------------------------------------------------------<br />';
+$pActivity = new ActivityParse();
+
+$activity = $pActivity->saveActivity($activity);
+
+echo $activity;
+echo '<br />-------------------------------------------------------------------------------<br />';
+echo "<br/><br/>Recupero l'oggetto salvato su Parse: objectId: ". $activity->getObjectId() ."  <br/><br/>";
+echo '<br />-------------------------------------------------------------------------------<br />';
+$pActivity = new ActivityParse();
+$activity = $pActivity->getActivity($activity->getObjectId());
+
+echo $activity;
+echo '<br />-------------------------------------------------------------------------------<br />';
+echo '<br />Inizializzo i valori <br />';
+echo '<br />-------------------------------------------------------------------------------<br />';
+$accepted = true;
+$active = true;
+$album = "iH2oruHDLk";
+$comment = "JEMROjxGB7";
+$event = "19OuVtOhfJ";
+$fromUser = "sDqka2c1WB";
+$image = "Sahxw6cA54";
+$playlist = "s1eWGF8rkU";
+$question = "YvGtWTXV0O";
+$read = false;
+$record = "dHzayJzbKZ";
+$song = "otkv0QlSOs";
+$status = "Test su Activity";
+$toUser = "1l9es3F5WO";
+$type = "TEST_ACTIVITY_SAVE";
+$userStatus = "1WZPRyq39Z";
+$video = "ihcPvm6BIv";
 $activity->setAccepted($accepted);
 $activity->setActive($active);        
 $activity->setAlbum($album);
@@ -81,8 +91,46 @@ $activity->setType($type);
 $activity->setUserStatus($userStatus);
 $activity->setVideo($video);
 $activity->setACL($ACL);
-
-echo "Stampo Activity prima del salvataggio: <br/>";
+echo '<br />-------------------------------------------------------------------------------<br />';
+echo "<br/><br/> Stampo Activity prima del salvataggio:  <br/><br/>";
+echo '<br />-------------------------------------------------------------------------------<br />';
 echo $activity;
+echo '<br />-------------------------------------------------------------------------------<br />';
+echo "<br/><br/> Stampo Activity restituita dal salvataggio:  <br/><br/>";
+echo '<br />-------------------------------------------------------------------------------<br />';
+$pActivity = new ActivityParse();
 
+$activity = $pActivity->getActivity($activity->getObjectId());
+
+echo $activity;
+echo '<br />-------------------------------------------------------------------------------<br />';
+echo "<br/><br/>Recupero l'oggetto salvato su Parse: objectId: ". $activity->getObjectId() ."  <br/><br/>";
+echo '<br />-------------------------------------------------------------------------------<br />';
+$pActivity = new ActivityParse();
+$activity = $pActivity->getActivity($activity->getObjectId());
+
+echo $activity;
+echo '<br />-------------------------------------------------------------------------------<br />';
+echo "<br/><br/> UPDATE dell'oggetto salvato su Parse: objectId: ". $activity->getObjectId() ."<br/><br/>";
+echo '<br />-------------------------------------------------------------------------------<br />';
+$pActivity = new ActivityParse();
+$activity = $pActivity->getActivity($activity->getObjectId());
+$activity->setType("TEST_ACTIVITY_UPDATED");
+$pActivity->saveActivity($activity);
+$activity = $pActivity->getActivity($activity->getObjectId());
+
+echo $activity;
+echo '<br />-------------------------------------------------------------------------------<br />';
+echo "<br/><br/> DELETE dell'oggetto salvato su Parse: objectId: ". $activity->getObjectId() ."<br/><br/>";
+echo '<br />-------------------------------------------------------------------------------<br />';
+$ret = $pActivity->deleteActivity($activity->getObjectId());
+
+echo "Fine delete";
+echo '<br />-------------------------------------------------------------------------------<br />';
+echo "<br/><br/> DELETE Provo a recuperare l'oggetto cancellato (deve avere active = FALSE): ". $activity->getObjectId() ."<br/><br/>";
+echo '<br />-------------------------------------------------------------------------------<br />';
+$pActivity = new ActivityParse();
+$activity = $pActivity->getActivity($activity->getObjectId());
+echo $activity;
+echo '<br />-------------------------------------------------------------------------------<br />';
 ?>
