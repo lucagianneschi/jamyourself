ee425f9b412e198d9db761f9d10f5b3b03a5f58c
Stefano <stefanom@jamyourself.com>
2013-06-25 21:21:00 +0200

    tolti i requires inutili

--- classes/albumParse.class.php
+++ classes/albumParse.class.php
@@ -25,7 +25,6 @@ if (!defined('ROOT_DIR'))
 require_once ROOT_DIR . 'config.php';
 require_once PARSE_DIR . 'parse.php';
 require_once CLASSES_DIR . 'utils.class.php';
-require_once CLASSES_DIR . 'errorParse.class.php';
 require_once CLASSES_DIR . 'imageParse.class.php';
 require_once CLASSES_DIR . 'album.class.php';
 
