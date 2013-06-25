26a5d4878cfee76fa53424635c45ad69b9366afa
Stefano <stefanom@jamyourself.com>
2013-06-25 21:20:24 +0200

    tolti i requires inutili

--- classes/songParse.class.php
+++ classes/songParse.class.php
@@ -24,18 +24,8 @@ if (!defined('ROOT_DIR'))
 require_once ROOT_DIR . 'config.php';
 require_once PARSE_DIR . 'parse.php';
 require_once CLASSES_DIR . 'utils.class.php';
+require_once CLASSES_DIR . 'song.class.php';
 
-require_once CLASSES_DIR . 'error.class.php';
-require_once CLASSES_DIR . 'errorParse.class.php';
-
-require_once CLASSES_DIR . 'user.class.php';
-require_once CLASSES_DIR . 'userParse.class.php';
-
-require_once CLASSES_DIR . 'comment.class.php';
-require_once CLASSES_DIR . 'commentParse.class.php';
-
-require_once CLASSES_DIR . 'recordParse.class.php';
-require_once CLASSES_DIR . 'record.class.php';
 
 class SongParse {
 
