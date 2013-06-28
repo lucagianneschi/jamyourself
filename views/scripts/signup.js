fd12d2149a71fbc45a0873cfe8d2e78f0cab5447
Stefano <stefanom@jamyourself.com>
2013-06-29 01:59:54 +0200

    Signup controllers
    Due esempi di controllers per due funzionalità differenti:
    in view\signu.php c'è un esempio di come si possano fare i controlli
    veloci su campi come username per avvertire l'utente che il nick scelto
    non è disponibile

---
+++ views/scripts/signup.js
@@ -0,0 +1,159 @@
+function checkUserExists() {
+    hideMessage();
+    hideError();
+    var data = null;
+    var request = null;
+    data = {};
+
+    username = $('#username').val();
+
+    if (username.length > 0) {
+        data['username'] = username;
+
+        json = JSON.stringify(data);
+
+        // preparo la richiesta
+        request = {};
+        request['action'] = "userExists";
+        request['data'] = json;
+
+        // jquery per recuperare la playlist
+
+        risp = $.ajax({
+            type: "POST",
+            url: "../controllers/ajax.signup.controller.php",
+            data: request,
+            dataType: "json",
+            async: true,
+            success: function(data) {
+                if (data.error != null) {
+                    showError(json.error);
+                    return null;
+                }
+                else {
+                    //data.message contiene il numero di utenti con quel nickname
+                    if (data.message != 0)
+                        showError("Username non disponibile!");
+                    else
+                        showMessage("Username disponibile!");
+                }
+            },
+            error: function(data) {
+                showError("Errore di rete");
+            }
+        });
+
+    }
+
+}
+
+function login() {
+    //recupero i valori
+    var username = $("#username").val();
+    var password = $("#password").val();
+
+    console.log("Username : " + username);
+    console.log("Password : " + password);
+
+    //preparo il json
+    var json = {};
+    json['username'] = username;
+    json['password'] = password;
+
+    //invio la richiesta
+    $risp = $.ajax({
+        type: "POST",
+        url: "../controllers/user/loginController.php",
+        data: json,
+        dataType: json,
+        async: false
+    });
+
+    //gestisco la risposta
+    console.log("[login ] : RISPOSTA = " + $risp['responseText']);
+
+    showMessage("Logged");
+
+    var session = 0;
+//	var risposta = $risp['responseText'];
+
+
+    var obj = JSON.parse($risp['responseText']);
+    session = obj.userId;
+
+    //inizializzo la sessione
+    starSession(session);
+
+
+}
+
+function showError(text) {
+    $("#error").html("<span>" + text + "</span>");
+}
+
+function hideError() {
+    $("#error").html("");
+}
+
+function showMessage(text) {
+    $("#message").html("<span>" + text + "</span>");
+}
+
+function hideMessage() {
+    $("#message").html("");
+}
+
+
+function starSession(session) {
+
+    //creo un form fittizzio e passo session come post value
+    $html = "<form action=''  name='login' method='post' style='display:none;'>";
+    $html += "<input type='hidden' name='session' value='" + session + "'  />";
+    $html += "</form>";
+    $('#login').html($html);
+
+    //invio il form
+    document.forms['login'].submit();
+
+}
+
+function endSession() {
+    //creo un form fittizzio e passo session come post value
+    $html = "<form action=''  name='logout' method='post' style='display:none;'>";
+    $html += "<input type='hidden' name='logout'/>";
+    $html += "</form>";
+    $('#logout').html($html);
+
+    //invio il form
+    document.forms['logout'].submit();
+}
+
+$().ready(function() {
+
+//	http://jquery.bassistance.de/validate/demo/
+
+    // validate il form di login 
+    $("#formlogin").validate({
+        rules: {
+            username: {
+                required: true,
+                minlength: 2
+            },
+            password: {
+                required: true,
+                minlength: 5
+            },
+        },
+        messages: {
+            username: {
+                required: "Please enter a username",
+                minlength: "Your username must consist of at least 2 characters"
+            },
+            password: {
+                required: "Please provide a password",
+                minlength: "Your password must be at least 5 characters long"
+            },
+        }
+    });
+
+});
