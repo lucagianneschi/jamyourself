afc501644911539d05a631ee7cf7965ca8682b48
Stefano <stefanom@jamyourself.com>
2013-06-29 22:49:11 +0200

    estensione contrroller

--- controllers/signup.controller.php
+++ controllers/signup.controller.php
@@ -19,76 +19,65 @@ class SignupController {
         
     }
 
-    public function signup(User $user) {
-        $isValid = true;
-        require_once 'CLASSES_DIR' . 'userParse.class.php';
-
-        if (is_null($user) || is_null($user->getType()))
-            return throwError(new Exception("Invalid parameter"), __CLASS__, __FUNCTION__, func_get_args());
-        //verifica che i parametri dell'utente siano corretti
-
-        $this->post = $_POST;
-
+    public function signup($user) {
 
         //recupero le variabili
-        switch ($user->getType()) {
-            case "SPOTTER" :
-                $isValid = $this->checkSpotter($user);
-                break;
-            case "JAMMER" :
-                $isValid = $this->checkVenue($user);
-                break;
-            case "VENUE" :
-                $isValid = $this->checkJammer($user);
-                break;
-            default :
-                //errore                
-                break;
-        }
-
-        if ($isValid == false)
+        if ($this->checkUser($user) == false)
             return throwError(new Exception("Invalid User"), __CLASS__, __FUNCTION__, func_get_args());
 
         //tenta di effettuare il salvataggio
         $pUser = new UserParse();
-        $result = $pUser->saveUser($user);
+        $user = $pUser->saveUser($user);
 
-        if (is_a($result, "Error")) {
+        if (is_a($user, "Error")) {
             //result è un errore e contiene il motivo dell'errore
-            return $result;
+            return $user;
         }
 
-        $user = $result;
+        //se va a buon fine salvo una nuova activity       
+        $activity = new Activity();        
+        $activity->setAccepted(true);
+        $activity->setActive(true);
+        $activity->setFromUser($user->getObjectId());
+        $activity->setRead(true);
+        $activity->setStatus("A");
+        $activity->setType("SIGNEDUP");
+        $activity->setACL(toParseDefaultACL()) ;
         
-        //se va a buon fine salvo una nuova activity
-        $this->saveSignupActivity($user->getObjectId());
+        $pActivity = new ActivityParse();
+        $pActivity->saveActivity($activity);
 
         //aggiorno l'oggetto User in sessione
         session_start();
         $_SESSION['currentUser'] = $user;
         //restituire true o lo user....
     }
-    
-    private function checkSpotter($user) {
+
+    private function checkUser($user) {
         //controllo dei parametri specifici per Spotter
-    }
+        //comune a tutti
+        if (is_null($user) || !is_a($user, "User") || is_null($user->getEmail()) || is_null($user->getUsername()) ||
+                is_null($user->getPassword()) || is_null($user->getCity()) || is_null($user->getCountry()) || is_null($user->getType()))
+            return false;
 
-    private function checkVenue($user) {
-        //controllo dei parametri specifici per Venue
-    }
+        else {
+            switch ($user->getType()) {
+                case "VENUE":
+                    if (is_null($user->getDescription()) || is_null($user->getLocalType()))
+                        return false;
 
-    private function checkJammer($user) {
-        //controllo dei parametri specifici per Jammer
-    }
-    
-    private function saveSignupActivity($userId){
-        $pActivity = new ActivityParse();
-        $activity = new Activity();
-        $activity->setFromUser($userId);
-        $activity->setType("SIGNUP");
-        $pActivity->saveActivity($activity);
-    }
+                    break;
+                case "JAMMER":
+                    if (is_null($user->getJammerType()) || is_null($user->getDescription()) || is_null($user->getMusic()))
+                        return false;
 
+                    break;
+                default:
+                    break;
+            }
+        }
+        return true;
+    }
 }
 
 ?>
