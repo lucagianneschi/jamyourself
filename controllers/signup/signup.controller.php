66edaa5a87d98f7ada5de5b23857b6dc6896eaaa
Stefano <stefanom@jamyourself.com>
2013-07-14 18:12:43 +0200

    terminato il controller per la registrazione (da testare)

--- controllers/signup/signup.controller.php
+++ controllers/signup/signup.controller.php
@@ -5,31 +5,74 @@ if (!defined('ROOT_DIR'))
 
 require_once ROOT_DIR . 'config.php';
 require_once PARSE_DIR . 'parse.php';
-require_once CLASSES_DIR . 'utils.class.php';
 require_once CLASSES_DIR . 'userParse.class.php';
 require_once CLASSES_DIR . 'activityParse.class.php';
 require_once CONTROLLERS_DIR . 'restController.php';
 
-class SignupController extends REST  {
+////////////////////////////////////////////////////////////////////////////////
+//
+// DEFINIZIONE DELLE FUNZIONI DEL CONTROLLER PER LA REGISTRAZIONE
+//  
+//////////////////////////////////////////////////////////////////////////////// 
 
-    private $session;
-    private $post;
-    private $get;
+class SignupController extends REST {
 
-    function __construct() {
+    /**
+     * Viene chiamata al caircamento della View Signup.php e inizializza 
+     * in sessione tutte le informazioni che possono essere necessarie
+     * poter essere visualizzata correttamente
+     * 
+     * ACTION :  GET
+     * DATA: non c'è bisogno di inviare nulla
+     * 
+     */
+    public function onLoad() {
+        // Verifico che sia una GET altrimenti restituisco un codice di Errore 
+        // "Not Acceptable"
+        if ($this->get_request_method() != "GET") {
+            $this->response('', 406);
+        }
         
+        //ad esempio si può inizializzare un utente a NULL
+        $_SESSION['currentUser'] = null;
+        
+        //restituisco messaggio di conferma
+        $this->response(array("OK"),200);
     }
 
     /**
-     * \fn	string signup($user)
-     * \brief	Return the new registred user
-     * \return	string
+     * Effettua l'iscrizione di un utente al sito
+     * 
+     * ACTION :  POST
+     * DATA: tra i parametri mi aspetto un "newUser"
+     * 
      */
-    public function signup($user) {
+    public function signup() {
+        
+        if ($this->get_request_method() != "POST") {
+            $this->response('', 406);
+        }
 
+        //controllo che l'utente sia già collegato
+        if(!isset($_SESSION['currentUser'])){
+        // If invalid inputs "Bad Request" status message and reason
+            $error = array('status' => "Unauthorized", "msg" => "User already Logged");
+            $this->response($this->json($error), 401);
+        }
+        
+        if(!isset($this->request['newUser'])){
+          // If invalid inputs "Bad Request" status message and reason
+            $error = array('status' => "Bad Request", "msg" => "No new user specified");
+            $this->response($this->json($error), 400);          
+        }
+        
         //recupero le variabili
-        if ($this->checkUser($user) == false)
-            return throwError(new Exception("Invalid User"), __CLASS__, __FUNCTION__, func_get_args());
+        if ($this->checkUser($user) == false){
+          // If invalid inputs "Bad Request" status message and reason
+            $error = array('status' => "Bad Request", "msg" => "Invalid new user");
+            $this->response($this->json($error), 400);  
+        }
+            
 
         //tenta di effettuare il salvataggio
         $pUser = new UserParse();
@@ -37,7 +80,8 @@ class SignupController extends REST  {
 
         if (is_a($user, "Error")) {
             //result è un errore e contiene il motivo dell'errore
-            return $user;
+            $error = array('status' => "Service Unavailable", "msg" => "Cannot create a new user");
+            $this->response($this->json($error), 503);  
         }
 
         //se va a buon fine salvo una nuova activity       
@@ -54,9 +98,9 @@ class SignupController extends REST  {
         $pActivity->saveActivity($activity);
 
         //aggiorno l'oggetto User in sessione
-        session_start();
         $_SESSION['currentUser'] = $user;
         //restituire true o lo user....
+        $this->response($this->json(array("OK")), 200);  
     }
 
     private function checkUser($user) {
@@ -86,5 +130,18 @@ class SignupController extends REST  {
     }
 
 }
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// ESECUZIONE DELLO SCRIPT 
+//  
+//////////////////////////////////////////////////////////////////////////////// 
+
+//inizializza la sessione
+session_start();
+
+// Initiiate Library
+$controller = new SignupController();
+$controller->processApi();
 
 ?>
