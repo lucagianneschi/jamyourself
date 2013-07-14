ef062c82431cd4d4b45d970129224832752aee23
Stefano <stefanom@jamyourself.com>
2013-07-14 18:07:00 +0200

    modificato _request in request per un migliore e più intuitivo utilizzo della variabile nei controller

--- controllers/restController.php
+++ controllers/restController.php
@@ -8,7 +8,7 @@ class REST {
 
     public $_allow = array();
     public $_content_type = "application/json";
-    public $_request = array();
+    public $request = array();
     private $_method = "";
     private $_code = 200;
     public $data = "";
@@ -81,15 +81,15 @@ class REST {
     private function inputs() {
         switch ($this->get_request_method()) {
             case "POST":
-                $this->_request = $this->cleanInputs($_POST);
+                $this->request = $this->cleanInputs($_POST);
                 break;
             case "GET":
             case "DELETE":
-                $this->_request = $this->cleanInputs($_GET);
+                $this->request = $this->cleanInputs($_GET);
                 break;
             case "PUT":
-                parse_str(file_get_contents("php://input"), $this->_request);
-                $this->_request = $this->cleanInputs($this->_request);
+                parse_str(file_get_contents("php://input"), $this->request);
+                $this->request = $this->cleanInputs($this->request);
                 break;
             default:
                 $this->response('', 406);
