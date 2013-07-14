ccd36b4b234cbb06755bb5bdd7a0344f00f65eb4
Stefano <stefanom@jamyourself.com>
2013-07-14 19:02:10 +0200

    per la registrazione dell'utente

---
+++ javascripts/custom/signup.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    //inizializzazione
+    onLoad();
+    
+});
+
+function signup(){
+    //recupero tutti i campi che l'utente
+    //ha inizializzato nel form
+    var newUser = getFormFieldValues();    
+    
+     var json_signup = {};
+    json_signup.rquest = "signup";
+    json_signup.newUser = newUser;
+
+    $.ajax({
+        type: "POST",
+        url: "../controllers/signup",
+        data: json_signup,
+        async: false,
+        "beforeSend": function(xhr) {
+            xhr.setRequestHeader("X-AjaxRequest", "1");
+        },
+        success: function(data, status) {
+            console.log("[onLoad] [SUCCESS] Status: " + status);
+        },
+        error: function(data, status) {
+            console.log("[onLoad] [ERROR] Status: " + status);
+        }
+    });     
+    
+}
+
+function getFormFieldValues(){
+    
+}
