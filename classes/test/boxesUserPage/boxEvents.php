fd34fe95bbae1e69d41e521fb3addcc7254ae6a8
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-07-23 22:56:50 +0200

    ok test

--- classes/test/boxesUserPage/boxEvents.php
+++ classes/test/boxesUserPage/boxEvents.php
@@ -31,87 +31,53 @@ $i_end = microtime();
 
 //$id = '7fes1RyY77';LDF
 //$id = 'uMxy47jSjg';//ROSESINBLOOM
-$id = '1oT7yYrpfZ';
-$user_start = microtime();
-$userParse = new UserParse();
-$user = $userParse->getUser($id);
-$user_stop = microtime(); 
+$id = 'HDgcsTLpEx';
 
-$type = $user->getType();
-$event_start = microtime();
-switch ($type) {
-     case 'VENUE':
-		$eventParse = new EventParse();
-		$eventParse->setLimit(4);
-		$eventParse->wherePointer('fromUser', '_User', $id);
-		$eventParse->where('active', true);
-		$eventParse->orderByDescending('createdAt');
-		$events = $eventParse->getEvents();
-		if ($events != 0) {
-			if (get_class($events) == 'Error') {
-			echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $events->getErrorMessage() . '<br/>';
-			} else {
-			foreach ($events as $event) {
-				echo '<br />[title] => ' . $event->getTitle() . '<br />';
-				echo '<br />[eventDate] => ' . $event->getEventDate()->format('d-m-Y H:i:s') . '<br />';
-				echo '<br />[locationName] => ' . $event->getLocationName() . '<br />';
-				echo '<br />[loveCounter] => ' . $event->getLoveCounter() . '<br />';
-				echo '<br />[commentCounter] => ' . $event->getCommentCounter() . '<br />';
-				echo '<br />[shareCounter] => ' . $event->getShareCounter() . '<br />';
-				$geopoint = $event->getLocation();
-				$string = '[location] => ' . $geopoint->location['latitude'] . ', ' . $geopoint->location['longitude'] . '<br />';
-				echo $string;
-				if ($event->getFeaturing() != 0) {
-				echo '<br />----------------------!=0---------------------------<br />';
-				foreach ($event->getFeaturing() as $user) {
-					$userParse = new UserParse();
-					$feat = $userParse->getUser($user);
-					echo '<br />[feat] => ' . $feat->getUsername() . '<br />';
-				}
-				}
-			}
-			}
+
+$eventParse = new EventParse();
+$eventParse->wherePointer('fromUser', '_User', $id);
+$eventParse->where('active', true);
+$eventParse->setLimit(1000);
+$eventParse->orderByDescending('createdAt');
+$events = $eventParse->getEvents();
+if ($events != 0) {
+    if (get_class($events) == 'Error') {
+	echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $events->getErrorMessage() . '<br/>';
+    } else {
+	foreach ($events as $event) {
+	    echo '<br />[thumbnail] => ' . $event->getThumbnail() . '<br />';
+	    echo '<br />[locationName] => ' . $event->getLocationName() . '<br />';
+	    echo '<br />[title] => ' . $event->getTitle() . '<br />';
+	    if ($event->getTags() != 0) {
+		foreach ($event->getTags() as $tags) {
+		    $string .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+		    $string .= '[tags] => ' . $tags . '<br />';
+		    echo $string;
 		}
-		break;
-	case 'JAMMER':
-		$eventParse = new EventParse();
-		$eventParse->setLimit(4);
-		$eventParse->wherePointer('fromUser', '_User', $id);
-		$eventParse->where('active', true);
-		$eventParse->orderByDescending('createdAt');
-		$events = $eventParse->getEvents();
-		if ($events != 0) {
-			if (get_class($events) == 'Error') {
-			echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $events->getErrorMessage() . '<br/>';
-			} else {
-			foreach ($events as $event) {
-				echo '<br />[title] => ' . $event->getTitle() . '<br />';
-				echo '<br />[eventDate] => ' . $event->getEventDate()->format('d-m-Y H:i:s') . '<br />';
-				echo '<br />[locationName] => ' . $event->getLocationName() . '<br />';
-				echo '<br />[loveCounter] => ' . $event->getLoveCounter() . '<br />';
-				echo '<br />[commentCounter] => ' . $event->getCommentCounter() . '<br />';
-				echo '<br />[shareCounter] => ' . $event->getShareCounter() . '<br />';
-				if ($event->getFeaturing() != 0) {
-				echo '<br />----------------------!=0---------------------------<br />';
-				foreach ($event->getFeaturing() as $user) {
-					$userParse = new UserParse();
-					$feat = $userParse->getUser($user);
-					echo '<br />[feat] => ' . $feat->getUsername() . '<br />';
-				}
-				}
-			}
-			}
+	    }
+	    $parseUser = new UserParse();
+	    $parseUser->whereRelatedTo('featuring', 'Event', $id);
+	    $parseUser->where('active', true);
+	    $featuring = $parseUser->getUsers();
+	    if (count($featuring) != 0) {
+		foreach ($featuring as $user) {
+		    echo '<br />[username] => ' . $user->getUsername() . '<br />';
 		}
-		break;
-	default:
-		break;
+	    }
+	    echo '<br />[eventDate] => ' . $event->getEventDate()->format('d-m-Y H:i:s') . '<br />';
+	    echo '<br />[city] => ' . $event->getCity() . '<br />';
+	    echo '<br />[address] => ' . $event->getAddress() . '<br />';
+	    echo '<br />[loveCounter] => ' . $event->getLoveCounter() . '<br />';
+	    echo '<br />[commentCounter] => ' . $event->getCommentCounter() . '<br />';
+	    echo '<br />[shareCounter] => ' . $event->getShareCounter() . '<br />';
+	    echo '<br />[reviewCounter] => ' . $event->getReviewCounter() . '<br />';
 	}
-$event_stop = microtime();
+    }
+}
+
 $t_end = microtime();
 echo '<br />----------------------TIMERS---------------------------<br />';
 echo 'Tempo include ' . executionTime($i_start, $i_end) . '<br />';
-echo 'Tempo recupero User proprietario pagina ' . executionTime($user_start, $user_stop) . '<br />';
-echo 'Tempo recupero ultimi 4 eventi ' . executionTime($event_start, $event_stop) . '<br />';
 echo 'Tempo totale ' . executionTime($t_start, $t_end) . '<br />';
 echo '<br />----------------------TIMERS---------------------------<br />';
 ?>
