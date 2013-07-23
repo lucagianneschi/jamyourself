a93769b327f8a11428389773e7b8eb01fb4aab80
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-07-23 10:39:10 +0200

    file di elenco delle cartelle contenute nella cartella test

--- classes/test/index.php
+++ classes/test/index.php
@@ -1,8 +1,12 @@
 <?php
 
 echo "<h2> Elenco delle pagine di test della Jam/Parse library</h2>";
-
+/*
 foreach (glob("*.php") as $filename) {
+    echo "<a  href='$filename'>$filename</a><br>";
+}
+*/
+foreach (scandir('.') as $filename) {
     echo "<a  href='$filename'>$filename</a><br>";
 }
 ?>
