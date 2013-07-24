fc3c80be46735c5f8b819e7433a8494fd254ab96
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-07-24 16:43:54 +0200

    aggiunto controllo sul risultato della query

--- boxes/info.box.php
+++ boxes/info.box.php
@@ -29,100 +29,103 @@ class infoBox {
     public function sendInfo($objectId) {
 	$userParse = new UserParse();
 	$user = $userParse->getUser($objectId);
+	if (get_class($user) == 'Error') {
+	    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $user->getErrorMessage() . '<br/>';
+	} else {
+	    $userName = $user->getUsername();
+	    $backGround = $user->getBackGround();
+	    $profilePicture = $user->getProfilePicture();
+	    $description = $user->getDescription();
+	    $city = $user->getCity();
+	    $country = $user->getCountry();
+	    $fbPage = $user->getFbPage();
+	    $twitterPage = $user->getTwitterPage();
+	    $webSite = $user->getWebsite();
+	    $youtubeChannel = $user->getYoutubeChannel();
+	    $googlePlusPage = $user->getGooglePlusPage();
+	    $level = $user->getLevel();
+	    $levelValue = $user->getLevelValue();
 
-	$userName = $user->getUsername();
-	$backGround = $user->getBackGround();
-	$profilePicture = $user->getProfilePicture();
-	$description = $user->getDescription();
-	$city = $user->getCity();
-	$country = $user->getCountry();
-	$fbPage = $user->getFbPage();
-	$twitterPage = $user->getTwitterPage();
-	$webSite = $user->getWebsite();
-	$youtubeChannel = $user->getYoutubeChannel();
-	$googlePlusPage = $user->getGooglePlusPage();
-	$level = $user->getLevel();
-	$levelValue = $user->getLevelValue();
-
-	$type = $user->getType();
-	switch ($type) {
-	    case 'SPOTTER':
-		$followingCounter = $user->getFollowingCounter();
-		$frindshipCounter = $user->getFriendshipCounter();
-		$music = $user->getMusic();
-		$resultArray = array('userName' => $userName,
-		    'backGround' => $backGround,
-		    'profilePicture' => $profilePicture,
-		    'description' => $description,
-		    'city' => $city,
-		    'country' => $country,
-		    'fbPage' => $fbPage,
-		    'twitterPage' => $twitterPage,
-		    'webSite' => $webSite,
-		    'youtubeChannel' => $youtubeChannel,
-		    'googlePlusPage' => $googlePlusPage,
-		    'level' => $level,
-		    'levelValue' => $levelValue,
-		    'music' => $music,
-		    'followingCounter' => $followingCounter,
-		    'frindshipCounter' => $frindshipCounter);
-		break;
-	    case 'JAMMER':
-		$jammerType = $user->getJammerType();
-		$followersCounter = $user->getFollowersCounter();
-		$collaborationCounter = $user->getCollaborationCounter();
-		$venueCounter = $user->getVenueCounter();
-		$jammerCounter = $user->getJammerCounter();
-		$music = $user->getMusic();
-		$members = $user->getMembers();
-		$resultArray = array('userName' => $userName,
-		    'backGround' => $backGround,
-		    'profilePicture' => $profilePicture,
-		    'description' => $description,
-		    'city' => $city,
-		    'country' => $country,
-		    'fbPage' => $fbPage,
-		    'twitterPage' => $twitterPage,
-		    'webSite' => $webSite,
-		    'youtubeChannel' => $youtubeChannel,
-		    'googlePlusPage' => $googlePlusPage,
-		    'level' => $level,
-		    'levelValue' => $levelValue,
-		    'music' => $music,
-		    'jammerType' => $jammerType,
-		    'followersCounter' => $followersCounter,
-		    'collaborationCounter' => $collaborationCounter,
-		    'venueCounter' => $venueCounter,
-		    'jammerCounter' => $jammerCounter,
-		    'members' => $members);
-		break;
-	    case 'VENUE':
-		$localType = $user->getlocalType();
-		$followersCounter = $user->getFollowersCounter();
-		$collaborationCounter = $user->getCollaborationCounter();
-		$venueCounter = $user->getVenueCounter();
-		$jammerCounter = $user->getJammerCounter();
-		$resultArray = array('userName' => $userName,
-		    'backGround' => $backGround,
-		    'profilePicture' => $profilePicture,
-		    'description' => $description,
-		    'city' => $city,
-		    'country' => $country,
-		    'fbPage' => $fbPage,
-		    'twitterPage' => $twitterPage,
-		    'webSite' => $webSite,
-		    'youtubeChannel' => $youtubeChannel,
-		    'googlePlusPage' => $googlePlusPage,
-		    'level' => $level,
-		    'levelValue' => $levelValue,
-		    'localType' => $localType,
-		    'followersCounter' => $followersCounter,
-		    'collaborationCounter' => $collaborationCounter,
-		    'venueCounter' => $venueCounter,
-		    'jammerCounter' => $jammerCounter);
-		break;
-	    default:
-		break;
+	    $type = $user->getType();
+	    switch ($type) {
+		case 'SPOTTER':
+		    $followingCounter = $user->getFollowingCounter();
+		    $frindshipCounter = $user->getFriendshipCounter();
+		    $music = $user->getMusic();
+		    $resultArray = array('userName' => $userName,
+			'backGround' => $backGround,
+			'profilePicture' => $profilePicture,
+			'description' => $description,
+			'city' => $city,
+			'country' => $country,
+			'fbPage' => $fbPage,
+			'twitterPage' => $twitterPage,
+			'webSite' => $webSite,
+			'youtubeChannel' => $youtubeChannel,
+			'googlePlusPage' => $googlePlusPage,
+			'level' => $level,
+			'levelValue' => $levelValue,
+			'music' => $music,
+			'followingCounter' => $followingCounter,
+			'frindshipCounter' => $frindshipCounter);
+		    break;
+		case 'JAMMER':
+		    $jammerType = $user->getJammerType();
+		    $followersCounter = $user->getFollowersCounter();
+		    $collaborationCounter = $user->getCollaborationCounter();
+		    $venueCounter = $user->getVenueCounter();
+		    $jammerCounter = $user->getJammerCounter();
+		    $music = $user->getMusic();
+		    $members = $user->getMembers();
+		    $resultArray = array('userName' => $userName,
+			'backGround' => $backGround,
+			'profilePicture' => $profilePicture,
+			'description' => $description,
+			'city' => $city,
+			'country' => $country,
+			'fbPage' => $fbPage,
+			'twitterPage' => $twitterPage,
+			'webSite' => $webSite,
+			'youtubeChannel' => $youtubeChannel,
+			'googlePlusPage' => $googlePlusPage,
+			'level' => $level,
+			'levelValue' => $levelValue,
+			'music' => $music,
+			'jammerType' => $jammerType,
+			'followersCounter' => $followersCounter,
+			'collaborationCounter' => $collaborationCounter,
+			'venueCounter' => $venueCounter,
+			'jammerCounter' => $jammerCounter,
+			'members' => $members);
+		    break;
+		case 'VENUE':
+		    $localType = $user->getlocalType();
+		    $followersCounter = $user->getFollowersCounter();
+		    $collaborationCounter = $user->getCollaborationCounter();
+		    $venueCounter = $user->getVenueCounter();
+		    $jammerCounter = $user->getJammerCounter();
+		    $resultArray = array('userName' => $userName,
+			'backGround' => $backGround,
+			'profilePicture' => $profilePicture,
+			'description' => $description,
+			'city' => $city,
+			'country' => $country,
+			'fbPage' => $fbPage,
+			'twitterPage' => $twitterPage,
+			'webSite' => $webSite,
+			'youtubeChannel' => $youtubeChannel,
+			'googlePlusPage' => $googlePlusPage,
+			'level' => $level,
+			'levelValue' => $levelValue,
+			'localType' => $localType,
+			'followersCounter' => $followersCounter,
+			'collaborationCounter' => $collaborationCounter,
+			'venueCounter' => $venueCounter,
+			'jammerCounter' => $jammerCounter);
+		    break;
+		default:
+		    break;
+	    }
 	}
 	return $resultArray;
     }
