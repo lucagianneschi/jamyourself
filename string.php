272806e56e6638d131a3ddd22b36c1d237e68670
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-07-25 14:38:28 +0200

    file di utilità per stringhe da mostrare a video

---
+++ string.php
@@ -0,0 +1,6 @@
+<?php
+
+define('NODATA', 'NO YET DATA TO DISPLAY');
+define('NOTDEFINED', 'NOT DEFINED FOR THIS TYPE OF USER');
+define('DEFAULTAVATAR', '/var/www/vhosts/socialmusicdiscovering.com/httpdocs/script/wp_daniele/root/images/default/dafultAvatar.jpg');
+?>
