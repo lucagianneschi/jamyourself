960b94cfdd919e91c485fdd87d3dd9e76c8690b8
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-08-01 11:44:01 +0200

    file adattato nuova versione box

--- test/boxesUserPage/testBoxPosts.php
+++ test/boxesUserPage/testBoxPosts.php
@@ -22,49 +22,128 @@ if (!defined('ROOT_DIR'))
 ini_set('display_errors', '1');
 require_once ROOT_DIR . 'config.php';
 require_once PARSE_DIR . 'parse.php';
-require_once CLASSES_DIR . 'comment.class.php';
-require_once CLASSES_DIR . 'commentParse.class.php';
-require_once CLASSES_DIR . 'user.class.php';
-require_once CLASSES_DIR . 'userParse.class.php';
+require_once BOXES_DIR . 'post.box.php';
 $i_end = microtime();
 
-$id = '7fes1RyY77';//LDF
-$userParse = new UserParse();
-$user = $userParse->getUser($id);
+//SPOTTER
+$mari = '1oT7yYrpfZ'; //MARI
+$FLAVYCAP = 'oN7Pcp2lxf'; //FLAVYCAP 
+$Kessingtong = '2OgmANcYaT'; //Kessingtong
+//JAMMER
+$ROSESINBLOOM = 'uMxy47jSjg'; //ROSESINBLOOM
+$Stanis = 'HdqSpIhiXo'; //Stanis
+$LDF = '7fes1RyY77'; //LDF
+//Venue
+$ZonaPlayed = '2K5Lv7qxzw'; //ZonaPlayed  
+$Ultrasuono = 'iovioSH5mq'; //Ultrasuono 
+$jump = 'wrpgRuSgRA'; //jump rock club
 
-$post_start = microtime();
-$parsePost = new CommentParse();
-$parsePost->wherePointer('toUser', '_User', $id);
-$parsePost->where('type', 'P');
-$parsePost->where('active', true);
-$parsePost->setLimit(3);
-$parsePost->whereInclude('fromUser');
-$parsePost->orderByDescending('createdAt');
-$last3post = $parsePost->getComments();
-if ($last3post != 0) {
-    if (get_class($last3post) == 'Error') {
-	echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $last3post->getErrorMessage() . '<br/>';
-    } else {
 
-	foreach ($last3post->fromUser as $fromUser) {
-	    echo '<br />[username] => ' . $fromUser->getUsername() . '<br />';
-	    echo '<br />[type] => ' . $fromUser->getType() . '<br />';
-	    echo '<br />[thumb] => ' . $fromUser->getProfileThumbnail() . '<br />';
-	}
-	foreach ($last3post as $post) {
-	    echo '<br />[testo] => ' . $post->getText() . '<br />';
-	    echo '<br />[data creazione] => ' . $post->getCreatedAt() . '<br />';
-	    echo '<br />[loveCounter] => ' . $post->getLoveCounter() . '<br />';
-	    echo '<br />[shareCounter] => ' . $post->getShareCounter() . '<br />';
-	    echo '<br />[commentCounter] => ' . $post->getCommentCounter() . '<br />';
-	}
-    }
-}
+echo '<br />-------------------------------------------------------------------------------------------<br />';
+echo '<br />-------------------------TEST  POST BOX -------------------------------------------<br />';
+echo '<br />-------------------------------SPOTTER-------------------------------------------<br />';
+echo '<br />------------------------- TEST POST BOX mari-------------------------------------------<br />';
+$post1_start = microtime(); 
+$recordPostP = new PostBox();
+$recordPost = $recordPostP->init($mari);
+print "<pre>";
+print_r($recordPost);
+print "</pre>";
+$post1_stop = microtime();
+echo '<br />-------------------------FINE TEST POST BOX mari-------------------------------------------<br />';
+echo '<br />-------------------------FINE TEST POST BOX -------------------------------------------<br />';
+echo '<br />------------------------- TEST POST BOX $FLAVYCAP-------------------------------------------<br />';
+$post2_start = microtime(); 
+$recordPostP = new PostBox();
+$recordPost = $recordPostP->init($FLAVYCAP);
+print "<pre>";
+print_r($recordPost);
+print "</pre>";
+$post2_stop = microtime();
+echo '<br />-------------------------FINE TEST POST BOX $FLAVYCAP-------------------------------------------<br />';
+echo '<br />------------------------- TEST POST BOX $Kessingtong-------------------------------------------<br />';
+$post3_start = microtime(); 
+$recordPostP = new PostBox();
+$recordPost = $recordPostP->init($LDF);
+print "<pre>";
+print_r($recordPost);
+print "</pre>";
+$post3_stop = microtime();
+echo '<br />-------------------------FINE TEST POST BOX $Kessingtong-------------------------------------------<br />';
+echo '<br />-------------------------------JAMMER-------------------------------------------<br />';
+echo '<br />------------------------- TEST POST BOX ROSESINBLOOM-------------------------------------------<br />';
+$post4_start = microtime(); 
+$recordPostP = new PostBox();
+$recordPost = $recordPostP->init($ROSESINBLOOM);
+print "<pre>";
+print_r($recordPost);
+print "</pre>";
+$post4_stop = microtime();
+echo '<br />-------------------------FINE TEST POST BOX ROSESINBLOOM-------------------------------------------<br />';
+echo '<br />-------------------------FINE TEST POST BOX -------------------------------------------<br />';
+echo '<br />------------------------- TEST POST BOX Stanis-------------------------------------------<br />';
+$post5_start = microtime(); 
+$recordPostP = new PostBox();
+$recordPost = $recordPostP->init($Stanis);
+print "<pre>";
+print_r($recordPost);
+print "</pre>";
+$post5_stop = microtime();
+echo '<br />-------------------------FINE TEST POST BOX Stanis-------------------------------------------<br />';
+echo '<br />------------------------- TEST POST BOX LDF-------------------------------------------<br />';
+$post6_start = microtime(); 
+$recordPostP = new PostBox();
+$recordPost = $recordPostP->init($LDF);
+print "<pre>";
+print_r($recordPost);
+print "</pre>";
+$post6_stop = microtime();
+echo '<br />-------------------------FINE TEST POST BOX LDF-------------------------------------------<br />';
+echo '<br />-------------------------FINE TEST POST BOX -------------------------------------------<br />';
+echo '<br />-------------------------------------------------------------------------------------------<br />';
+echo '<br />-------------------------------VENUE-------------------------------------------<br />';
+echo '<br />------------------------- TEST POST BOX ZonaPlayed-------------------------------------------<br />';
+$post7_start = microtime(); 
+$recordPostP = new PostBox();
+$recordPost = $recordPostP->init($ZonaPlayed);
+print "<pre>";
+print_r($recordPost);
+print "</pre>";
+$post7_stop = microtime();
+echo '<br />-------------------------FINE TEST POST BOX ZonaPlayed-------------------------------------------<br />';
+echo '<br />-------------------------FINE TEST POST BOX -------------------------------------------<br />';
+echo '<br />------------------------- TEST POST BOX Ultrasuono-------------------------------------------<br />';
+$post8_start = microtime();
+$recordPostP = new PostBox();
+$recordPost = $recordPostP->init($Ultrasuono);
+print "<pre>";
+print_r($recordPost);
+print "</pre>";
+$post8_stop = microtime();
+echo '<br />-------------------------FINE TEST POST BOX Ultrasuono-------------------------------------------<br />';
+echo '<br />------------------------- TEST POST BOX JUMP-------------------------------------------<br />';
+$post9_start = microtime();
+$recordPostP = new PostBox();
+$recordPost = $recordPostP->init($jump);
+print "<pre>";
+print_r($recordPost);
+print "</pre>";
+echo '<br />-------------------------FINE TEST POST BOX JUMP-------------------------------------------<br />';
+echo '<br />-------------------------FINE TEST POST BOX -------------------------------------------<br />';
 $post_stop = microtime();
 $t_end = microtime();
+$post9_stop = microtime();
 echo '<br />----------------------TIMERS---------------------------<br />';
 echo 'Tempo include ' . executionTime($i_start, $i_end) . '<br />';
-echo 'Tempo recupero ultimi 3 post ' . executionTime($post_start, $post_stop) . '<br />';
+echo 'Tempo recupero ultimi 3 post ' . executionTime($post1_start, $post1_stop) . '<br />';
+echo 'Tempo recupero ultimi 3 post ' . executionTime($post2_start, $post2_stop) . '<br />';
+echo 'Tempo recupero ultimi 3 post ' . executionTime($post3_start, $post3_stop) . '<br />';
+echo 'Tempo recupero ultimi 3 post ' . executionTime($post4_start, $post4_stop) . '<br />';
+echo 'Tempo recupero ultimi 3 post ' . executionTime($post5_start, $post5_stop) . '<br />';
+echo 'Tempo recupero ultimi 3 post ' . executionTime($post6_start, $post6_stop) . '<br />';
+echo 'Tempo recupero ultimi 3 post ' . executionTime($post7_start, $post7_stop) . '<br />';
+echo 'Tempo recupero ultimi 3 post ' . executionTime($post8_start, $post8_stop) . '<br />';
+echo 'Tempo recupero ultimi 3 post ' . executionTime($post9_start, $post9_stop) . '<br />';
 echo 'Tempo totale ' . executionTime($t_start, $t_end) . '<br />';
 echo '<br />----------------------TIMERS---------------------------<br />';
 ?>
