277ca4917b24511744184451781cacf291166ffa
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-08-04 15:19:25 +0200

    intestazione di pagina per i box da caricare in tutte le pagine

--- test/boxesGeneral/index.php
+++ test/boxesGeneral/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "<h2> Elenco dei file di test per pagina Media Event </h2>";
+echo "<h2> Elenco dei file di test per box generali, da caricare su tutte le tipologie di pagina </h2>";
 
 foreach (glob("*.php") as $filename) {
     echo "<a  href='$filename'>$filename</a><br>";
