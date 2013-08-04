a7d30ff8b0e2d59424ccc04c8b71602bef73ea3c
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-08-04 15:20:08 +0200

    nuova versione file di test per nuove impostazioni dei box

--- test/boxesUserPage/testboxReviewEvents.php
+++ test/boxesUserPage/testboxReviewEvents.php
@@ -24,111 +24,23 @@ if (!defined('ROOT_DIR'))
 ini_set('display_errors', '1');
 require_once ROOT_DIR . 'config.php';
 require_once PARSE_DIR . 'parse.php';
-require_once CLASSES_DIR . 'activity.class.php';
-require_once CLASSES_DIR . 'activityParse.class.php';
-require_once CLASSES_DIR . 'event.class.php';
-require_once CLASSES_DIR . 'eventParse.class.php';
-require_once CLASSES_DIR . 'user.class.php';
-require_once CLASSES_DIR . 'userParse.class.php';
-
+require_once BOXES_DIR . 'reviewEvent.box.php';
 $i_end = microtime();
 
-$id = '7fes1RyY77';//LDF
 
-$user_start = microtime();
-$userParse = new UserParse();
-$user_stop = microtime();
-$user = $userParse->getUser($id);
-
-$type = $user->getType();
+$objectId = 'sveemvaUN8';
+	
 $reviewEvent_start = microtime();
-switch ($type) {
-    case 'SPOTTER':	
-		$parseReviewEvent = new ActivityParse();
-		$parseReviewEvent->where('type','EVENTREVIEW');
-		$parseReviewEvent->wherePointer('fromUser', '_User', $id);
-		$parseReviewEvent->where('active', true);
-		$parseReviewEvent->whereInclude('event');
-		$parseReviewEvent->orderByDescending('createdAt');
-		$activities = $parseReviewEvent->getActivities();
-		if ($activities != 0) {
-			if (get_class($activities) == 'Error') {
-			echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $activities->getErrorMessage() . '<br/>';
-			} else {
-				foreach ($activities->event as $event) {
-					echo '<br />[title] => ' . $event->getTitle() . '<br />';
-					echo '<br />[eventDate] => ' . $event->getEventDate()->format('d-m-Y H:i:s') . '<br />';
-					echo '<br />[locationName] => ' . $event->getLocationName() . '<br />';
-					echo '<br />[loveCounter] => ' . $event->getLoveCounter() . '<br />';
-					echo '<br />[commentCounter] => ' . $event->getCommentCounter() . '<br />';
-					echo '<br />[shareCounter] => ' . $event->getShareCounter() . '<br />';
-					$geopoint = $event->getLocation();
-					$string = '[location] => ' . $geopoint->location['latitude'] . ', ' . $geopoint->location['longitude'] . '<br />';
-					}
-				}
-			}
-		$activitiesCounter = $parseReviewEvent->getCount();	
-			break;
-	case 'VENUE':
-		$parseReviewEvent = new ActivityParse();
-		$parseReviewEvent->wherePointer('toUser', '_User', $id);
-		$parseReviewEvent->where('type','EVENTREVIEW');
-		$parseReviewEvent->where('active', true);
-		$parseReviewEvent->whereInclude('event');
-		$parseReviewEvent->orderByDescending('createdAt');
-		$activities = $parseReviewEvent->getActivities();
-		if ($activities != 0) {
-			if (get_class($activities) == 'Error') {
-			echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $activities->getErrorMessage() . '<br/>';
-			} else {
-				foreach ($activities->event as $event) {
-					echo '<br />[title] => ' . $event->getTitle() . '<br />';
-					echo '<br />[eventDate] => ' . $event->getEventDate()->format('d-m-Y H:i:s') . '<br />';
-					echo '<br />[locationName] => ' . $event->getLocationName() . '<br />';
-					echo '<br />[loveCounter] => ' . $event->getLoveCounter() . '<br />';
-					echo '<br />[commentCounter] => ' . $event->getCommentCounter() . '<br />';
-					echo '<br />[shareCounter] => ' . $event->getShareCounter() . '<br />';
-					$geopoint = $event->getLocation();
-					$string = '[location] => ' . $geopoint->location['latitude'] . ', ' . $geopoint->location['longitude'] . '<br />';
-					}
-				}
-			}
-			$activitiesCounter = $parseReviewEvent->getCount();
-			break;
-	case 'JAMMER':	
-		$parseReviewEvent = new ActivityParse();
-		$parseReviewEvent->wherePointer('toUser', '_User', $id);
-		$parseReviewEvent->where('type','EVENTREVIEW');
-		$parseReviewEvent->where('active', true);
-		$parseReviewEvent->whereInclude('event');
-		$parseReviewEvent->orderByDescending('createdAt');
-		$activities = $parseReviewEvent->getActivities();
-		if ($activities != 0) {
-			if (get_class($activities) == 'Error') {
-			echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $activities->getErrorMessage() . '<br/>';
-			} else {
-				foreach ($activities->event as $event) {
-					echo '<br />[title] => ' . $event->getTitle() . '<br />';
-					echo '<br />[eventDate] => ' . $event->getEventDate()->format('d-m-Y H:i:s') . '<br />';
-					echo '<br />[locationName] => ' . $event->getLocationName() . '<br />';
-					echo '<br />[loveCounter] => ' . $event->getLoveCounter() . '<br />';
-					echo '<br />[commentCounter] => ' . $event->getCommentCounter() . '<br />';
-					echo '<br />[shareCounter] => ' . $event->getShareCounter() . '<br />';
-					$geopoint = $event->getLocation();
-					$string = '[location] => ' . $geopoint->location['latitude'] . ', ' . $geopoint->location['longitude'] . '<br />';
-					}
-				}
-			}
-			$activitiesCounter = $parseReviewEvent->getCount();
-		break;
-	default:
-		break;
-	}
-	$reviewEvent_stop = microtime();
-	$t_end = microtime();
+$reviewEventBoxP = new ReviewRecordBox();
+$reviewEventBox = $reviewEventBoxP->init($objectId);
+print "<pre>";
+print_r($reviewEventBox);
+print "</pre>";
+$reviewEvent_stop = microtime();
+
+$t_end = microtime();
 echo '<br />----------------------TIMERS---------------------------<br />';
 echo 'Tempo include ' . executionTime($i_start, $i_end) . '<br />';
-echo 'Tempo recupero User proprietario pagina ' . executionTime($user_start, $user_stop) . '<br />';
 echo 'Tempo review' . executionTime($reviewEvent_start, $reviewEvent_stop) . '<br />';
 echo 'Tempo totale ' . executionTime($t_start, $t_end) . '<br />';
 echo '<br />----------------------TIMERS---------------------------<br />';
