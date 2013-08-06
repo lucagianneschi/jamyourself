68358b590505d67736535c6deb0ca0043bc41ecb
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-08-06 13:54:08 +0200

    adattato a nuova versione del box

--- test/boxesRecordPage/testBoxRecordInfo.php
+++ test/boxesRecordPage/testBoxRecordInfo.php
@@ -16,85 +16,32 @@
 $t_start = microtime(); //timer tempo totale
 $i_start = microtime(); //timer include
 
+
 if (!defined('ROOT_DIR'))
     define('ROOT_DIR', '../../');
 
 ini_set('display_errors', '1');
 require_once ROOT_DIR . 'config.php';
 require_once PARSE_DIR . 'parse.php';
-require_once CLASSES_DIR . 'record.class.php';
-require_once CLASSES_DIR . 'recordParse.class.php';
-require_once CLASSES_DIR . 'song.class.php';
-require_once CLASSES_DIR . 'songParse.class.php';
-require_once CLASSES_DIR . 'user.class.php';
-require_once CLASSES_DIR . 'userParse.class.php';
+require_once BOXES_DIR . 'recordInfo.box.php';
 $i_end = microtime();
 
 
 $id = 'b8r9q9b5se';
 $record_start = microtime();
-$recordP = new RecordParse();
-$record = $recordP->getRecord($id);
+echo '<br />TEST  INFO RECORD MEDIA PAGE<br />';
+$recordBoxP = new RecordInfoBox();
+$recordBox = $recordBoxP->init($id);
+print "<pre>";
+print_r($recordBox);
+print "</pre>";
+echo '<br />FINE TEST  INFO RECORD MEDIA PAGE<br />';
 $record_stop = microtime();
 
-echo '<br />----------------------DX---------------------------<br />';
-echo '<br />[title] => ' . $record->getTitle() . '<br />';
-echo '<br />[cover] => ' . $record->getCover() . '<br />';
-echo '<br />[genre] => ' . $record->getGenre() . '<br />';
-echo '<br />[record in year] => ' . $record->getYear() . '<br />';
-echo '<br />[description] => ' . $record->getDescription() . '<br />';
-echo '<br />[buyLink] => ' . $record->getBuyLink() . '<br />';
-echo '<br />[locationName] => ' . $record->getLocationName() . '<br />';
-
-$fromUserP = new UserParse();
-$fromUser = $fromUserP->getUser($record->getFromUser());
-echo '<br />[username] => ' . $fromUser->getUsername() . '<br />';
-echo '<br />[thumbnail] => ' . $fromUser->getProfileThumbnail() . '<br />';
-echo '<br />[type] => ' . $fromUser->getProfileThumbnail() . '<br />';
-
-$featuring_start = microtime();
-$parseUser = new UserParse();
-$parseUser->whereRelatedTo('featuring', '_User', $id);
-$parseUser->where('active', true);
-$featuring = $parseUser->getUsers();
-if (count($featuring) != 0) {
-    foreach ($featuring as $user) {
-	echo '<br />[username] => ' . $user->getUsername() . '<br />';
-	echo '<br />[thumbnail] => ' . $user->getProfileThumbnail() . '<br />';
-	echo '<br />[type] => ' . $user->getProfileThumbnail() . '<br />';
-    }
-}
-
-
-$featuring_stop = microtime();
-$tracklist_start = microtime();
-$tracklist = new SongParse();
-$tracklist->wherePointer('record', 'Record', $id);
-$tracklist->setLimit(50);
-$tracklist->where('active', true);
-$tracklist->orderByAscending('createdAt');
-$songs = $tracklist->getSongs();
-if (count($songs) != 0) {
-    foreach ($songs as $song) {
-	echo '<br />[title] => ' . $song->getTitle() . '<br />';
-	echo '<br />[duration] => ' . $song->getDuration() . '<br />';
-	echo '<br />[loveCounter] => ' . $song->getLoveCounter() . '<br />';
-	echo '<br />[shareCounter] => ' . $song->getShareCounter() . '<br />';
-    }
-}
-$tracklist_stop = microtime();
-echo '<br />----------------------DX---------------------------<br />';
-echo '<br />----------------------SX---------------------------<br />';
-echo '<br />[loveCounter] => ' . $record->getLoveCounter() . '<br />';
-echo '<br />[commentCounter] => ' . $record->getCommentCounter() . '<br />';
-echo '<br />[shareCounter] => ' . $record->getShareCounter() . '<br />';
-echo '<br />----------------------SX---------------------------<br />';
 $t_end = microtime();
 echo '<br />----------------------TIMERS---------------------------<br />';
 echo 'Tempo include ' . executionTime($i_start, $i_end) . '<br />';
 echo 'Tempo recupero info record ' . executionTime($record_start, $record_stop) . '<br />';
-echo 'Tempo featuring ' . executionTime($featuring_start, $featuring_stop) . '<br />';
-echo 'Tempo tracklist ' . executionTime($tracklist_start, $tracklist_stop) . '<br />';
 echo 'Tempo totale ' . executionTime($t_start, $t_end) . '<br />';
 echo '<br />----------------------TIMERS---------------------------<br />';
 ?>
