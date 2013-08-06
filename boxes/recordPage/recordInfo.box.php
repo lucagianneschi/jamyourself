cd5dd40b2d0de0ec59b6c532cb3f07c8030b93de
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-08-06 13:25:09 +0200

    corretti errori e warning vari

--- boxes/recordPage/recordInfo.box.php
+++ boxes/recordPage/recordInfo.box.php
@@ -29,112 +29,113 @@ require_once CLASSES_DIR . 'songParse.class.php';
 require_once CLASSES_DIR . 'user.class.php';
 require_once CLASSES_DIR . 'userParse.class.php';
 
-class SongInfo{
-   
-   public $duration;
-   public $title;
-   public $loveCounter;
-   public $shareCounter;
-   
-   function __construct($duration,$loveCounter,$shareCounter,$title){
-		is_null($duration) ? $this->duration = NODATA : $this->duration = $duration;
-		is_null($loveCounter) ? $this->loveCounter = NODATA : $this->loveCounter = $loveCounter;
-		is_null($shareCounter) ? $this->shareCounter = NODATA : $this->shareCounter = $shareCounter;
-		is_null($title) ? $this->title = NODATA : $this->title = $title;
-   }
+class SongInfo {
+
+    public $duration;
+    public $title;
+    public $loveCounter;
+    public $shareCounter;
+
+    function __construct($duration, $loveCounter, $shareCounter, $title) {
+	is_null($duration) ? $this->duration = NODATA : $this->duration = $duration;
+	is_null($loveCounter) ? $this->loveCounter = NODATA : $this->loveCounter = $loveCounter;
+	is_null($shareCounter) ? $this->shareCounter = NODATA : $this->shareCounter = $shareCounter;
+	is_null($title) ? $this->title = NODATA : $this->title = $title;
+    }
+
 }
 
-class UserInfo{
-	
-	public $thumbnail;
-	public $type;
-	public $username;
+class UserInfo {
 
-	function __construct($thumbnail,$type,$username){
-		is_null($thumbnail) ? $this->thumbnail = NODATA : $this->thumbnail = $thumbnail;
-		is_null($type) ? $this->type = NODATA : $this->type = $type;
-		is_null($username) ? $this->username= NODATA : $this->username = $username;
-	}
+    public $thumbnail;
+    public $type;
+    public $username;
+
+    function __construct($thumbnail, $type, $username) {
+	is_null($thumbnail) ? $this->thumbnail = NODATA : $this->thumbnail = $thumbnail;
+	is_null($type) ? $this->type = NODATA : $this->type = $type;
+	is_null($username) ? $this->username = NODATA : $this->username = $username;
+    }
+
 }
 
 class RecordInfoBox {
 
-	public $buylink;
-	public $cover;
-	public $description;
-	public $featuring;
-	public $genre;
-	public $songList;
+    public $buylink;
+    public $cover;
+    public $description;
+    public $featuring;
+    public $genre;
+    public $songList;
     public $title;
-	public $year;
-	
-	public function init($objectId) {
-	
+    public $year;
+
+    public function init($objectId) {
+
 	$recordInfoBox = new RecordInfoBox ();
-	$songList = array();
 	
 	$recordP = new RecordParse();
 	$record = $recordP->getRecord($objectId);
-	
-	if($record->getActive() = true){
-		is_null($event->getBuylink()) ? $eventInfoBox->buylink = NODATA : $eventInfoBox->$buylink = $event->getBuylink();
-		is_null($event->getCover()) ? $eventInfoBox->cover = NODATA : $eventInfoBox->$cover = $event->getCover();
-		is_null($event->getDescription()) ? $eventInfoBox->description = NODATA : $eventInfoBox->description = $event->getDescription();
-		is_null($event->getFeaturing()) ? $eventInfoBox->featuring = NODATA : $eventInfoBox->featuring = $event->getFeaturing();
-		is_null($event->getGenre()) ? $eventInfoBox->genre = NODATA : $eventInfoBox->genre = $event->getGenre();
-		is_null($event->getTitle()) ? $eventInfoBox->title = NODATA : $eventInfoBox->$title = $event->getTitle();
-		
-		$fromUserP = new UserParse();
-		$fromUser = $fromUserP->getUser($event->getFromUser);
-		if ($fromUser != null){
-			is_null($user->getUsername()) ? $eventInfoBox->username = NODATA : $eventInfoBox->username = $user->getUsername();
-			is_null($user->getProfilePictureThumbnail) ? $eventInfoBox->thumbnail = NODATA : $eventInfoBox->thumbnail = $user->getProfilePictureThumbnail;
+
+	if ($record->getActive() == true) {
+	    is_null($record->getBuylink()) ? $recordInfoBox->buylink = NODATA : $recordInfoBox->buylink = $record->getBuylink();
+	    is_null($record->getCover()) ? $recordInfoBox->cover = NODATA : $recordInfoBox->cover = $record->getCover();
+	    is_null($record->getDescription()) ? $recordInfoBox->description = NODATA : $recordInfoBox->description = $record->getDescription();
+	    is_null($record->getFeaturing()) ? $recordInfoBox->featuring = NODATA : $recordInfoBox->featuring = $record->getFeaturing();
+	    is_null($record->getGenre()) ? $recordInfoBox->genre = NODATA : $recordInfoBox->genre = $record->getGenre();
+	    is_null($record->getTitle()) ? $recordInfoBox->title = NODATA : $recordInfoBox->$title = $record->getTitle();
+
+	    $fromUserP = new UserParse();
+	    $fromUser = $fromUserP->getUser($record->getFromUser);
+	    if ($fromUser != null) {
+		is_null($fromUser->getUsername()) ? $recordInfoBox->username = NODATA : $recordInfoBox->username = $fromUser->getUsername();
+		is_null($fromUser->getProfilePictureThumbnail()) ? $recordInfoBox->thumbnail = NODATA : $recordInfoBox->thumbnail = $fromUser->getProfilePictureThumbnail;
+	    }
+
+	    $featuring = array();
+	    $parseUser = new UserParse();
+	    $parseUser->whereRelatedTo('featuring', 'Record', $objectId);
+	    $parseUser->where('active', true);
+	    $parseUser->setLimit(1000);
+	    $feats = $parseUser->getUsers();
+	    if (count($feats) == 0) {
+		$recordInfoBox->featuring = NODATA;
+	    } elseif (get_class($feats) == 'Error') {
+		echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $feats->getErrorMessage() . '<br/>';
+	    } else {
+		foreach ($feats as $user) {
+		    $thumbnail = $user->getProfilePictureThumbnail();
+		    $type = $user->getType();
+		    $username = $user->getUsername();
+		    $userInfo = new UserInfo($thumbnail, $type, $username);
+		    array_push($featuring, $userInfo);
 		}
-				
-		$featuring = array();
-		$parseUser = new UserParse();
-		$parseUser->whereRelatedTo('featuring','Record', $objectId);
-		$parseUser->where('active', true);
-		$parseUser->setLimit(1000);
-		$feats = $parseUser->getUsers();
-		if(count($feats) == 0) {
-			$eventInfoBox->featuring = NODATA;
-		}elseif(get_class($feats) == 'Error') {
-			echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $events->getErrorMessage() . '<br/>';
-		} else {
-			foreach ($feats as $user) {
-				$thumbnail = $user->getProfilePictureThumbnail();
-				$type = $user->getType();
-				$username = $user->getUsername();
-				$userInfo = new UserInfo($thumbnail,$type,$username);
-				array_push($featuring, $userInfo);
-			}
-			$eventInfoBox->featuring = $featuring;
-		}	
-	}	
-	
-		$songList = array();
-		$parseSong = new SongParse();
-		$parseSong->where('active', true);
-		$parseSong->setLimit(50);
-		$songs = $parseSong->getSongs();
-		if(count($songs) == 0){
-			$eventInfoBox->songList = NODATA;
-		} elseif(get_class($songs) == 'Error'){
-			echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $events->getErrorMessage() . '<br/>';
-		} else {
-			foreach ($songs as $song) {
-				$duration = $song->getDuration();
-				$title = song->getTitle();
-				$loveCounter = $song->getLoveCounter();
-				$shareCounter = $song->getShareCounter();
-				$songInfo = new SongInfo($duration,$loveCounter,$shareCounter,$title);
-				array_push($songList, $songInfo);
-			}
-			$eventInfoBox->songList = $songList;
-		}
-	return $recordInfoBox;
+		$recordInfoBox->featuring = $featuring;
+	    }
 	}
+
+	$songList = array();
+	$parseSong = new SongParse();
+	$parseSong->where('active', true);
+	$parseSong->setLimit(50);
+	$songs = $parseSong->getSongs();
+	if (count($songs) == 0) {
+	    $recordInfoBox->songList = NODATA;
+	} elseif (get_class($songs) == 'Error') {
+	    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $songs->getErrorMessage() . '<br/>';
+	} else {
+	    foreach ($songs as $song) {
+		$duration = $song->getDuration();
+		$title = $song->getTitle();
+		$loveCounter = $song->getLoveCounter();
+		$shareCounter = $song->getShareCounter();
+		$songInfo = new SongInfo($duration, $loveCounter, $shareCounter, $title);
+		array_push($songList, $songInfo);
+	    }
+	    $recordInfoBox->songList = $songList;
+	}
+	return $recordInfoBox;
+    }
 
 }
 
