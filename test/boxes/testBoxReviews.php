62d3c4f6953440f889deef89bb84fa7c08ca1aa3
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-08-07 12:40:36 +0200

    adattato al nuovo box review

--- test/boxes/testBoxReviews.php
+++ test/boxes/testBoxReviews.php
@@ -27,8 +27,8 @@ require_once PARSE_DIR . 'parse.php';
 require_once BOXES_DIR . 'review.box.php';
 $i_end = microtime();
 
-$objectId = 'GuUAj83MGH';
-$objectId1 = 'nXvzqUuPHW';
+$objectId = 'GuUAj83MGH'; //spatafora
+$objectId1 = 'nXvzqUuPHW'; //disinteresse
 	
 $reviewRecord_start = microtime();
 $reviewRecordBoxP = new ReviewBox();
@@ -48,11 +48,29 @@ print "</pre>";
 $reviewRecord1_stop = microtime();
 
 
+$reviewEvent_start = microtime();
+$reviewEventBoxP = new ReviewBox();
+$reviewEventBox = $reviewEventBoxP->init($objectId,'SPOTTER','Event');
+print "<pre>";
+print_r($reviewEventBox);
+print "</pre>";
+$reviewEvent_stop = microtime();
+
+$reviewEvent1_start = microtime();
+$reviewEventBoxP = new ReviewBox();
+$reviewEventBox = $reviewEventBoxP->init($objectId1,'JAMMER','Event');
+print "<pre>";
+print_r($reviewEventBox);
+print "</pre>";
+$reviewEvent1_stop = microtime();
+
 $t_end = microtime();
 echo '<br />----------------------TIMERS---------------------------<br />';
 echo 'Tempo include ' . executionTime($i_start, $i_end) . '<br />';
-echo 'Tempo review' . executionTime($reviewRecord_start, $reviewRecord_stop) . '<br />';
-echo 'Tempo review' . executionTime($reviewRecord1_start, $reviewRecord1_stop) . '<br />';
+echo 'Tempo review record' . executionTime($reviewRecord_start, $reviewRecord_stop) . '<br />';
+echo 'Tempo review record' . executionTime($reviewRecord1_start, $reviewRecord1_stop) . '<br />';
+echo 'Tempo review event' . executionTime($reviewEvent_start, $reviewEvent_stop) . '<br />';
+echo 'Tempo review event' . executionTime($reviewEvent1_start, $reviewEvent1_stop) . '<br />';
 echo 'Tempo totale ' . executionTime($t_start, $t_end) . '<br />';
 echo '<br />----------------------TIMERS---------------------------<br />';
 ?>
