0097eeb060c3c537a141d190fc0bfd21585c6ec5
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-08-07 17:10:06 +0200

    utilizzo Counters Object

--- boxes/event.box.php
+++ boxes/event.box.php
@@ -26,39 +26,38 @@ require_once CLASSES_DIR . 'event.class.php';
 require_once CLASSES_DIR . 'eventParse.class.php';
 require_once CLASSES_DIR . 'user.class.php';
 require_once CLASSES_DIR . 'userParse.class.php';
+require_once BOXES_DIR . 'utils.box.php';
 
 class EventInfo {
 
     public $address;
     public $city;
-    public $commentCounter;
+    public $counters;
     public $eventDate;
     public $featuring;
+    public $fromUserInfo;
     public $locationName;
-    public $loveCounter;
     public $reviewCounter;
-    public $shareCounter;
     public $tags;
     public $thumbnail;
     public $title;
 
-    function __construct($address, $city, $commentCounter, $eventDate, $featuring, $locationName, $loveCounter, $reviewCounter, $shareCounter, $tags, $thumbnail, $title) {
+    function __construct($address, $city, $counters, $eventDate,$fromUserInfo, $featuring, $locationName, $reviewCounter, $tags, $thumbnail, $title) {
 	is_null($address) ? $this->address = NODATA : $this->address = $address;
 	is_null($city) ? $this->city = NODATA : $this->city = $city;
-	is_null($commentCounter) ? $this->commentCounter = NODATA : $this->commentCounter = $commentCounter;
+	is_null($counters) ? $this->counters = NODATA : $this->counters = $counters;
 	is_null($eventDate) ? $this->eventDate = NODATA : $this->eventDate = $eventDate;
 	is_null($featuring) ? $this->featuring = NODATA : $this->featuring = $featuring;
+	is_null($fromUserInfo) ? $this->fromUserInfo = NODATA : $this->fromUserInfo = $fromUserInfo;
 	is_null($locationName) ? $this->locationName = NODATA : $this->locationName = $locationName;
-	is_null($loveCounter) ? $this->loveCounter = NODATA : $this->loveCounter = $loveCounter;
 	is_null($reviewCounter) ? $this->reviewCounter = NODATA : $this->reviewCounter = $reviewCounter;
-	is_null($shareCounter) ? $this->shareCounter = NODATA : $this->shareCounter = $shareCounter;
 	is_null($tags) ? $this->tags = NODATA : $this->tags = $tags;
 	is_null($thumbnail) ? $this->thumbnail = NODATA : $this->thumbnail = $thumbnail;
 	is_null($title) ? $this->title = NODATA : $this->title = $title;
     }
 
 }
-
+//rivedere se vengono mostrati diversi a seconda del profilo
 class EventBox {
 
     public $eventInfoArray;
@@ -97,6 +96,9 @@ class EventBox {
 			    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $events->getErrorMessage() . '<br/>';
 			} else {
 			    foreach ($feats as $user) {
+				//da verficare
+				var_dump($user);
+				//da verficare
 				if ($user != null) {
 				    $username = $user->getUsername();
 				}
@@ -110,6 +112,7 @@ class EventBox {
 		    $loveCounter = $event->getLoveCounter();
 		    $reviewCounter = $event->getReviewCounter();
 		    $shareCounter = $event->getShareCounter();
+		    $counters = new Counters($commentCounter, $loveCounter, $shareCounter);
 		    $tags = array();
 		    if (count($event->getTags()) != 0 && $event->getTags() != null) {
 			foreach ($event->getTags() as $tag) {
@@ -118,7 +121,7 @@ class EventBox {
 		    }
 		    $thumbnail = $event->getThumbnail();
 		    $title = $event->getTitle();
-		    $eventInfo = new EventInfo($address, $city, $commentCounter, $eventDate, $featuring, $locationName, $loveCounter, $reviewCounter, $shareCounter, $tags, $thumbnail, $title);
+		    $eventInfo = new EventInfo($address, $city, $counters, $eventDate, $featuring, $locationName,$reviewCounter, $tags, $thumbnail, $title);
 		    array_push($info, $eventInfo);
 		}
 		$eventBox->eventInfoArray = $info;
