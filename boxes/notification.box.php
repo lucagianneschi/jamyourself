67ae178a97dc7746e4c4908984b8fe6753c155f6
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-08-07 12:52:04 +0200

    corrette imprecisioni e warnings

--- boxes/notification.box.php
+++ boxes/notification.box.php
@@ -25,33 +25,31 @@ require_once PARSE_DIR . 'parse.php';
 require_once CLASSES_DIR . 'activity.class.php';
 require_once CLASSES_DIR . 'activityParse.class.php';
 
-class NotificationInfo{
-    public $eventTitle;
-	public $sender;
-	public $createdAt;
-	}
+class NotificationInfo {
 
-	
+    public $eventTitle;
+    public $sender;
+    public $createdAt;
+
+}
+
 class NotificationBox {
 
     public $invitationCounter;
     public $messageCounter;
-	public $relationCounter;
+    public $relationCounter;
 
     /**
      * \fn	init($objectId,$type)
      * \brief	Init NotificationBox instance
-	 * \param	$objectId
-	 * \param	$type
+     * \param	$objectId
+     * \param	$type
      * \return	infoBox
      */
-    public function init($objectId,$type) {
-	
-    $notificationBox = new NotificationBox();
-	$invitations = array();
-	$messages = array();
-	$relations = array();
-	
+    public function init($objectId, $type) {
+
+	$notificationBox = new NotificationBox();
+
 	$activity0 = new ActivityParse();
 	$activity0->wherePointer('toUser', 'User', $objectId);
 	$activity0->where('type', 'INVITED');
@@ -59,11 +57,11 @@ class NotificationBox {
 	$activity0->where('active', true);
 	$invitationCounter = $activity0->getCount();
 	if (get_class($invitationCounter) == 'Error') {
-		echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $invitationCounter->getErrorMessage() . '<br/>';
-		} else {
-		$notificationBox->invitationCounter = $invitationCounter;
+	    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $invitationCounter->getErrorMessage() . '<br/>';
+	} else {
+	    $notificationBox->invitationCounter = $invitationCounter;
 	}
-	
+
 	$activity1 = new ActivityParse();
 	$activity1->wherePointer('toUser', 'User', $objectId);
 	$activity1->where('type', 'MESSAGESENT');
@@ -71,42 +69,43 @@ class NotificationBox {
 	$activity1->where('active', true);
 	$messageCounter = $activity1->getCount();
 	if (get_class($messageCounter) == 'Error') {
-		echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $messageCounter->getErrorMessage() . '<br/>';
-		} else {
-		$notificationBox->messageCounter = $messageCounter;
+	    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $messageCounter->getErrorMessage() . '<br/>';
+	} else {
+	    $notificationBox->messageCounter = $messageCounter;
 	}
 	switch ($type) {
-		case 'SPOTTER':
-			$activity2 = new ActivityParse();
-			$activity2->wherePointer('toUser', '_User', $objectId);
-			$activity2->where('type', 'FRIENDREQUEST');
-			$activity2->where('status', 'W');
-			$activity2->where('read', false);
-			$activity2->where('active', true);
-			$relationCounter = $activity2->getCount();
-			if (get_class($invitations) == 'Error') {
-				echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $relationCounter->getErrorMessage() . '<br/>';
-			} else {
-				$notificationBox->relationCounter = $relationCounter;
-			}
-			break;
-		default:
-			$activityTypes = array(array('type' => 'COLLABORATIONREQUEST'),array('type' => 'FOLLOWING'));
-			$activity2 = new ActivityParse();
-			$activity2->wherePointer('toUser', '_User', $objectId);
-			$activity2->whereOr($activityTypes);
-			$activity2->where('status', 'W');
-			$activity2->where('read', false);
-			$activity2->where('active', true);
-			$relations = $activity2->getCount();
-			if (get_class($invitations) == 'Error') {
-				echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $relations->getErrorMessage() . '<br/>';
-			} else {
-				$notificationBox->relationCounter = $relations;
-			}
-			break;
+	    case 'SPOTTER':
+		$activity2 = new ActivityParse();
+		$activity2->wherePointer('toUser', '_User', $objectId);
+		$activity2->where('type', 'FRIENDREQUEST');
+		$activity2->where('status', 'W');
+		$activity2->where('read', false);
+		$activity2->where('active', true);
+		$relationCounter = $activity2->getCount();
+		if (get_class($relationCounter) == 'Error') {
+		    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $relationCounter->getErrorMessage() . '<br/>';
+		} else {
+		    $notificationBox->relationCounter = $relationCounter;
 		}
+		break;
+	    default:
+		$activityTypes = array(array('type' => 'COLLABORATIONREQUEST'), array('type' => 'FOLLOWING'));
+		$activity2 = new ActivityParse();
+		$activity2->wherePointer('toUser', '_User', $objectId);
+		$activity2->whereOr($activityTypes);
+		$activity2->where('status', 'W');
+		$activity2->where('read', false);
+		$activity2->where('active', true);
+		$relationCounter = $activity2->getCount();
+		if (get_class($relationCounter) == 'Error') {
+		    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $relationCounter->getErrorMessage() . '<br/>';
+		} else {
+		    $notificationBox->relationCounter = $relationCounter;
+		}
+		break;
 	}
+    }
+
 }
 
 ?>
