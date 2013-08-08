02db5dafb79fc965f357599b29448fe7cacd5ff8
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-08-08 10:59:35 +0200

    aggiunta stringa per parametri non definiti in alcuni box

--- string.php
+++ string.php
@@ -2,5 +2,6 @@
 
 define('NODATA', 'NO YET DATA TO DISPLAY');
 define('NOTDEFINED', 'NOT DEFINED FOR THIS TYPE OF USER');
+define('NOTDEFINEDBOX', 'NOT DEFINED FOR THIS TYPE OF BOX');
 define('DEFAULTAVATAR', '/var/www/vhosts/socialmusicdiscovering.com/httpdocs/script/wp_daniele/root/images/default/dafultAvatar.jpg');
 ?>
