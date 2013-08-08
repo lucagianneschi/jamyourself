d89dc5109b0d5ab6ddb188116073ce0c8039b2b4
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-08-08 11:00:26 +0200

    ridefinizione della classe recordInfo, introduzione dei metodi specializzati per pagina

--- boxes/record.box.php
+++ boxes/record.box.php
@@ -24,7 +24,39 @@ require_once CLASSES_DIR . 'record.class.php';
 require_once CLASSES_DIR . 'recordParse.class.php';
 require_once BOXES_DIR . 'utils.box.php';
 
-class RecordInfo {
+class RecordInfoForMediaPage {
+
+    public $buylink;
+    public $counters;
+    public $cover;
+    public $description;
+    public $featuring;
+    public $genre;
+    public $label;
+    public $locationName;
+    public $reviewCounter;
+    public $tracklist;
+    public $title;
+    public $year;
+
+    function __construct($buylink, $counters, $cover, $description, $featuring, $genre, $label, $locationName, $reviewCounter, $tracklist, $title, $year) {
+	is_null($buylink) ? $this->buylink = NODATA : $this->buylink = $buylink;
+	is_null($counters) ? $this->counters = NODATA : $this->counters = $counters;
+	is_null($cover) ? $this->cover = NODATA : $this->cover = $cover;
+	is_null($description) ? $this->description = NODATA : $this->description = $description;
+	is_null($featuring) ? $this->featuring = NODATA : $this->featuring = $featuring;
+	is_null($genre) ? $this->genre = NODATA : $this->genre = $genre;
+	is_null($label) ? $this->label = NODATA : $this->label = $label;
+	is_null($locationName) ? $this->locationName = NODATA : $this->locationName = $locationName;
+	is_null($reviewCounter) ? $this->reviewCounter = NODATA : $this->reviewCounter = $reviewCounter;
+	is_null($tracklist) ? $this->tracklist = NODATA : $this->tracklist = $tracklist;
+	is_null($title) ? $this->title = NODATA : $this->title = $title;
+	is_null($year) ? $this->year = NODATA : $this->year = $year;
+    }
+
+}
+
+class RecordInfoForPersonalPage {
 
     public $counters;
     public $genre;
@@ -46,13 +78,120 @@ class RecordInfo {
 
 }
 
+class SongInfo {
+
+    public $counters;
+    public $duration;
+    public $title;
+
+    function __construct($counters, $duration, $title) {
+	is_null($counters) ? $this->counters = NODATA : $this->counters = $counters;
+	is_null($duration) ? $this->duration = NODATA : $this->duration = $duration;
+	is_null($title) ? $this->title = NODATA : $this->title = $title;
+    }
+
+}
+
 class RecordBox {
 
-    public $recordInfoArray;
+    public $fromUserInfo;
     public $recordCounter;
+    public $recordInfoArray;
 
-    public function init($objectId) {
+    public function initForMediaPage($objectId) {
+
+	require_once CLASSES_DIR . 'song.class.php';
+	require_once CLASSES_DIR . 'songParse.class.php';
+	require_once CLASSES_DIR . 'user.class.php';
+	require_once CLASSES_DIR . 'userParse.class.php';
+
 	$recordBox = new RecordBox();
+	$recordP = new RecordParse();
+	$record = $recordP->getRecord($objectId);
+	if (get_class($record) == 'Error') {
+	    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $record->getErrorMessage() . '<br/>';
+	} elseif ($record->getActive() == true) {
+	    $buylink = $record->getBuylink();
+	    $commentCounter = $record->getCommentCounter();
+	    $loveCounter = $record->getLoveCounter();
+	    $shareCounter = $record->getShareCounter();
+	    $counters = new Counters($commentCounter, $loveCounter, $shareCounter);
+	    
+	    $cover = $record->getCover();
+	    $description = $record->getDescription();
+	    
+	    $featuring = array();
+	    $parseUser = new UserParse();
+	    $parseUser->whereRelatedTo('featuring', 'Record', $objectId);
+	    $parseUser->where('active', true);
+	    $parseUser->setLimit(1000);
+	    $feats = $parseUser->getUsers();
+	    if (count($feats) == 0) {
+		$featuring = NODATA;
+	    } elseif (get_class($feats) == 'Error') {
+		echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $feats->getErrorMessage() . '<br/>';
+	    } else {
+		foreach ($feats as $user) {
+		    $thumbnail = $user->getProfileThumbnail();
+		    $type = $user->getType();
+		    $username = $user->getUsername();
+		    $userInfo = new UserInfo($thumbnail, $type, $username);
+		    array_push($featuring, $userInfo);
+		}
+	    }
+
+	    $genre = $record->getGenre();
+	    $label = $record->getLabel();
+	    $locationName = $record->getLocationName();
+	    $reviewCounter = $record->getReviewCounter();
+	    $title = $record->getTitle();
+
+	    $tracklist = array();
+	    $parseSong = new SongParse();
+	    $parseSong->wherePointer('record', 'Record', $objectId);
+	    $parseSong->where('active', true);
+	    $parseSong->setLimit(50);
+	    $songs = $parseSong->getSongs();
+	    if (count($songs) == 0) {
+		$tracklist = NODATA;
+	    } elseif (get_class($songs) == 'Error') {
+		echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $songs->getErrorMessage() . '<br/>';
+	    } else {
+		foreach ($songs as $song) {
+		    $duration = $song->getDuration();
+		    $title = $song->getTitle();
+		    $commentCounter = null;
+		    $loveCounter = $song->getLoveCounter();
+		    $shareCounter = $song->getShareCounter();
+		    $counters = new Counters($commentCounter, $loveCounter, $shareCounter);
+		    $songInfo = new SongInfo($counters, $duration, $title);
+		    array_push($tracklist, $songInfo);
+		}
+	    }
+	    $year = $record->getYear();
+	    $recordInfo = new RecordInfoForMediaPage($buylink, $counters, $cover, $description, $featuring, $genre, $label, $locationName, $reviewCounter, $tracklist, $title, $year);
+
+	    $fromUserId = $record->getFromUser();
+	    $userP = new UserParse();
+	    $fromUser = $userP->getUser($fromUserId);
+	    if (get_class($fromUser) == 'Error') {
+		echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $fromUser->getErrorMessage() . '<br/>';
+	    } else {
+		$thumbnail = $fromUser->getProfileThumbnail();
+		$type = $fromUser->getType();
+		$username = $fromUser->getUsername();
+		$userInfo = new UserInfo($thumbnail, $type, $username);
+	    }
+	    $recordBox->fromUserInfo = $userInfo;
+	    $recordBox->recordCounter = NOTDEFINEDBOX;
+	    $recordBox->recordInfoArray = $recordInfo;
+	}
+	return $recordBox;
+    }
+
+    public function initForPersonalPage($objectId) {
+	$recordBox = new RecordBox();
+
 	$info = array();
 	$counter = 0;
 	$record = new RecordParse();
@@ -78,11 +217,12 @@ class RecordBox {
 		    $title = $record->getTitle();
 		    $year = $record->getYear();
 		    $counters = new Counters($commentCounter, $loveCounter, $shareCounter);
-		    $recordInfo = new RecordInfo($counters, $genre, $reviewCounter, $songCounter, $thumbnailCover, $title, $year);
+		    $recordInfo = new RecordInfoForPersonalPage($counters, $genre, $reviewCounter, $songCounter, $thumbnailCover, $title, $year);
 		    array_push($info, $recordInfo);
 		}
-		$recordBox->recordInfoArray = $info;
+		$recordBox->fromUserInfo = NOTDEFINEDBOX;
 		$recordBox->recordCounter = $counter;
+		$recordBox->recordInfoArray = $info;
 	    }
 	}
 	return $recordBox;
