5357e46ae3a0e00c7b59a8d2abf322d764e220c1
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-09-04 11:45:45 +0200

    aggiunte stringhe
    di default per le immagine di default del profilo utente

--- string.php
+++ string.php
@@ -3,5 +3,7 @@
 define('NODATA', 'NO YET DATA TO DISPLAY');
 define('ND', 'NOT DEFINED FOR THIS TYPE OF USER');
 define('NDB', 'NOT DEFINED FOR THIS TYPE OF BOX');
-define('DEFAULTAVATAR', '/var/www/vhosts/socialmusicdiscovering.com/httpdocs/script/wp_daniele/root/images/default/dafultAvatar.jpg');
+define('DEFAVATAR', '/var/www/vhosts/socialmusicdiscovering.com/httpdocs/script/wp_daniele/root/images/default/dafultAvatar.jpg');
+define('DEFTHUMB', '/var/www/vhosts/socialmusicdiscovering.com/httpdocs/script/wp_daniele/root/images/default/dafultAvatarThumb.jpg');
+define('DEFBGD', '/var/www/vhosts/socialmusicdiscovering.com/httpdocs/script/wp_daniele/root/images/default/dafultBackground.jpg');
 ?>
