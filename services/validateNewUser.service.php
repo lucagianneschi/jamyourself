eb59eb5900d7f316477f33b215acec2115b08528
Stefano <stefanom@jamyourself.com>
2013-09-11 00:28:56 +0200

    corrette alcune funzioni in base ai file ricevuti via mail da Alessandro sulla definizione dei valori del form impostate le intestazioni di alcune funzionalità che saranno da completare in base a questa nuova informazione

--- services/validateNewUser.service.php
+++ services/validateNewUser.service.php
@@ -8,8 +8,11 @@ class ValidateNewUserService {
 
     private $isValid;
     private $errors;            //lista delle properties che sono errate
+    private $config;            //puntatore al JSON di configurazione della registsrazione
 
     public function __construct($configFile) {
+        if ($confiFile == null)
+            return null;
         $this->isValid = true;
         $this->errors = array();
         $this->config = $configFile;
@@ -52,7 +55,11 @@ class ValidateNewUserService {
         if (!isset($user->type) || is_null($user->type))
             $this->setInvalid("type");
 
+
 //verifico i campi specifici per tipologia di utente
+        if (!isset($user->description) || is_null($user->description) || !$this->checkDescription($user->description))
+            $this->setInvalid("description");
+
         switch ($user->type) {
             case "SPOTTER":
                 $this->checkNewSpotter($user);
@@ -67,15 +74,15 @@ class ValidateNewUserService {
 
 //verifico la correttezza dei campi social (comuni a tutti e 3 i profili)
 //se sono stati definiti e non sono null controllo...
-        if (isset($user->facebook) && !is_null($user->facebook) && !$this->checkURL($user->facebook))
+        if (isset($user->facebook) || is_null($user->facebook) || $this->checkUrl($user->facebook))
             $this->setInvalid("facebook");
-        if (isset($user->twitter) && !is_null($user->twitter) && !$this->checkURL($user->twitter))
+        if (isset($user->twitter) || is_null($user->twitter) || $this->checkUrl($user->twitter))
             $this->setInvalid("twitter");
-        if (isset($user->youtube) && !is_null($user->youtube) && !$this->checkURL($user->youtube))
+        if (isset($user->youtube) || is_null($user->youtube) || $this->checkUrl($user->youtube))
             $this->setInvalid("youtube");
-        if (isset($user->google) && !is_null($user->google) && !$this->checkURL($user->google))
+        if (isset($user->google) || is_null($user->google) || $this->checkUrl($user->google))
             $this->setInvalid("google");
-        if (isset($user->web) && !is_null($user->web) && !$this->checkURL($user->web))
+        if (isset($user->web) || is_null($user->web) || $this->checkUrl($user->web))
             $this->setInvalid("web");
 
         //restituisco il risultato dell'analisi
@@ -99,8 +106,6 @@ class ValidateNewUserService {
             $this->setInvalid("address");
         if (!isset($user->number) || is_null($user->number))
             $this->setInvalid("number");
-        if (!isset($user->description) || is_null($user->description) || $this->checkDescription($user->description))
-            $this->setInvalid("description");
         if (!isset($user->genre) || is_null($user->genre) || $this->checkLocalType($user->genre))
             $this->setInvalid("genre");
 
@@ -116,40 +121,36 @@ class ValidateNewUserService {
     private function checkNewSpotter($user) {
 
         //@stefano : continua da qui
-        if (!isset($user->lastname) || !$this->checkFirstName($user->lastname))
+        if (!isset($user->lastname) || is_null($user->lastname) || !$this->checkLastname($user->lastname))
             $this->setInvalid("lastname");
-        if (!isset($user->firstname) || !$this->checkFirstName($user->firstname))
+        if (!isset($user->firstname) || is_null($user->firstname) || !$this->checkFirstname($user->firstname))
             $this->setInvalid("firstname");
-        if (!isset($user->birthday) || !$this->checkBirthday($user->sex)) {
+        if (!isset($user->birthday) || is_null($user->birthday) || !$this->checkBirthday($user->sex)) {
             $this->setInvalid("birthday");
         } else {
             
         }
-        if (!isset($user->city))
+        if (!isset($user->city) || is_null($user->city))
             $this->setInvalid("city");
-        if (!isset($user->country))
+        if (!isset($user->country) || is_null($user->country))
             $this->setInvalid("country");
-        if (!isset($user->description) || !$this->checkDescription($user->sex))
-            $this->setInvalid("description");
-        if (!isset($user->genre) || !$this->checkMusic($user->sex))
+        if (!isset($user->genre) || is_null($user->genre) || !$this->checkMusic($user->genre))
             $this->setInvalid("genre");
-        if (!isset($user->sex) || !$this->checkSex($user->sex))
+        if (!isset($user->sex) || is_null($user->sex) || !$this->checkSex($user->sex))
             $this->setInvalid("sex");
     }
 
     private function checkNewJammer($user) {
 
-        if (!isset($user->city))
+        if (!isset($user->city) || is_null($user->city))
             $this->setInvalid("city");
-        if (!isset($user->country))
+        if (!isset($user->country) || is_null($user->country))
             $this->setInvalid("country");
-        if (!isset($user->description) || !$this->checkDescription($user->description))
-            $this->setInvalid("description");
-        if (!isset($user->genre) || !$this->checkMusic($user->genre))
+        if (!isset($user->genre) || is_null($user->genre) || !$this->checkMusic($user->genre))
             $this->setInvalid("genre");
-        if (!isset($user->jammerType) || !$this->checkJammerType($user->jammerType))
+        if (!isset($user->jammerType) || is_null($user->jammerType) || !$this->checkJammerType($user->jammerType))
             $this->setInvalid("jammerType");
-        if (!isset($user->members) || !$this->checkMembers($user->members))
+        if (!isset($user->members) || is_null($user->members) || !$this->checkMembers($user->members))
             $this->setInvalid("members");
     }
 
@@ -162,18 +163,13 @@ class ValidateNewUserService {
 
 
     public function checkUsername($username) {
-//        if(preg_match('/^\w{5,}$/', $username)) { // \w equals "[0-9A-Za-z_]"
-//        // valid username, alphanumeric & longer than or equals 5 chars
-//        }
-//        if(preg_match('/^[a-zA-Z0-9]{5,}$/', $username)) { // for english chars + numbers only
-//            // valid username, alphanumeric & longer than or equals 5 chars
-//        }
-
-        if (preg_match('/^\w{5,}$/', $username) || preg_match('/^[a-zA-Z0-9]{5,}$/', $username)) {
-            return true;
-        }
-        else
-            return false;
+//Campo ‘Artist/Group Name’. (campo DB: username - string) A) Max numero caratteri pari a 50 B)
+//Controllo che non sia presente spazio alla fine dell’ultima parola; Gli spazi interni sono consentiti Es.
+//‘Modena City Ramblers’ C) Controllare che non siano presenti caratteri speciali non stampabili (vedi
+//lista allegata a mail) D) Gestione lettere accentate E) Case Sensitive attivo per distinzione
+//maiuscole/minuscole e non creare problemi su DB F) Controllare che Username non sia già presente nel
+//DB
+        return true;
     }
 
     /**
@@ -182,17 +178,13 @@ class ValidateNewUserService {
      * @return boolean
      */
     public function checkPassword($password) {
-//    Numbers, letters, CAPS:
-//    Remember most users dont like passwords with 
-//    symbols(because of keyboard differences), 
-//    you can exclude symbol-check. 
-//    Just check length, letters, caps and numbers.
-
-        if (preg_match("#.*^(?=.{8,20})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$#", $password)) {
-            return true;
-        } else {
-            return false;
-        }
+//Campo ‘Password’ (campo DB: password - string ) A) Max numero caratteri pari a 50 B) Controllo che
+//nel campo non siano presenti spazi sia all’interno della password che alla fine C) Controllare che nel
+//campo non siano presenti caratteri speciali non stampabili (vedi lista allegata a mail) D) Controllare che
+//nel campo non siano presenti lettere accentate E) Controllo che password non sia composta da un solo
+//ed unico carattere F) Controllo che dimensione minima password 8 caratteri G) Case Sensitive attivo
+//per la password (distinzione maiuscole minuscole)
+        return true;
     }
 
     public function checkVerifyPassword($verifyPassword, $password) {
@@ -205,7 +197,6 @@ class ValidateNewUserService {
     public function checkBirthday($birthdayJSON) {
         $birthday = json_decode(json_encode($birthdayJSON));
         if (
-                is_null($birthday) ||
                 !isset($birthday->day) || is_null($birthday->day) ||
                 !isset($birthday->month) || is_null($birthday->month) ||
                 !isset($birthday->year) || is_null($birthday->year)
@@ -216,97 +207,34 @@ class ValidateNewUserService {
     }
 
     public function checkDescription($description) {
-        define("MAX_DESCRIPTION_LENGTH", 1000);
+//Campo ‘Description’ (campo DB: description - string) A) Max Caratteri pari a 800 B) Controllo che non
+//sia presente spazio alla fine dell’ultima parola; Gli spazi interni sono consentiti Es. ‘Modena City
+//Ramblers’ C) Gestione lettere accentate da impostare E) Case Sensitive attivo per distinzione
+//maiuscole/minuscole e non creaare problemi su DB F) Unico carattere speciale per il momento non
+//consentito “ (vedere mail Mari)
         if (
-                is_null($description) ||
                 count($description) <= 0 ||
-                count($description) > MAX_DESCRIPTION_LENGTH
+                count($description) > $this->config->maxDescriptionLength
         )
             return false;
     }
 
     public function checkEmail($email) {
-
-// First, we check that there's one @ symbol, 
-// and that the lengths are right.
-        if (!ereg("^[^@]{1,64}@[^@]{1,255}$", $email)) {
-// Email invalid because wrong number of characters 
-// in one section or wrong number of @ symbols.
-            return false;
-        }
-// Split it into sections to make life easier
-        $email_array = explode("@", $email);
-        $local_array = explode(".", $email_array[0]);
-        for ($i = 0; $i < sizeof($local_array); $i++) {
-            if
-            (!ereg("^(([A-Za-z0-9!#$%&'*+/=?^_`{|}~-][A-Za-z0-9!#$%&
-?'*+/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$", $local_array[$i])) {
-                return false;
-            }
-        }
-// Check if domain is IP. If not, 
-// it should be valid domain name
-        if (!ereg("^\[?[0-9\.]+\]?$", $email_array[1])) {
-            $domain_array = explode(".", $email_array[1]);
-            if (sizeof($domain_array) < 2) {
-                return false; // Not enough parts to domain
-            }
-            for ($i = 0; $i < sizeof($domain_array); $i++) {
-                if
-                (!ereg("^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|
-?([A-Za-z0-9]+))$", $domain_array[$i])) {
-                    return false;
-                }
-            }
-        }
-        return true && filter_var($email, FILTER_VALIDATE_EMAIL);
-    }
-
-    public function checkFirstName($firstname) {
-        if (preg_match("/^(?:[A-Za-z]+(?:\s+|$)){2,3}$/", $firstname)) {
-            return true;
-        }
-        else
-            return false;
+//Campo ‘mail’ (campo DB: email – string) A) Max numero caratteri pari a 50 B) Controllo che nel campo
+//non siano presenti spazi sia all’interno della mail che alla fine C) Controllo che nel campo sia presente
+//‘@’ D) Controllo che nel campo sia presente il . (punto) D) Controllare che non siano presenti nel
+//campo caratteri speciali non stampabili (vedi lista allegata a mail) E) Controllare che non siano presenti
+//nel campo lettere accentate F) Controllare che indirizzo mail inserito sia un indirizzo mail valido G)
+//Controllare che indirizzo mail non sia già presente nel DB H) Validazione indirizzo mail anche se presenti
+//caratteri Underscore _ o -
     }
 
     public function checkJammerType($jammerType) {
-        $jammerTypeList = array('Musician', 'Band');
-        if (!is_null($jammerType) || !in_array($jammerType, $jammerTypeList))
+        $jammerTypeList = $this->config->jammerType;
+        if (!in_array($jammerType, $jammerTypeList))
             return false;
         else
             return true;
-    }
-
-    public function checkLastname($lastname) {
-        if (preg_match("/^(?:[A-Za-z]+(?:\s+|$)){2,3}$/", $lastname)) {
-            return true;
-        }
-        else
-            return false;
-    }
-
-    public function checkLocalType($localType) {
-        define("MAX_LOCALTYPE_SIZE", 5);
-        $localTypeList = array("happy_hour-appetizer",
-            "dinner",
-            "live_music",
-            "contest_Area",
-            "pub",
-            "happy_hour-appetizer",
-            "dinner",
-            "pub",
-            "live_music");
-
-        if (is_null($localType) || !is_array($localType) || count($localType) <= 0 || count($localType) > MAX_LOCALTYPE_SIZE)
-            return false;
-
-        foreach ($localType as $val) {
-            if (!in_array($val, $localTypeList))
-                return false;
-        }
-
-        return true;
     }
 
     public function checkLocation($location) {
@@ -318,49 +246,53 @@ class ValidateNewUserService {
     }
 
     public function checkMembers($members) {
-        if (is_null($members) || !is_array($members) || count($members) <= 0)
+        if (!is_array($members))
             return false;
-        else
-            return true;
+        else {
+            if (count($members) > 0) {
+                foreach ($members as $component) {
+                    if (is_null($component) || !$this->checkBandComponent($component))
+                        return false;
+                }
+            }
+        }
+        return true;
     }
 
     private function checkMusic($music) {
-        define("MAX_MUSIC_SIZE", 10);
-
-        $musicList = array(
-            "rock",
-            "indie-rock",
-            "metal",
-            "songwriter",
-            "punk",
-            "rap_hip-hop",
-            "ska",
-            "pop",
-            "instrumental",
-            "electronic",
-            "dance",
-            "jazz-blues",
-            "esperimental",
-            "folk",
-            "ambient",
-            "acoustic",
-            "hard-core",
-            "house",
-            "techno",
-            "punk",
-            "grounge",
-            "progessive",
-            "dark"
-        );
-
-        if (is_null($music) || !is_array($music) || count($music) <= 0 || count($musicList) > MAX_MUSIC_SIZE)
+//Campo ‘Genre’ (campo DB: music - array ) A) Controllo che almeno 1 Genere sia indicato dall’utente B)
+//Max numero selezioni da utente pari a 5. Controllare che selezioni dell’utente siano pari o inferiori a 5
+        if (is_null($music) || !is_array($music) || count($music) <= 0 || count($music) > $this->config->maxMusicSize)
             return false;
-
+        //controllo se sono numeri perché i valori nel form sono mappati come numeri (indici del corrispondente array)
         foreach ($music as $val) {
-            if (!in_array($val, $musicList))
+            if (!is_numeric($val))
                 return false;
         }
 
+        return true;
+    }
+
+    private function checkLocalType($localType) {
+        if (!is_array($localType) || count($localType) <= 0 || count($localType) > $this->config->maxLocalTypeSize)
+            return false;
+        //controllo se sono numeri perché i valori nel form sono mappati come numeri (indici del corrispondente array)
+        foreach ($localType as $val) {
+            if (!is_numeric($val))
+                return false;
+        }
+
+        return true;
+    }
+
+    private function checkBandComponent($componentJSON) {
+        $component = json_decode(json_encode($componentJSON));
+//Campo ‘Components’ (campo DB: members - array) si compone di due campi: ‘name’ a compilazione
+//libera da parte dell’utente A) Max numero caratteri pari a 50 B) Controllo che non sia presente spazio
+//alla fine dell’ultima parola; Gli spazi interni sono consentiti Es. ‘Dino Baggio’ C) Controllare che non
+//siano presenti caratteri speciali non stampabili (vedi lista allegata a mail) D) Gestione lettere accentate
+//E) Case Sensitive attivo per distinzione maiuscole/minuscole e non creare problemi su DB. Per Campo
+//“Instrument” utilizziamo lista strumenti fornita da Pinni (allegato file .txt)
         return true;
     }
 
@@ -376,18 +308,31 @@ class ValidateNewUserService {
         }
     }
 
-    private function checkURL($url) {
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_NOBODY, true);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_exec($ch);
-        $retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-        if (200 == $retcode) {
-            return true;
-        } else {
-            return false;
-        }
+    private function checkUrl($url) {
+//Campo ‘I’m also on’ (campo DB: fbPage – string ; twitterPage – string ; youtubeChannel – string ;
+//website - string ) A) Controllo che campo sia una URL
+
+        return true;
+    }
+
+    private function checkFirstname($firstname) {
+//Campo “First Name” (campo DB: firstname - string) A) Max numero caratteri pari a 50 B) Controllo che
+//non sia presente spazio alla fine dell’ultima parola; Gli spazi interni sono consentiti Es. “Gian Maria” C)
+//Controllare che non siano presenti caratteri speciali non stampabili (vedi lista allegata a mail) D)
+//Gestione lettere accentate E) Case Sensitive attivo per distinzione maiuscole/minuscole e non creare
+//problemi su DB
+
+        return true;
+    }
+
+    private function checkLastname($lastname) {
+//Campo “Last Name” (campo DB: lastname - string) A) Max numero caratteri pari a 50 B) Controllo che
+//non sia presente spazio alla fine dell’ultima parola; Gli spazi interni sono consentiti Es. “Gian Maria” C)
+//Controllare che non siano presenti caratteri speciali non stampabili (vedi lista allegata a mail) D)
+//Gestione lettere accentate E) Case Sensitive attivo per distinzione maiuscole/minuscole e non creare
+//problemi su DB 
+
+        return true;
     }
 
 }
