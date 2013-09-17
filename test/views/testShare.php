a1dfc6d2defc4f5e47bf13891eed827619366efd
danielecaldelli <daniele.caldelli@gmail.com>
2013-09-17 10:23:29 +0200

    Pagina intermedia con redirect per i bottoni di share

---
+++ test/views/testShare.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta property="og:title" content="Il titolo della pagina di Share" />
+<meta property="og:description" content="La descrizione della pagina di Share" />
+<meta property="og:image" content="http://launchrock-assets.s3.amazonaws.com/logo-files/CM3BNZBC_1365245910190.png" />
+</head>
+<body>
+<script type="text/javascript">window.location="http://www.piombino2.it/page.php?id=1"</script>
+JavaScript redirect unsuccessful. Click <a href="http://www.piombino2.it/page.php?id=1">here</a> to continue.
+</body>
+</body>
+</html>
