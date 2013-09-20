4d81798db37f150bc6e306d019f52ea6284d50c1
danielecaldelli <daniele.caldelli@gmail.com>
2013-09-20 09:44:36 +0200

    rinominato il controller da profile.controller.php a love.controller.php e modificato il suo metodo decrementLove

---
+++ controllers/profile/love.controller.php
@@ -0,0 +1,140 @@
+<?php
+if (!defined('ROOT_DIR'))
+    define('ROOT_DIR', '../../');
+
+require_once ROOT_DIR . 'config.php';
+require_once CONTROLLERS_DIR . 'restController.php';
+require_once PARSE_DIR . 'parse.php';
+
+class ProfileController extends REST {
+
+    public function init() {
+		session_start();
+    }
+
+    public function incrementLove() {
+		try {
+			//if ($this->get_request_method() != 'POST' || !isset($_SESSION['currentUser'])) {
+			if ($this->get_request_method() != 'POST') {
+				$this->response('', 406);
+			}
+			$objectId = $_REQUEST['objectId'];
+			$classType = $_REQUEST['classType'];
+			switch ($classType) {
+				case 'Album':
+					require_once CLASSES_DIR . 'albumParse.class.php';
+					$albumParse = new AlbumParse();
+					$res = $albumParse->incrementAlbum($objectId, 'loveCounter', 1);
+					break;
+				case 'Comment':
+					require_once CLASSES_DIR . 'commentParse.class.php';
+					$commentParse = new CommentParse();
+					$res = $commentParse->incrementComment($objectId, 'loveCounter', 1);
+					break;
+				case 'Event':
+					require_once CLASSES_DIR . 'eventParse.class.php';
+					$eventParse = new EventParse();
+					$res = $eventParse->incrementEvent($objectId, 'loveCounter', 1);
+					break;
+				case 'Image':
+					require_once CLASSES_DIR . 'imageParse.class.php';
+					$imageParse = new ImageParse();
+					$res = $imageParse->incrementImage($objectId, 'loveCounter', 1);
+					break;
+				case 'Record':
+					require_once CLASSES_DIR . 'recordParse.class.php';
+					$recordParse = new RecordParse();
+					$res = $recordParse->incrementRecord($objectId, 'loveCounter', 1);
+					break;
+				case 'Song':
+					require_once CLASSES_DIR . 'songParse.class.php';
+					$songParse = new SongParse();
+					$res = $songParse->incrementSong($objectId, 'loveCounter', 1);
+					break;
+				case 'Status':
+					require_once CLASSES_DIR . 'statusParse.class.php';
+					$statusParse = new StatusParse();
+					$res = $statusParse->incrementStatus($objectId, 'loveCounter', 1);
+					break;
+				case 'Video':
+					require_once CLASSES_DIR . 'videoParse.class.php';
+					$videoParse = new VideoParse();
+					$res = $videoParse->incrementVideo($objectId, 'loveCounter', 1);
+					break;
+			}
+			
+			if (get_class($res) != 'Error') {
+				$this->response(array($res), 200);
+			} else {
+				$this->response(array($res), 503);
+			}
+			
+		} catch (Exception $e) {
+			$this->response(array($e), 503);
+		}
+    }
+	
+	public function decrementLove() {
+		try {
+			//if ($this->get_request_method() != 'POST' || !isset($_SESSION['currentUser'])) {
+			if ($this->get_request_method() != 'POST') {
+				$this->response('', 406);
+			}
+		
+			$objectId = $_REQUEST['objectId'];
+			$classType = $_REQUEST['classType'];
+			switch ($classType) {
+				case 'Album':
+					require_once CLASSES_DIR . 'albumParse.class.php';
+					$albumParse = new AlbumParse();
+					$res = $albumParse->decrementAlbum($objectId, 'loveCounter', 1);
+					break;
+				case 'Comment':
+					require_once CLASSES_DIR . 'commentParse.class.php';
+					$commentParse = new CommentParse();
+					$res = $commentParse->decrementComment($objectId, 'loveCounter', 1);
+					break;
+				case 'Event':
+					require_once CLASSES_DIR . 'eventParse.class.php';
+					$eventParse = new EventParse();
+					$res = $eventParse->decrementEvent($objectId, 'loveCounter', 1);
+					break;
+				case 'Image':
+					require_once CLASSES_DIR . 'imageParse.class.php';
+					$imageParse = new ImageParse();
+					$res = $imageParse->decrementImage($objectId, 'loveCounter', 1);
+					break;
+				case 'Record':
+					require_once CLASSES_DIR . 'recordParse.class.php';
+					$recordParse = new RecordParse();
+					$res = $recordParse->decrementRecord($objectId, 'loveCounter', 1);
+					break;
+				case 'Song':
+					require_once CLASSES_DIR . 'songParse.class.php';
+					$songParse = new SongParse();
+					$res = $songParse->decrementSong($objectId, 'loveCounter', 1);
+					break;
+				case 'Status':
+					require_once CLASSES_DIR . 'statusParse.class.php';
+					$statusParse = new StatusParse();
+					$res = $statusParse->decrementStatus($objectId, 'loveCounter', 1);
+					break;
+				case 'Video':
+					require_once CLASSES_DIR . 'videoParse.class.php';
+					$videoParse = new VideoParse();
+					$res = $videoParse->decrementVideo($objectId, 'loveCounter', 1);
+					break;
+			}
+			
+        	if (get_class($res) != 'Error') {
+				$this->response(array($res), 200);
+			} else {
+				$this->response(array($res), 503);
+			}
+			
+		} catch (Exception $e) {
+			$this->response(array($e), 503);
+		}
+
+	}
+?>
