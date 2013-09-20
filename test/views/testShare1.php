1c30677af1a2a9eb2809fab189eb6f5cbacb9fcf
danielecaldelli <daniele.caldelli@gmail.com>
2013-09-20 13:56:51 +0200

    Creati i nuovi file per i test dello Share

---
+++ test/views/testShare1.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Il titolo della pagina</title>
+<meta name="description" content="La descrizione della pagina" />
+</head>
+<body>
+Share di un Album<br />
+<!-- AddThis Button BEGIN -->
+<div class="addthis_toolbox addthis_default_style addthis_32x32_style"
+	 addThis:url="http://socialmusicdiscovering.com/script/wp_daniele/root/test/views/testShare2.php?classe=Album"
+	 addThis:title="Titolo della pagina di un album">
+<a class="addthis_button_facebook"></a>
+<a class="addthis_button_twitter"></a>
+<a class="addthis_button_google_plusone_share"></a>
+</div>
+<script type="text/javascript">
+	var addthis_config = {
+		"data_track_addressbar":true
+	};
+	var addthis_share = {
+		url_transforms : {
+			shorten: {
+				twitter: 'bitly'
+			}
+		}, 
+		shorteners : {
+			bitly : {}
+		}
+	}
+</script>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-522dd258579a55ca"></script>
+<!-- AddThis Button END -->
+<hr />
+Share di un Record<br />
+<!-- AddThis Button BEGIN -->
+<div class="addthis_toolbox addthis_default_style addthis_32x32_style"
+	 addThis:url="http://socialmusicdiscovering.com/script/wp_daniele/root/test/views/testShare2.php?classe=Record"
+	 addThis:title="Titolo della pagina di un record">
+<a class="addthis_button_facebook"></a>
+<a class="addthis_button_twitter"></a>
+<a class="addthis_button_google_plusone_share"></a>
+</div>
+<script type="text/javascript">
+	var addthis_config = {
+		"data_track_addressbar":true
+	};
+	var addthis_share = {
+		url_transforms : {
+			shorten: {
+				twitter: 'bitly'
+			}
+		}, 
+		shorteners : {
+			bitly : {}
+		}
+	}
+</script>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-522dd258579a55ca"></script>
+<!-- AddThis Button END -->
+
+</body>
+</html>
