adb238b2060591e4ca4f7d9a93ceb14cf45be151
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-09-26 11:34:20 +0200

    prosecuzione funzioni di accetazione e rifiuto di una richiesta di relazione

--- controllers/relation.controller.php
+++ controllers/relation.controller.php
@@ -43,7 +43,8 @@ class RelationController extends REST {
 	 * \todo    usare la sessione
 	 */
 	public function acceptRelationRequest(){
-	#TODO
+		require_once ROOT_DIR . 'services/mail.service.php'; //meglio definire una SERVICE_DIR
+		#TODO
 		//simulo che l'utente in sessione sia GuUAj83MGH
 		require_once CLASSES_DIR . 'user.class.php';
 		$currentUser = new User('SPOTTER');
@@ -53,7 +54,35 @@ class RelationController extends REST {
 			//if ($this->get_request_method() != 'POST' || !isset($_SESSION['currentUser'])) {
 			if ($this->get_request_method() != 'POST') {
 				$this->response('', 406);
-			}			
+			}	
+			$activityId = $_REQUEST['objectId'];            //mettere valori corretti 
+			$fromUserObjectId = $_REQUEST['objectId'];      //mettere valori corretti 
+			$toUserObjectId = $_REQUEST['objectId'];        //mettere valori corretti 
+			$fromUserType = $_REQUEST['fromUserType'];		//mettere valori corretti
+			$toUserType = $_REQUEST['toUserType'];          //mettere valori corretti
+			
+			$activityP = new ParseActivity();
+			$activityP->updateField($activityId, 'status', 'A'); //passa da pending a accettata
+			$activityP->updateField($activityId, 'read', true); //passa da non letta  a letta
+			
+			$fromUserP = new UserParse();
+			$toUserP = new UserParse();
+			switch($fromUserType){
+				case 'SPOTTER':
+					if($toUserType == 'SPOTTER'){
+						//friendship
+					} else {
+						//following;	
+					}
+				break;
+				default : //le relazioni saranno uguali come richiesta per VENUE e JAMMER
+					if($toUserType == 'SPOTTER'){
+						$this->response(array(RELDENIED), 200);
+					} else {
+						//collaboration
+					}
+				break;
+			}
 			$this->response(array($res), 200);
 						
 		} catch (Exception $e) {
@@ -77,7 +106,11 @@ class RelationController extends REST {
 			//if ($this->get_request_method() != 'POST' || !isset($_SESSION['currentUser'])) {
 			if ($this->get_request_method() != 'POST') {
 				$this->response('', 406);
-			}			
+			}
+			$activityId = $_REQUEST['objectId'];            //mettere valori corretti 		
+			$activityP = new ParseActivity();
+			$activityP->updateField($activityId, 'status', 'R'); //passa da pending a refused
+			$activityP->updateField($activityId, 'read', true); //passa da non letta  a letta
 			$this->response(array($res), 200);
 						
 		} catch (Exception $e) {
@@ -115,7 +148,7 @@ class RelationController extends REST {
 	 * \todo    usare la sessione
 	 */
     public function sendRelationRequest() {
-		
+		require_once ROOT_DIR . 'services/mail.service.php'; //meglio definire una SERVICE_DIR
 		#TODO
 		//simulo che l'utente in sessione sia GuUAj83MGH
 
@@ -127,10 +160,10 @@ class RelationController extends REST {
 			if ($this->get_request_method() != 'POST') {
 				$this->response('', 406);
 			}
-			$fromUserObjectId = $_REQUEST['objectId'];
-			$toUserObjectId = $_REQUEST['objectId'];
-			$fromUserType = $_REQUEST['fromUserType'];
-			$toUserType = $_REQUEST['toUserType'];
+			$fromUserObjectId = $_REQUEST['objectId'];		//mettere valori corretti
+			$toUserObjectId = $_REQUEST['objectId'];        //mettere valori corretti
+			$fromUserType = $_REQUEST['fromUserType'];		//mettere valori corretti
+			$toUserType = $_REQUEST['toUserType'];          //mettere valori corretti
 			
 			//se l'utente cerca di avere relazione con se stesso esco con risposta a schermo
 			if ($fromUserObjectId == $toUserObjectId ){
@@ -142,7 +175,7 @@ class RelationController extends REST {
             $activity->setActive(true);
 			$activity->setCounter(0);
             $activity->setFromUser($currentUser->getObjectId());
-            $activity->setRead(true);
+            $activity->setRead(false);
             $activity->setStatus("P");
             
 			switch($fromUserType){
