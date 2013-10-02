c86d1256fa5976942cb06020b6c108f954a6ba47
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-10-02 16:55:25 +0200

    sorgente stringhe dei controller

--- languages/controllers/en.controllers.lang.php
+++ languages/controllers/en.controllers.lang.php
@@ -1,4 +1,35 @@
 <?php
 
-
+define('POSTSAVED', 'Your post has been saved');
+define('SHORTPOST', 'Your post il too short | lenght:');
+define('LONGPOST', 'Your post il too long | lenght:');
+define('NOTOUSER', 'No toUser specified');
+define('NOFROMUSER', "No fromUser specified");
+define('NOPOST', 'No post specified');
+define('NOCOMMENT', 'No comment specified');
+define('SHORTPOST', 'Your comment is too short | lenght:');
+define('LONGPOST', 'Your comment is too long | lenght:');
+define('COMMENTSAVED', 'Your comment has been saved');
+define('ROLLKO', 'ROLLBACK KO');
+define('ROLLOK', 'ROLLBACK OK');
+define('CND', 'Cannot delete this element: you are not its owner!');
+define('SBJ', 'Account delation confirmed');
+define('DELERR', 'Error deleting this element');
+define('LOVEPLUSERR', 'Error incrementing Love');
+define('LOVEMINUSERR', 'Error decrementing Love');
+define('NOMESSAGE', "No message specified");
+define('SHORTMESSAGE', 'Your message is too short | lenght:');
+define('MESSAGESAVED', 'Your message has been sent');
+define('RELDENIED', 'You are not allowed to send a relationship request to this user!');
+define('SELF', 'Don&apos;t be shy, ask someone else to be your friend or your collaborator!');
+define('SBJ', 'Relation Request');
+define('SBJOK', 'Relation Request Accepted');
+define('RELSAVED', 'Your request has been sent correctly');
+define('NORELDEL', 'No relation to remove');
+define('SBJE', 'Your Event has been reviewed');
+define('SBJR', 'Your Record has been reviewed');
+define('NOREW', 'No review specified');
+define('SHORTREW', 'Your review is too short | lenght:');
+define('LONGREW', 'Your review is too long | lenght:');
+define('REWSAVED', 'Your review has been saved');
 ?>
--- string.php
+++ string.php
@@ -18,6 +18,39 @@ define('NOFOLLOWERS', 'NON HAI ANCORA FOLLOWERS');
 define('NOVENUE', 'NESSUNA COLLABORAZIONE CON VENUE AL MOMENTO');
 define('NOJAMMER', 'NESSUNA COLLABORAZIONE CON JAMMER AL MOMENTO');
 
+//CONTROLLERS
+define('POSTSAVED', 'Il tuo post è stato salvato');
+define('SHORTPOST', 'Dimensione post troppo corta | lungh:');
+define('LONGPOST', 'Dimensione post troppo lunga | lungh:');
+define('NOTOUSER', 'toUser non specificato');
+define('NOFROMUSER', "fromUser  non specificato");
+define('NOPOST', 'Post non specificato');
+define('NOCOMMENT', 'Commento non specificato');
+define('SHORTCOMMENT', 'Dimensione commento troppo corta | lungh:');
+define('LONGCOMMENT', 'Dimensione commment troppo lunga | lungh:');
+define('COMMENTSAVED', 'Il tuo commento è stato salvato');
+define('ROLLKO', 'ROLLBACK KO');
+define('ROLLOK', 'ROLLBACK OK');
+define('CND', 'Impossibile eliminare questo elemento: non sei il proprietario');
+define('SBJ', 'Il tuo account è stato cancellato');
+define('DELERR', 'Errore in fase di cancellazione');
+define('LOVEPLUSERR', 'Errore incrementando Love');
+define('LOVEMINUSERR', 'Error decrementando Love');
+define('NOMESSAGE', 'Messaggio non specificato');
+define('MESSAGESAVED', 'Messaggio inviato');
+define('RELDENIED', 'Invio relazione negato');
+define('SELF', 'Non essere timido, invia la richiesta di relazione ad altri!');
+define('SBJ', 'Richiesta di relazione');
+define('SBJOK', 'Richiesta di relazione accettata');
+define('RELSAVED', 'La tua richiesta è stata correttamente inviata');
+define('NORELDEL', 'Nessuna relazione da cancellare');
+define('SBJE', 'Il tuo evento ha ricevuto una recensione');
+define('SBJR', 'Il tuo record ha ricevuto una recensione');
+define('NOREW', 'Nessuna recensione specificata');
+define('SHORTREW', 'Dimensione recensione troppo corta | lungh:');
+define('LONGREW', 'Dimensione recensione troppo lunga | lungh:');
+define('REWSAVED', 'Recensione salvata con successo');
+
 //USER
 define('DEFAVATAR', '/var/www/vhosts/socialmusicdiscovering.com/httpdocs/images/default/dafultAvatar.jpg');
 define('DEFTHUMB', '/var/www/vhosts/socialmusicdiscovering.com/httpdocs/images/default/dafultAvatarThumb.jpg');
