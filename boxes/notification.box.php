fc41d6d2decf36eae738e57f58e2a687e694bb71
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-10-02 10:47:51 +0200

    aggiunti controlli

--- boxes/notification.box.php
+++ boxes/notification.box.php
@@ -39,11 +39,11 @@ class NotificationForDetailedList {
     }
 
 }
+
 /**
  * \brief	NotificationBox class 
  * \details	box class to pass info to the view for the notification
  */
- 
 class NotificationBox {
 
     public $notificationArray;
@@ -121,163 +121,174 @@ class NotificationBox {
 	return $notificationBox;
     }
 
-   /**
+    /**
      * \fn	initForMessageList($objectId,$type)
      * \brief	Init NotificationBox instance for message list
      * \param	$objectId
      * \return	infoBox
      */
     public function initForMessageList($objectId) {
-	  $notificationBox = new NotificationBox();
-      
-	  $notificationBox->invitationCounter = NDB;
-	  $notificationBox->messageCounter = NDB;
-	  $notificationBox->relationCounter = NDB;
-      $notificationBox->notificationArray = NDB;
-	  
-	  $messageArray = array();
-      
-	  $activity = new ActivityParse();
-	  $activity->wherePointer('toUser', 'User', $objectId);
-	  $activity->where('type', 'MESSAGESENT');
-	  $activity->where('read', false);
-	  $activity->where('active', true);
-	  $messages = $activity->getActivities();
-	  if (get_class($messages) == 'Error') {
-	      echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $messages->getErrorMessage() . '<br/>';
-	  } else {
-	      foreach ($messages as $message) {
-			$createdAt = $message->getCreatedAt();
-			$userId = $message->getFromUser();
-			$userP = new UserParse();
-			$user = $userP->getUser($userId);
-			
-			$objectId = $user->getObjectId();
-			$thumbnail = $user->getProfileThumbnail();
-			$type = $user->getType();
-			$encodedUsername = $user->getUsername();
-			$username = parse_decode_string($encodedUsername);
-			$userInfo = new UserInfo($objectId,$thumbnail, $type, $username);
-			
-			$notificationInfo = new NotificationForDetailedList($createdAt, $userInfo);
-			array_push($messageArray, $notificationInfo);
-			}
-			if(empty($messageArray)){
-				$notificationBox->messageArray = NODATA;
-			} else {
-				$notificationBox->messageArray = $messageArray;
-			}
-	  }
-	  return $notificationBox;
-    } 
+	$notificationBox = new NotificationBox();
 
-	/**
+	$notificationBox->invitationCounter = NDB;
+	$notificationBox->messageCounter = NDB;
+	$notificationBox->relationCounter = NDB;
+	$notificationBox->notificationArray = NDB;
+
+	$messageArray = array();
+
+	$activity = new ActivityParse();
+	$activity->wherePointer('toUser', 'User', $objectId);
+	$activity->where('type', 'MESSAGESENT');
+	$activity->where('read', false);
+	$activity->where('active', true);
+	$messages = $activity->getActivities();
+	if (get_class($messages) == 'Error') {
+	    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $messages->getErrorMessage() . '<br/>';
+	} else {
+	    foreach ($messages as $message) {
+		$createdAt = $message->getCreatedAt();
+		$userId = $message->getFromUser();
+		$userP = new UserParse();
+		$user = $userP->getUser($userId);
+		if (get_class($user) == 'Error') {
+		    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $user->getErrorMessage() . '<br/>';
+		    return $user;
+		}
+
+		$objectId = $user->getObjectId();
+		$thumbnail = $user->getProfileThumbnail();
+		$type = $user->getType();
+		$encodedUsername = $user->getUsername();
+		$username = parse_decode_string($encodedUsername);
+		$userInfo = new UserInfo($objectId, $thumbnail, $type, $username);
+
+		$notificationInfo = new NotificationForDetailedList($createdAt, $userInfo);
+		array_push($messageArray, $notificationInfo);
+	    }
+	    if (empty($messageArray)) {
+		$notificationBox->messageArray = NODATA;
+	    } else {
+		$notificationBox->messageArray = $messageArray;
+	    }
+	}
+	return $notificationBox;
+    }
+
+    /**
      * \fn	initForEventList($objectId,$type)
      * \brief	Init NotificationBox instance for event list
      * \param	$objectId
      * \return	infoBox
      */
-	
-	public function initForEventList($objectId){
-		$notificationBox = new NotificationBox();
-		
-		$notificationBox->invitationCounter = NDB;
-		$notificationBox->messageCounter = NDB;
-		$notificationBox->messageArray = NDB;
-		$notificationBox->relationCounter = NDB;
-		
-		$invitationArray = array();
-	
-	    $activity = new ActivityParse();
-	    $activity->wherePointer('toUser', 'User', $objectId);
-	    $activity->where('type', 'INVITED');
-	    $activity->where('active', true);
-	    $invitations = $activity->getActivity();	
-	    if (get_class($invitation) == 'Error') {
-	        echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $invitations->getErrorMessage() . '<br/>';
+    public function initForEventList($objectId) {
+	$notificationBox = new NotificationBox();
+
+	$notificationBox->invitationCounter = NDB;
+	$notificationBox->messageCounter = NDB;
+	$notificationBox->messageArray = NDB;
+	$notificationBox->relationCounter = NDB;
+
+	$invitationArray = array();
+
+	$activity = new ActivityParse();
+	$activity->wherePointer('toUser', 'User', $objectId);
+	$activity->where('type', 'INVITED');
+	$activity->where('active', true);
+	$invitations = $activity->getActivity();
+	if (get_class($invitations) == 'Error') {
+	    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $invitations->getErrorMessage() . '<br/>';
+	} else {
+	    foreach ($invitations as $invitation) {
+		$createdAt = $invitation->getCreatedAt();
+		$userId = $invitation->getFromUser();
+		$userP = new UserParse();
+		$user = $userP->getUser($userId);
+		if (get_class($user) == 'Error') {
+		    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $user->getErrorMessage() . '<br/>';
+		    return $user;
+		}
+
+		$objectId = $user->getObjectId();
+		$thumbnail = $user->getProfileThumbnail();
+		$type = $user->getType();
+		$encodedUsername = $user->getUsername();
+		$username = parse_decode_string($encodedUsername);
+		$userInfo = new UserInfo($objectId, $thumbnail, $type, $username);
+		$notificationInfo = new NotificationForDetailedList($createdAt, $userInfo);
+		array_push($invitationArray, $notificationInfo);
+	    }
+	    if (empty($invitationArray)) {
+		$notificationBox->notificationArray = NODATA;
 	    } else {
-	        foreach($invitations as $invitation){
-				$createdAt = $invitation->getCreatedAt();
-				$userId = $invitation->getFromUser();
-				$userP = new UserParse();
-				$user = $userP->getUser($userId);
-				
-				$objectId = $user->getObjectId();
-				$thumbnail = $user->getProfileThumbnail();
-				$type = $user->getType();
-				$encodedUsername = $user->getUsername();
-				$username = parse_decode_string($encodedUsername);
-				$userInfo = new UserInfo($objectId,$thumbnail, $type, $username);
-				$notificationInfo = new NotificationForDetailedList($createdAt, $userInfo);
-				array_push($invitationArray, $notificationInfo);
-			}
-			if(empty($invitationArray)){
-				$notificationBox->notificationArray = NODATA;
-			} else {
-				$notificationBox->notificationArray = $invitationArray;
-			}
-	    }   
-	    return $notificationBox;
+		$notificationBox->notificationArray = $invitationArray;
+	    }
 	}
-	
-	/**
+	return $notificationBox;
+    }
+
+    /**
      * \fn	initForRelationList($objectId,$type)
      * \brief	Init NotificationBox instancef for relation list
      * \param	$objectId
-	 * \param	$type
+     * \param	$type
      * \return	infoBox
      */
-	
-	 public function initForRelationList($objectId,$type) {
-	  $notificationBox = new NotificationBox();
-	  
-	  $notificationBox->invitationCounter = NDB;
-	  $notificationBox->messageCounter = NDB;
-	  $notificationBox->messageArray = NDB;
-	  $notificationBox->relationCounter = NDB;
-	  
-	  $relationArray = array();
-	  
-	  $activity = new ActivityParse();
-	  $activity->wherePointer('toUser', '_User', $objectId);
-	  switch ($type) {
-		case 'SPOTTER':
-			$activity->where('type', 'FRIENDREQUEST');
+    public function initForRelationList($objectId, $type) {
+	$notificationBox = new NotificationBox();
+
+	$notificationBox->invitationCounter = NDB;
+	$notificationBox->messageCounter = NDB;
+	$notificationBox->messageArray = NDB;
+	$notificationBox->relationCounter = NDB;
+
+	$relationArray = array();
+
+	$activity = new ActivityParse();
+	$activity->wherePointer('toUser', '_User', $objectId);
+	switch ($type) {
+	    case 'SPOTTER':
+		$activity->where('type', 'FRIENDREQUEST');
 		break;
-		default:
-			$activityTypes = array(array('type' => 'COLLABORATIONREQUEST'), array('type' => 'FOLLOWING'));
-			$activity->whereOr($activityTypes);
+	    default:
+		$activityTypes = array(array('type' => 'COLLABORATIONREQUEST'), array('type' => 'FOLLOWING'));
+		$activity->whereOr($activityTypes);
 		break;
+	}
+	$activity->where('status', 'W');
+	$activity->where('active', true);
+	$relations = $activity->getActivities();
+	if (get_class($relations) == 'Error') {
+	    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $relations->getErrorMessage() . '<br/>';
+	} else {
+	    foreach ($relations as $relation) {
+		$createdAt = $relation->getCreatedAt();
+		$userId = $relation->getFromUser();
+		$userP = new UserParse();
+		$user = $userP->getUser($userId);
+		if (get_class($user) == 'Error') {
+		    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $user->getErrorMessage() . '<br/>';
+		    return $user;
 		}
-	  $activity->where('status', 'W');
-	  $activity->where('active', true);
-	  $relations = $activity->getActivities();
-		if (get_class($relations) == 'Error') {
-		    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $relations->getErrorMessage() . '<br/>';
-		} else {
-		    foreach($relations as $relation){
-				$createdAt = $relation->getCreatedAt();
-				$userId = $relation->getFromUser();
-				$userP = new UserParse();
-				$user = $userP->getUser($userId);
-					
-				$objectId = $user->getObjectId();
-				$thumbnail = $user->getProfileThumbnail();
-				$type = $user->getType();
-				$encodedUsername = $user->getUsername();
-				$username = parse_decode_string($encodedUsername);
-				$userInfo = new UserInfo($objectId,$thumbnail, $type, $username);
-				$notificationInfo = new NotificationForDetailedList($createdAt, $userInfo);
-				array_push($relationArray, $notificationInfo);
-			}
-			if(empty($relationArray)){
-				$notificationBox->notificationArray = NODATA;
-			} else {
-				$notificationBox->notificationArray = $relationArray;
-			}
-		}
-		return $notificationBox;
-	  }	
+
+		$objectId = $user->getObjectId();
+		$thumbnail = $user->getProfileThumbnail();
+		$type = $user->getType();
+		$encodedUsername = $user->getUsername();
+		$username = parse_decode_string($encodedUsername);
+		$userInfo = new UserInfo($objectId, $thumbnail, $type, $username);
+		$notificationInfo = new NotificationForDetailedList($createdAt, $userInfo);
+		array_push($relationArray, $notificationInfo);
+	    }
+	    if (empty($relationArray)) {
+		$notificationBox->notificationArray = NODATA;
+	    } else {
+		$notificationBox->notificationArray = $relationArray;
+	    }
+	}
+	return $notificationBox;
+    }
+
 }
 
 ?>
