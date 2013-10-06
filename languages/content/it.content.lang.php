74ba2c8fe292b533119af30ffd1eb7db9934d639
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-10-06 12:53:05 +0200

    impostazione delle cartelle per traduzione content e views

---
+++ languages/content/it.content.lang.php
@@ -0,0 +1,109 @@
+<?php
+
+/* ! \par Info Generali:
+ *  \author    Luca Gianneschi
+ *  \version   1.0
+ *  \date      2013
+ *  \copyright Jamyourself.com 2013
+ *
+ *  \par       Info:
+ *  \brief     Italian strings for controllers
+ *  \warning
+ *  \bug
+ *  \todo
+ *
+ *  
+ *  
+ */
+
+$controllers = array(
+    'POSTSAVED' => 'Il tuo post è stato salvato',
+    'SHORTPOST' => 'Dimensione post troppo corta | lungh:',
+    'LONGPOST' => 'Dimensione post troppo lunga | lungh:',
+    'NOTOUSER' => 'toUser non specificato',
+    'NOFROMUSER' => "fromUser  non specificato",
+    'NOPOST' => 'Post non specificato',
+    'NOCOMMENT' => 'Commento non specificato',
+    'SHORTCOMMENT' => 'Dimensione commento troppo corta | lungh:',
+    'LONGCOMMENT' => 'Dimensione commment troppo lunga | lungh:',
+    'COMMENTSAVED' => 'Il tuo commento è stato salvato',
+    'ROLLKO' => 'ROLLBACK KO',
+    'ROLLOK' => 'ROLLBACK OK',
+    'CND' => 'Impossibile eliminare questo elemento: non sei il proprietario',
+    'SBJ' => 'Il tuo account è stato cancellato',
+    'DELERR' => 'Errore in fase di cancellazione',
+    'LOVEPLUSERR' => 'Errore incrementando Love',
+    'LOVEMINUSERR' => 'Error decrementando Love',
+    'NOMESSAGE' => 'Messaggio non specificato',
+    'SHORTMESSAGE' => 'Dimensione messaggio troppo corta | lungh:',
+    'MESSAGESAVED' => 'Messaggio inviato',
+    'RELDENIED' => 'Invio relazione negato',
+    'SELF' => 'Non essere timido, invia la richiesta di relazione ad altri!',
+    'SBJ' => 'Richiesta di relazione',
+    'SBJOK' => 'Richiesta di relazione accettata',
+    'RELSAVED' => 'La tua richiesta è stata correttamente inviata',
+    'NORELDEL' => 'Nessuna relazione da cancellare',
+    'SBJE' => 'Il tuo evento ha ricevuto una recensione',
+    'SBJR' => 'Il tuo record ha ricevuto una recensione',
+    'NOREW' => 'Nessuna recensione specificata',
+    'SHORTREW' => 'Dimensione recensione troppo corta | lungh:',
+    'LONGREW' => 'Dimensione recensione troppo lunga | lungh:',
+    'REWSAVED' => 'Recensione salvata con successo',
+    'NOLOGIN' => 'Credenziali di accesso non valide',
+    'OKLOGIN' => 'Login eseguito',
+    'NOLOGOUT' => 'Impossibile effettuare logout',
+    'OKLOGOUT' => 'Logout Eseguito',
+    '100' => 'Continua',
+    '101' => 'Cambio Di Protocolli',
+    '200' => 'OK',
+    '201' => 'Creato',
+    '202' => 'Accettato',
+    '203' => 'Informazione Non Interessante',
+    '204' => 'Nessun Contenuto',
+    '205' => 'Reset del contenuto',
+    '206' => 'Contenuto Parziale',
+    '300' => 'Scelta Multipla',
+    '301' => 'Spostato Definitivamente',
+    '302' => 'Trovato',
+    '303' => 'Vedi Altro',
+    '304' => 'Non MOdificato',
+    '305' => 'Usa Proxy',
+    '306' => '(Non Usato)',
+    '307' => 'Redirect Temporaneo',
+    '400' => 'Richiesta Non Valida',
+    '401' => 'Non Autorizzato',
+    '402' => 'Richiesto Pagamento',
+    '403' => 'Non Consentito',
+    '404' => 'Non Trovato',
+    '405' => 'Metodo Non Consentito',
+    '406' => 'Non Accettato',
+    '407' => 'Richiesta Autenticazione Proxy',
+    '408' => 'Richiesta Time Out',
+    '409' => 'Conflitto',
+    '410' => 'Andato',
+    '411' => 'Lunghezza richiesta',
+    '412' => 'Requisito Non Soddisfatto',
+    '413' => 'Request Entity Troppo Estesa',
+    '414' => 'Request-URI Troppo Lungo',
+    '415' => 'Media Non Supportato',
+    '416' => 'Requested Range Not Satisfiable',
+    '417' => 'Richiesta Fallita',
+    '500' => 'Errore Server',
+    '501' => 'Non Implementato',
+    '502' => 'Gateway Non Valido',
+    '503' => 'Servizio Non Disponibile',
+    '504' => 'Gateway Timeout',
+    '505' => 'Versione HTTP Non Supportata'
+);
+
+$mail_files = array(
+    'USERDELETED' => 'it.userDeletion.html',
+    'FRINDSHIPREQUESTEMAIL' => 'it.friendshipRequest.html',
+    'COLLABORATIONREQUESTEMAIL' => 'it.collaborationRequest.html',
+    'FRIENDSHIPACCEPTEDEMAIL' => 'it.friendshipRequestAccepted.html',
+    'COLLABORATIONACCEPTEDEMAIL' => 'it.collaborationRequestAccepted.html',
+    'FOLLOWINGEMAIL' => 'it.following.html',
+    'EVENTREVIEWEMAIL' => 'it.eventReviewReceived.html',
+    'RECORDREVIEWEMAIL' => 'it.recordReviewReceived.html'
+);
+?>
