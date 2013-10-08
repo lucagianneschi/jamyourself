da3ca463f35d315a86f661c86bf44c5cfe0bfd24
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-10-08 17:49:03 +0200

    aggiunto file di request

---
+++ controllers/request/playlistRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+/* ! \par		Info Generali:
+ * \author		Luca Gianneschi
+ * \version		1.0
+ * \date		2013
+ * \copyright	Jamyourself.com 2013
+ * \par			Info Classe:
+ * \brief		file di request per il controller playlist
+ * \par			Commenti:
+ * \warning
+ * \bug
+ *
+ */
+ 
+if (!defined('ROOT_DIR'))
+    define('ROOT_DIR', '../../');
+	
+require_once ROOT_DIR . 'config.php';
+require_once CONTROLLERS_DIR . 'playlist.controller.php';
+
+try{
+	session_start();
+	$controller = new PlaylistController();
+	$controller->processApi();    
+}catch(Exception $e){
+   
+}
+
+
+?>
