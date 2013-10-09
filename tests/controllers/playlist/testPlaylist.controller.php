fdfafeae74e77635beb435a2f681c635e34ba9ca
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-10-10 01:03:26 +0200

    corretti errori sintassi

--- tests/controllers/playlist/testPlaylist.controller.php
+++ tests/controllers/playlist/testPlaylist.controller.php
@@ -3,10 +3,10 @@
     <head>
 	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 	<script type="text/javascript">
-	    function playlist(playlistId, songId, opType,fromUserId) {
+	    function playlist(playlistId, songId, opType, fromUserId) {
 
 		var json_playlist = {};
-		if (opType == 'add') {
+		if (opType === 'add') {
 		    json_playlist.request = 'addSong';
 		} else {
 		    json_playlist.request = 'removeSong';
@@ -15,7 +15,7 @@
 		json_playlist.playlistId = playlistId;
 		json_playlist.songId = songId;
 		json_playlist.fromUserId = fromUserId;
-		
+
 		$.ajax({
 		    type: "POST",
 		    url: "../../../controllers/request/playlistRequest.php",
@@ -41,13 +41,13 @@
     <body>
 	Cliccando i bottoni si aggiungono o tolgono canzoni prescelte sulla playlist di SPATAFORA<br />
 	<br />
-	<button type="button" onclick="playlist('EWlkBSXQJt', 'nBF3KVDGxZ', 'add','fromUserId','GuUAj83MGH')">AGGIUNGI Song nBF3KVDGxZ alla playslist di SPATAFORA </button>
+	<button type="button" onclick="playlist('EWlkBSXQJt', 'nBF3KVDGxZ', 'add', 'GuUAj83MGH')">AGGIUNGI Song nBF3KVDGxZ alla playslist di SPATAFORA </button>
 	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-	<button type="button" onclick="playlist('EWlkBSXQJt', 'nBF3KVDGxZ', 'remove','GuUAj83MGH')">RIMUOVI Song nBF3KVDGxZ alla playslist di SPATAFORA </button>
+	<button type="button" onclick="playlist('EWlkBSXQJt', 'nBF3KVDGxZ', 'remove', 'GuUAj83MGH')">RIMUOVI Song nBF3KVDGxZ alla playslist di SPATAFORA </button>
 	&nbsp;<hr>
-	<button type="button" onclick="playlist('EWlkBSXQJt', 'MSJfcWb9Qk', 'add','GuUAj83MGH')">AGGIUNGI Song MSJfcWb9Qk alla playslist di SPATAFORA </button>
+	<button type="button" onclick="playlist('EWlkBSXQJt', 'MSJfcWb9Qk', 'add', 'GuUAj83MGH')">AGGIUNGI Song MSJfcWb9Qk alla playslist di SPATAFORA </button>
 	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-	<button type="button" onclick="playlist('EWlkBSXQJt', 'MSJfcWb9Qk', 'remove','GuUAj83MGH')">RIMUOVI Song MSJfcWb9Qk alla playslist di SPATAFORA </button>
+	<button type="button" onclick="playlist('EWlkBSXQJt', 'MSJfcWb9Qk', 'remove', 'GuUAj83MGH')">RIMUOVI Song MSJfcWb9Qk alla playslist di SPATAFORA </button>
 	&nbsp;<hr>
     </body>
 </html>
