65f35cfe55d87d6afc022a68ac5da48640d1bc9c
danielecaldelli <daniele.caldelli@gmail.com>
2013-10-10 14:11:20 +0200

    Corretti alcuni bug

--- controllers/message.controller.php
+++ controllers/message.controller.php
@@ -43,8 +43,8 @@ class MessageController extends REST {
      * \brief   load config file for the controller
      */
     function __construct() {
-	parent::__construct();
-	$this->config = json_decode(file_get_contents(CONTROLLERS_DIR . "config/message.config.json"), false);
+		parent::__construct();
+		$this->config = json_decode(file_get_contents(CONTROLLERS_DIR . "config/message.config.json"), false);
     }
 
     /**
@@ -52,7 +52,7 @@ class MessageController extends REST {
      * \brief   start the session
      */
     public function init() {
-	session_start();
+		session_start();
     }
 
     /**
@@ -60,130 +60,129 @@ class MessageController extends REST {
      * \brief   save a message an the related activity
      * \todo    usare la sessione
      */
-    public function sendMessage() {
-	
-	global $controllers;
-	#TODO
-	//in questa fase di debug, il fromUser lo passo staticamente e non lo recupero dalla session
-	//questa sezione prima del try-catch dovrà sparire
-	$fromUser = new User('SPOTTER');
-	$fromUser->setObjectId('GuUAj83MGH');
+	public function sendMessage() {
+		global $controllers;
+		
+		#TODO
+		//in questa fase di debug, il fromUser lo passo staticamente e non lo recupero dalla session
+		//questa sezione prima del try-catch dovrà sparire
+		$fromUser = new User('SPOTTER');
+		$fromUser->setObjectId('GuUAj83MGH');
+		
+		try {
 
-	try {
+			//controllo la richiesta
+			//if ($this->get_request_method() != 'POST' || !isset($_SESSION['currentUser'])) {
+			if ($this->get_request_method() != "POST") {
+				$this->response('', 406);
+			}
 
-	    //controllo la richiesta
-	    //if ($this->get_request_method() != 'POST' || !isset($_SESSION['currentUser'])) {
-	    if ($this->get_request_method() != "POST") {
-		$this->response('', 406);
-	    }
+			//recupero l'utente che effettua il commento
+			//$currentUser = $_SESSION['currentUser'];
+			
+			//controllo i parametri
+			if (!isset($this->request['text'])) {
+				$this->response(array('status' => "Bad Request", "msg" => $controllers['NOMESSAGE']), 400);
+			} elseif (!isset($this->request['toUser'])) {
+				$this->response(array('status' => "Bad Request", "msg" => $controllers['NOTOUSER']), 400);
+			}
+			
+			//recupero gli utenti fromUser, toUser e il messaggio
+			//$fromUser = $_SESSION['currentUser'];
+			$toUserObjectId = $this->request['toUser'];
+			$fromUserObjectId = $fromUser->getObjectId();
+			$text = $this->request['text'];
 
-	    //recupero l'utente che effettua il commento
-	    //$currentUser = $_SESSION['currentUser'];
-	    //controllo i parametri
-	    if (!isset($this->request['text'])) {
-		$this->response(array('status' => "Bad Request", "msg" => $controllers['NOMESSAGE']), 400);
-	    } elseif (!isset($this->request['toUser'])) {
-		$this->response(array('status' => "Bad Request", "msg" => $controllers['NOTOUSER']), 400);
-	    } elseif (!isset($this->request['fromUser'])) {
-		$this->response(array('status' => "Bad Request", "msg" => $controllers['NOFROMUSER']), 400);
-	    }
+			if (strlen($text) < $this->config->minMessageSize) {
+				$this->response(array($controllers['SHORTMESSAGE'] . strlen($text)), 400);
+			}
 
-	    $text = $_REQUEST['text'];
-	    $toUserObjectId = $this->request['toUser'];
-	    $fromUserObjectId = $this->request['fromUser'];
+			$message = new Comment();
+			$message->setActive(true);
+			$message->setAlbum(null);
+			$message->setComment(null);
+			$message->setCommentCounter(0);
+			$message->setCommentators(null);
+			$message->setComments(null);
+			$message->setCounter(0);
+			$message->setEvent(null);
+			$message->setImage(null);
+			$message->setLocation(null);
+			$message->setLoveCounter(0);
+			$message->setLovers(null);
+			$message->setOpinions(null);
+			$message->setRecord(null);
+			$message->setShareCounter(0);
+			$message->setStatus(null);
+			$message->setTags(null);
+			$encodedText = parse_encode_string($text);
+			$message->setText($encodedText);
+			$message->setTitle(null);
+			$message->setType('M');
+			$message->setVideo(null);
+			$message->setVote(null);
 
-	    if (strlen($text) < $this->config->minMessageSize) {
-		$this->response(array($controllers['SHORTMESSAGE'] . strlen($text)), 200);
-	    }
+			#TODO
+			//$message->setFromUser($currentUser);
+			$message->setFromUser($fromUserObjectId);
 
-	    $message = new Comment();
-	    $message->setActive(true);
-	    $message->setAlbum(null);
-	    $message->setComment(null);
-	    $message->setCommentCounter(0);
-	    $message->setCommentators(null);
-	    $message->setComments(null);
-	    $message->setCounter(0);
-	    $message->setEvent(null);
-	    $message->setImage(null);
-	    $message->setcation(null);
-	    $message->setLoveCounter(0);
-	    $message->setLovers(null);
-	    $message->setOpinions(null);
-	    $message->setRecord(null);
-	    $message->setShareCounter(0);
-	    $message->setStatus(null);
-	    $message->setTags(null);
-	    $encodedText = parse_encode_string($text);
-	    $message->setText($encodedText);
-	    $message->setTitle(null);
-	    $message->setType('M');
-	    $message->setVideo(null);
-	    $message->setVote(null);
+			//$userParse = new UserParse();
+			//$toUser = $userParse->getUser($toUserObjectId);
+			$message->setToUser($toUserObjectId);
 
-	    #TODO
-	    //$message->setFromUser($currentUser);
-	    $message->setFromUser($fromUserObjectId);
+			//imposto i valori per il salvataggio dell'activity collegata al post
+			$activity = new Activity();
+			$activity->setActive(true);
+			$activity->setAccepted(true);
+			$activity->setAlbum(null);
+			$activity->setComment(null);
+			$activity->setCounter(0);
+			$activity->setEvent(null);
 
-	    //$userParse = new UserParse();
-	    //$toUser = $userParse->getUser($this->request['fromUser']);
-	    $message->setToUser($toUserObjectId);
+			#TODO
+			//$activity->setFromUser($currentUser);
+			$activity->setFromUser($fromUser->getObjectId());
+			$activity->setImage(null);
+			$activity->setPlaylist(null);
+			$activity->setQuestion(null);
+			$activity->setRead(false);
+			$activity->setRecord(null);
+			$activity->setSong(null);
+			$activity->setStatus('A');
+			$activity->setToUser($toUserObjectId);
+			$activity->setType('MESSAGESENT');
+			$activity->setUserStatus(null);
+			$activity->setVideo(null);
 
-	    //imposto i valori per il salvataggio dell'activity collegata al post
-	    $activity = new Activity();
-	    $activity->setActive(true);
-	    $activity->setAccepted(true);
-	    $activity->setAlbum(null);
-	    $activity->setComment(null);
-	    $activity->setCounter(0);
-	    $activity->setEvent(null);
-
-	    #TODO
-	    //$activity->setFromUser($currentUser);
-	    $activity->setFromUser($fromUser->getObjectId());
-	    $activity->setImage(null);
-	    $activity->setPlaylist(null);
-	    $activity->setQuestion(null);
-	    $activity->setRead(false);
-	    $activity->setRecord(null);
-	    $activity->setSong(null);
-	    $activity->setStatus('A');
-	    $activity->setToUser($toUserObjectId);
-	    $activity->setType('MESSAGESENT');
-	    $activity->setUserStatus(null);
-	    $activity->setVideo(null);
-
-	    //salvo post
-	    $commentParse = new CommentParse();
-	    $resCmt = $commentParse->saveComment($message);
-	    if (get_class($resCmt) == 'Error') {
-		$this->response(array($resCmt), 503);
-	    } else {
-		//salvo activity
-		$activityParse = new ActivityParse();
-		$resActivity = $activityParse->saveActivity($activity);
-		if (get_class($resActivity) == 'Error') {
-		    $this->rollback($resCmt->getObjectId());
+			//salvo post
+			$commentParse = new CommentParse();
+			$resCmt = $commentParse->saveComment($message);
+			if (get_class($resCmt) == 'Error') {
+				$this->response(array($resCmt), 503);
+			} else {
+				//salvo activity
+				$activityParse = new ActivityParse();
+				$resActivity = $activityParse->saveActivity($activity);
+				if (get_class($resActivity) == 'Error') {
+					$this->rollback($resCmt->getObjectId());
+				}
+			}
+			$this->response(array($controllers['MESSAGESAVED']), 200);
+		} catch (Exception $e) {
+			$this->response(array('status' => "Service Unavailable", "msg" => $e->getMessage()), 503);
 		}
-	    }
-	    $this->response(array($controllers['MESSAGESAVED']), 200);
-	} catch (Exception $e) {
-	    $this->response(array('status' => "Service Unavailable", "msg" => $e->getMessage()), 503);
-	}
     }
 
     private function rollback($objectId) {
-	
-	global $controllers;
-	$commentParse = new CommentParse();
-	$res = $commentParse->deleteComment($objectId);
-	if (get_class($res) == 'Error') {
-	    $this->response(array($controllers['ROLLKO']), 503);
-	} else {
-	    $this->response(array($controllers['ROLLOK']), 503);
-	}
+		global $controllers;
+		$commentParse = new CommentParse();
+		$res = $commentParse->deleteComment($objectId);
+		if (get_class($res) == 'Error') {
+			$this->response(array($controllers['ROLLKO']), 503);
+		} else {
+			$this->response(array($controllers['ROLLOK']), 503);
+		}
     }
-
 }
 
 ?>
