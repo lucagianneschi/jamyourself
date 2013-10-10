dd079d8dd8e88f761e98ddf34efd44bad8f8bda1
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-10-10 15:58:40 +0200

    impostazione di base delle funzioni del controller

---
+++ controllers/userUtilities.controller.php
@@ -0,0 +1,67 @@
+<?php
+
+/* ! \par		Info Generali:
+ * \author		Luca Gianneschi
+ * \version		1.0
+ * \date		2013
+ * \copyright	Jamyourself.com 2013
+ * \par			Info Classe:
+ * \brief		controller di operazioni lgate all'utente
+ * \details		controller di utilities riferite alla classe utente
+ * \par			Commenti:
+ * \warning
+ * \bug
+ * \todo		implementare i parametri di sessione (es currentUser)
+ *
+ */
+if (!defined('ROOT_DIR'))
+    define('ROOT_DIR', '../');
+
+require_once ROOT_DIR . 'config.php';
+require_once SERVICES_DIR . 'lang.service.php';
+require_once LANGUAGES_DIR . 'controllers/' . getLanguage() . '.controllers.lang.php';
+require_once CONTROLLERS_DIR . 'restController.php';
+require_once CLASSES_DIR . 'activity.class.php';
+require_once CLASSES_DIR . 'activityParse.class.php';
+require_once CLASSES_DIR . 'user.class.php';
+require_once CLASSES_DIR . 'userParse.class.php';
+require_once DEBUG_DIR . 'debug.php';
+
+/**
+ * \brief	UserUtilitiesController class 
+ * \details	controller di utilities riferite alla classe utente
+ */
+class UserUtilitiesController extends REST {
+
+    /**
+     * \fn		init()
+     * \brief   start the session
+     */
+    public function init() {
+	session_start();
+    }
+
+    /**
+     * \fn		passwordReset()
+     * \brief   user login
+     * \todo    usare la sessione
+     */
+    public function passwordReset() {}
+	
+	/**
+     * \fn		linkSocialAccount()
+     * \brief   user login
+     * \todo    usare la sessione
+     */
+	public function linkSocialAccount(){}
+	
+	/**
+     * \fn		unLinkSocialAccount()
+     * \brief   user login
+     * \todo    usare la sessione
+     */
+	public function unLinkSocialAccount(){}
+
+}
+
+?>
