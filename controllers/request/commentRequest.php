3a215246c7a120eddb9f48fd3bd51c346b5a1f1a
danielecaldelli <daniele.caldelli@gmail.com>
2013-10-10 14:54:18 +0200

    Eliminata la creazione della sessione

--- controllers/request/commentRequest.php
+++ controllers/request/commentRequest.php
@@ -11,14 +11,11 @@
  * \bug
  *
  */
-
 if (!defined('ROOT_DIR'))
     define('ROOT_DIR', '../../');
 
 require_once ROOT_DIR . 'config.php';
 require_once CONTROLLERS_DIR . 'comment.controller.php';
-
-session_start();
 
 $controller = new CommentController();
 $controller->processApi();
