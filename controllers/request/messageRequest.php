5b4c5da6cb2dc1fdc03a8c1ed8a2342b07ace65d
danielecaldelli <daniele.caldelli@gmail.com>
2013-10-10 14:11:48 +0200

    Eliminata al creazione della sessione

--- controllers/request/messageRequest.php
+++ controllers/request/messageRequest.php
@@ -17,8 +17,6 @@ if (!defined('ROOT_DIR'))
 require_once ROOT_DIR . 'config.php';
 require_once CONTROLLERS_DIR . 'message.controller.php';
 
-session_start();
-
 $controller = new MessageController();
 $controller->processApi();
 ?>
