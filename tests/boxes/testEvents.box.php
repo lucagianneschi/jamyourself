b619e3653b800755764cb213d09477d6f4f40511
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-10-17 15:12:04 +0200

    test su nuovi dati nel DB

--- tests/boxes/testEvents.box.php
+++ tests/boxes/testEvents.box.php
@@ -26,11 +26,9 @@ require_once PARSE_DIR . 'parse.php';
 require_once BOXES_DIR . 'event.box.php';
 $i_end = microtime();
 
-$id1 = 'HDgcsTLpEx';//test1499427772
-$id2 = 'GuUAj83MGH';//SPATAFORA
+$id1 = '7fes1RyY77';//LDF
 
-echo '<br />-------------------------------------------------------------------------------------------<br />';
-echo '<br />-------------------------TEST EVENT BOX-------------------------------------------<br />';
+echo '<br />-------------------------TEST EVENT LDF-------------------------------------------<br />';
 echo '<br />TEST EVENT BOX test1499427772<br />';
 $event1_start = microtime();
 $eventBoxP = new EventBox();
@@ -39,18 +37,6 @@ print "<pre>";
 print_r($eventBox);
 print "</pre>";
 $event1_stop = microtime();
-echo '<br />TEST TEST EVENT BOX test1499427772<br />';
-echo '<br />TEST EVENT BOX SPATAFORA<br />';
-$event2_start = microtime();
-$eventBoxP2 = new EventBox();
-$eventBox2 = $eventBoxP2->initForPersonalPage($id2);
-print "<pre>";
-print_r($eventBox2);
-print "</pre>";
-$event2_stop = microtime();
-echo '<br />FINE TEST EVENT BOX SPATAFORA<br />';
-
-$id = 'AdPPB6Rcao';
 $event_start = microtime();
 echo '<br />TEST  INFO EVENT MEDIA PAGE<br />';
 $eventBoxP = new EventBox;
@@ -60,7 +46,6 @@ print_r($eventBox);
 print "</pre>";
 echo '<br />FINE TEST  INFO EVENT MEDIA PAGE<br />';
 $event_stop = microtime();
-
 $event3_start = microtime();
 echo '<br />TEST  INFO EVENT UPLOADREVIEW PAGE<br />';
 $eventBoxP = new EventBox;
@@ -71,16 +56,13 @@ print "</pre>";
 echo '<br />FINE TEST  INFO EVENT UPLOADREVIEW PAGE<br />';
 $event3_stop = microtime();
 
-
-
 echo '<br />-------------------------FINE TEST EVENT BOX-------------------------------------------<br />';
 $t_end = microtime();
 echo '<br />----------------------TIMERS---------------------------<br />';
 echo 'Tempo include ' . executionTime($i_start, $i_end) . '<br />';
-echo 'Tempo event 1:  ' . executionTime($event1_start, $event1_stop) . '<br />';
-echo 'Tempo event 2:  ' . executionTime($event2_start, $event2_stop) . '<br />';
+echo 'Tempo event personal page:  ' . executionTime($event1_start, $event1_stop) . '<br />';
 echo 'Tempo event media page:  ' . executionTime($event_start, $event_stop) . '<br />';
-echo 'Tempo event uploadREview:  ' . executionTime($event3_start, $event3_stop) . '<br />';
+echo 'Tempo event uploadRrview:  ' . executionTime($event3_start, $event3_stop) . '<br />';
 echo 'Tempo totale ' . executionTime($t_start, $t_end) . '<br />';
 echo '<br />----------------------TIMERS---------------------------<br />';
 ?>
