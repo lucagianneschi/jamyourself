21a112cb9eda5636f6e984ffe67aa11a7b413319
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-10-23 11:46:53 +0200

    aggiunto file di utilità che verrà utilizzato dai controller

---
+++ controllers/utilsController.php
@@ -0,0 +1,32 @@
+<?php
+
+/* ! \par Info	Generali:
+ *  \author		Luca Gianneschi
+ *  \version	1.0
+ *  \date		2013
+ *  \copyright	Jamyourself.com 2013
+ *
+ *  \par		Info Classe:
+ *  \brief		Utils class
+ *  \details	Classe di utilità sfruttata dai controller  per snellire il codice
+ *
+ *  \par		Commenti:
+ *  \warning
+ *  \bug
+ *  \todo       decommentare la funzione encode e eliminarla dalla utils.php che è dentro la cartella classes
+ *
+ */
+
+/**
+ * \fn		string parse_encode_string($string)
+ * \brief	The function returns a string that can be saved to Parse
+ * \param	$string 	represent the string to be saved
+ * \return	string		the string encoded for Parse
+ */
+// function parse_encode_string($string) {
+	// $string = htmlentities($string, ENT_QUOTES, 'UTF-8');
+	// $string = str_replace(array("\r\n", "\r", "\n"), "<br />", $string);
+	// return $string;
+// }
+
+?>
