02ed64af929f6050c2365d76df6ac8fa362eed3f
marifree <marialaurafresu@gmail.com>
2013-10-26 17:44:31 +0200

    html upload review

---
+++ views/content/uploadReview/main.php
@@ -0,0 +1,142 @@
+<div class="bg-white">
+<div class="row">
+		<div class="large-12 columns">
+			<div class="box-upload-review-event">
+				<h3>Review</h3>
+				<div class="row">
+					<div class="large-12 columns">
+						<div class="box">
+							<div class="row box-upload-title">
+								<div class="large-12 columns">
+									<h2>Create a new review about...</h2>
+								</div>
+							</div>
+							<div class="row">
+								<div class="large-4 columns">
+									<div class="sidebar">
+										
+                                        <div class="oggetto-review">
+                                        
+                                            <div class="row">
+                                                <div class="small-3 columns ">
+                                                    <div class="coverThumb"><img src="http://socialmusicdiscovering.com/media/images/default/defaultEventThumb.jpg" onerror="this.src='http://socialmusicdiscovering.com/media/images/default/defaultEventThumb.jpg'"></div>						
+                                                </div>
+                                                <div class="small-9 columns ">
+                                                    <div class="row ">							
+                                                        <div class="small-12 columns ">
+                                                            <div class="sottotitle grey-dark">Album Title</div>
+                                                            <a class="ico-label _tag inline text grey">Rock, Indie</a>
+                                                        </div>	
+                                                    </div>	
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="large-12 columns"><div class="line"></div></div>
+                                            </div>
+                                            <div class="row">						
+                                                <div class="small-3 columns ">
+                                                    <div class="note grey">Rating</div>
+                                                </div>
+                                                <div class="small-9 columns ">
+                                                    <a class="icon-propriety _star-orange"></a><a class="icon-propriety _star-grey"></a><a class="icon-propriety _star-grey"></a><a class="icon-propriety _star-grey"></a><a class="icon-propriety _star-grey"></a>
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+										
+										<div class="row ">
+											<div class="large-12 columns ">
+                                                <div class="text orange">Performed by</div>
+                                                <div class="row">
+                                                    <div class="small-12 columns">
+                                                        <div class="box-membre" id="">
+                                                            <div class="row">
+                                                                <div class="small-2 columns ">
+                                                                    <div class="icon-header">
+                                                                        <img src="http://socialmusicdiscovering.com/media/images/default/defaultAvatarThumb.jpg" onerror="this.src='http://socialmusicdiscovering.com/media/images/default/defaultAvatarThumb.jpg'">
+                                                                    </div>
+                                                                </div>
+                                                                <div class="small-10 columns ">
+                                                                    <div class="text grey-dark breakOffTest">Doctors of Chaos Records</div>
+                                                                </div>		
+                                                            </div>	
+                                                        </div>
+                                                    </div>
+                                                </div>	
+                                            </div>								
+                                        </div>
+                                          <br> <br>
+                                         <div class="row ">
+											<div class="large-12 columns ">
+                                                <div class="text orange">Feauturing</div>
+                                                <div class="row">
+                                                    <div class="small-12 columns">
+                                                        
+                                                        <div class="box-membre" id="">
+                                                            <div class="row">
+                                                                <div class="small-2 columns ">
+                                                                    <div class="icon-header">
+                                                                        <img src="http://socialmusicdiscovering.com/media/images/default/defaultAvatarThumb.jpg" onerror="this.src='http://socialmusicdiscovering.com/media/images/default/defaultAvatarThumb.jpg'">
+                                                                    </div>
+                                                                </div>
+                                                                <div class="small-10 columns ">
+                                                                    <div class="text grey-dark breakOffTest">Doctors of Chaos Records</div>
+                                                                </div>		
+                                                            </div>	
+                                                        </div>
+                                                        
+                                                        <div class="box-membre" id="">
+                                                            <div class="row">
+                                                                <div class="small-2 columns ">
+                                                                    <div class="icon-header">
+                                                                        <img src="http://socialmusicdiscovering.com/media/images/default/defaultAvatarThumb.jpg" onerror="this.src='http://socialmusicdiscovering.com/media/images/default/defaultAvatarThumb.jpg'">
+                                                                    </div>
+                                                                </div>
+                                                                <div class="small-10 columns ">
+                                                                    <div class="text grey-dark breakOffTest">Doctors of Chaos Records</div>
+                                                                </div>		
+                                                            </div>	
+                                                        </div>
+                                                        
+                                                    </div>
+                                                </div>	
+                                            </div>								
+                                        </div>
+                                            
+                                    </div>
+                                </div>
+								
+								<div class="large-6 large-offset-1 end columns">
+                                    <div class="row">
+                                        <div class="large-12 columns ">
+                                            <h5>Write here your review</h5>
+                                            <textarea></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="row rating">
+                                        <div class="large-3 columns ">
+                                            <h5>Your Rating</h5>
+                                        </div>
+                                        <div class="large-9 columns big-star">
+                                            <a class="icon-propriety _star-orange-big"></a><a class="icon-propriety _star-orange-big"></a><a class="icon-propriety _star-orange-big"></a><a class="icon-propriety _star-grey-big"></a><a class="icon-propriety _star-grey-big"></a>
+                                        </div>
+                                    </div>
+                                    
+								</div>
+                                
+							</div>
+                            
+                          <br><br>
+                            <div class="row">
+                                <div class="large-12">
+                                   <input type="button" class="buttonNext" value="Publish">
+                                </div>
+                            </div>
+                            
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="bg-grey">
