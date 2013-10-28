eb2f39e37886da4c06f894840be938ac004c5ec4
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-10-28 22:43:47 +0100

    implementazione del file di config per i box

--- boxes/event.box.php
+++ boxes/event.box.php
@@ -23,8 +23,6 @@ require_once SERVICES_DIR . 'lang.service.php';
 require_once LANGUAGES_DIR . 'boxes/' . getLanguage() . '.boxes.lang.php';
 require_once CLASSES_DIR . 'event.class.php';
 require_once CLASSES_DIR . 'eventParse.class.php';
-require_once CLASSES_DIR . 'user.class.php';
-require_once CLASSES_DIR . 'userParse.class.php';
 require_once BOXES_DIR . 'utilsBox.php';
 
 /**
@@ -160,9 +158,18 @@ class EventInfoForUploadReviewPage {
  */
 class EventBox {
 
+    public $config;
     public $eventCounter;
     public $eventInfoArray;
     public $fromUserInfo;
+
+    /**
+     * \fn	__construct()
+     * \brief	class construct to import config file
+     */
+    function __construct() {
+	$this->config = json_decode(file_get_contents(CONFIG_DIR . "boxes/event.config.json"), false);
+    }
 
     /**
      * \fn	initForMediaPage($objectId)
@@ -175,118 +182,118 @@ class EventBox {
 	global $boxes;
 	$eventBox = new EventBox();
 	$eventP = new EventParse();
-	$event = $eventP->getEvent($objectId);
-	if (get_class($event) == 'Error') {
-	    return $event;
-	} elseif ($event->getActive() == true) {
-	    $encodedAddress = $event->getAddress();
-	    $address = parse_decode_string($encodedAddress);
-	    $attendee = array();
-	    $parseUser = new UserParse();
-	    $parseUser->whereRelatedTo('attendee', 'Event', $objectId);
-	    $parseUser->where('active', true);
-	    $parseUser->setLimit(10);
-	    $att = $parseUser->getUsers();
-	    if (get_class($att) == 'Error') {
-		return $att;
-	    } else {
-		foreach ($att as $user) {
-		    $objectId = $user->getObjectId();
-		    $thumbnail = $user->getProfileThumbnail();
-		    $type = $user->getType();
-		    $encodedUsername = $user->getUsername();
-		    $username = parse_decode_string($encodedUsername);
-		    $userInfo = new UserInfo($objectId, $thumbnail, $type, $username);
-		    array_push($attendee, $userInfo);
+	$eventBox->eventCounter = $boxes['NDB'];
+	$eventP->where('objectId', $objectId);
+	$eventP->where('active', true);
+	$eventP->whereInclude('fromUser');
+	$eventP->setLimit($this->config->limitEventForMediaPage);
+	$events = $eventP->getEvents();
+	if (get_class($events) == 'Error') {
+	    return $events;
+	} elseif (count($events) == 0) {
+	    $eventBox->eventInfoArray = $boxes['NODATA'];
+	    $eventBox->fromUserInfo = $boxes['NODATA'];
+	} else {
+	    require_once CLASSES_DIR . 'user.class.php';
+	    require_once CLASSES_DIR . 'userParse.class.php';
+	    foreach ($events as $event) {
+		$encodedAddress = $event->getAddress();
+		$address = parse_decode_string($encodedAddress);
+		$attendee = array();
+		$parseUser = new UserParse();
+		$parseUser->whereRelatedTo('attendee', 'Event', $objectId);
+		$parseUser->where('active', true);
+		$parseUser->setLimit($this->config->limitAttendeeForMediaPage);
+		$attendees = $parseUser->getUsers();
+		if (get_class($attendees) == 'Error') {
+		    return $attendees;
+		} elseif (count($attendees) == 0) {
+		    $attendee = $boxes['NOATTENDEE'];
+		} else {
+		    foreach ($attendees as $user) {
+			$objectId = $user->getObjectId();
+			$thumbnail = $user->getProfileThumbnail();
+			$type = $user->getType();
+			$encodedUsername = $user->getUsername();
+			$username = parse_decode_string($encodedUsername);
+			$userInfo = new UserInfo($objectId, $thumbnail, $type, $username);
+			array_push($attendee, $userInfo);
+		    }
 		}
-	    }
-	    $encodedCity = $event->getCity();
-	    $city = parse_decode_string($encodedCity);
-	    $commentCounter = $event->getCommentCounter();
-	    $encodedDescription = $event->getDescription();
-	    $description = parse_decode_string($encodedDescription);
-	    $eventDate = $event->getEventDate()->format('d-m-Y H:i:s');
-	    $featuring = array();
-	    $parseUser1 = new UserParse();
-	    $parseUser1->whereRelatedTo('featuring', 'Event', $objectId);
-	    $parseUser1->where('active', true);
-	    $parseUser1->setLimit(10);
-	    $feats = $parseUser1->getUsers();
-	    if (get_class($feats) == 'Error') {
-		return $feats;
-	    } else {
-		foreach ($feats as $user) {
-		    $objectId = $user->getObjectId();
-		    $thumbnail = $user->getProfileThumbnail();
-		    $type = $user->getType();
-		    $encodedUsername = $user->getUsername();
-		    $username = parse_decode_string($encodedUsername);
-		    $userInfo = new UserInfo($objectId, $thumbnail, $type, $username);
-		    array_push($featuring, $userInfo);
+		$encodedCity = $event->getCity();
+		$city = parse_decode_string($encodedCity);
+		$commentCounter = $event->getCommentCounter();
+		$encodedDescription = $event->getDescription();
+		$description = parse_decode_string($encodedDescription);
+		$eventDate = $event->getEventDate()->format('d-m-Y H:i:s');
+		$featuring = array();
+		$parseUser1 = new UserParse();
+		$parseUser1->whereRelatedTo('featuring', 'Event', $objectId);
+		$parseUser1->where('active', true);
+		$parseUser1->setLimit($this->config->limitFeaturingForMediaPage);
+		$feats = $parseUser1->getUsers();
+		if (get_class($feats) == 'Error') {
+		    return $feats;
+		} elseif (count($feats) == 0) {
+		    $featuring = $boxes['NOFEATEVE'];
+		} else {
+		    foreach ($feats as $user) {
+			$objectId = $user->getObjectId();
+			$thumbnail = $user->getProfileThumbnail();
+			$type = $user->getType();
+			$encodedUsername = $user->getUsername();
+			$username = parse_decode_string($encodedUsername);
+			$userInfo = new UserInfo($objectId, $thumbnail, $type, $username);
+			array_push($featuring, $userInfo);
+		    }
 		}
-	    }
-	    $image = $event->getImage();
-	    $invited = array();
-	    $parseUser2 = new UserParse();
-	    $parseUser2->whereRelatedTo('invited', 'Event', $objectId);
-	    $parseUser2->where('active', true);
-	    $parseUser2->setLimit(10);
-	    $inv = $parseUser2->getUsers();
-	    if (get_class($inv) == 'Error') {
-		return $inv;
-	    } else {
-		foreach ($inv as $user) {
-		    $objectId = $user->getObjectId();
-		    $thumbnail = $user->getProfileThumbnail();
-		    $type = $user->getType();
-		    $encodedUsername = $user->getUsername();
-		    $username = parse_decode_string($encodedUsername);
-		    $userInfo = new UserInfo($objectId, $thumbnail, $type, $username);
-		    array_push($invited, $userInfo);
+		$image = $event->getImage();
+		$invited = array();
+		$parseUser2 = new UserParse();
+		$parseUser2->whereRelatedTo('invited', 'Event', $objectId);
+		$parseUser2->where('active', true);
+		$parseUser2->setLimit($this->config->limitInvitedForMediaPage);
+		$userInvited = $parseUser2->getUsers();
+		if (get_class($userInvited) == 'Error') {
+		    return $userInvited;
+		} elseif (count($userInvited) == 0) {
+		    $invited = $boxes['NOINVITED'];
+		} else {
+		    foreach ($userInvited as $user) {
+			$objectId = $user->getObjectId();
+			$thumbnail = $user->getProfileThumbnail();
+			$type = $user->getType();
+			$encodedUsername = $user->getUsername();
+			$username = parse_decode_string($encodedUsername);
+			$userInfo = new UserInfo($objectId, $thumbnail, $type, $username);
+			array_push($invited, $userInfo);
+		    }
 		}
-	    }
-	    $geopoint = $event->getLocation();
-	    $location = array('latitude' => $geopoint->location['latitude'], 'longitude' => $geopoint->location['longitude']);
-	    $encodedLocationName = $event->getLocationName();
-	    $locationName = parse_decode_string($encodedLocationName);
-	    $loveCounter = $event->getLoveCounter();
-	    $reviewCounter = $event->getReviewCounter();
-	    $shareCounter = $event->getShareCounter();
-	    $tags = array();
-	    if (count($event->getTags()) > 0) {
-		foreach ($event->getTags() as $tag) {
-		    $tag = parse_decode_string($tag);
-		    array_push($tags, $tag);
+		$geopoint = $event->getLocation();
+		$location = array('latitude' => $geopoint->location['latitude'], 'longitude' => $geopoint->location['longitude']);
+		$encodedLocationName = $event->getLocationName();
+		$locationName = parse_decode_string($encodedLocationName);
+		$loveCounter = $event->getLoveCounter();
+		$reviewCounter = $event->getReviewCounter();
+		$shareCounter = $event->getShareCounter();
+		$tags = array();
+		if (count($event->getTags()) > 0) {
+		    foreach ($event->getTags() as $tag) {
+			$tag = parse_decode_string($tag);
+			array_push($tags, $tag);
+		    }
 		}
-	    }
-	    $encodedTitle = $event->getTitle();
-	    $title = parse_decode_string($encodedTitle);
-	    $counters = new Counters($commentCounter, $loveCounter, $reviewCounter, $shareCounter);
-	    if (empty($attendee)) {
-		$attendee = $boxes['NOATTENDEE'];
-	    } elseif (empty($featuring)) {
-		$featuring = $boxes['NOFEATEVE'];
-	    } elseif (empty($invited)) {
-		$invited = $boxes['NOINVITED'];
-	    }
-	    $eventInfo = new EventInfoForMediaPage($address, $attendee, $city, $counters, $description, $eventDate, $featuring, $image, $invited, $location, $locationName, $reviewCounter, $tags, $title);
-
-
-	    //devi sostituire questo codice con la whereInclude
-	    $fromUserId = $event->getFromUser();
-	    $userP = new UserParse();
-	    $fromUser = $userP->getUser($fromUserId);
-	    if (get_class($fromUser) == 'Error') {
-		return $fromUser;
-	    } else {
-		$objectId = $user->getObjectId();
-		$thumbnail = $user->getProfileThumbnail();
-		$type = $user->getType();
-		$encodedUsername = $user->getUsername();
+		$encodedTitle = $event->getTitle();
+		$title = parse_decode_string($encodedTitle);
+		$counters = new Counters($commentCounter, $loveCounter, $reviewCounter, $shareCounter);
+		$eventInfo = new EventInfoForMediaPage($address, $attendee, $city, $counters, $description, $eventDate, $featuring, $image, $invited, $location, $locationName, $reviewCounter, $tags, $title);
+		$objectId = $event->getFromUser()->getObjectId();
+		$thumbnail = $event->getFromUser()->getProfileThumbnail();
+		$type = $event->getFromUser()->getType();
+		$encodedUsername = $event->getFromUser()->getUsername();
 		$username = parse_decode_string($encodedUsername);
 		$userInfo = new UserInfo($objectId, $thumbnail, $type, $username);
 	    }
-	    $eventBox->eventCounter = $boxes['NDB'];
 	    $eventBox->eventInfoArray = $eventInfo;
 	    $eventBox->fromUserInfo = $userInfo;
 	}
@@ -301,18 +308,23 @@ class EventBox {
      */
     public function initForPersonalPage($objectId) {
 	global $boxes;
-	$eventBox = new EventBox();
 	$info = array();
 	$counter = 0;
+	$eventBox = new EventBox();
+	$eventBox->fromUserInfo = $boxes['NDB'];
 	$event = new EventParse();
 	$event->wherePointer('fromUser', '_User', $objectId);
 	$event->where('active', true);
-	$event->setLimit(10);
+	$event->setLimit($this->config->limitEventForPersonalPage);
 	$event->orderByDescending('eventDate');
 	$events = $event->getEvents();
 	if (get_class($events) == 'Error') {
 	    return $events;
+	} elseif (count($events) == 0) {
+	    $eventBox->eventInfoArray = $boxes['NODATA'];
 	} else {
+	    require_once CLASSES_DIR . 'user.class.php';
+	    require_once CLASSES_DIR . 'userParse.class.php';
 	    foreach ($events as $event) {
 		$counter = ++$counter;
 		$encodedAddress = $event->getAddress();
@@ -329,10 +341,12 @@ class EventBox {
 		$parseUser = new UserParse();
 		$parseUser->whereRelatedTo('featuring', 'Event', $objectId);
 		$parseUser->where('active', true);
-		$parseUser->setLimit(10);
+		$parseUser->setLimit($this->config->limitFeaturingForPersonalPage);
 		$feats = $parseUser->getUsers();
 		if (get_class($feats) == 'Error') {
 		    return $feats;
+		} elseif (count($feats) == 0) {
+		    $featuring = $boxes['NOFEATEVE'];
 		} else {
 		    foreach ($feats as $user) {
 			$objectId = $user->getObjectId();
@@ -358,15 +372,11 @@ class EventBox {
 		$thumbnail = $event->getThumbnail();
 		$encodedTitle = $event->getTitle();
 		$title = parse_decode_string($encodedTitle);
-		if (empty($featuring)) {
-		    $featuring = $boxes['NOFEATEVE'];
-		}
 		$eventInfo = new EventInfoForPersonalPage($address, $city, $counters, $eventDate, $fromUserInfo, $featuring, $locationName, $objectId, $tags, $thumbnail, $title);
 		array_push($info, $eventInfo);
 	    }
 	    $eventBox->eventCounter = $counter;
 	    $eventBox->eventInfoArray = $info;
-	    $eventBox->fromUserInfo = $boxes['NDB'];
 	}
 	return $eventBox;
     }
@@ -379,67 +389,70 @@ class EventBox {
      * \return	eventBox
      */
     public function initForUploadReviewPage($objectId) {
-
 	global $boxes;
 	$eventBox = new EventBox();
 	$eventBox->eventCounter = $boxes['NDB'];
 	$recordP = new EventParse();
 	$recordP->where('objectId', $objectId);
-	$recordP->setLimit(1);
+	$recordP->setLimit($this->config->limitEventForUploadReviewPage);
 	$recordP->whereInclude('fromUser');
 	$events = $recordP->getEvents();
 	if (get_class($events) == 'Error') {
 	    return $events;
-	}
-	foreach ($events as $event) {
-	    $encodedAddress = $event->getAddress();
-	    $address = parse_decode_string($encodedAddress);
-	    $encodedCity = $event->getCity();
-	    $city = parse_decode_string($encodedCity);
-	    $eventDate = $event->getEventDate();
-	    $featuring = array();
-	    $parseUser = new UserParse();
-	    $parseUser->whereRelatedTo('featuring', 'Event', $objectId);
-	    $parseUser->where('active', true);
-	    $parseUser->setLimit(10);
-	    $feats = $parseUser->getUsers();
-	    if (get_class($feats) == 'Error') {
-		return $feats;
-	    }
-	    foreach ($feats as $user) {
-		$objectId = $user->getObjectId();
-		$thumbnail = $user->getProfileThumbnail();
-		$type = $user->getType();
-		$encodedUsername = $user->getUsername();
-		$username = parse_decode_string($encodedUsername);
-		$userInfo = new UserInfo($objectId, $thumbnail, $type, $username);
-		array_push($featuring, $userInfo);
-	    }
-	    if (empty($featuring)) {
-		$featuring = $boxes['NOFEATEVE'];
-	    }
-	    $encodedLocationName = $event->getLocationName();
-	    $locationName = parse_decode_string($encodedLocationName);
-	    $tags = array();
-	    if (count($event->getTags()) > 0) {
-		foreach ($event->getTags() as $tag) {
-		    $tag = parse_decode_string($tag);
-		    array_push($tags, $tag);
+	} elseif (count($events) == 0) {
+	    $eventBox->recordInfoArray = $boxes['NODATA'];
+	} else {
+	    require_once CLASSES_DIR . 'user.class.php';
+	    require_once CLASSES_DIR . 'userParse.class.php';
+	    foreach ($events as $event) {
+		$encodedAddress = $event->getAddress();
+		$address = parse_decode_string($encodedAddress);
+		$encodedCity = $event->getCity();
+		$city = parse_decode_string($encodedCity);
+		$eventDate = $event->getEventDate();
+		$featuring = array();
+		$parseUser = new UserParse();
+		$parseUser->whereRelatedTo('featuring', 'Event', $objectId);
+		$parseUser->where('active', true);
+		$parseUser->setLimit($this->config->limitFeaturingForUploadReviewPage);
+		$feats = $parseUser->getUsers();
+		if (get_class($feats) == 'Error') {
+		    return $feats;
+		} elseif (count($feats) == 0) {
+		    $featuring = $boxes['NOFEATEVE'];
+		} else {
+		    foreach ($feats as $user) {
+			$objectId = $user->getObjectId();
+			$thumbnail = $user->getProfileThumbnail();
+			$type = $user->getType();
+			$encodedUsername = $user->getUsername();
+			$username = parse_decode_string($encodedUsername);
+			$userInfo = new UserInfo($objectId, $thumbnail, $type, $username);
+			array_push($featuring, $userInfo);
+		    }
 		}
+		$encodedLocationName = $event->getLocationName();
+		$locationName = parse_decode_string($encodedLocationName);
+		$tags = array();
+		if (count($event->getTags()) > 0) {
+		    foreach ($event->getTags() as $tag) {
+			$tag = parse_decode_string($tag);
+			array_push($tags, $tag);
+		    }
+		}
+		$thumbnail = $event->getThumbnail();
+		$encodedTitle = $event->getTitle();
+		$title = parse_decode_string($encodedTitle);
+		$eventInfo = new EventInfoForUploadReviewPage($address, $city, $eventDate, $featuring, $locationName, $tags, $thumbnail, $title);
+		$eventBox->recordInfoArray = $eventInfo;
+		$objectIdUser = $event->getFromUser()->getObjectId();
+		$thumbnailUser = $event->getFromUser()->getProfileThumbnail();
+		$type = $event->getFromUser()->getType();
+		$encodedUsername = $event->getFromUser()->getUsername();
+		$username = parse_decode_string($encodedUsername);
+		$userInfo = new UserInfo($objectIdUser, $thumbnailUser, $type, $username);
+		$eventBox->fromUserInfo = $userInfo;
 	    }
-	    $thumbnail = $event->getThumbnail();
-	    $encodedTitle = $event->getTitle();
-	    $title = parse_decode_string($encodedTitle);
-	    $eventInfo = new EventInfoForUploadReviewPage($address, $city, $eventDate, $featuring, $locationName, $tags, $thumbnail, $title);
-	    $eventBox->recordInfoArray = $eventInfo;
-
-	    $objectIdUser = $event->getFromUser()->getObjectId();
-	    $thumbnailUser = $event->getFromUser()->getProfileThumbnail();
-	    $type = $event->getFromUser()->getType();
-	    $encodedUsername = $event->getFromUser()->getUsername();
-	    $username = parse_decode_string($encodedUsername);
-	    $userInfo = new UserInfo($objectIdUser, $thumbnailUser, $type, $username);
-	    $eventBox->fromUserInfo = $userInfo;
 	}
 	return $eventBox;
     }
