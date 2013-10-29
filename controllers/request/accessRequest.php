42f754848a2d49b469097f2651017640a1b5e4b1
danielecaldelli <daniele.caldelli@gmail.com>
2013-10-29 12:20:03 +0100

    Implementata la login

--- controllers/request/accessRequest.php
+++ controllers/request/accessRequest.php
@@ -15,10 +15,11 @@
 if (!defined('ROOT_DIR'))
     define('ROOT_DIR', '../../');
 
-session_start();
-
 require_once ROOT_DIR . 'config.php';
 require_once CONTROLLERS_DIR . 'access.controller.php';
+
+require_once CLASSES_DIR . 'userParse.class.php';
+session_start();
 
 $controller = new AccessController();
 $controller->processApi();
--- views/login.php
+++ views/login.php
@@ -8,17 +8,20 @@
 if (!defined('ROOT_DIR'))
 	define('ROOT_DIR', '../');
 
+require_once ROOT_DIR . 'config.php';
+require_once CLASSES_DIR . 'userParse.class.php';
+
 session_start();
 
-echo 'Sessione: ';
 print_r($_SESSION);
 
 ?>
 <script type="text/javascript" src="<?php echo ROOT_DIR; ?>views/resources/javascripts/plugins/jquery/jquery-1.8.3.min.js"></script>
-<script type="text/javascript" src="<?php echo ROOT_DIR; ?>tests/controllers/access/access.js"></script>
+<script type="text/javascript" src="<?php echo ROOT_DIR; ?>views/resources/javascripts/customs/access.js"></script>
 <form action="javascript:access($('#user').val(), $('#pass').val(), 'login', null)">
 	<input type="text" id="user" placeholder="username" />
 	<input type="password" id="pass" placeholder="password" />
 	<input type="submit" value="Login" />
 </form>
-Ldf - MHURRg5X
+Ldf<br />
+MHURRg5X
