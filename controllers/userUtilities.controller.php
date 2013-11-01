a4a9d6fbc3ba6a4b7803eaa8f0b3267a230e1c68
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-11-01 20:42:27 +0100

    prime modifiche ai metodi di link e unlink e ottimizzazioni alla passwordReset

--- controllers/userUtilities.controller.php
+++ controllers/userUtilities.controller.php
@@ -1,5 +1,4 @@
 <?php
-
 /* ! \par		Info Generali:
  * \author		Luca Gianneschi
  * \version		1.0
@@ -39,48 +38,52 @@ class UserUtilitiesController extends REST {
      * \todo    usare la sessione
      */
     public function linkSocialAccount() {
-        try {
-            require_once PARSE_DIR . 'parse.php';
-            global $controllers;
-            if ($this->get_request_method() != "POST") {
-                $this->response(array('status' => $controllers['NOPOSTREQUEST']), 405);
-            } elseif (!isset($_SESSION['currentUser'])) {
-                $this->response(array('status' => $controllers['USERNOSES']), 403);
-            }
-            $objectId = $this->request['objectId'];
-            $userP = new UserParse();
-            $user = $userP->getUser($objectId);
-            if ($user instanceof Error) {
-                $this->response(array('USERNOTFOUND'), 503);
-            } else {
-                $sessionToken = $user->getSessionToken();
-                $userLib = new parseUser();
-                $userLib->linkAccounts($objectId, $sessionToken);
-                $activity = new Activity();
-                $activity->setActive(true);
-                $activity->setAlbum(null);
-                $activity->setComment(null);
-                $activity->setCounter(0);
-                $activity->setEvent(null);
-                $activity->setFromUser($objectId);
-                $activity->setImage(null);
-                $activity->setPlaylist(null);
-                $activity->setQuestion(null);
-                $activity->setRecord(null);
-                $activity->setRead(true);
-                $activity->setSong(null);
-                $activity->setStatus('A');
-                $activity->setToUser(null);
-                $activity->setType('SOCIALACCOUNTLINKED');
-                $activity->setUserStatus(null);
-                $activity->setVideo(null);
-                $activityParse = new ActivityParse();
-                $activityParse->saveActivity($activity);
-                $this->response(array($controllers['OKSOCIALLINK']), 200);
-            }
-        } catch (Exception $e) {
-            $this->response(array('status' => $e->getMessage()), 503);
-        }
+	try {
+	    global $controllers;
+	    if ($this->get_request_method() != "POST") {
+		$this->response(array('status' => $controllers['NOPOSTREQUEST']), 405);
+	    } elseif (!isset($this->request['currentUser'])) {
+		$this->response(array('status' => $controllers['USERNOSES']), 403);
+	    }
+	    $currentUser = $this->request['currentUser'];
+	    $objectId = $currentUser->getObjectId();
+	    $sessionToken = $currentUser->getSessionToken();
+	    $userP = new UserParse();
+	    $user = $userP->getUser($objectId);
+	    if ($user instanceof Error) {//questo controllo posso ometterlo, se l'utente è in sessione esiste
+		$this->response(array('status' => $controllers['USERNOTFOUND']), 503);
+	    } else {
+		require_once PARSE_DIR . 'parse.php';
+		$userLib = new parseUser();
+		$link = $userLib->linkAccounts($objectId, $sessionToken);
+		if ($link instanceof ParseLibraryException) {
+		    $this->response(array('NOLINK'), 503);
+		}
+		$activity = new Activity();
+		$activity->setActive(true);
+		$activity->setAlbum(null);
+		$activity->setComment(null);
+		$activity->setCounter(0);
+		$activity->setEvent(null);
+		$activity->setFromUser($objectId);
+		$activity->setImage(null);
+		$activity->setPlaylist(null);
+		$activity->setQuestion(null);
+		$activity->setRecord(null);
+		$activity->setRead(true);
+		$activity->setSong(null);
+		$activity->setStatus('A');
+		$activity->setToUser(null);
+		$activity->setType('SOCIALACCOUNTLINKED');
+		$activity->setUserStatus(null);
+		$activity->setVideo(null);
+		$activityParse = new ActivityParse();
+		$activityParse->saveActivity($activity);
+		$this->response(array($controllers['OKSOCIALLINK']), 200);
+	    }
+	} catch (Exception $e) {
+	    $this->response(array('status' => $e->getMessage()), 500);
+	}
     }
 
     /**
@@ -89,38 +92,43 @@ class UserUtilitiesController extends REST {
      * \todo    usare la sessione
      */
     public function passwordReset() {
-        try {
-            global $controllers;
-            $email = $this->request['email'];
-            $userP = new UserParse();
-            $user = $userP->passwordReset($email);
-            if ($user instanceof Error) {
-                $this->response(array('status' => $e->getMessage()), 503);
-            }
-            $activity = new Activity();
-            $activity->setActive(true);
-            $activity->setAlbum(null);
-            $activity->setComment(null);
-            $activity->setCounter(0);
-            $activity->setEvent(null);
-            $activity->setFromUser($user->getObjectId());
-            $activity->setImage(null);
-            $activity->setPlaylist(null);
-            $activity->setQuestion(null);
-            $activity->setRecord(null);
-            $activity->setRead(true);
-            $activity->setSong(null);
-            $activity->setStatus('A');
-            $activity->setToUser(null);
-            $activity->setType('PASSWORDRESETREQUEST');
-            $activity->setUserStatus(null);
-            $activity->setVideo(null);
-            $activityParse = new ActivityParse();
-            $activityParse->saveActivity($activity);
-            $this->response(array($controllers['OKPASSWORDRESETREQUEST']), 200);
-        } catch (Exception $e) {
-            $this->response(array('status' => $e->getMessage()), 503);
-        }
+	try {
+	    global $controllers;
+	    if ($this->get_request_method() != "POST") {
+		$this->response(array('status' => $controllers['NOPOSTREQUEST']), 405);
+	    } elseif (!isset($this->request['email'])) {
+		$this->response(array('status' => $controllers['NOEMAILFORRESETPASS']), 403);
+	    }
+	    $email = $this->request['email'];
+	    $userP = new UserParse();
+	    $user = $userP->passwordReset($email);
+	    if ($user instanceof Error) {
+		$this->response(array('status' => $controllers['USERNOTFOUNDFORPASSRESET']), 503);
+	    }
+	    $activity = new Activity();
+	    $activity->setActive(true);
+	    $activity->setAlbum(null);
+	    $activity->setComment(null);
+	    $activity->setCounter(0);
+	    $activity->setEvent(null);
+	    $activity->setFromUser($user->getObjectId());
+	    $activity->setImage(null);
+	    $activity->setPlaylist(null);
+	    $activity->setQuestion(null);
+	    $activity->setRecord(null);
+	    $activity->setRead(true);
+	    $activity->setSong(null);
+	    $activity->setStatus('A');
+	    $activity->setToUser(null);
+	    $activity->setType('PASSWORDRESETREQUEST');
+	    $activity->setUserStatus(null);
+	    $activity->setVideo(null);
+	    $activityParse = new ActivityParse();
+	    $activityParse->saveActivity($activity);
+	    $this->response(array($controllers['OKPASSWORDRESETREQUEST']), 200);
+	} catch (Exception $e) {
+	    $this->response(array('status' => $e->getMessage()), 500);
+	}
     }
 
     /**
@@ -129,49 +137,44 @@ class UserUtilitiesController extends REST {
      * \todo    usare la sessione
      */
     public function unLinkSocialAccount() {
-        try {
-            require_once PARSE_DIR . 'parse.php';
-            global $controllers;
-            if ($this->get_request_method() != "POST") {
-                $this->response(array('status' => $controllers['NOPOSTREQUEST']), 405);
-            } elseif (!isset($_SESSION['currentUser'])) {
-                $this->response(array('status' => $controllers['USERNOSES']), 403);
-            }
-            $objectId = $this->request['objectId'];
-            $userP = new UserParse();
-            $user = $userP->getUser($objectId);
-            if ($user instanceof Error) {
-                $this->response(array($user), 503);
-            } else {
-                $sessionToken = $user->getSessionToken();
-                $userLib = new parseUser();
-                $userLib->unlinkAccounts($objectId, $sessionToken, null);
-
-                $activity = new Activity();
-                $activity->setActive(true);
-                $activity->setAlbum(null);
-                $activity->setComment(null);
-                $activity->setCounter(0);
-                $activity->setEvent(null);
-                $activity->setFromUser($objectId);
-                $activity->setImage(null);
-                $activity->setPlaylist(null);
-                $activity->setQuestion(null);
-                $activity->setRecord(null);
-                $activity->setRead(true);
-                $activity->setSong(null);
-                $activity->setStatus('A');
-                $activity->setToUser(null);
-                $activity->setType('SOCIALACCOUNTUNLINKED');
-                $activity->setUserStatus(null);
-                $activity->setVideo(null);
-                $activityParse = new ActivityParse();
-                $activityParse->saveActivity($activity);
-                $this->response(array($controllers['OKSOCIALUNLINK']), 200);
-            }
-        } catch (Exception $e) {
-            $this->response(array('status' => $e->getMessage()), 503);
-        }
+	try {
+	    global $controllers;
+	    if ($this->get_request_method() != "POST") {
+		$this->response(array('status' => $controllers['NOPOSTREQUEST']), 405);
+	    } elseif (!isset($_SESSION['currentUser'])) {
+		$this->response(array('status' => $controllers['USERNOSES']), 403);
+	    }
+	    $currentUser = $this->request['currentUser'];
+	    $objectId = $currentUser->getObjectId();
+	    $sessionToken = $currentUser->getSessionToken();
+	    //spostare questa funzione dentro la userParse
+	    require_once PARSE_DIR . 'parse.php';
+	    $userLib = new parseUser();
+	    $userLib->unlinkAccounts($objectId, $sessionToken, null);
+	    $activity = new Activity();
+	    $activity->setActive(true);
+	    $activity->setAlbum(null);
+	    $activity->setComment(null);
+	    $activity->setCounter(0);
+	    $activity->setEvent(null);
+	    $activity->setFromUser($objectId);
+	    $activity->setImage(null);
+	    $activity->setPlaylist(null);
+	    $activity->setQuestion(null);
+	    $activity->setRecord(null);
+	    $activity->setRead(true);
+	    $activity->setSong(null);
+	    $activity->setStatus('A');
+	    $activity->setToUser(null);
+	    $activity->setType('SOCIALACCOUNTUNLINKED');
+	    $activity->setUserStatus(null);
+	    $activity->setVideo(null);
+	    $activityParse = new ActivityParse();
+	    $activityParse->saveActivity($activity);
+	    $this->response(array($controllers['OKSOCIALUNLINK']), 200);
+	} catch (Exception $e) {
+	    $this->response(array('status' => $e->getMessage()), 500);
+	}
     }
 
     /**
@@ -180,54 +183,56 @@ class UserUtilitiesController extends REST {
      * \todo    usare la sessione
      */
     public function updateSetting() {
-        try {
-            if ($this->get_request_method() != "POST") {
-                $this->response(array('status' => $controllers['NOPOSTREQUEST']), 405);
-            } elseif (!isset($_SESSION['currentUser'])) {
-                $this->response(array('status' => $controllers['USERNOSES']), 403);
-            }
-            $userId = $this->request['objectId'];
-            $settings = $this->request['setting'];
+	try {
+	    if ($this->get_request_method() != "POST") {
+		$this->response(array('status' => $controllers['NOPOSTREQUEST']), 405);
+	    } elseif (!isset($_SESSION['currentUser'])) {
+		$this->response(array('status' => $controllers['USERNOSES']), 403);
+	    }
+	    $userId = $this->request['objectId'];
+	    $settings = $this->request['setting'];
 
-            $userP = new UserParse();
-            $user = $userP->getuser($userId);
-            if ($user instanceof Error) {
-                $this->response(array('Error: ' . $user->getMessage()), 503);
-            }
-            $res = $userP->updateField($userId, 'settings', array($settings));
-            if ($res instanceof Error) {
-                $this->response(array('NOSETTINGUPDATE'), 503);
-            }
+	    $userP = new UserParse();
+	    $user = $userP->getuser($userId);
+	    if ($user instanceof Error) {
+		$this->response(array('Error: ' . $user->getMessage()), 503);
+	    }
+	    $res = $userP->updateField($userId, 'settings', array($settings));
+	    if ($res instanceof Error) {
+		$this->response(array('NOSETTINGUPDATE'), 503);
+	    }
 
-            $activity = new Activity();
-            $activity->setActive(true);
-            $activity->setAlbum(null);
-            $activity->setComment(null);
-            $activity->setCounter(0);
-            $activity->setEvent(null);
-            $activity->setFromUser($userId);
-            $activity->setImage(null);
-            $activity->setPlaylist(null);
-            $activity->setQuestion(null);
-            $activity->setRead(true);
-            $activity->setRecord(null);
-            $activity->setSong(null);
-            $activity->setStatus('A');
-            $activity->setToUser(null);
-            $activity->setType("USERSETTINGSUPDATED");
-            $activity->setUserStatus(null);
-            $activity->setVideo(null);
-            $activityParse = new ActivityParse();
-            $resActivity = $activityParse->saveActivity($activity);
-            // if (get_class($resActivity) == 'Error') {
-            // $this->rollback($playlistId, $songId, 'remove');
-            // }
-            $this->response(array($resActivity), 200);
-        } catch (Exception $e) {
-            $this->response(array('status' => $e->getMessage()), 503);
-        }
+	    $activity = new Activity();
+	    $activity->setActive(true);
+	    $activity->setAlbum(null);
+	    $activity->setComment(null);
+	    $activity->setCounter(0);
+	    $activity->setEvent(null);
+	    $activity->setFromUser($userId);
+	    $activity->setImage(null);
+	    $activity->setPlaylist(null);
+	    $activity->setQuestion(null);
+	    $activity->setRead(true);
+	    $activity->setRecord(null);
+	    $activity->setSong(null);
+	    $activity->setStatus('A');
+	    $activity->setToUser(null);
+	    $activity->setType("USERSETTINGSUPDATED");
+	    $activity->setUserStatus(null);
+	    $activity->setVideo(null);
+	    $activityParse = new ActivityParse();
+	    $resActivity = $activityParse->saveActivity($activity);
+	    // if (get_class($resActivity) == 'Error') {
+	    // $this->rollback($playlistId, $songId, 'remove');
+	    // }
+	    $this->response(array($resActivity), 200);
+	} catch (Exception $e) {
+	    $this->response(array('status' => $e->getMessage()), 503);
+	}
     }
 
 }
+
+
 
 ?>
