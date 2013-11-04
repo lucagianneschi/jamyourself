8026c654c8e9b1124c47af14d119cc1453a38813
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-11-04 11:19:48 +0100

    prima stesura codice

--- controllers/search.controller.php
+++ controllers/search.controller.php
@@ -32,41 +32,65 @@ class SearchController extends REST {
     public $config;
 
     function __construct() {
-		parent::__construct();
-		$this->config = json_decode(file_get_contents(CONFIG_DIR . "controllers/search.config.json"), false);
+        parent::__construct();
+        $this->config = json_decode(file_get_contents(CONFIG_DIR . "controllers/search.config.json"), false);
     }
+
     /**
      * \fn		init()
      * \brief   start the session
      */
     public function init() {
-	session_start();
+        session_start();
     }
 
-	 /**
+    /**
      * \fn		search()
      * \brief   effettua la ricerca
      * \todo    tutto
      */
-	    public function search() {
-			try {
-				if ($this->get_request_method() != 'POST') {
-					$this->response('', 406);
-				}
-				if (!isset($this->request['text'])) {
-					$this->response(array('status' => "Bad Request", "msg" => $controllers['NOSEARCHTEXT']), 400);
-				} elseif (strlen($text) < $this->config->minSearchTextSize) {
-					$this->response(array($controllers['SHORTSEARCHTEXT'] . strlen($text)), 200);
-				}
-				
-				$classType = $this->request['classType'];
-				$text = $this->request['text'];
-								
-		} catch (Exception $e){
-			$this->response(array('status' => "Service Unavailable", "msg" => $e->getMessage()), 503);
-		}
-	}
-	
+    public function search() {
+        try {
+            if ($this->get_request_method() != "POST") {
+                $this->response(array('status' => $controllers['NOPOSTREQUEST']), 405);
+            } elseif (!isset($this->request['text'])) {
+                $this->response(array('status' => $controllers['NOSEARCHTEXT']), 400);
+            } elseif (strlen($text) < $this->config->minSearchTextSize) {
+                $this->response(array($controllers['SHORTSEARCHTEXT'] . strlen($text)), 200);
+            }
+            $classType = $this->request['classType'];
+            $text = $this->request['text'];
+            require_once CLASSES_DIR . 'activity.class.php';
+            require_once CLASSES_DIR . 'activityParse.class.php';
+            $activity = new Activity();
+            $activity->setActive(true);
+            $activity->setAlbum(null);
+            $activity->setComment(null);
+            $activity->setCounter(0);
+            $activity->setEvent(null);
+            $activity->setFromUser(null);
+            $activity->setImage(null);
+            $activity->setPlaylist(null);
+            $activity->setQuestion(null);
+            $activity->setRead(false);
+            $activity->setRecord(null);
+            $activity->setSong(null);
+            $activity->setStatus('A');
+            $activity->setToUser(null);
+            $activity->setType('SEARCH');
+            $activity->setUserStatus(null);
+            $activity->setVideo(null);
+            $activityParse = new ActivityParse();
+            $resActivity = $activityParse->saveActivity($activity);
+            if ($resActivity instanceof Error) {
+            $this->response(array('status' => $controllers['SEARCHOK']), 200);
+            }
+            $this->response(array('status' => $controllers['SEARCHOK']), 200);
+        } catch (Exception $e) {
+            $this->response(array('status' => $e->getMessage()), 503);
+        }
+    }
+
 }
 
 ?>
