ab682f8f7786e054243f2b5486bdfac08934a98a
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-11-06 12:38:02 +0100

    aggiunti commenti standard

--- services/debug.service.php
+++ services/debug.service.php
@@ -1,7 +1,24 @@
 <?php
+
+/* ! \par		Info Generali:
+ *  \author		Daniele Caldelli
+ *  \version            1.0
+ *  \date		2013
+ *  \copyright          Jamyourself.com 2013
+ *  \par		Info :
+ *  \brief		Debub function
+ *  \details            Funzione per scrivere su file un bug
+ *  \par		Commenti:
+ *  \warning
+ *  \bug
+ *  \todo
+ *
+ */
+
 function debug($path, $file, $msg) {
-	$fp = fopen($path . $file, 'a+');
-	fwrite($fp, $msg . "\n");
-	fclose($fp);
+    $fp = fopen($path . $file, 'a+');
+    fwrite($fp, $msg . "\n");
+    fclose($fp);
 }
+
 ?>
