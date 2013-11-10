9f185f6503991b4d556d40f1534c96500d787b9a
Stefano <stefanom@jamyourself.com>
2013-11-11 00:30:43 +0100

    aggiunto il caricamento dei dati nella view

---
+++ views/resources/javascripts/customs/uploadReview.js
@@ -0,0 +1,30 @@
+
+function sendRequest(_action, _data, callback, _async) {
+    if (_action === undefined || _action === null || _data === undefined || _data === null) {
+        callback(null);
+    }
+    _data.request = _action;
+    var url = "../controllers/request/uploadReviewRequest.php";
+    var type = "POST";
+    var async = true;
+    if (async !== undefined && async !== null)
+        async = _async;
+
+    $.ajax({
+        type: type,
+        url: url,
+        data: _data,
+        async: async,
+        success: function(data, status) {
+            //gestione success
+            callback(data, status);
+        },
+        error: function(data, status) {
+            callback(data, status);
+        }
+    });
+}
+
+function publish(){
+    
+}
