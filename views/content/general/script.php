b617c430c8bf83834ce3bda9e7ba664c0d560e3e
Stefano <stefanom@jamyourself.com>
2013-11-11 00:30:50 +0100

    aggiunto il caricamento dei dati nella view

--- views/content/general/script.php
+++ views/content/general/script.php
@@ -108,7 +108,7 @@ switch (basename($_SERVER['PHP_SELF'])) {
         break;
     case "uploadReview.php":
         ?>
-
+        <script type="text/javascript" src="resources/javascripts/customs/uploadReview.js"></script>
         <?php
         break;
 }
