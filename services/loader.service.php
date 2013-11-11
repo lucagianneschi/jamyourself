1cb604bab567e2ee78c82ca0c3d717446f5611c0
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-11-11 23:59:38 +0100

    modifica importazione del servizio di lingua

--- services/loader.service.php
+++ services/loader.service.php
@@ -23,19 +23,17 @@ function dynamicLoading($className) {
     require_once ROOT_DIR . 'config.php';
     require_once PARSE_DIR . 'parse.php';
     require_once SERVICES_DIR . 'debug.service.php';
+    require_once SERVICES_DIR . 'lang.service.php';
     if (strpos($className, 'Parse') !== false) {
 	require_once CLASSES_DIR . 'utilsClass.php';
-	require_once SERVICES_DIR . 'lang.service.php';
 	require_once LANGUAGES_DIR . 'classes/' . getLanguage() . '.classes.lang.php';
 	$file = CLASSES_DIR . lcfirst($className) . '.class.php';
     } elseif (strpos($className, 'Box') !== false) {
-	require_once SERVICES_DIR . 'lang.service.php';
 	require_once LANGUAGES_DIR . 'boxes/' . getLanguage() . '.boxes.lang.php';
 	require_once BOXES_DIR . 'utilsBox.php';
 	$filename = strtolower(str_replace('Box', '', $className));
 	$file = BOXES_DIR . lcfirst($filename) . '.box.php';
     } elseif (strpos($className, 'Controller') !== false) {
-	require_once SERVICES_DIR . 'lang.service.php';
 	require_once LANGUAGES_DIR . 'controllers/' . getLanguage() . '.controllers.lang.php';
 	require_once CONTROLLERS_DIR . 'restController.php';
 	$file = CONTROLLERS_DIR . lcfirst($className) . '.box.php';
@@ -49,5 +47,6 @@ function dynamicLoading($className) {
     }
     require_once $file;
 }
+
 spl_autoload_register('dynamicLoading');
 ?>
