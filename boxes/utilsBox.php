f89255be7e333e0ef74376ba5cfb41810a26fa17
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-11-13 00:14:02 +0100

    metodo getRelatedUser : prima stesura

--- boxes/utilsBox.php
+++ boxes/utilsBox.php
@@ -32,10 +32,10 @@ class Counters {
      * \param	$commentCounter, $loveCounter,$reviewCounter, $shareCounter
      */
     function __construct($commentCounter, $loveCounter, $reviewCounter, $shareCounter) {
-        is_null($commentCounter) ? $this->commentCounter = 0 : $this->commentCounter = $commentCounter;
-        is_null($loveCounter) ? $this->loveCounter = 0 : $this->loveCounter = $loveCounter;
-        is_null($reviewCounter) ? $this->reviewCounter = 0 : $this->reviewCounter = $reviewCounter;
-        is_null($shareCounter) ? $this->shareCounter = 0 : $this->shareCounter = $shareCounter;
+	is_null($commentCounter) ? $this->commentCounter = 0 : $this->commentCounter = $commentCounter;
+	is_null($loveCounter) ? $this->loveCounter = 0 : $this->loveCounter = $loveCounter;
+	is_null($reviewCounter) ? $this->reviewCounter = 0 : $this->reviewCounter = $reviewCounter;
+	is_null($shareCounter) ? $this->shareCounter = 0 : $this->shareCounter = $shareCounter;
     }
 
 }
@@ -57,14 +57,14 @@ class UserInfo {
      * \param	$objectId, $thumbnail, $type, $username
      */
     function __construct($objectId, $thumbnail, $type, $username) {
-        require_once SERVICES_DIR . 'lang.service.php';
-        require_once LANGUAGES_DIR . 'boxes/' . getLanguage() . '.boxes.lang.php';
-        global $boxes;
-        global $default_img;
-        is_null($objectId) ? $this->objectId = $boxes['NODATA'] : $this->objectId = $objectId;
-        is_null($thumbnail) ? $this->thumbnail = $default_img['DEFAVATARTHUMB'] : $this->thumbnail = $thumbnail;
-        is_null($type) ? $this->type = $boxes['NODATA'] : $this->type = $type;
-        is_null($username) ? $this->username = $boxes['NODATA'] : $this->username = $username;
+	require_once SERVICES_DIR . 'lang.service.php';
+	require_once LANGUAGES_DIR . 'boxes/' . getLanguage() . '.boxes.lang.php';
+	global $boxes;
+	global $default_img;
+	is_null($objectId) ? $this->objectId = $boxes['NODATA'] : $this->objectId = $objectId;
+	is_null($thumbnail) ? $this->thumbnail = $default_img['DEFAVATARTHUMB'] : $this->thumbnail = $thumbnail;
+	is_null($type) ? $this->type = $boxes['NODATA'] : $this->type = $type;
+	is_null($username) ? $this->username = $boxes['NODATA'] : $this->username = $username;
     }
 
 }
@@ -94,10 +94,63 @@ function sessionChecker() {
     $currentUserId = $boxes['NOID'];
     session_start();
     if (isset($_SESSION['currentUser'])) {
-        $currentUser = $_SESSION['currentUser'];
-        $currentUserId = $currentUser->getObjectId();
+	$currentUser = $_SESSION['currentUser'];
+	$currentUserId = $currentUser->getObjectId();
     }
     return $currentUserId;
+}
+
+/**
+ * \fn	getRelatedUsers($objectId, $field, $className, $all, $limit)
+ * \brief	Convenience method to get all kind of related User to a class for each page
+ * \param	$objectId for the istance of the class the user is supposed to be related to, $field to be related to, $all BOOL: Yes to retrieve all related users or using the limit from config file, $page the page which calls the method
+ * \return	userArray array of userInfo object
+ */
+function getRelatedUsers($objectId, $field, $className, $all, $limit) {
+    global $boxes;
+    $userArray = array();
+    require_once CLASSES_DIR . 'user.class.php';
+    require_once CLASSES_DIR . 'userParse.class.php';
+    $parseUser = new UserParse();
+    $parseUser->whereRelatedTo($field, $className, $objectId);
+    $parseUser->where('active', true);
+    if ($all == true) {
+	$parseUser->setLimit(1000);
+    } else {
+	$parseUser->setLimit($limit);
+    }
+    $users = $parseUser->getUsers();
+    if ($users instanceof Error) {
+	return $users;
+    } elseif (is_null($users)) {
+	if ($className == 'Record') {
+	    $users = $boxes['NOFEATRECORD'];
+	} else {
+	    switch ($field) {
+		case 'attendee':
+		    $users = $boxes['NOATTENDEE'];
+		    break;
+		case 'featuring':
+		    $users = $boxes['NOFEATEVE'];
+
+		    break;
+		case 'invited':
+		    $users = $boxes['NOINVITED'];
+		    break;
+	    }
+	}
+	return $users;
+    } else {
+	foreach ($users as $user) {
+	    $userId = $user->getObjectId();
+	    $thumbnail = $user->getProfileThumbnail();
+	    $type = $user->getType();
+	    $username = parse_decode_string($user->getUsername());
+	    $userInfo = new UserInfo($userId, $thumbnail, $type, $username);
+	    array_push($userArray, $userInfo);
+	}
+    }
+    return $userArray;
 }
 
 ?>
