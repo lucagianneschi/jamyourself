0bba805ffc97ed67dd02ce7972ac5e45748909ab
marifree <marialaurafresu@gmail.com>
2013-11-16 19:32:44 +0100

    profile:modifica style share

--- views/content/profile/box-social/box-recordReview.php
+++ views/content/profile/box-social/box-recordReview.php
@@ -167,7 +167,7 @@ $typeUser = $_POST['typeUser'];
 		<!-- AddThis Button BEGIN -->		
 		<div class="addthis_toolbox">   
 		    <div class="hover_menu">
-		        <div class="addthis_toolbox addthis_default_style addthis_32x32_style"
+		        <div class="addthis_toolbox addthis_default_style"
 			addThis:url="http://socialmusicdiscovering.com/tests/controllers/share/testShare2.controller.php?classe=Album"
 			addThis:title="Titolo della pagina di un album">
 		        <a class="addthis_button_twitter"></a>
