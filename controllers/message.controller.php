2d602d8070a6a708dc2a4afa263bca30b30d94b8
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-11-19 12:06:56 +0100

    utilizzo rollBackController

--- controllers/message.controller.php
+++ controllers/message.controller.php
@@ -36,8 +36,8 @@ class MessageController extends REST {
      * \brief   load config file for the controller
      */
     function __construct() {
-	parent::__construct();
-	$this->config = json_decode(file_get_contents(CONFIG_DIR . "controllers/message.config.json"), false);
+        parent::__construct();
+        $this->config = json_decode(file_get_contents(CONFIG_DIR . "controllers/message.config.json"), false);
     }
 
     /**
@@ -46,34 +46,36 @@ class MessageController extends REST {
      * \todo    testare
      */
     public function read() {
-	global $controllers;
-	try {
-	    if ($this->get_request_method() != "POST") {
-		$this->response(array('status' => $controllers['NOPOSTREQUEST']), 405);
-	    } elseif (!isset($_SESSION['currentUser'])) {
-		$this->response(array('status' => $controllers['USERNOSES']), 403);
-	    } elseif ($this->request['objectId']) {
-		$this->response(array('status' => $controllers['NOOBJECTID']), 403);
-	    }
-	    require_once CLASSES_DIR . 'activityParse.class.php';
-	    $objectId = $this->request['objectId'];
-	    $activityP = new ActivityParse();
-	    $activity = $activityP->getActivity($objectId);
-	    if ($activity instanceof Error) {
-		$this->response(array('status' => $controllers['NOACTFORREADMESS']), 503);
-	    }
-	    if ($activity->getRead() != false) {
-		$this->response(array('status' => $controllers['ALREADYREAD']), 503);
-	    } else{
+        global $controllers;
+        try {
+            if ($this->get_request_method() != "POST") {
+                $this->response(array('status' => $controllers['NOPOSTREQUEST']), 405);
+            } elseif (!isset($_SESSION['currentUser'])) {
+                $this->response(array('status' => $controllers['USERNOSES']), 403);
+            } elseif ($this->request['objectId']) {
+                $this->response(array('status' => $controllers['NOOBJECTID']), 403);
+            }
+            require_once CLASSES_DIR . 'activityParse.class.php';
+            $objectId = $this->request['objectId'];
+            $activityP = new ActivityParse();
+            $activity = $activityP->getActivity($objectId);
+            if ($activity instanceof Error) {
+                $this->response(array('status' => $controllers['NOACTFORREADMESS']), 503);
+            }
+            if ($activity->getRead() != false) {
+                $this->response(array('status' => $controllers['ALREADYREAD']), 503);
+            } else {
                 $res = $activityP->updateField($objectId, 'read', true);
             }
-	    if ($res instanceof Error) {
-		$this->rollback($objectId, 'readMessage');
-	    }
-	    $this->response(array($controllers['MESSAGEREAD']), 200);
-	} catch (Exception $e) {
-	    $this->response(array('status' => $e->getMessage()), 503);
-	}
+            if ($res instanceof Error) {
+                require_once CONTROLLERS_DIR . 'rollBack.controller.php';
+                $rollBackController = new RollBackController();
+                $rollBackController->rollbackMessageController($objectId, 'readMessage');
+            }
+            $this->response(array($controllers['MESSAGEREAD']), 200);
+        } catch (Exception $e) {
+            $this->response(array('status' => $e->getMessage()), 503);
+        }
     }
 
     /**
@@ -82,111 +84,95 @@ class MessageController extends REST {
      * \todo    testare, possibilità di invio a utenti multipli, controllo della relazione
      */
     public function message() {
-	global $controllers;
-	try {
-	    if ($this->get_request_method() != "POST") {
-		$this->response(array('status' => $controllers['NOPOSTREQUEST']), 405);
-	    } elseif (!isset($_SESSION['currentUser'])) {
-		$this->response(array('status' => $controllers['USERNOSES']), 403);
-	    } elseif (!isset($this->request['message'])) {
-		$this->response(array('status' => $controllers['NOMESSAGE']), 403);
-	    } elseif (!isset($this->request['toUser'])) {
-		$this->response(array('status' => $controllers['NOTOUSER']), 403);
-	    } elseif (!isset($this->request['title'])) {
-		$this->response(array('status' => $controllers['NOMESSAGETITLE']), 400);
-	    }
-	    $currentUser = $this->request['currentUser'];
-	    $toUserId = $this->request['toUser'];
-	    $toUserType = $this->request['toUserType'];
-	    if (relationChecker($currentUser->getObjectId(), $currentUser->getType(), $toUserId, $toUserType)) {
-		$this->response(array('status' => $controllers['NOSPAM']), 401);
-	    }
-	    $text = $this->request['message'];
-	    $title = $this->request['title'];
-	    if (strlen($text) < $this->config->minMessageSize) {
-		$this->response(array('status' => $controllers['SHORTMESSAGE'] . strlen($text)), 406);
-	    } elseif (strlen($title) < $this->config->minTitleSize) {
-		$this->response(array('status' => $controllers['SHORTTITLEMESSAGE'] . strlen($text)), 406);
-	    }
-	    require_once CONTROLLERS_DIR . 'utilsController.php';
-	    require_once CLASSES_DIR . 'comment.class.php';
-	    require_once CLASSES_DIR . 'commentParse.class.php';
-	    $message = new Comment();
-	    $message->setActive(true);
-	    $message->setAlbum(null);
-	    $message->setComment(null);
-	    $message->setCommentCounter(0);
-	    $message->setCommentators(null);
-	    $message->setComments(null);
-	    $message->setCounter(0);
-	    $message->setEvent(null);
-	    $message->setFromUser($currentUser->getObjectId());
-	    $message->setImage(null);
-	    $message->setLocation(null);
-	    $message->setLoveCounter(0);
-	    $message->setLovers(null);
-	    $message->setRecord(null);
-	    $message->setShareCounter(0);
-	    $message->setStatus(null);
-	    $message->setTags(null);
-	    $message->setText(parse_encode_string($text));
-	    $message->setTitle(parse_encode_string($title));
-	    $message->setToUser($toUserId);
-	    $message->setType('M');
-	    $message->setVideo(null);
-	    $message->setVote(null);
-	    $commentParse = new CommentParse();
-	    $resCmt = $commentParse->saveComment($message);
-	    if ($resCmt instanceof Error) {
-		$this->response(array('status' => 'NOSAVEMESS'), 503);
-	    }
-	    require_once CLASSES_DIR . 'activity.class.php';
-	    require_once CLASSES_DIR . 'activityParse.class.php';
-	    $activity = new Activity();
-	    $activity->setActive(true);
-	    $activity->setAlbum(null);
-	    $activity->setComment(null);
-	    $activity->setCounter(0);
-	    $activity->setEvent(null);
-	    $activity->setFromUser($currentUser->getObjectId());
-	    $activity->setImage(null);
-	    $activity->setPlaylist(null);
-	    $activity->setQuestion(null);
-	    $activity->setRead(false);
-	    $activity->setRecord(null);
-	    $activity->setSong(null);
-	    $activity->setStatus('A');
-	    $activity->setToUser($toUserId);
-	    $activity->setType('MESSAGESENT');
-	    $activity->setUserStatus(null);
-	    $activity->setVideo(null);
-	    $activityParse = new ActivityParse();
-	    $resActivity = $activityParse->saveActivity($activity);
-	    if ($resActivity instanceof Error) {
-		$this->rollback($resCmt->getObjectId(), 'sendMessage');
-	    }
-	    $this->response(array($controllers['MESSAGESAVED']), 200);
-	} catch (Exception $e) {
-	    $this->response(array('status' => $e->getMessage()), 503);
-	}
-    }
-
-    private function rollback($objectId, $function) {
-	global $controllers;
-	if ($function == 'sendMessage') {
-	    require_once CLASSES_DIR . 'commentParse.class.php';
-	    $commentParse = new CommentParse();
-	    $res = $commentParse->deleteComment($objectId);
-	} else {
-	    require_once CLASSES_DIR . 'activityParse.class.php';
-	    $activityParse = new ActivityParse();
-	    $res = $activityParse->updateField($objectId, 'read', true);
-	}
-	if ($res instanceof Error) {
-	    $this->response(array('status' => $controllers['ROLLKO']), 503);
-	} else {
-	    $this->response(array('status' => $controllers['ROLLOK']), 503);
-	}
+        global $controllers;
+        try {
+            if ($this->get_request_method() != "POST") {
+                $this->response(array('status' => $controllers['NOPOSTREQUEST']), 405);
+            } elseif (!isset($_SESSION['currentUser'])) {
+                $this->response(array('status' => $controllers['USERNOSES']), 403);
+            } elseif (!isset($this->request['message'])) {
+                $this->response(array('status' => $controllers['NOMESSAGE']), 403);
+            } elseif (!isset($this->request['toUser'])) {
+                $this->response(array('status' => $controllers['NOTOUSER']), 403);
+            } elseif (!isset($this->request['title'])) {
+                $this->response(array('status' => $controllers['NOMESSAGETITLE']), 400);
+            }
+            $currentUser = $this->request['currentUser'];
+            $toUserId = $this->request['toUser'];
+            $toUserType = $this->request['toUserType'];
+            if (relationChecker($currentUser->getObjectId(), $currentUser->getType(), $toUserId, $toUserType)) {
+                $this->response(array('status' => $controllers['NOSPAM']), 401);
+            }
+            $text = $this->request['message'];
+            $title = $this->request['title'];
+            if (strlen($text) < $this->config->minMessageSize) {
+                $this->response(array('status' => $controllers['SHORTMESSAGE'] . strlen($text)), 406);
+            } elseif (strlen($title) < $this->config->minTitleSize) {
+                $this->response(array('status' => $controllers['SHORTTITLEMESSAGE'] . strlen($text)), 406);
+            }
+            require_once CONTROLLERS_DIR . 'utilsController.php';
+            require_once CLASSES_DIR . 'comment.class.php';
+            require_once CLASSES_DIR . 'commentParse.class.php';
+            $message = new Comment();
+            $message->setActive(true);
+            $message->setAlbum(null);
+            $message->setComment(null);
+            $message->setCommentCounter(0);
+            $message->setCommentators(null);
+            $message->setComments(null);
+            $message->setCounter(0);
+            $message->setEvent(null);
+            $message->setFromUser($currentUser->getObjectId());
+            $message->setImage(null);
+            $message->setLocation(null);
+            $message->setLoveCounter(0);
+            $message->setLovers(null);
+            $message->setRecord(null);
+            $message->setShareCounter(0);
+            $message->setStatus(null);
+            $message->setTags(null);
+            $message->setText(parse_encode_string($text));
+            $message->setTitle(parse_encode_string($title));
+            $message->setToUser($toUserId);
+            $message->setType('M');
+            $message->setVideo(null);
+            $message->setVote(null);
+            $commentParse = new CommentParse();
+            $resCmt = $commentParse->saveComment($message);
+            if ($resCmt instanceof Error) {
+                $this->response(array('status' => 'NOSAVEMESS'), 503);
+            }
+            require_once CLASSES_DIR . 'activity.class.php';
+            require_once CLASSES_DIR . 'activityParse.class.php';
+            $activity = new Activity();
+            $activity->setActive(true);
+            $activity->setAlbum(null);
+            $activity->setComment(null);
+            $activity->setCounter(0);
+            $activity->setEvent(null);
+            $activity->setFromUser($currentUser->getObjectId());
+            $activity->setImage(null);
+            $activity->setPlaylist(null);
+            $activity->setQuestion(null);
+            $activity->setRead(false);
+            $activity->setRecord(null);
+            $activity->setSong(null);
+            $activity->setStatus('A');
+            $activity->setToUser($toUserId);
+            $activity->setType('MESSAGESENT');
+            $activity->setUserStatus(null);
+            $activity->setVideo(null);
+            $activityParse = new ActivityParse();
+            $resActivity = $activityParse->saveActivity($activity);
+            if ($resActivity instanceof Error) {
+                require_once CONTROLLERS_DIR . 'rollBack.controller.php';
+                $rollBackController = new RollBackController();
+                $rollBackController->rollbackMessageController($resCmt->getObjectId(), 'sendMessage');
+            }
+            $this->response(array($controllers['MESSAGESAVED']), 200);
+        } catch (Exception $e) {
+            $this->response(array('status' => $e->getMessage()), 503);
+        }
     }
 
 }
