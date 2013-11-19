cb002716a9fc9a09dc3afe5ce0575264fb0c1b67
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-11-19 09:56:56 +0100

    corretto errore dopo uso getReletedUsers, testato

--- boxes/relation.box.php
+++ boxes/relation.box.php
@@ -34,112 +34,103 @@ class RelationsBox {
      * \brief	class construct to import config file
      */
     function __construct() {
-	$this->config = json_decode(file_get_contents(CONFIG_DIR . "boxes/relation.config.json"), false);
+        $this->config = json_decode(file_get_contents(CONFIG_DIR . "boxes/relation.config.json"), false);
     }
 
     public function initForPersonalPage($objectId, $type) {
-	global $boxes;
-	$info = array();
-	$followingArray = array();
-	$friendshipArray = array();
-	$venuesArray = array();
-	$jammersArray = array();
-	$followersArray = array();
-	$relationsBox = new RelationsBox();
-	if ($type == 'SPOTTER') {
-	    $activityFollowing = new ActivityParse();
-	    $activityFollowing->wherePointer('fromUser', '_User', $objectId);
-	    $activityFollowing->whereEqualTo('type', 'FOLLOWING');
-	    $activityFollowing->where('active', true);
-	    $activityFollowing->whereInclude('toUser');
-	    $activityFollowing->setLimit($this->config->following);
-	    $activityFollowing->orderByDescending('createdAt');
-	    $followings = $activityFollowing->getActivities();
-	    if ($followings instanceof Error) {
-		return $followings;
-	    } elseif (is_null($followings)) {
-		$followingArray = $boxes['NOFOLLOWING'];
-	    } else {
-		foreach ($followings as $following) {
-		    $followingId = $following->getToUser()->getObjectId();
-		    $thumbnail = $following->getToUser()->getProfileThumbnail();
-		    $type = $following->getToUser()->getType();
-		    $username = parse_decode_string($following->getToUser()->getUserName());
-		    $userInfo = new UserInfo($followingId, $thumbnail, $type, $username);
-		    array_push($followingArray, $userInfo);
-		}
-	    }
-	    $activityFriendship = new ActivityParse();
-	    $activityFriendship->setLimit($this->config->friends);
-	    $activityFriendship->wherePointer('fromUser', '_User', $objectId);
-	    $activityFriendship->whereEqualTo('type', 'FRIENDSHIPREQUEST');
-	    $activityFriendship->whereEqualTo('status', 'A');
-	    $activityFriendship->where('active', true);
-	    $activityFriendship->whereInclude('toUser');
-	    $activityFriendship->orderByDescending('createdAt');
-	    $friendships = $activityFriendship->getActivities();
-	    if ($friendships instanceof Error) {
-		return $friendships;
-	    } elseif (is_null($friendships)) {
-		$friendshipArray = $boxes['NOFRIENDS'];
-	    } else {
-		foreach ($friendships as $friendship) {
-		    $friendshipId = $friendship->getToUser()->getObjectId();
-		    $thumbnail = $friendship->getToUser()->getProfileThumbnail();
-		    $type = $friendship->getToUser()->getType();
-		    $username = parse_decode_string($friendship->getToUser()->getUserName());
-		    $userInfo = new UserInfo($friendshipId, $thumbnail, $type, $username);
-		    array_push($friendshipArray, $userInfo);
-		}
-	    }
-	    $info = array('followers' => $boxes['ND'], 'following' => $followingArray, 'friendship' => $friendshipArray, 'venuesCollaborators' => $boxes['ND'], 'jammersCollaborators' => $boxes['ND']);
-	} else {
-	    $collaborators = getRelatedUsers($objectId, 'collaboration', '_User', false, $this->config->collaborations, 0);
-	    if ($collaborators instanceof Error) {
-		return $collaborators;
-	    } elseif (is_null($collaborators)) {
-		$venuesArray = $boxes['NOVENUE'];
-		$jammersArray = $boxes['NOJAMMER'];
-	    } else {
-		foreach ($collaborators as $collaborator) {
-		    $collaboratorId = $collaborator->getObjectId();
-		    $thumbnail = $collaborator->getProfileThumbnail();
-		    $type = $collaborator->getType();
-		    $username = parse_decode_string($collaborator->getUserName());
-		    $userInfo = new UserInfo($collaboratorId, $thumbnail, $type, $username);
-		    if ($type == 'VENUE') {
-			array_push($venuesArray, $userInfo);
-		    } else {
-			array_push($jammersArray, $userInfo);
-		    }
-		}
-	    }
-	    $following = new ActivityParse();
-	    $following->wherePointer('toUser', '_User', $objectId);
-	    $following->whereEqualTo('type', 'FOLLOWING');
-	    $following->where('active', true);
-	    $following->whereInclude('fromUser');
-	    $following->setLimit($this->config->followingProfessional);
-	    $following->orderByDescending('createdAt');
-	    $followers = $following->getActivities();
-	    if ($followers instanceof Error) {
-		return $followers;
-	    } elseif (is_null($followers)) {
-		$followersArray = $boxes['NOFOLLOWERS'];
-	    } else {
-		foreach ($followers as $follower) {
-		    $followerId = $follower->getFromUser()->getObjectId();
-		    $thumbnail = $follower->getFromUser()->getProfileThumbnail();
-		    $type = $follower->getFromUser()->getType();
-		    $username = parse_decode_string($follower->getFromUser()->getUserName());
-		    $userInfo = new UserInfo($followerId, $thumbnail, $type, $username);
-		    array_push($followersArray, $userInfo);
-		}
-	    }
-	    $info = array('followers' => $followersArray, 'following' => $boxes['ND'], 'friendship' => $boxes['ND'], 'venuesCollaborators' => $venuesArray, 'jammersCollaborators' => $jammersArray);
-	}
-	$relationsBox->relationArray = $info;
-	return $relationsBox;
+        global $boxes;
+        $info = array();
+        $followingArray = array();
+        $friendshipArray = array();
+        $venuesArray = array();
+        $jammersArray = array();
+        $followersArray = array();
+        $relationsBox = new RelationsBox();
+        if ($type == 'SPOTTER') {
+            $activityFollowing = new ActivityParse();
+            $activityFollowing->wherePointer('fromUser', '_User', $objectId);
+            $activityFollowing->whereEqualTo('type', 'FOLLOWING');
+            $activityFollowing->where('active', true);
+            $activityFollowing->whereInclude('toUser');
+            $activityFollowing->setLimit($this->config->following);
+            $activityFollowing->orderByDescending('createdAt');
+            $followings = $activityFollowing->getActivities();
+            if ($followings instanceof Error) {
+                return $followings;
+            } elseif (is_null($followings)) {
+                $followingArray = $boxes['NOFOLLOWING'];
+            } else {
+                foreach ($followings as $following) {
+                    $followingId = $following->getToUser()->getObjectId();
+                    $thumbnail = $following->getToUser()->getProfileThumbnail();
+                    $type = $following->getToUser()->getType();
+                    $username = parse_decode_string($following->getToUser()->getUserName());
+                    $userInfo = new UserInfo($followingId, $thumbnail, $type, $username);
+                    array_push($followingArray, $userInfo);
+                }
+            }
+            $activityFriendship = new ActivityParse();
+            $activityFriendship->setLimit($this->config->friends);
+            $activityFriendship->wherePointer('fromUser', '_User', $objectId);
+            $activityFriendship->whereEqualTo('type', 'FRIENDSHIPREQUEST');
+            $activityFriendship->whereEqualTo('status', 'A');
+            $activityFriendship->where('active', true);
+            $activityFriendship->whereInclude('toUser');
+            $activityFriendship->orderByDescending('createdAt');
+            $friendships = $activityFriendship->getActivities();
+            if ($friendships instanceof Error) {
+                return $friendships;
+            } elseif (is_null($friendships)) {
+                $friendshipArray = $boxes['NOFRIENDS'];
+            } else {
+                foreach ($friendships as $friendship) {
+                    $friendshipId = $friendship->getToUser()->getObjectId();
+                    $thumbnail = $friendship->getToUser()->getProfileThumbnail();
+                    $type = $friendship->getToUser()->getType();
+                    $username = parse_decode_string($friendship->getToUser()->getUserName());
+                    $userInfo = new UserInfo($friendshipId, $thumbnail, $type, $username);
+                    array_push($friendshipArray, $userInfo);
+                }
+            }
+            $info = array('followers' => $boxes['ND'], 'following' => $followingArray, 'friendship' => $friendshipArray, 'venuesCollaborators' => $boxes['ND'], 'jammersCollaborators' => $boxes['ND']);
+        } else {
+            $collaborators = getRelatedUsers($objectId, 'collaboration', '_User', false, $this->config->collaborations, 0);
+            if ($collaborators instanceof Error) {
+                return $collaborators;
+            } elseif (is_null($collaborators)) {
+                $venuesArray = $boxes['NOVENUE'];
+                $jammersArray = $boxes['NOJAMMER'];
+            } else {
+                foreach ($collaborators as $collaborator) {
+                    ($collaborator->type == 'VENUE') ? array_push($venuesArray, $collaborator) : array_push($jammersArray, $collaborator);
+                }
+            }
+            $following = new ActivityParse();
+            $following->wherePointer('toUser', '_User', $objectId);
+            $following->whereEqualTo('type', 'FOLLOWING');
+            $following->where('active', true);
+            $following->whereInclude('fromUser');
+            $following->setLimit($this->config->followingProfessional);
+            $following->orderByDescending('createdAt');
+            $followers = $following->getActivities();
+            if ($followers instanceof Error) {
+                return $followers;
+            } elseif (is_null($followers)) {
+                $followersArray = $boxes['NOFOLLOWERS'];
+            } else {
+                foreach ($followers as $follower) {
+                    $followerId = $follower->getFromUser()->getObjectId();
+                    $thumbnail = $follower->getFromUser()->getProfileThumbnail();
+                    $type = $follower->getFromUser()->getType();
+                    $username = parse_decode_string($follower->getFromUser()->getUserName());
+                    $userInfo = new UserInfo($followerId, $thumbnail, $type, $username);
+                    array_push($followersArray, $userInfo);
+                }
+            }
+            $info = array('followers' => $followersArray, 'following' => $boxes['ND'], 'friendship' => $boxes['ND'], 'venuesCollaborators' => $venuesArray, 'jammersCollaborators' => $jammersArray);
+        }
+        $relationsBox->relationArray = $info;
+        return $relationsBox;
     }
 
 }
