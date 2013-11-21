3ae66db163bdf325f3765d47c9c2d64c5074b57e
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-11-21 14:14:43 +0100

    eliminata funzione doppia

--- controllers/utilsController.php
+++ controllers/utilsController.php
@@ -8,7 +8,7 @@
  *
  *  \par		Info Classe:
  *  \brief		Utils class
- *  \details	Classe di utilità sfruttata dai controller  per snellire il codice
+ *  \details            Classe di utilità sfruttata dai controller  per snellire il codice
  *
  *  \par		Commenti:
  *  \warning
@@ -16,63 +16,6 @@
  *  \todo       
  *
  */
-if (!defined('ROOT_DIR'))
-    define('ROOT_DIR', '../');
-
-require_once ROOT_DIR . 'config.php';
-require_once SERVICES_DIR . 'lang.service.php';
-require_once LANGUAGES_DIR . 'controllers/' . getLanguage() . '.controllers.lang.php';
-require_once SERVICES_DIR . 'debug.service.php';
-
-/**
- * \fn	rollbackCommentInstanceController($objectId)
- * \brief   rollback for CommentController e PostController
- * \param   $objectId dell'oggetto su cui fare delete
- */
-function rollbackCommentController($objectId, $classType) {
-    global $controllers;
-    require_once CLASSES_DIR . 'commentParse.class.php';
-    $commentParse = new CommentParse();
-    $resCmt = $commentParse->deleteComment($objectId);
-    switch ($classType) {
-	case 'Album':
-	    require_once CLASSES_DIR . 'albumParse.class.php';
-	    $albumParse = new AlbumParse();
-	    $res = $albumParse->decrementAlbum($objectId, 'commentCounter', 1);
-	    break;
-	case 'Comment':
-	    $res = $commentParse->decrementComment($objectId, 'commentCounter', 1);
-	    break;
-	case 'Event':
-	    require_once CLASSES_DIR . 'eventParse.class.php';
-	    $eventParse = new EventParse();
-	    $res = $eventParse->decrementEvent($objectId, 'commentCounter', 1);
-	    break;
-	case 'Image':
-	    require_once CLASSES_DIR . 'imageParse.class.php';
-	    $imageParse = new ImageParse();
-	    $res = $imageParse->decrementImage($objectId, 'commentCounter', 1);
-	    break;
-	case 'Status':
-	    require_once CLASSES_DIR . 'statusParse.class.php';
-	    $statusParse = new StatusParse();
-	    $res = $statusParse->decrementStatus($objectId, 'commentCounter', 1);
-	    break;
-	case 'Record':
-	    require_once CLASSES_DIR . 'recordParse.class.php';
-	    $recordParse = new RecordParse();
-	    $res = $recordParse->decrementRecord($objectId, 'commentCounter', 1);
-	    break;
-	case 'Video':
-	    require_once CLASSES_DIR . 'videoParse.class.php';
-	    $videoParse = new VideoParse();
-	    $res = $videoParse->incrementVideo($objectId, 'commentCounter', 1);
-	    break;
-    }
-    $message = ($res instanceof Error || $resCmt instanceof Error) ? $controllers['ROLLKO'] : $controllers['ROLLOK'];
-    return $message;
-}
-
 
 
 /**
