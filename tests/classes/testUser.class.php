60c20f20cfa97f0d74c74696ea614663ad716b82
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-11-23 23:13:52 +0100

    aggiornato con nuova property

--- tests/classes/testUser.class.php
+++ tests/classes/testUser.class.php
@@ -1,22 +1,23 @@
 <?php
+
 /* ! \par	    Info Generali:
-* \author	    Daniele Caldelli
-* \version	    1.0
-* \date		    2013
-* \copyright	    Jamyourself.com 2013
-*
-* \par		    Info Classe:
-* \brief	    Classe di test
-* \details	    Classe di test per la classe User
-*
-* \par Commenti:
-* \warning
-* \bug
-* \todo 
-*
-*/
+ * \author	    Daniele Caldelli
+ * \version	    1.0
+ * \date		    2013
+ * \copyright	    Jamyourself.com 2013
+ *
+ * \par		    Info Classe:
+ * \brief	    Classe di test
+ * \details	    Classe di test per la classe User
+ *
+ * \par Commenti:
+ * \warning
+ * \bug
+ * \todo 
+ *
+ */
 if (!defined('ROOT_DIR'))
-	define('ROOT_DIR', '../../');
+    define('ROOT_DIR', '../../');
 
 ini_set('display_errors', '1');
 
@@ -28,15 +29,15 @@ require_once CLASSES_DIR . 'userParse.class.php';
 // TODO - cambiare il tipo a piacimento per eseguire i test
 $tipo = 'SPOTTER';
 try {
-	if ($tipo == 'VENUE') {
-		$user = new User('VENUE');
-	} elseif ($tipo == 'JAMMER') {
-		$user = new User('JAMMER');
-	} elseif ($tipo == 'SPOTTER') {
-		$user = new User('SPOTTER');
-	}
-} catch (Exception $e){
-	die($e->getMessage());
+    if ($tipo == 'VENUE') {
+	$user = new User('VENUE');
+    } elseif ($tipo == 'JAMMER') {
+	$user = new User('JAMMER');
+    } elseif ($tipo == 'SPOTTER') {
+	$user = new User('SPOTTER');
+    }
+} catch (Exception $e) {
+    die($e->getMessage());
 }
 
 $user->setObjectId();
@@ -56,7 +57,7 @@ $user->setCollaborationCounter(666);
 $user->setComments(array("nJr1ulgfVo"));
 $user->setCountry('Un paese');
 $user->setDescription('Una descrizione');
-$user->setEmail('test'. $r .'@xxx.xx');
+$user->setEmail('test' . $r . '@xxx.xx');
 $user->setFbPage('Una pagina FB');
 $user->setFollowersCounter(666);
 $user->setFollowingCounter(666);
@@ -106,9 +107,9 @@ echo '<br />INIZIO IL SALVATAGGIO DELLO User APPENA CREATO<br />';
 $userParse = new UserParse();
 $resSave = $userParse->saveUser($user);
 if (get_class($resSave) == 'Error') {
-	echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $resSave->getErrorMessage() . '<br/>';
+    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $resSave->getErrorMessage() . '<br/>';
 } else {
-	echo '<br />User SAVED:<br />' . $resSave . '<br />';
+    echo '<br />User SAVED:<br />' . $resSave . '<br />';
 }
 
 echo '<br />FINITO IL SALVATAGGIO DELLO User APPENA CREATO<br />';
@@ -120,9 +121,9 @@ echo '<br />INIZIO IL RECUPERO DI UNO User<br /><br />';
 $userParse = new UserParse();
 $resGet = $userParse->getUser($resSave->getObjectId());
 if (get_class($resGet) == 'Error') {
-	echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $resGet->getErrorMessage() . '<br/>';
+    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $resGet->getErrorMessage() . '<br/>';
 } else {
-	echo $resGet;
+    echo $resGet;
 }
 
 echo '<br />FINITO IL RECUPERO DI UNO User<br />';
@@ -134,9 +135,9 @@ echo '<br />INIZIO LA CANCELLAZIONE DI UNO User<br />';
 $userParse = new UserParse();
 $resDelete = $userParse->deleteUser($resSave->getObjectId());
 if (get_class($resDelete)) {
-	echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $resDelete->getErrorMessage() . '<br/>';
+    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $resDelete->getErrorMessage() . '<br/>';
 } else {
-	echo '<br />User DELETED<br />';
+    echo '<br />User DELETED<br />';
 }
 
 echo '<br />FINITO LA CANCELLAZIONE DI UNO User<br />';
@@ -151,11 +152,11 @@ $userParse->orderByDescending('createdAt');
 $userParse->setLimit(5);
 $resGets = $userParse->getUsers();
 if (get_class($resGets) == 'Error') {
-	echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $resGets->getErrorMessage() . '<br/>';
+    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $resGets->getErrorMessage() . '<br/>';
 } else {
-	foreach($resGets as $user) {
-		echo '<br />' . $user->getObjectId() . '<br />';
-	}
+    foreach ($resGets as $user) {
+	echo '<br />' . $user->getObjectId() . '<br />';
+    }
 }
 
 echo '<br />FINITO IL RECUPERO DI PIU\' User<br />';
@@ -169,9 +170,9 @@ $user = $userParse->getUser($resSave->getObjectId());
 $user->setLevel(99);
 $resUpdate = $userParse->saveUser($user);
 if (get_class($resUpdate)) {
-	echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $resUpdate->getErrorMessage() . '<br/>';
+    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $resUpdate->getErrorMessage() . '<br/>';
 } else {
-	echo '<br />User UPDATED<br />';
+    echo '<br />User UPDATED<br />';
 }
 
 echo '<br />FINITO L\'AGGIORNAMENTO DI UNO User<br />';
@@ -185,9 +186,9 @@ $username = $userEpass;
 $password = $userEpass;
 $resLogin = $userParse->loginUser($username, $password);
 if (get_class($resLogin) == 'Error') {
-	echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $resLogin->getErrorMessage() . '<br/>';
+    echo '<br />ATTENZIONE: e\' stata generata un\'eccezione: ' . $resLogin->getErrorMessage() . '<br/>';
 } else {
-	echo '<br />User LOGGED<br />';
+    echo '<br />User LOGGED<br />';
 }
 
 echo '<br />FINITO IL LOGIN DI UNO User<br />';
@@ -198,43 +199,43 @@ echo '<br />INIZIO L\'AGGIORNAMENTO DEI SINGOLI CAMPI DELLO User<br />';
 
 $userParse = new UserParse();
 
-$userParse->updateField($resSave->getObjectId(), $resSave->getSessionToken(), 'active', true);
+$userParse->updateField($resSave->getObjectId(), 'active', true);
 echo 'Aggiornato un campo boolean<br />';
-$userParse->updateField($resSave->getObjectId(), $resSave->getSessionToken(), 'address', 'Un indirizzo');
+$userParse->updateField($resSave->getObjectId(), 'address', 'Un indirizzo');
 echo 'Aggiornato un campo string<br />';
-$userParse->updateField($resSave->getObjectId(), $resSave->getSessionToken(), 'level', 101);
+$userParse->updateField($resSave->getObjectId(), 'level', 101);
 echo 'Aggiornato un campo number<br />';
-$userParse->updateField($resSave->getObjectId(), $resSave->getSessionToken(), 'settings', array('set1', 'set2'));
+$userParse->updateField($resSave->getObjectId(), 'settings', array('set1', 'set2'));
 echo 'Aggiornato un campo array<br />';
 
 //$userParse->updateField($resSave->getObjectId(), 'image', toParsePointer('Image', 'MuTAFCZIKd'));
 //echo 'Aggiornato un campo Pointer<br />';
 
 $parseGeoPoint = new parseGeoPoint('56.78', '12.34');
-$userParse->updateField($resSave->getObjectId(), $resSave->getSessionToken(), 'geoCoding', toParseGeoPoint($parseGeoPoint));
+$userParse->updateField($resSave->getObjectId(), 'geoCoding', toParseGeoPoint($parseGeoPoint));
 echo 'Aggiornato un campo GeoPoint<br />';
 
 $parseACL = new parseACL();
 $parseACL->setPublicWriteAccess(false);
-$userParse->updateField($resSave->getObjectId(), $resSave->getSessionToken(), 'ACL', toParseACL($parseACL));
+$userParse->updateField($resSave->getObjectId(), 'ACL', toParseACL($parseACL));
 echo 'Aggiornato un campo ACL<br />';
 
-$userParse->updateField($resSave->getObjectId(), $resSave->getSessionToken(), 'collaboration', array('n1TXVlIqHw', 'WeTEWWfASn'), true, 'add', '_User');
+$userParse->updateField($resSave->getObjectId(), 'collaboration', array('n1TXVlIqHw', 'WeTEWWfASn'), true, 'add', '_User');
 echo 'Aggiornato (add) un campo Relation<br />';
 
-$userParse->updateField($resSave->getObjectId(), $resSave->getSessionToken(), 'collaboration', array('WeTEWWfASn'), true, 'remove', '_User');
+$userParse->updateField($resSave->getObjectId(), 'collaboration', array('WeTEWWfASn'), true, 'remove', '_User');
 echo 'Aggiornato (remove) un campo Relation<br />';
 
-$userParse->updateField($resSave->getObjectId(), $resSave->getSessionToken(), 'followers', array('n1TXVlIqHw', 'WeTEWWfASn'), true, 'add', '_User');
+$userParse->updateField($resSave->getObjectId(), 'followers', array('n1TXVlIqHw', 'WeTEWWfASn'), true, 'add', '_User');
 echo 'Aggiornato (add) un campo Relation<br />';
 
-$userParse->updateField($resSave->getObjectId(), $resSave->getSessionToken(), 'followers', array('WeTEWWfASn'), true, 'remove', '_User');
+$userParse->updateField($resSave->getObjectId(), 'followers', array('WeTEWWfASn'), true, 'remove', '_User');
 echo 'Aggiornato (remove) un campo Relation<br />';
 
-$userParse->updateField($resSave->getObjectId(), $resSave->getSessionToken(), 'following', array('n1TXVlIqHw', 'WeTEWWfASn'), true, 'add', '_User');
+$userParse->updateField($resSave->getObjectId(), 'following', array('n1TXVlIqHw', 'WeTEWWfASn'), true, 'add', '_User');
 echo 'Aggiornato (add) un campo Relation<br />';
 
-$userParse->updateField($resSave->getObjectId(), $resSave->getSessionToken(), 'following', array('WeTEWWfASn'), true, 'remove', '_User');
+$userParse->updateField($resSave->getObjectId(), 'following', array('WeTEWWfASn'), true, 'remove', '_User');
 echo 'Aggiornato (remove) un campo Relation<br />';
 
 echo '<br />FINITO L\'AGGIORNAMENTO DEI SINGOLI CAMPI DEL Comment<br />';
