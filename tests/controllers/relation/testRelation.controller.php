e6d2cd1138c96437169150bb66d9b5353f701388
Daniele Caldelli <daniele.caldelli@gmail.com>
2013-11-24 01:25:03 +0100

    Modificata per utilizzarla con un parametro

--- tests/controllers/relation/testRelation.controller.php
+++ tests/controllers/relation/testRelation.controller.php
@@ -102,7 +102,8 @@
     <body>
 	Cliccando i bottoni seguenti si inviano richieste di relazioni, si accettano richieste o si declinano richieste<br />
 	<br />
-	<button type="button" onclick="sendRelation('GuUAj83MGH')">Relazione currentUser -> SPOTTER (GuUAj83MGH)</button>
+	<input type="text" id="objectIdRichiedi" placeholder="objectIdRichiedi"/>
+	<button type="button" onclick="sendRelation($('#objectIdRichiedi').val())">Richiedi Relazione</button>
 	<br />
 	<input type="text" id="objectIdDaAccettare" placeholder="objectIdDaAccettare"/>
 	<input type="text" id="toUserobjectIdDaAccettare" placeholder="toUserobjectIdDaAccettare"/>
@@ -114,34 +115,5 @@
 	<input type="text" id="objectIdDaCancellare" placeholder="objectIdDaCancellare"/>
 	<input type="text" id="toUserobjectIdDaCancellare" placeholder="toUserobjectIdDaCancellare"/>
 	<button type="button" onclick="removeRelation($('#objectIdDaCancellare').val(), $('#toUserobjectIdDaCancellare').val())">Cancella la Relazione</button>
-	&nbsp;<hr>
-	<button type="button" onclick="love('Comment', 'rEJJMsGCTo', 'increment')">Increment Love Comment rEJJMsGCTo</button>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-	<button type="button" onclick="love('Comment', 'rEJJMsGCTo', 'decrement')">Decrement Love Comment rEJJMsGCTo</button>
-	&nbsp;<hr>
-	<button type="button" onclick="love('Event', 'OXi0VJUoao', 'increment')">Increment Love Event OXi0VJUoao</button>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-	<button type="button" onclick="love('Event', 'OXi0VJUoao', 'decrement')">Decrement Love Event OXi0VJUoao</button>
-	&nbsp;<hr>
-	<button type="button" onclick="love('Image', 'gdZowTbFRk', 'increment')">Increment Love Image gdZowTbFRk</button>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-	<button type="button" onclick="love('Image', 'gdZowTbFRk', 'decrement')">Decrement Love Image gdZowTbFRk</button>
-	&nbsp;<hr>
-	<button type="button" onclick="love('Record', 'Xbu7rDWqpj', 'increment')">Increment Love Record Xbu7rDWqpj</button>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-	<button type="button" onclick="love('Record', 'Xbu7rDWqpj', 'decrement')">Decrement Love Record Xbu7rDWqpj</button>
-	&nbsp;<hr>
-	<button type="button" onclick="love('Song', 'j0AM1J4YIR', 'increment')">Increment Love Song j0AM1J4YIR</button>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-	<button type="button" onclick="love('Song', 'j0AM1J4YIR', 'decrement')">Decrement Love Song j0AM1J4YIR</button>
-	&nbsp;<hr>
-	<button type="button" onclick="love('Status', '4byv8FeP7S', 'increment')">Increment Love Status 4byv8FeP7S</button>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-	<button type="button" onclick="love('Status', '4byv8FeP7S', 'decrement')">Decrement Love Status 4byv8FeP7S</button>
-	&nbsp;<hr>
-	<button type="button" onclick="love('Video', 'ihcPvm6BIv', 'increment')">Increment Love Video ihcPvm6BIv</button>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-	<button type="button" onclick="love('Video', 'ihcPvm6BIv', 'decrement')">Decrement Love Video ihcPvm6BIv</button>
-	<hr>
     </body>
 </html>
