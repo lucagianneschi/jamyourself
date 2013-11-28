cc98d5ffae062433b83a162c25132196419d2b17
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-11-28 23:54:58 +0100

    eliminate require_once non più utili

--- boxes/faq.box.php
+++ boxes/faq.box.php
@@ -18,8 +18,6 @@ if (!defined('ROOT_DIR'))
     define('ROOT_DIR', '../');
 
 require_once ROOT_DIR . 'config.php';
-require_once SERVICES_DIR . 'lang.service.php';
-require_once LANGUAGES_DIR . 'boxes/' . getLanguage() . '.boxes.lang.php';
 require_once CLASSES_DIR . 'faq.class.php';
 require_once CLASSES_DIR . 'faqParse.class.php';
 
