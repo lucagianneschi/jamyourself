6b3eace84c528a4e23c378cc033ab008f8e7782f
Stefano <stefanom@jamyourself.com>
2013-12-01 22:58:48 +0100

    individuata in "publishRecord" la funzione che crea l'anomialia della pagina bianca.

--- controllers/uploadRecord.controller.php
+++ controllers/uploadRecord.controller.php
@@ -64,128 +64,128 @@ class uploadRecordController extends REST {
 
     }
 
-//    public function albumCreate() {
-//
-//        global $controllers;
-//
-//        if ($this->get_request_method() != "POST" || !isset($_SESSION['currentUser'])) {
-//            $this->response('', 406);
-//        }
-//
-//        $albumJSON = $this->request;
-//        $newAlbum = json_decode(json_encode($albumJSON), false);
-//
-////        
-////            "albumTitle": $("#albumTitle").val(),
-////            "description": $("#description").val(),
-////            "tags": getTagsAlbumCreate()        
-//
-//        if (!isset($newAlbum->albumTitle) || is_null($newAlbum->albumTitle) || !(strlen($newAlbum->albumTitle) > 0) ||
-//                !isset($newAlbum->description) || is_null($newAlbum->description) || !(strlen($newAlbum->description) > 0) ||
-//                !isset($newAlbum->tags) || is_null($newAlbum->tags) || !is_array($newAlbum->tags) || !(count($newAlbum->tags) > 0)
-//        ) {
-//            $error = array('status' => "Bad Request", "msg" => "Invalid new album");
-//            $this->response($error, 400);
-//        }
-//
-//        $user = $_SESSION['currentUser'];
-//        $userId = $user->getObjectId();
-//
-//        if ($user->getType() != "JAMMER") {
-//            $error = array('status' => "Bad Request", "msg" => "Invalid user type");
-//            $this->response($error, 400);
-//        }
-//
-//        $pRecord = new RecordParse();
-//        $record = new Record();
-//
-//        $record->setActive(true);
-//        $record->setBuyLink($newAlbum->urlBuy);
-//        $record->setCommentCounter(0);
-//        $record->setCounter(0);
-////$record->setCoverFile();
-//        $imgInfo = $this->getImages($newAlbum);
-//        $record->setCover($imgInfo['RecordPicture']);
-//        $record->setThumbnailCover($imgInfo['RecordThumbnail']);
-//
-//        $record->setDescription($newAlbum->albumTitle);
-//        $record->setDuration(0);
-//        $record->setFeaturing($newAlbum->albumFeaturing);
-//        $record->setFromUser($userId);
-//        $record->setGenre($this->getTags($newAlbum->tags));
-//        $record->setLabel($newAlbum->label);
-//
-//        if (($location = GeocoderService::getLocation($newAlbum->city))) {
-//            $parseGeoPoint = new parseGeoPoint($location['lat'], $location['lng']);
-//            $record->setLocation($parseGeoPoint);
-//        }
-//
-//        $record->setLoveCounter(0);
-//        $record->setReviewCounter(0);
-//        $record->setShareCounter(0);
-//        $record->setTitle($newAlbum->albumTitle);
-//        $record->setYear($newAlbum->year);
-//
-//        $newRecord = $pRecord->saveRecord($record);
-//
-//        if ($newRecord instanceof Error) {
-////result è un errore e contiene il motivo dell'errore
-//            $error = array('status' => "Service Unavailable", "msg" => $newRecord->getErrorMessage());
-//            $this->response($error, 503);
-//        }
-//
-////se va a buon fine salvo una nuova activity       
-//        $activity = new Activity();
-//        $activity->setActive(true);
-//        $activity->setFromUser($userId);
-//        $activity->setRead(true);
-//        $activity->setStatus("A");
-//        $activity->setType("CREATEDRECORD");
-////            $activity->setACL(toParseDefaultACL());
-//
-//        $pActivity = new ActivityParse();
-//        $pActivity->saveActivity($activity);
-//
-//        $this->createFolderForRecord($userId, $newRecord->getObjectId());
-//
-//        $dirThumbnailDest = USERS_DIR . $userId . "/images/recordcover";
-//        $dirCoverDest = USERS_DIR . $userId . "/images/recordcoverthumb";
-//
-//
-////SPOSTO LE IMMAGINI NELLE RISPETTIVE CARTELLE         
-//        if (!is_null($record->getThumbnailCover()) && strlen($record->getThumbnailCover()) > 0 && strlen($record->getCover()) && !is_null($record->getCover())) {
-//            rename(MEDIA_DIR . "cache/" . $record->getThumbnailCover(), $dirThumbnailDest . DIRECTORY_SEPARATOR . $record->getThumbnailCover());
-//            rename(MEDIA_DIR . "cache/" . $record->getCover(), $dirCoverDest . DIRECTORY_SEPARATOR . $record->getCover());
-//        }
-//
-//        unset($_SESSION['currentUserFeaturingArray']);
-//
-//        $this->response(array("res" => "OK", "recordId" => $newRecord->getObjectId()), 200);
-//    }
+    public function albumCreate() {
 
-//    private function getTags($list) {
-//        return implode(",", $list);
-//    }
+        global $controllers;
 
-//    private function createFolderForRecord($userId, $albumId) {
-//        try {
-//            if (!is_null($userId) && strlen($userId) > 0) {
-//                mkdir(USERS_DIR . $userId . DIRECTORY_SEPARATOR . "songs" . DIRECTORY_SEPARATOR . $albumId, 0, true);
-//
-//                if (!is_dir(USERS_DIR . $userId . DIRECTORY_SEPARATOR . "images")) {
-//                    $bool = mkdir(USERS_DIR . $userId . DIRECTORY_SEPARATOR . "images");
-//                }
-//                if (!is_dir(USERS_DIR . $userId . DIRECTORY_SEPARATOR . "images" . DIRECTORY_SEPARATOR . "recordcover")) {
-//                    $bool = mkdir(USERS_DIR . $userId . DIRECTORY_SEPARATOR . "images" . DIRECTORY_SEPARATOR . "recordcover");
-//                }
-//                if (!is_dir(USERS_DIR . $userId . DIRECTORY_SEPARATOR . "images" . DIRECTORY_SEPARATOR . "recordcoverthumb")) {
-//                    $bool = mkdir(USERS_DIR . $userId . DIRECTORY_SEPARATOR . "images" . DIRECTORY_SEPARATOR . "recordcoverthumb");
-//                }
-//            }
-//        } catch (Exception $e) {
-//            return false;
-//        }
-//    }
+        if ($this->get_request_method() != "POST" || !isset($_SESSION['currentUser'])) {
+            $this->response('', 406);
+        }
+
+        $albumJSON = $this->request;
+        $newAlbum = json_decode(json_encode($albumJSON), false);
+
+//        
+//            "albumTitle": $("#albumTitle").val(),
+//            "description": $("#description").val(),
+//            "tags": getTagsAlbumCreate()        
+
+        if (!isset($newAlbum->albumTitle) || is_null($newAlbum->albumTitle) || !(strlen($newAlbum->albumTitle) > 0) ||
+                !isset($newAlbum->description) || is_null($newAlbum->description) || !(strlen($newAlbum->description) > 0) ||
+                !isset($newAlbum->tags) || is_null($newAlbum->tags) || !is_array($newAlbum->tags) || !(count($newAlbum->tags) > 0)
+        ) {
+            $error = array('status' => "Bad Request", "msg" => "Invalid new album");
+            $this->response($error, 400);
+        }
+
+        $user = $_SESSION['currentUser'];
+        $userId = $user->getObjectId();
+
+        if ($user->getType() != "JAMMER") {
+            $error = array('status' => "Bad Request", "msg" => "Invalid user type");
+            $this->response($error, 400);
+        }
+
+        $pRecord = new RecordParse();
+        $record = new Record();
+
+        $record->setActive(true);
+        $record->setBuyLink($newAlbum->urlBuy);
+        $record->setCommentCounter(0);
+        $record->setCounter(0);
+//$record->setCoverFile();
+        $imgInfo = $this->getImages($newAlbum);
+        $record->setCover($imgInfo['RecordPicture']);
+        $record->setThumbnailCover($imgInfo['RecordThumbnail']);
+
+        $record->setDescription($newAlbum->albumTitle);
+        $record->setDuration(0);
+        $record->setFeaturing($newAlbum->albumFeaturing);
+        $record->setFromUser($userId);
+        $record->setGenre($this->getTags($newAlbum->tags));
+        $record->setLabel($newAlbum->label);
+
+        if (($location = GeocoderService::getLocation($newAlbum->city))) {
+            $parseGeoPoint = new parseGeoPoint($location['lat'], $location['lng']);
+            $record->setLocation($parseGeoPoint);
+        }
+
+        $record->setLoveCounter(0);
+        $record->setReviewCounter(0);
+        $record->setShareCounter(0);
+        $record->setTitle($newAlbum->albumTitle);
+        $record->setYear($newAlbum->year);
+
+        $newRecord = $pRecord->saveRecord($record);
+
+        if ($newRecord instanceof Error) {
+//result è un errore e contiene il motivo dell'errore
+            $error = array('status' => "Service Unavailable", "msg" => $newRecord->getErrorMessage());
+            $this->response($error, 503);
+        }
+
+//se va a buon fine salvo una nuova activity       
+        $activity = new Activity();
+        $activity->setActive(true);
+        $activity->setFromUser($userId);
+        $activity->setRead(true);
+        $activity->setStatus("A");
+        $activity->setType("CREATEDRECORD");
+//            $activity->setACL(toParseDefaultACL());
+
+        $pActivity = new ActivityParse();
+        $pActivity->saveActivity($activity);
+
+        $this->createFolderForRecord($userId, $newRecord->getObjectId());
+
+        $dirThumbnailDest = USERS_DIR . $userId . "/images/recordcover";
+        $dirCoverDest = USERS_DIR . $userId . "/images/recordcoverthumb";
+
+
+//SPOSTO LE IMMAGINI NELLE RISPETTIVE CARTELLE         
+        if (!is_null($record->getThumbnailCover()) && strlen($record->getThumbnailCover()) > 0 && strlen($record->getCover()) && !is_null($record->getCover())) {
+            rename(MEDIA_DIR . "cache/" . $record->getThumbnailCover(), $dirThumbnailDest . DIRECTORY_SEPARATOR . $record->getThumbnailCover());
+            rename(MEDIA_DIR . "cache/" . $record->getCover(), $dirCoverDest . DIRECTORY_SEPARATOR . $record->getCover());
+        }
+
+        unset($_SESSION['currentUserFeaturingArray']);
+
+        $this->response(array("res" => "OK", "recordId" => $newRecord->getObjectId()), 200);
+    }
+
+    private function getTags($list) {
+        return implode(",", $list);
+    }
+
+    private function createFolderForRecord($userId, $albumId) {
+        try {
+            if (!is_null($userId) && strlen($userId) > 0) {
+                mkdir(USERS_DIR . $userId . DIRECTORY_SEPARATOR . "songs" . DIRECTORY_SEPARATOR . $albumId, 0, true);
+
+                if (!is_dir(USERS_DIR . $userId . DIRECTORY_SEPARATOR . "images")) {
+                    $bool = mkdir(USERS_DIR . $userId . DIRECTORY_SEPARATOR . "images");
+                }
+                if (!is_dir(USERS_DIR . $userId . DIRECTORY_SEPARATOR . "images" . DIRECTORY_SEPARATOR . "recordcover")) {
+                    $bool = mkdir(USERS_DIR . $userId . DIRECTORY_SEPARATOR . "images" . DIRECTORY_SEPARATOR . "recordcover");
+                }
+                if (!is_dir(USERS_DIR . $userId . DIRECTORY_SEPARATOR . "images" . DIRECTORY_SEPARATOR . "recordcoverthumb")) {
+                    $bool = mkdir(USERS_DIR . $userId . DIRECTORY_SEPARATOR . "images" . DIRECTORY_SEPARATOR . "recordcoverthumb");
+                }
+            }
+        } catch (Exception $e) {
+            return false;
+        }
+    }
 
 //    public function publishRecords() {
 //        global $controllers;
@@ -233,72 +233,72 @@ class uploadRecordController extends REST {
 //        $this->response(array($controllers['RECORDSAVED']), 200);
 //    }
 
-//    private function saveMp3($userId, $recordId, $songId) {
-//        if (file_exists(MEDIA_DIR . "cache" . DIRECTORY_SEPARATOR . $songId)) {
-//            $dir = USERS_DIR . $userId . DIRECTORY_SEPARATOR . "songs" . DIRECTORY_SEPARATOR . $recordId;
-//            if (!is_dir($dir)) {
-//                mkdir($dir, 0, true);
-//            }
-//
-//            if (!is_null($userId) && !is_null($recordId) && !is_null($songId)) {
-//                rename(MEDIA_DIR . "cache" . DIRECTORY_SEPARATOR . $songId, $dir . DIRECTORY_SEPARATOR . $songId);
-//            }
-//        }
-//    }
-//
-//    private function getImages($decoded) {
-////in caso di anomalie ---> default
-//        if (!isset($decoded->crop) || is_null($decoded->crop) ||
-//                !isset($decoded->image) || is_null($decoded->image)) {
-//            return array("RecordPicture" => null, "RecordThumbnail" => null);
-//        }
-//
-//        $PROFILE_IMG_SIZE = 300;
-//        $THUMBNAIL_IMG_SIZE = 150;
-//
-////recupero i dati per effettuare l'editing
-//        $cropInfo = json_decode(json_encode($decoded->crop), false);
-//
-//        if (!isset($cropInfo->x) || is_null($cropInfo->x) || !is_numeric($cropInfo->x) ||
-//                !isset($cropInfo->y) || is_null($cropInfo->y) || !is_numeric($cropInfo->y) ||
-//                !isset($cropInfo->w) || is_null($cropInfo->w) || !is_numeric($cropInfo->w) ||
-//                !isset($cropInfo->h) || is_null($cropInfo->h) || !is_numeric($cropInfo->h)) {
-//            return array("RecordPicture" => null, "RecordThumbnail" => null);
-//        }
-//        $cacheDir = MEDIA_DIR . "cache/";
-//        $cacheImg = $cacheDir . $decoded->image;
-//
-////Preparo l'oggetto per l'editign della foto
-//        $cis = new CropImageService();
-//
-////gestione dell'immagine di profilo
-//        $coverId = $cis->cropImage($cacheImg, $cropInfo->x, $cropInfo->y, $cropInfo->w, $cropInfo->h, $PROFILE_IMG_SIZE);
-//        $coverUrl = $cacheDir . $coverId;
-//
-////gestione del thumbnail
-//        $thumbId = $cis->cropImage($coverUrl, 0, 0, $PROFILE_IMG_SIZE, $PROFILE_IMG_SIZE, $THUMBNAIL_IMG_SIZE);
-//        $thumbUrl = $cacheDir . $thumbId;
-//
-////CANCELLAZIONE DELLA VECCHIA IMMAGINE
-//        unlink($cacheImg);
-////RETURN        
-//        return array('RecordPicture' => $coverId, 'RecordThumbnail' => $thumbId);
-//    }
-//
-//    public function getFeaturingJSON() {
-//
-//        $currentUserFeaturingArray = null;
-//        if (isset($_SESSION['currentUserFeaturingArray'])) {
-////caching dell'array
-//            $currentUserFeaturingArray = $_SESSION['currentUserFeaturingArray'];
-//        } else {
-//            $currentUserFeaturingArray = $this->getFeaturingArray();
-//            $_SESSION['currentUserFeaturingArray'] = $currentUserFeaturingArray;
-//        }
-//
-//        echo json_encode($currentUserFeaturingArray);
-//    }
-//
+    private function saveMp3($userId, $recordId, $songId) {
+        if (file_exists(MEDIA_DIR . "cache" . DIRECTORY_SEPARATOR . $songId)) {
+            $dir = USERS_DIR . $userId . DIRECTORY_SEPARATOR . "songs" . DIRECTORY_SEPARATOR . $recordId;
+            if (!is_dir($dir)) {
+                mkdir($dir, 0, true);
+            }
+
+            if (!is_null($userId) && !is_null($recordId) && !is_null($songId)) {
+                rename(MEDIA_DIR . "cache" . DIRECTORY_SEPARATOR . $songId, $dir . DIRECTORY_SEPARATOR . $songId);
+            }
+        }
+    }
+
+    private function getImages($decoded) {
+//in caso di anomalie ---> default
+        if (!isset($decoded->crop) || is_null($decoded->crop) ||
+                !isset($decoded->image) || is_null($decoded->image)) {
+            return array("RecordPicture" => null, "RecordThumbnail" => null);
+        }
+
+        $PROFILE_IMG_SIZE = 300;
+        $THUMBNAIL_IMG_SIZE = 150;
+
+//recupero i dati per effettuare l'editing
+        $cropInfo = json_decode(json_encode($decoded->crop), false);
+
+        if (!isset($cropInfo->x) || is_null($cropInfo->x) || !is_numeric($cropInfo->x) ||
+                !isset($cropInfo->y) || is_null($cropInfo->y) || !is_numeric($cropInfo->y) ||
+                !isset($cropInfo->w) || is_null($cropInfo->w) || !is_numeric($cropInfo->w) ||
+                !isset($cropInfo->h) || is_null($cropInfo->h) || !is_numeric($cropInfo->h)) {
+            return array("RecordPicture" => null, "RecordThumbnail" => null);
+        }
+        $cacheDir = MEDIA_DIR . "cache/";
+        $cacheImg = $cacheDir . $decoded->image;
+
+//Preparo l'oggetto per l'editign della foto
+        $cis = new CropImageService();
+
+//gestione dell'immagine di profilo
+        $coverId = $cis->cropImage($cacheImg, $cropInfo->x, $cropInfo->y, $cropInfo->w, $cropInfo->h, $PROFILE_IMG_SIZE);
+        $coverUrl = $cacheDir . $coverId;
+
+//gestione del thumbnail
+        $thumbId = $cis->cropImage($coverUrl, 0, 0, $PROFILE_IMG_SIZE, $PROFILE_IMG_SIZE, $THUMBNAIL_IMG_SIZE);
+        $thumbUrl = $cacheDir . $thumbId;
+
+//CANCELLAZIONE DELLA VECCHIA IMMAGINE
+        unlink($cacheImg);
+//RETURN        
+        return array('RecordPicture' => $coverId, 'RecordThumbnail' => $thumbId);
+    }
+
+    public function getFeaturingJSON() {
+
+        $currentUserFeaturingArray = null;
+        if (isset($_SESSION['currentUserFeaturingArray'])) {
+//caching dell'array
+            $currentUserFeaturingArray = $_SESSION['currentUserFeaturingArray'];
+        } else {
+            $currentUserFeaturingArray = $this->getFeaturingArray();
+            $_SESSION['currentUserFeaturingArray'] = $currentUserFeaturingArray;
+        }
+
+        echo json_encode($currentUserFeaturingArray);
+    }
+
     private function getFeaturingArray() {
         error_reporting(E_ALL ^ E_NOTICE);
         if (isset($_SESSION['currentUser'])) {
