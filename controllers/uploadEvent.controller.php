9a4097afa9902559a0df21e174b652119ce257a9
Stefano <stefanom@jamyourself.com>
2013-12-08 18:25:36 +0100

    corretti nomi delle classi

--- controllers/uploadEvent.controller.php
+++ controllers/uploadEvent.controller.php
@@ -4,21 +4,18 @@ if (!defined('ROOT_DIR'))
     define('ROOT_DIR', '../');
 
 require_once ROOT_DIR . 'config.php';
-require_once SERVICES_DIR . 'lang.service.php';
-require_once SERVICES_DIR . 'geocoder.service.php';
-require_once SERVICES_DIR . 'cropImage.service.php';
 require_once CLASSES_DIR . 'user.class.php';
 require_once CLASSES_DIR . 'userParse.class.php';
+require_once SERVICES_DIR . 'lang.service.php';
 require_once LANGUAGES_DIR . 'controllers/' . getLanguage() . '.controllers.lang.php';
 require_once CONTROLLERS_DIR . 'restController.php';
-require_once CLASSES_DIR . 'recordParse.class.php';
 require_once CLASSES_DIR . 'activityParse.class.php';
-require_once CLASSES_DIR . 'song.class.php';
-require_once CLASSES_DIR . 'songParse.class.php';
+require_once CLASSES_DIR . 'event.class.php';
+require_once CLASSES_DIR . 'eventParse.class.php';
 require_once BOXES_DIR . "record.box.php";
-require_once SERVICES_DIR . 'mp3.service.php';
 
-class UploadRecordController extends REST {
+
+class UploadEventController extends REST {
 
     public $viewRecordList;
 
