baffea65247c4f3a952a28af6d8c846ff6a99b80
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-12-12 17:16:12 +0100

    eliminate stringhe non utilizzate

--- languages/boxes/en.boxes.lang.php
+++ languages/boxes/en.boxes.lang.php
@@ -5,58 +5,22 @@
  *  \version   1.0
  *  \date      2013
  *  \copyright Jamyourself.com 2013
- *
  *  \par       Info:
  *  \brief     English string for boxes
  *  \warning
  *  \bug
  *  \todo
- *
- *  
- *  
  */
-$default_img = array(
-    'DEFALBUMTHUMB' => 'images/default/defaultAlbumCoverThumb.jpg',
-    'DEFEVENTIMAGE' => 'images/default/defaultEventImage.jpg',
-    'DEFEVENTTHUMB' => 'images/default/defaultEventThumb.jpg',
-    'DEFRECORDCOVER' => 'images/default/defaultRecordImage.jpg',
-    'DEFRECORDTHUMB' => 'images/default/defaultRecordThumb.jpg',
-    'DEFIMAGETHUMB' => 'images/default/defaultImageThumb.jpg',
-    'DEFAVATAR' => 'images/default/defaultAvatar.jpg',
-    'DEFAVATARTHUMB' => 'images/default/defaultAvatarThumb.jpg',
-    'DEFBGD' => 'images/default/defaultBackground.jpg',
-    'DEFIMAGE' => 'images/default/defaultImage.jpg'
-);
 
 $boxes = array(
     'NODATA' => 'NO DATA TO DISPLAY',
-    'ND' => 'NOT DEFINED FOR THIS USER',
     'NDB' => 'NOT DEFINED FOR THIS BOX',
-    'NOIMGS' => 'YOUR ALBUM DOES NOT CONTAIN ANY IMAGE',
-    'NOLSNGLST' => 'NO DATA FOR LAST SONG LISTENED',
-    'NOLSTEVNT' => 'NO DATA FOR LAST CONFIRMED EVENT',
-    'IAL' => 'INFO ALREADY LOADED',
-    'NOTAG' => 'NO TAGS TO DISPLAY',
-    'NOATTENDEE' => 'NO ATTENDEE AT THE MOMENT FOR THIS EVENT',
-    'NOFEATEVE' => 'NO FEATURING FOR THIS EVENT',
-    'NOINVITED' => 'NO INVITED FOR THIS EVENT',
-    'NOTRACK' => 'YOUR TRACKLIST IS EMPTY',
-    'NOFOLLOWING' => 'YOU ARE CURRENTLY FOLLOWING NOONE',
-    'NOFRIENDS' => 'YOU HAVE NO FRIENDS YET',
-    'NOFOLLOWERS' => 'NO FOLLOWERS YET',
-    'NOVENUE' => 'NO COLLABORATION WITH VENUES YET',
-    'NOJAMMER' => 'NO COLLABORATION WITH JAMMERS YET',
-    'NOFEATRECORD' => 'NO FEATURING FOR THIS RECORD',
     'ONLYIFLOGGEDIN' => 'AVAILABLE ONLY FOR LOGGED USERS',
-    'NOLOCALTYPE' => 'LOCALTYPE UNDEFINED',
-    'NOMEMBER' => 'MEMBERS UNDEFINED',
     'MESSAGEFORLIST' => 'sent you a message',
     'EVENTFORLIST' => 'invited you to an event',
     'FRIENDSHIPFORLIST' => 'want to be your friend',
     'COLLABORATIONFORLIST' => 'want to collaborate with you',
     'FOLLOWINGFORLIST'=> 'is now following you',
-    'INVALIDMESSAGEBOX' => 'Invalid ID for messabox init',
-    'INVALIDNOTIFICATIONBOX'=> 'Invalid ID for notification box init',
     '404' =>'Object not found'
 );
 ?>
--- languages/boxes/it.boxes.lang.php
+++ languages/boxes/it.boxes.lang.php
@@ -5,58 +5,22 @@
  *  \version   1.0
  *  \date      2013
  *  \copyright Jamyourself.com 2013
- *
  *  \par       Info:
  *  \brief     Italian strings for boxes
  *  \warning
  *  \bug
  *  \todo
- *
- *  
- *  
  */
-$default_img = array(
-    'DEFALBUMTHUMB' => 'images/default/defaultAlbumCoverThumb.jpg',
-    'DEFEVENTIMAGE' => 'images/default/defaultEventImage.jpg',
-    'DEFEVENTTHUMB' => 'images/default/defaultEventThumb.jpg',
-    'DEFRECORDCOVER' => 'images/default/defaultRecordImage.jpg',
-    'DEFRECORDTHUMB' => 'images/default/defaultRecordThumb.jpg',
-    'DEFIMAGETHUMB' => 'images/default/defaultImageThumb.jpg',
-    'DEFAVATAR' => 'images/default/defaultAvatar.jpg',
-    'DEFAVATARTHUMB' => 'images/default/defaultAvatarThumb.jpg',
-    'DEFBGD' => 'images/default/defaultBackground.jpg',
-    'DEFIMAGE' => 'images/default/defaultImage.jpg'
-);
 
 $boxes = array(
     'NODATA' => 'NESSUN DATO DA MOSTRARE',
-    'ND' => 'NON DEFINITO PER QUESTO TIPO DI UTENTE',
     'NDB' => 'NON DEFINITO PER QUESTO TIPO DI BOX',
-    'NOIMGS' => 'IL TUO ALBUM NON CONTIENE IMMAGINI',
-    'NOLSNGLST' => 'NESSUN DATO SUL ULTIMO BRANO ASCOLTATO',
-    'NOLSTEVNT' => 'NESSUN DATO SULL&apos;ULTIMO EVENTO A CUI HAI PARTECIPATO',
-    'IAL' => 'INFO PRECEDENTEMENTE CARICATE',
-    'NOTAG' => 'NESSUN TAG',
-    'NOATTENDEE' => 'NESSUN PARTECIPANTE A QUESTO EVENTO PER ORA',
-    'NOFEATEVE' => 'NESSUN FEATURING PER QUESTO EVENTO',
-    'NOINVITED' => 'NESSUN INVITATO A QUESTO EVENTO',
-    'NOTRACK' => 'LA TUA TRACKLIST NON CONTIENE CANZONI',
-    'NOFOLLOWING' => 'NON STAI ANCORA SEGUENDO NESSUNO',
-    'NOFRIENDS' => 'NON HAI ANCORA AMICI',
-    'NOFOLLOWERS' => 'NON HAI ANCORA FOLLOWERS',
-    'NOVENUE' => 'NESSUNA COLLABORAZIONE CON VENUE AL MOMENTO',
-    'NOJAMMER' => 'NESSUNA COLLABORAZIONE CON JAMMER AL MOMENTO',
-    'NOFEATRECORD' => 'NESSUN FEATURING PER QUESTO RECORD',
     'ONLYIFLOGGEDIN' => 'DISPONIBILE SOLO PER UTENTI LOGGATI',
-    'NOLOCALTYPE' => 'LOCALTYPE NON DEFINITO',
-    'NOMEMBER' => 'MEMBERS NON DEFINITO',
     'MESSAGEFORLIST' => 'ti ha scritto un messaggio',
     'EVENTFORLIST' => 'ti ha inviato ad un evento',
     'FRIENDSHIPFORLIST' => 'vuole essere tuo amico',
     'COLLABORATIONFORLIST' => 'vuole collaborare con te',
     'FOLLOWINGFORLIST' => 'ti sta seguendo',
-    'INVALIDMESSAGEBOX' => 'ID non valido per inizializzazione box messaggi',
-    'INVALIDNOTIFICATIONBOX' => 'ID non valido per inizializzazione box notifiche',
     '404' =>'Oggetto non trovato'
 );
 ?>
