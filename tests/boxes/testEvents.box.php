137fd41a8c46d42889fe11ceb633a6dc250cb9a2
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-12-13 14:55:43 +0100

    prima stesura del metodo initForTimeline

--- tests/boxes/testEvents.box.php
+++ tests/boxes/testEvents.box.php
@@ -29,47 +29,51 @@ $i_end = microtime();
 
 $objectId = '7fes1RyY77'; //LDF
 $idEvent = 'Imx9idjTGj';
+
+
+
+
+
+$eventBoxP3 = new EventBox;
+$eventBoxP3->initForTimeLine();
+print "<pre>";
+print_r($eventBoxP3);
+print "</pre>";
+
+
+
+
+
+
+
+
 echo '<br />-------------------------TEST EVENT LDF-------------------------------------------<br />';
 $event1_start = microtime();
 $eventBoxP1 = new EventBox();
-$eventBox1 = $eventBoxP1->initForPersonalPage($objectId);
+$eventBoxP1->initForPersonalPage($objectId);
 print "<pre>";
-print_r($eventBox1);
+print_r($eventBoxP1);
 print "</pre>";
 $event1_stop = microtime();
 $event2_start = microtime();
 echo '<br />TEST  INFO EVENT MEDIA PAGE<br />';
 $eventBoxP2 = new EventBox;
-$eventBox2 = $eventBoxP2->initForMediaPage($idEvent);
+$eventBoxP2->initForMediaPage($idEvent);
 print "<pre>";
-print_r($eventBox2);
+print_r($eventBoxP2);
 print "</pre>";
 echo '<br />FINE TEST  INFO EVENT MEDIA PAGE<br />';
 $event2_stop = microtime();
-echo '<br />ERRORE PERSONAL PAGE<br />';
-$error1_start = microtime();
-$errorBoxP = new EventBox();
-$errorBox = $errorBoxP->initForPersonalPage('pipopopopopopopo');
-print "<pre>";
-print_r($errorBox);
-print "</pre>";
-$error1_stop = microtime();
-echo '<br />ERRORE MEDIA PAGE<br />';
-$error2_start = microtime();
-$errorBoxP2 = new EventBox();
-$errorBox2 = $errorBoxP2->initForMediaPage('ciaciaciaoicoai');
-print "<pre>";
-print_r($errorBox2);
-print "</pre>";
-$error2_stop = microtime();
+
+
+
+
 echo '<br />-------------------------FINE TEST EVENT BOX-------------------------------------------<br />';
 $t_end = microtime();
 echo '<br />----------------------TIMERS---------------------------<br />';
 echo 'Tempo include ' . executionTime($i_start, $i_end) . '<br />';
 echo 'Tempo event personal page:  ' . executionTime($event1_start, $event1_stop) . '<br />';
 echo 'Tempo event media page:  ' . executionTime($event2_start, $event2_stop) . '<br />';
-echo 'Tempo event ERRORE PERSONALPAGE:  ' . executionTime($error1_start, $error1_stop) . '<br />';
-echo 'Tempo event ERRORE MEDIAPAGE:  ' . executionTime($error2_start, $error2_stop) . '<br />';
 echo 'Tempo totale ' . executionTime($t_start, $t_end) . '<br />';
 echo '<br />----------------------TIMERS---------------------------<br />';
 ?>
