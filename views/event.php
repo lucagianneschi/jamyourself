cf9d1d144341d3da5385f42b7b869ef06106075b
Daniele Caldelli <daniele.caldelli@gmail.com>
2013-12-16 14:05:32 +0100

    Gestione dell'objectId errato nella chiamata e dell'utente non in sessione

--- views/event.php
+++ views/event.php
@@ -8,41 +8,48 @@ require_once SERVICES_DIR . 'debug.service.php';
 require_once LANGUAGES_DIR . 'boxes/' . getLanguage() . '.boxes.lang.php';
 require_once LANGUAGES_DIR . 'views/' . getLanguage() . '.views.lang.php';
 require_once BOXES_DIR . 'event.box.php';
+require_once CLASSES_DIR . 'userParse.class.php';
+session_start();
 
-$objectId = $_GET['event'];
-
-$eventBox = new EventBox();
-$eventBox->initForMediaPage($objectId);
-if (is_null($eventBox->error)) {
-	$event = $eventBox->eventArray[$objectId];
-	?>
-	<!DOCTYPE html>
-	<!--[if IE 8]><html class="no-js lt-ie9" lang="en" ><![endif]-->
-	<!--[if gt IE 8]><!--><html class="no-js" lang="en" ><!--<![endif]-->
-		<head>
-            <title>Jamyourself</title>
-			<!-------------------------- METADATI --------------------------->
-			<?php require_once(VIEWS_DIR . "content/general/meta.php"); ?>
-		</head>
-		<body>
-			<!-------------------------- HEADER --------------------------->
-			<?php require_once(VIEWS_DIR . 'content/header/main.php'); ?>
-			<!-------------------------- BODY --------------------------->
-			<?php require_once(VIEWS_DIR . 'content/event/main.php'); ?>
-			<!-------------------------- FOOTER --------------------------->
-			<?php require_once(VIEWS_DIR . 'content/general/footer.php'); ?>	
-			<!-------------------------- SCRIPT --------------------------->
-			<?php require_once(VIEWS_DIR . "content/general/script.php"); ?>
-			<script type="text/javascript">
-				loadBoxInformationFeaturing();
-				loadBoxInformationAttendee();
-				loadBoxInformationInvited();
-				loadBoxStatus();
-				loadBoxEventReview(3, 0);
-				loadBoxComment(3, 0);
-			</script>
-		</body>
-	</html>
-	<?php
+if(!isset($_SESSION['currentUser'])) {
+    header('Location: login.php');
+} else {
+    $objectId = $_GET['event'];
+    $eventBox = new EventBox();
+    $eventBox->initForMediaPage($objectId);
+    if (is_null($eventBox->error) && !empty($recordBox->recordArray)) {
+        $event = $eventBox->eventArray[$objectId];
+        ?>
+        <!DOCTYPE html>
+        <!--[if IE 8]><html class="no-js lt-ie9" lang="en" ><![endif]-->
+        <!--[if gt IE 8]><!--><html class="no-js" lang="en" ><!--<![endif]-->
+            <head>
+                <title>Jamyourself</title>
+                <!-------------------------- METADATI --------------------------->
+                <?php require_once(VIEWS_DIR . "content/general/meta.php"); ?>
+            </head>
+            <body>
+                <!-------------------------- HEADER --------------------------->
+                <?php require_once(VIEWS_DIR . 'content/header/main.php'); ?>
+                <!-------------------------- BODY --------------------------->
+                <?php require_once(VIEWS_DIR . 'content/event/main.php'); ?>
+                <!-------------------------- FOOTER --------------------------->
+                <?php require_once(VIEWS_DIR . 'content/general/footer.php'); ?>	
+                <!-------------------------- SCRIPT --------------------------->
+                <?php require_once(VIEWS_DIR . "content/general/script.php"); ?>
+                <script type="text/javascript">
+                    loadBoxInformationFeaturing();
+                    loadBoxInformationAttendee();
+                    loadBoxInformationInvited();
+                    loadBoxStatus();
+                    loadBoxEventReview(3, 0);
+                    loadBoxComment(3, 0);
+                </script>
+            </body>
+        </html>
+        <?php
+    } else {
+        echo 'L\'objectId non esiste';
+    }
 }
 ?>
