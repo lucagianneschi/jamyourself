b18cfc84736c4cad5a3d256d265ca36dd7ed6de1
Stefano <stefanom@jamyourself.com>
2013-12-17 23:44:16 +0100

    provando a generalizzare lo spinnger/loader

--- views/resources/javascripts/customs/utils.js
+++ views/resources/javascripts/customs/utils.js
@@ -72,3 +72,39 @@ function prepareLocationObj(_result) {
         window.console.log("prepareLocationObj | An error occurred - message : " + err.message);
     }
 }
+
+function startLoader(id) {
+    try {
+        var opts = {
+            lines: 9, // The number of lines to draw
+            length: 0, // The length of each line
+            width: 4, // The line thickness
+            radius: 9, // The radius of the inner circle
+            corners: 1, // Corner roundness (0..1)
+            rotate: 4, // The rotation offset
+            direction: 1, // 1: clockwise, -1: counterclockwise
+            color: '#000', // #rgb or #rrggbb or array of colors
+            speed: 2.2, // Rounds per second
+            trail: 56, // Afterglow percentage
+            shadow: false, // Whether to render a shadow
+            hwaccel: false, // Whether to use hardware acceleration
+            className: 'spinner', // The CSS class to assign to the spinner
+            zIndex: 2e9, // The z-index (defaults to 2000000000)
+            top: 'auto', // Top position relative to parent in px
+            left: 'auto' // Left position relative to parent in px
+        };
+         
+        new Spinner(opts).spin(document.getElementById(id));
+    } catch (err) {
+        window.console.err("startLoader | An error occurred - message : " + err.message);
+    }
+
+}
+
+function stopLoader(spinner) {
+    try {
+        spinner.stop();
+    } catch (err) {
+        window.console.err("stopLoader | An error occurred - message : " + err.message);
+    }
+}
