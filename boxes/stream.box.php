29ee74412e4ce0dfdbb9f19c640b6e387bea8b8f
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-12-18 12:05:34 +0100

    rinominato timeline.box.php in strem.box.php e relativo file di config

--- boxes/stream.box.php
+++ boxes/stream.box.php
@@ -35,7 +35,7 @@ class StreamBox {
      * \brief	class construct to import config file
      */
     function __construct() {
-        $this->config = json_decode(file_get_contents(CONFIG_DIR . "boxes/timeline.config.json"), false);
+        $this->config = json_decode(file_get_contents(CONFIG_DIR . "boxes/stream.config.json"), false);
     }
 
     /**
