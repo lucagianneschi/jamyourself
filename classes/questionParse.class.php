262411691034caad3a6043f97f38ae8283812a17
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-12-19 12:18:22 +0100

    ripristinate funzioni utilizzabili per questa classe

--- classes/questionParse.class.php
+++ classes/questionParse.class.php
@@ -235,6 +235,16 @@ class QuestionParse {
     }
 
     /**
+     * \fn		void whereContainedIn($field, $value)
+     * \brief	Sets a condition for which the field $field must value one or more $value
+     * \param	$field	the string which represent the field
+     * \param	$value	the array which represent the values
+     */
+    public function whereContainedIn($field, $values) {
+        $this->parseQuery->whereContainedIn($field, $values);
+    }
+
+    /**
      * \fn		void whereEqualTo($field, $value)
      * \brief	Sets a condition for which the field $field must value $value
      * \param	$field	the string which represent the field
@@ -291,6 +301,16 @@ class QuestionParse {
      */
     public function whereLessThanOrEqualTo($field, $value) {
         $this->parseQuery->whereLessThanOrEqualTo($field, $value);
+    }
+
+    /**
+     * \fn		void whereNotContainedIn($field, $value)
+     * \brief	Sets a condition for which the field $field must not value one or more $value
+     * \param	$field	the string which represent the field
+     * \param	$value	the array which represent the values
+     */
+    public function whereNotContainedIn($field, $array) {
+        $this->parseQuery->whereNotContainedIn($field, $array);
     }
 
     /**
