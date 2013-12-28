ca82512981069b411667db0d449c7cdc7eaa39ac
Luca Gianneschi <luca.gianneschi@gmail.com>
2013-12-28 16:45:13 +0100

    adattato ai nuovi box

--- tests/boxes/testStream.box.php
+++ tests/boxes/testStream.box.php
@@ -25,29 +25,51 @@ require_once BOXES_DIR . 'stream.box.php';
 require_once BOXES_DIR . 'event.box.php';
 require_once BOXES_DIR . 'record.box.php';
 require_once PARSE_DIR . 'parse.php';
+require_once CLASSES_DIR . 'utilsClass.php';
+require_once CLASSES_DIR . 'userParse.class.php';
+
+//$t_start = microtime();
+//$userP = new UserParse();
+//$userP->whereNearSphere(43, 10);
+//$userP->setLimit(10);
+//$userP->getUsers();
+//$t_stop = microtime(); //timer tempo totale
+//echo 'Tempo totale ' . executionTime($t_start, $t_stop) . '<br />';
+
 
 echo '<br />------------------------- TEST TIMELINE LDF-------------------------------------------<br />';
 echo '<br />------------------------- EVENTFILTER-------------------------------------------------<br />';
+$t_start = microtime(); //timer tempo totale
 $eventFilter = new EventBox();
-$eventFilter->initForTimeline(array(), 'Viareggio');
+$eventFilter->initForStream(array(), 'Viareggio');
+$t_stop = microtime(); //timer tempo totale
+echo 'Tempo totale ' . executionTime($t_start, $t_stop) . '<br />';
 print "<pre>";
 print_r($eventFilter);
 print "</pre>";
 echo count($eventFilter->eventArray);
 
 echo '<br />------------------------- RECORDFILTER-------------------------------------------------<br />';
+$t_start = microtime(); //timer tempo totale
 $recordFilter = new RecordBox();
-$recordFilter->initForTimeline(array(), null, null, array('Uncatogorized', 'Rock'));
+$recordFilter->initForStream(array(), null, null, array('Uncatogorized', 'Rock'));
+$t_stop = microtime(); //timer tempo totale
+echo 'Tempo totale ' . executionTime($t_start, $t_stop) . '<br />';
 print "<pre>";
 print_r($recordFilter);
 print "</pre>";
 echo count($recordFilter->recordArray);
 
 echo '<br />------------------------- STREAM-------------------------------------------------<br />';
+$t_start = microtime(); //timer tempo totale
 $timeline = new StreamBox();
 $timeline->init(10, 0);
+$t_stop = microtime(); //timer tempo totale
+echo 'Tempo totale ' . executionTime($t_start, $t_stop) . '<br />';
 print "<pre>";
 print_r($timeline);
 print "</pre>";
 echo count($timeline->activitesArray);
+
+
 ?>
