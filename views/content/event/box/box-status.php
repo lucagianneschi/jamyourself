76e0c40a8b07412e007d2d584eab48e4523d2aa3
Daniele Caldelli <daniele.caldelli@gmail.com>
2014-01-02 16:51:41 +0100

    Corretta la visualizzazione del bottonre Review e inserito il multilanguage

--- views/content/event/box/box-status.php
+++ views/content/event/box/box-status.php
@@ -16,9 +16,9 @@ if (in_array($currentUser->getObjectId(), $event->getLovers())) {
 
 ?>
 <div id="social-status">
-	<div class="row">
-		<div class="small-8 columns">			
-			<h3><strong><?php echo $event->getReviewCounter(); ?> review</strong></h3>
+	<!-- div class="row">
+		<div class="small-8 columns">
+			<h3><strong><?php echo $event->getReviewCounter(); ?> <?php echo $views['REVIEW']; ?></strong></h3>
 		</div>
 		<div class="small-4 columns">
 			<p class="grey" style="float: right;">Spotter Rating</p>		
@@ -30,7 +30,7 @@ if (in_array($currentUser->getObjectId(), $event->getLovers())) {
 			    <input class="star" type="radio" name="test-1-rating-5" value="5"/>
 			</form>
 		</div>
-	</div>
+	</div -->
 	
 	<div class="row">
 		<div  class="large-12 columns"><div class="line"></div></div>
@@ -40,8 +40,8 @@ if (in_array($currentUser->getObjectId(), $event->getLovers())) {
 		<div class="box-propriety">
 			<div class="small-7 columns ">
 				<a class="note grey" onclick="love(this, 'Event', '<?php echo $event->getObjectId(); ?>', '<?php echo $currentUser->getObjectId(); ?>');"><?php echo $text_love; ?></a>
-				<a class="note grey" onclick="setCounter()">Comment</a>
-				<a class="note grey" onclick="share()">Share</a>
+				<a class="note grey" onclick="setCounter()"><?php echo $views['COMM']; ?></a>
+				<a class="note grey" onclick="share()"><?php echo $views['SHARE']; ?></a>
 			</div>
 			<div class="small-5 columns propriety ">					
 				<a class="icon-propriety <?php echo $css_love; ?>"><?php echo $event->getLoveCounter(); ?></a>
@@ -56,10 +56,17 @@ if (in_array($currentUser->getObjectId(), $event->getLovers())) {
 	</div>
 </div>
 
-<div class="row ">
-	<div  class="large-12 columns">
-	<div class="status-button">
-		<a href="#" class="button bg-orange"><div class="icon-button _follower_status">Add a review</div></a>
-	</div>
-	</div>
-</div>
+<?php
+if (strtoupper($currentUser->getType()) == 'SPOTTER') {
+    ?>
+    <div class="row ">
+        <div  class="large-12 columns">
+        <div class="status-button">
+            <a href="#" class="button bg-orange">
+                <div class="icon-button _follower_status"><?php echo $views['media']['ADDREVIEW']; ?></div>
+            </a>
+        </div>
+        </div>
+    </div>
+    <?php
+}
