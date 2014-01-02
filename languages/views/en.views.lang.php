d1ca5396a0c2e485ce5d914126a1bda1b4843f81
Daniele Caldelli <daniele.caldelli@gmail.com>
2014-01-02 16:49:36 +0100

    Aggiunte alcune voci mancanti

--- languages/views/en.views.lang.php
+++ languages/views/en.views.lang.php
@@ -25,188 +25,189 @@ $views = array(
     'SHARE' => 'Share',
     'REVIEW' => 'Review',
     'activity' => array(
-	'TITLE' => 'Activities',
-	'LASTALBUM' => 'Last album updated',
-	'RECORDED' => 'Recorded',
-	'VIEWALBUM' => 'View Album',
-	'NORECORD' => 'There is no Record',
-	'LASTEVENT' => 'Last Event',
-	'NOEVENT' => 'There is no Event',
-	'LASTLISTERING' => 'Last listening',
-	'ATTEVENT' => 'Attending the event',
-	'LASTPHOTO' => 'Last photo set updated',
-	'PHOTOS' => 'photos',
-	'NOPHOTO' => 'There are no Photo',
-	'LASTJAMMER' => 'Last collaboration with Jammer',
-	'LASTVENUE' => 'Last collaboration with Venue',
-	'NOCOLL' => 'There are no Collaboration',
-	'LASTFRIENDS' => 'Last friends',
-	'NOFRIENDS' => 'There are no Friends',
-	'LASTFOLL' => 'Last following',
-	'NOFOLL' => 'There are no following'
+        'TITLE' => 'Activities',
+        'LASTALBUM' => 'Last album updated',
+        'RECORDED' => 'Recorded',
+        'VIEWALBUM' => 'View Album',
+        'NORECORD' => 'There is no Record',
+        'LASTEVENT' => 'Last Event',
+        'NOEVENT' => 'There is no Event',
+        'LASTLISTERING' => 'Last listening',
+        'ATTEVENT' => 'Attending the event',
+        'LASTPHOTO' => 'Last photo set updated',
+        'PHOTOS' => 'photos',
+        'NOPHOTO' => 'There are no Photo',
+        'LASTJAMMER' => 'Last collaboration with Jammer',
+        'LASTVENUE' => 'Last collaboration with Venue',
+        'NOCOLL' => 'There are no Collaboration',
+        'LASTFRIENDS' => 'Last friends',
+        'NOFRIENDS' => 'There are no Friends',
+        'LASTFOLL' => 'Last following',
+        'NOFOLL' => 'There are no following'
     ),
     'album' => array(
-	'TITLE' => 'Photography',
-	'PHOTO' => 'photos',
-	'NODATA' => 'There are no Photo'
+        'TITLE' => 'Photography',
+        'PHOTO' => 'photos',
+        'NODATA' => 'There are no Photo'
     ),
     'collaboration' => array(
-	'TITLE' => 'Collaboration',
-	'NODATA' => 'There are no Collaboration'
+        'TITLE' => 'Collaboration',
+        'NODATA' => 'There are no Collaboration'
     ),
     'comment' => array(
-	'WRITE' => 'Write a comment',
-	'NODATA' => 'There are no Comment'
+        'WRITE' => 'Write a comment',
+        'NODATA' => 'There are no Comment'
     ),
     'event' => array(
-	'TITLE' => 'Event',
-	'CALENDAR' => 'Add to Calendar',
-	'NODATA' => 'There are no Events'
+        'TITLE' => 'Event',
+        'CALENDAR' => 'Add to Calendar',
+        'NODATA' => 'There are no Events'
     ),
     'EventReview' => array(
-	'TITLE' => 'Event Reviews',
-	'RATING' => 'Rating',
-	'READ' => 'Read',
-	'NODATA' => 'There are no reviews'
+        'TITLE' => 'Event Reviews',
+        'RATING' => 'Rating',
+        'READ' => 'Read',
+        'NODATA' => 'There are no reviews'
     ),
     'followers' => array(
-	'TITLE' => 'Followers',
-	'NODATA' => 'There are no Followers'
+        'TITLE' => 'Followers',
+        'NODATA' => 'There are no Followers'
     ),
     'following' => array(
-	'TITLE' => 'Following',
-	'NODATA' => 'There are no Following'
+        'TITLE' => 'Following',
+        'NODATA' => 'There are no Following'
     ),
     'friends' => array(
-	'TITLE' => 'Friends',
-	'NODATA' => 'There are no Friends'
+        'TITLE' => 'Friends',
+        'NODATA' => 'There are no Friends'
     ),
     'information' => array(
-	'TITLE' => 'Information',
-	'CONTENT1' => 'About',
-	'CONTENT2' => 'Membres',
-	'CONTENT3' => 'Map',
-	'CONTENT3_DIRECTION' => 'Get direction'
+        'TITLE' => 'Information',
+        'CONTENT1' => 'About',
+        'CONTENT2' => 'Membres',
+        'CONTENT3' => 'Map',
+        'CONTENT3_DIRECTION' => 'Get direction'
     ),
     'post' => array(
-	'TITLE' => 'Post',
-	'WRITE' => 'Write a post',
-	'NODATA' => 'There are no Post'
+        'TITLE' => 'Post',
+        'WRITE' => 'Write a post',
+        'NODATA' => 'There are no Post'
     ),
     'record' => array(
-	'TITLE' => 'Music',
-	'LIST' => 'Album List',
-	'RECORDED' => 'Recorded',
-	'PLAY' => 'Play Now',
-	'ADDPLAYLIST' => 'add to playlist',
-	'REMOVEPLAYLIST' => 'remove',
-	'NODATA' => 'There are no records'
+        'TITLE' => 'Music',
+        'LIST' => 'Album List',
+        'RECORDED' => 'Recorded',
+        'PLAY' => 'Play Now',
+        'ADDPLAYLIST' => 'add to playlist',
+        'REMOVEPLAYLIST' => 'remove',
+        'NODATA' => 'There are no records'
     ),
     'RecordReview' => array(
-	'TITLE' => 'Album Reviews',
-	'RATING' => 'Rating',
-	'READ' => 'Read',
-	'NODATA' => 'There are no reviews'
+        'TITLE' => 'Album Reviews',
+        'RATING' => 'Rating',
+        'READ' => 'Read',
+        'NODATA' => 'There are no reviews'
     ),
     'status' => array(
-	'SENDMSG' => 'Send Message',
-	'ADDFRIEND' => 'Add Friend',
-	'COLL' => 'Collaborate',
-	'FOLL' => 'Follow'
+        'SENDMSG' => 'Send Message',
+        'ADDFRIEND' => 'Add Friend',
+        'COLL' => 'Collaborate',
+        'FOLL' => 'Follow'
     ),
     'media' => array(
-	'Information' => array(
-	    'TITLE' => 'Information',
-	    'CONTENT1_EVENT' => 'Hosted by',
-	    'CONTENT1_RECORD' => 'Performed by',
-	    'CONTENT2' => 'Featuring',
-	    'CONTENT3' => 'Map',
-	    'CONTENT4' => 'Attending',
-	    'CONTENT5' => 'Invited',
-	),
-	'Comment' => array(
-	    'TITLE' => 'Comment',
-	),
-	'Record' => array(
-	    'TITLE' => 'Tracklist',
-	),
-	'RecordReview' => array(
-	    'TITLE' => 'Reviews',
-	),
-	'EventReview' => array(
-	    'TITLE' => 'Reviews',
-	),
+        'ADDREVIEW' => 'Add a review',
+        'Information' => array(
+            'TITLE' => 'Information',
+            'CONTENT1_EVENT' => 'Hosted by',
+            'CONTENT1_RECORD' => 'Performed by',
+            'CONTENT2' => 'Featuring',
+            'CONTENT3' => 'Map',
+            'CONTENT4' => 'Attending',
+            'CONTENT5' => 'Invited',
+        ),
+        'Comment' => array(
+            'TITLE' => 'Comment',
+        ),
+        'Record' => array(
+            'TITLE' => 'Tracklist',
+        ),
+        'RecordReview' => array(
+            'TITLE' => 'Reviews',
+        ),
+        'EventReview' => array(
+            'TITLE' => 'Reviews',
+        ),
     ),
     'header' => array(
-	'TITLE' => 'Notification',
-	'MESSAGE' => 'view all messages',
-	'EVENT' => 'view all events',
-	'RELATION' => 'view all requests'
+        'TITLE' => 'Notification',
+        'MESSAGE' => 'view all messages',
+        'EVENT' => 'view all events',
+        'RELATION' => 'view all requests'
     ),
     'tag' => array(
-	'music' => array(
-	    'acoustic' => 'Acoustic',
-	    'alternative' => 'Alternative',
-	    'ambient' => 'Ambient',
-	    'dance' => 'Dance',
-	    'dark' => 'Dark',
-	    'electronic' => 'Electronic',
-	    'experimental' => 'Experimental',
-	    'folk' => 'Folk',
-	    'funk' => 'Funk',
-	    'grunge' => 'Grunge',
-	    'hardcore' => 'Hardcore',
-	    'house' => 'House',
-	    'indie_rock' => 'Indie Rock',
-	    'instrumental' => 'Instrumental',
-	    'jazz_blues' => 'Jazz&Blues',
-	    'metal' => 'Metal',
-	    'other' => 'Other',
-	    'pop' => 'Pop',
-	    'progressive' => 'Progressive',
-	    'punk' => 'Punk',
-	    'rap_hip_hop' => 'Rap/Hip-Hop',
-	    'rock' => 'Rock',
-	    'ska' => 'Ska',
-	    'songwriter' => 'Songwriter',
-	    'techno' => 'Techno'
-	),
-	'localType' => array(
-	    'contest_area' => 'Contest Area',
-	    'dinner' => 'Dinner',
-	    'dj_set' => 'DJ Set',
-	    'happy_hour_apperizer' => 'Happy Hour/Apperizer',
-	    'jam_session_club' => 'Jam Session Club',
-	    'live_music' => 'Live Music',
-	    'night_life' => 'Night Life',
-	    'other' => 'Other',
-	    'pub' => 'Pub'
-	),
-	'instruments' => array(
-	    'accordion' => 'Accordion',
-	    'banjo' => 'Banjo',
-	    'bass' => 'Bass',
-	    'bongo_drum' => 'Bongo Drum',
-	    'choir' => 'Choir',
-	    'clarinet' => 'Clarinet',
-	    'classic_guitar' => 'Classic Guitar',
-	    'drum' => 'Drum',
-	    'double_bass' => 'Double Bass',
-	    'electric_guitar' => 'Electric Guitar',
-	    'flute' => 'Flute',
-	    'harp' => 'Harp',
-	    'harpsichord' => 'Harpsichord',
-	    'keyboard' => 'Keyboard',
-	    'mouth_organ' => 'Mouth Organ',
-	    'other' => 'Other',
-	    'piano' => 'Piano',
-	    'synthesizer' => 'Synthesizer',
-	    'trumpet' => 'Trumpet',
-	    'ukulele' => 'Ukulele',
-	    'violin' => 'Violin',
-	    'voice' => 'Voice',
-	    'xylophone' => 'Xylophone'
-	)
+        'music' => array(
+            'acoustic' => 'Acoustic',
+            'alternative' => 'Alternative',
+            'ambient' => 'Ambient',
+            'dance' => 'Dance',
+            'dark' => 'Dark',
+            'electronic' => 'Electronic',
+            'experimental' => 'Experimental',
+            'folk' => 'Folk',
+            'funk' => 'Funk',
+            'grunge' => 'Grunge',
+            'hardcore' => 'Hardcore',
+            'house' => 'House',
+            'indie_rock' => 'Indie Rock',
+            'instrumental' => 'Instrumental',
+            'jazz_blues' => 'Jazz&Blues',
+            'metal' => 'Metal',
+            'other' => 'Other',
+            'pop' => 'Pop',
+            'progressive' => 'Progressive',
+            'punk' => 'Punk',
+            'rap_hip_hop' => 'Rap/Hip-Hop',
+            'rock' => 'Rock',
+            'ska' => 'Ska',
+            'songwriter' => 'Songwriter',
+            'techno' => 'Techno'
+        ),
+        'localType' => array(
+            'contest_area' => 'Contest Area',
+            'dinner' => 'Dinner',
+            'dj_set' => 'DJ Set',
+            'happy_hour_apperizer' => 'Happy Hour/Apperizer',
+            'jam_session_club' => 'Jam Session Club',
+            'live_music' => 'Live Music',
+            'night_life' => 'Night Life',
+            'other' => 'Other',
+            'pub' => 'Pub'
+        ),
+        'instruments' => array(
+            'accordion' => 'Accordion',
+            'banjo' => 'Banjo',
+            'bass' => 'Bass',
+            'bongo_drum' => 'Bongo Drum',
+            'choir' => 'Choir',
+            'clarinet' => 'Clarinet',
+            'classic_guitar' => 'Classic Guitar',
+            'drum' => 'Drum',
+            'double_bass' => 'Double Bass',
+            'electric_guitar' => 'Electric Guitar',
+            'flute' => 'Flute',
+            'harp' => 'Harp',
+            'harpsichord' => 'Harpsichord',
+            'keyboard' => 'Keyboard',
+            'mouth_organ' => 'Mouth Organ',
+            'other' => 'Other',
+            'piano' => 'Piano',
+            'synthesizer' => 'Synthesizer',
+            'trumpet' => 'Trumpet',
+            'ukulele' => 'Ukulele',
+            'violin' => 'Violin',
+            'voice' => 'Voice',
+            'xylophone' => 'Xylophone'
+        )
     )
 );
 ?>
