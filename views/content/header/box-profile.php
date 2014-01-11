83bcec64d210ef63387a1264f75f4675138057b8
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-01-11 04:05:21 +0100

    formattazione

--- views/content/header/box-profile.php
+++ views/content/header/box-profile.php
@@ -19,152 +19,149 @@ $currentUser = $_SESSION['currentUser'];
 #TODO
 //decidere come gestire i possibili errori
 if (count($playlist->tracklist) == 0 && is_null($playlist->error)) {
-	echo 'Playlist vuota';
+    echo 'Playlist vuota';
 } elseif (count($playlist->tracklist) == 0 && !is_null($playlist->error)) {
-	echo $playlist->error;
+    echo $playlist->error;
 } elseif (count($playlist->tracklist) > 0) {
-	$_SESSION['playlist']['objectId'] = $playlist->objectId;
-	$_SESSION['playlist']['songs'] = array();
-	?>
-	<script>
-		var myPlaylist;
-		$(document).ready(function(){
-		   myPlaylist = getPlayer();
-		});
-	</script>
-	<div class="row">
-		<div  class="small-6 columns hide-for-small">
-			<h3><?php echo $playlist->name; ?></h3>
-			<div class="row">
-				<div  class="large-6 columns">
-					<div class="text white" style="margin-bottom: 15px;"><?php echo $views['header']['profile']['TITLE'] ?></div>
-				</div>	
-			</div>
-		</div>	
-	</div>
+    $_SESSION['playlist']['objectId'] = $playlist->objectId;
+    $_SESSION['playlist']['songs'] = array();
+    ?>
+    <script>
+        var myPlaylist;
+        $(document).ready(function() {
+    	myPlaylist = getPlayer();
+        });
+    </script>
+    <div class="row">
+        <div  class="small-6 columns hide-for-small">
+    	<h3><?php echo $playlist->name; ?></h3>
+    	<div class="row">
+    	    <div  class="large-6 columns">
+    		<div class="text white" style="margin-bottom: 15px;"><?php echo $views['header']['profile']['TITLE'] ?></div>
+    	    </div>	
+    	</div>
+        </div>	
+    </div>
 
-	
-	<div id="jp_container_N" class="jp-video jp-video-270p">
-			<div class="jp-type-playlist">
-				<div id="jquery_jplayer_N" class="jp-jplayer"></div>
-				<div class="jp-gui">
-					<div class="jp-video-play">
-						<a href="javascript:;" class="jp-video-play-icon" tabindex="1">play</a>
-					</div>
-					<div class="jp-interface">
-						<div class="jp-progress">
-							<div class="jp-seek-bar">
-								<div class="jp-play-bar"></div>
-							</div>
-						</div>
-						<div class="jp-current-time"></div>
-						<div class="jp-duration"></div>
-						<div class="jp-title">
-							<ul>
-								<li></li>
-							</ul>
-						</div>
-						<div class="jp-controls-holder">
-							<ul class="jp-controls">
-								<li><a href="javascript:;" class="jp-previous" tabindex="1">previous</a></li>
-								<li><a href="javascript:;" class="jp-play" tabindex="1">play</a></li>
-								<li><a href="javascript:;" class="jp-pause" tabindex="1">pause</a></li>
-								<li><a href="javascript:;" class="jp-next" tabindex="1">next</a></li>
-								<li><a href="javascript:;" class="jp-stop" tabindex="1">stop</a></li>
-								<li><a href="javascript:;" class="jp-mute" tabindex="1" title="mute">mute</a></li>
-								<li><a href="javascript:;" class="jp-unmute" tabindex="1" title="unmute">unmute</a></li>
-								<li><a href="javascript:;" class="jp-volume-max" tabindex="1" title="max volume">max volume</a></li>
-							</ul>
-							<div class="jp-volume-bar">
-								<div class="jp-volume-bar-value"></div>
-							</div>
-							<ul class="jp-toggles">								
-								<li><a href="javascript:;" class="jp-shuffle" tabindex="1" title="shuffle">shuffle</a></li>
-								<li><a href="javascript:;" class="jp-shuffle-off" tabindex="1" title="shuffle off">shuffle off</a></li>
-								<li><a href="javascript:;" class="jp-repeat" tabindex="1" title="repeat">repeat</a></li>
-								<li><a href="javascript:;" class="jp-repeat-off" tabindex="1" title="repeat off">repeat off</a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div class="jp-playlist">
-					<ul>
-						
-						<!-- The method Playlist.displayPlaylist() uses this unordered list -->
-						<li></li>
-					</ul>
-				</div>
-				<div class="jp-no-solution">
-					<span>Update Required</span>
-					To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
-				</div>
-			</div>
-		</div>
-		<div class="row">
-		<div  class="small-12 columns">					
-				<?php 
-				if (count($playlist->tracklist) > 0) {
-					
-					foreach ($playlist->tracklist as $key => $value) {						
-							$objectId = $value->getObjectId();						
-							$author_name = $value->getFromUser()->getUsername();
-							$author_objectId = $value->getFromUser()->getObjectId();
-							$duration = $value->getDuration();
-							$title = $value->getTitle();
-							$loveCounter = $value->getLoveCounter();
-							$shareCounter = $value->getShareCounter();
-							array_push($_SESSION['playlist']['songs'], $objectId);
-							
-						?>				
-						<script>
-						$(document).ready(function(){
-							myPlaylist.add({
-								objectId: "<?php echo $objectId ?>",
-								title:"<?php echo $title?>",
-								artist:"<?php echo $author_name?>",
-								mp3:"http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3" //ci va l'url dell'mp3
-							});
-						});
-						</script>						
-						<!--div class="row" id="<?php echo $objectId ?>"> 
-                                <div class="small-12 columns">
-                                	<div class="track">
-                                        <div class="row">
-                                                <div class="small-8 columns ">                                        
-                                                        <a class="ico-label _play-large text breakOffTest"><?php echo $title.' - '.$author_name;?></a>                                                                
-                                                </div>
-                                                <div class="small-2 columns">
-                                                        <p class="grey"><?php echo $duration ?></p>        
-                                                </div>
-                                                <div class="small-2 columns track-propriety align-right" style="padding-right: 15px;">                                        
-                                                        <a class="icon-propriety _menu-small note orange "> <?php echo $views['record']['REMOVEPLAYLIST'];?></a>
-                                                                                                                                                
-                                                </div>
-                                                                
-                                        </div>
-                                        <div class="row track-propriety" >
-                                                <div class="box-propriety album-single-propriety" style="padding: 10px;">
-                                                        <div class="small-5 columns ">
-                                                                <a class="note white" onclick="love(this, 'Song', '<?php echo $objectId ?>', '<?php echo $currentUser->getObjectId(); ?>')"><?php echo $views['LOVE'];?></a>
-                                                                <a class="note white" onclick="setCounter(this, '<?php echo $objectId ?>','Song')"><?php echo $views['SHARE'];?></a>        
-                                                        </div>
-                                                        <div class="small-5 columns propriety ">                                        
-                                                                <a class="icon-propriety _unlove grey" ><?php echo $loveCounter ?></a>
-                                                                <a class="icon-propriety _share" ><?php echo $shareCounter ?></a>                        
-                                                        </div>
-                                                </div>                
-                                        </div>
-                                	</div>
-                                </div>
-                        </div-->
-                        
-				<?php
-						
-					}
-				}
-				?>
-				
-		</div>
-	</div>
-	<?php
+    <div id="jp_container_N" class="jp-video jp-video-270p">
+        <div class="jp-type-playlist">
+    	<div id="jquery_jplayer_N" class="jp-jplayer"></div>
+    	<div class="jp-gui">
+    	    <div class="jp-video-play">
+    		<a href="javascript:;" class="jp-video-play-icon" tabindex="1">play</a>
+    	    </div>
+    	    <div class="jp-interface">
+    		<div class="jp-progress">
+    		    <div class="jp-seek-bar">
+    			<div class="jp-play-bar"></div>
+    		    </div>
+    		</div>
+    		<div class="jp-current-time"></div>
+    		<div class="jp-duration"></div>
+    		<div class="jp-title">
+    		    <ul>
+    			<li></li>
+    		    </ul>
+    		</div>
+    		<div class="jp-controls-holder">
+    		    <ul class="jp-controls">
+    			<li><a href="javascript:;" class="jp-previous" tabindex="1">previous</a></li>
+    			<li><a href="javascript:;" class="jp-play" tabindex="1">play</a></li>
+    			<li><a href="javascript:;" class="jp-pause" tabindex="1">pause</a></li>
+    			<li><a href="javascript:;" class="jp-next" tabindex="1">next</a></li>
+    			<li><a href="javascript:;" class="jp-stop" tabindex="1">stop</a></li>
+    			<li><a href="javascript:;" class="jp-mute" tabindex="1" title="mute">mute</a></li>
+    			<li><a href="javascript:;" class="jp-unmute" tabindex="1" title="unmute">unmute</a></li>
+    			<li><a href="javascript:;" class="jp-volume-max" tabindex="1" title="max volume">max volume</a></li>
+    		    </ul>
+    		    <div class="jp-volume-bar">
+    			<div class="jp-volume-bar-value"></div>
+    		    </div>
+    		    <ul class="jp-toggles">								
+    			<li><a href="javascript:;" class="jp-shuffle" tabindex="1" title="shuffle">shuffle</a></li>
+    			<li><a href="javascript:;" class="jp-shuffle-off" tabindex="1" title="shuffle off">shuffle off</a></li>
+    			<li><a href="javascript:;" class="jp-repeat" tabindex="1" title="repeat">repeat</a></li>
+    			<li><a href="javascript:;" class="jp-repeat-off" tabindex="1" title="repeat off">repeat off</a></li>
+    		    </ul>
+    		</div>
+    	    </div>
+    	</div>
+    	<div class="jp-playlist">
+    	    <ul>
+
+    		<!-- The method Playlist.displayPlaylist() uses this unordered list -->
+    		<li></li>
+    	    </ul>
+    	</div>
+    	<div class="jp-no-solution">
+    	    <span>Update Required</span>
+    	    To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
+    	</div>
+        </div>
+    </div>
+    <div class="row">
+        <div  class="small-12 columns">					
+	    <?php
+	    if (count($playlist->tracklist) > 0) {
+
+		foreach ($playlist->tracklist as $key => $value) {
+		    $objectId = $value->getObjectId();
+		    $author_name = $value->getFromUser()->getUsername();
+		    $author_objectId = $value->getFromUser()->getObjectId();
+		    $duration = $value->getDuration();
+		    $title = $value->getTitle();
+		    $loveCounter = $value->getLoveCounter();
+		    $shareCounter = $value->getShareCounter();
+		    array_push($_SESSION['playlist']['songs'], $objectId);
+		    ?>				
+	    	<script>
+	    	    $(document).ready(function() {
+	    		myPlaylist.add({
+	    		    objectId: "<?php echo $objectId ?>",
+	    		    title: "<?php echo $title ?>",
+	    		    artist: "<?php echo $author_name ?>",
+	    		    mp3: "http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3" //ci va l'url dell'mp3
+	    		});
+	    	    });
+	    	</script>						
+	    	<!--div class="row" id="<?php echo $objectId ?>"> 
+	    <div class="small-12 columns">
+	    <div class="track">
+	    <div class="row">
+	    	<div class="small-8 columns ">                                        
+	    		<a class="ico-label _play-large text breakOffTest"><?php echo $title . ' - ' . $author_name; ?></a>                                                                
+	    	</div>
+	    	<div class="small-2 columns">
+	    		<p class="grey"><?php echo $duration ?></p>        
+	    	</div>
+	    	<div class="small-2 columns track-propriety align-right" style="padding-right: 15px;">                                        
+	    		<a class="icon-propriety _menu-small note orange "> <?php echo $views['record']['REMOVEPLAYLIST']; ?></a>
+	    													
+	    	</div>
+	    			
+	    </div>
+	    <div class="row track-propriety" >
+	    	<div class="box-propriety album-single-propriety" style="padding: 10px;">
+	    		<div class="small-5 columns ">
+	    			<a class="note white" onclick="love(this, 'Song', '<?php echo $objectId ?>', '<?php echo $currentUser->getObjectId(); ?>')"><?php echo $views['LOVE']; ?></a>
+	    			<a class="note white" onclick="setCounter(this, '<?php echo $objectId ?>','Song')"><?php echo $views['SHARE']; ?></a>        
+	    		</div>
+	    		<div class="small-5 columns propriety ">                                        
+	    			<a class="icon-propriety _unlove grey" ><?php echo $loveCounter ?></a>
+	    			<a class="icon-propriety _share" ><?php echo $shareCounter ?></a>                        
+	    		</div>
+	    	</div>                
+	    </div>
+	    </div>
+	    </div>
+	    </div-->
+
+		    <?php
+		}
+	    }
+	    ?>
+
+        </div>
+    </div>
+    <?php
 }
--- views/content/header/main.php
+++ views/content/header/main.php
@@ -3,183 +3,181 @@ $css_not = 'no-display';
 $totNotification = '';
 
 $playlistCurrentUser = array();
-
 ?>
 <header>
-	<!------------------------------------- HIDE HEADER ----------------------------->
-	<div id="header-hide" class="no-display bg-black">
-		<?php
-				
-		if (isset($_SESSION['currentUser'])) {
-			$currentUser = $_SESSION['currentUser'];
-			$userObjectId = $currentUser->getObjectId();
-			$userType = $currentUser->getType();
-			?>		
-			<div  class="row hcento-hero" style="padding-bottom: 20px;">
-				<div id="header-profile" class="small-6 columns" style="padding-bottom: 20px">
-					<?php require_once './content/header/box-profile.php'; ?>
-				</div>
-				<!-- TODO - ci devo mettere una chiamata ajax al box per parallelizzare il caricamento-->					
-				<script type="text/javascript">
-						function loadBoxPlayList() {
-							var json_data = {};
-							json_data.objectId = '<?php  ?>';
-							$.ajax({
-								type: "POST",
-								url: "content/header/box-profile.php",
-								data: json_data,
-								beforeSend: function(xhr) {
-									//spinner.show();
-									console.log('Sono partito header box-profile');
-									goSpinnerBox('#header-profile','');
-								}
-							}).done(function(message, status, xhr) {
-								//spinner.hide();
-								$("#header-profile").html(message);
-								//plugin share
-								addthis.init();
-								addthis.toolbox(".addthis_toolbox");
-								//adatta pagina per scroll
-								hcento();
-								code = xhr.status;
-								//console.log("Code: " + code + " | Message: " + message);
-								console.log("Code: " + code + " | Message: <omitted because too large>");
-							}).fail(function(xhr) {
-								//spinner.hide();
-								console.log("Error: " + $.parseJSON(xhr));
-								//message = $.parseJSON(xhr.responseText).status;
-								//code = xhr.status;
-								//console.log("Code: " + code + " | Message: " + message);
-							});
-						}						
-				</script>			
-				<div id="header-social" class="small-6 columns" style="padding-bottom: 20px">				
-					<!-- TODO - ci devo mettere una chiamata ajax al box per parallelizzare il caricamento-->
-					<?php require_once './content/header/box-social.php'; ?>
-				</div>
-			</div>
-			<?php
-		}
-		?>
-	</div>
-	
-	<!------------------------------------- HEADER ----------------------------->
-	<div id="header">
+    <!------------------------------------- HIDE HEADER ----------------------------->
+    <div id="header-hide" class="no-display bg-black">
+	<?php
+	if (isset($_SESSION['currentUser'])) {
+	    $currentUser = $_SESSION['currentUser'];
+	    $userObjectId = $currentUser->getObjectId();
+	    $userType = $currentUser->getType();
+	    ?>		
+    	<div  class="row hcento-hero" style="padding-bottom: 20px;">
+    	    <div id="header-profile" class="small-6 columns" style="padding-bottom: 20px">
+    <?php require_once './content/header/box-profile.php'; ?>
+    	    </div>
+    	    <!-- TODO - ci devo mettere una chiamata ajax al box per parallelizzare il caricamento-->					
+    	    <script type="text/javascript">
+    		function loadBoxPlayList() {
+    		    var json_data = {};
+    		    json_data.objectId = '<?php ?>';
+    		    $.ajax({
+    			type: "POST",
+    			url: "content/header/box-profile.php",
+    			data: json_data,
+    			beforeSend: function(xhr) {
+    			    //spinner.show();
+    			    console.log('Sono partito header box-profile');
+    			    goSpinnerBox('#header-profile', '');
+    			}
+    		    }).done(function(message, status, xhr) {
+    			//spinner.hide();
+    			$("#header-profile").html(message);
+    			//plugin share
+    			addthis.init();
+    			addthis.toolbox(".addthis_toolbox");
+    			//adatta pagina per scroll
+    			hcento();
+    			code = xhr.status;
+    			//console.log("Code: " + code + " | Message: " + message);
+    			console.log("Code: " + code + " | Message: <omitted because too large>");
+    		    }).fail(function(xhr) {
+    			//spinner.hide();
+    			console.log("Error: " + $.parseJSON(xhr));
+    			//message = $.parseJSON(xhr.responseText).status;
+    			//code = xhr.status;
+    			//console.log("Code: " + code + " | Message: " + message);
+    		    });
+    		}
+    	    </script>			
+    	    <div id="header-social" class="small-6 columns" style="padding-bottom: 20px">				
+    		<!-- TODO - ci devo mettere una chiamata ajax al box per parallelizzare il caricamento-->
+    <?php require_once './content/header/box-social.php'; ?>
+    	    </div>
+    	</div>
+    <?php
+}
+?>
+    </div>
+
+    <!------------------------------------- HEADER ----------------------------->
+    <div id="header">
 	<div  class="row">
-		<div  class="large-12 columns">		
-			<div class="row">				
-				<div id="header-box-menu" class="large-5 small-1 columns">
-					<div class="header">
-						<!------------------------------------- MENU ----------------------------->
-						<div class="icon-header _menu" onClick="headerShow()"></div>
-						<!------------------------------------- thumbnail album ----------------------------->			
-						<div class="icon-header" id='header-box-thum'>
-							<img src="../media/<?php echo DEFRECORDTHUMB; ?>"  onerror="this.src='<?php echo DEFRECORDTHUMB;?>'">
-						</div>				
-					
-						<!------------------------------------- PLAYER ----------------------------->
-						<div id="header-box-player">
-							<!---------- TITLE --------->
-							<div class="title-player">In The Belly Of A Shark</div>
-							<!---------- TIME MUSIC  --------->
-							<small id="time-player" class="inline">0:58</small>
-							
-							<!---------- CONTROL --------->
-							<div id="controls-player" class="inline">
-								<div id="bar-player"  class="inline"></div>	
-							   	<div id="playhead-player"  class="inline"></div>			   	
-							    <div id="statusbar-player" class="inline"></div> 
-							</div>
-							<!---------- DURATION MUSIC  --------->
-						    <small id="duration-player"  class="inline">2:30</small>
-							
-							<div id="display">
-								<a id="back"  class="icon-player _back"></a>
-								<a id="pause" class="icon-player _pause play-pause"></a>
-								<a id="play"  class="icon-player _play play-pause" style="display: none;"></a>
-								<a id="next"  class="icon-player _next"></a>					
-								<span id="execution" ></span>
-							</div>			
-						</div>
-						
-					</div>			
+	    <div  class="large-12 columns">		
+		<div class="row">				
+		    <div id="header-box-menu" class="large-5 small-1 columns">
+			<div class="header">
+			    <!------------------------------------- MENU ----------------------------->
+			    <div class="icon-header _menu" onClick="headerShow()"></div>
+			    <!------------------------------------- thumbnail album ----------------------------->			
+			    <div class="icon-header" id='header-box-thum'>
+				<img src="../media/<?php echo DEFRECORDTHUMB; ?>"  onerror="this.src='<?php echo DEFRECORDTHUMB; ?>'">
+			    </div>				
+
+			    <!------------------------------------- PLAYER ----------------------------->
+			    <div id="header-box-player">
+				<!---------- TITLE --------->
+				<div class="title-player">In The Belly Of A Shark</div>
+				<!---------- TIME MUSIC  --------->
+				<small id="time-player" class="inline">0:58</small>
+
+				<!---------- CONTROL --------->
+				<div id="controls-player" class="inline">
+				    <div id="bar-player"  class="inline"></div>	
+				    <div id="playhead-player"  class="inline"></div>			   	
+				    <div id="statusbar-player" class="inline"></div> 
 				</div>
-				<!------------------------------------- LOGO --------------------------------------------->
-				<div class="large-2 columns logo" id="header-box-logo">				
-					<div id="logo">
-						 <img src="resources/images/logo.png">						
-					</div>					
-				</div>
-				<!------------------------------------- SWITCH -------------------------------------------->
-				<div class="small-5 columns no-display " id='header-box-switch'>
-					<div class="header">
-						<div class="switch round" onclick="getSwich()">
-						  <input id="z" name="switch-z" type="radio" checked>
-						  <label for="z">Profilo</label>
-						
-						  <input id="z1" name="switch-z" type="radio">
-						  <label for="z1">Social</label>
-						
-						  <span></span>
-						</div>
-					</div>
-				</div>	
-				<div class="large-5 small-6 columns" id="header-box-search">					
-					<div class="row">
-						<div class="large-10 small-10 columns">
-							<div class="row">
-								<div class="large-8 small-12 columns " style="padding: 0px;">
-									<div class="header inline">
-										<!---------------------------- SEARCH ------------------------------------>						
-										<form class="inline" action="">
-											<span><input id='header-btn-search' name='header-btn-search' type="search" class="search" placeholder="<?php echo $views['header']['SEARCH'] ?>"></span>
-											<!-- <span><input type="search" class="search-small show-for-small" placeholder="Cerca "></span> -->
-					  					</form>
-				  					</div>	
-				  				</div>
-				  				<div class="large-4 columns hide-for-small " id="header-btn-notify">
-				  					<div class="header inline" style="float: right;">
-				  					<a class="ico-label _flag inline" onclick="headerShow()" ><span class="round alert label iconNotification <?php echo $css_not ?>"><?php echo $totNotification ?></span></a>
-				  					<a class="ico-label _setting inline"></a>
-				  					</div>
-				  				</div>
-			  				</div>
-  						</div>
-						<div class="large-2 small-2 columns">
-							<a id="add-main-btn" class="add inline _add inline"></a>
-						</div>
-					</div>						
+				<!---------- DURATION MUSIC  --------->
+				<small id="duration-player"  class="inline">2:30</small>
+
+				<div id="display">
+				    <a id="back"  class="icon-player _back"></a>
+				    <a id="pause" class="icon-player _pause play-pause"></a>
+				    <a id="play"  class="icon-player _play play-pause" style="display: none;"></a>
+				    <a id="next"  class="icon-player _next"></a>					
+				    <span id="execution" ></span>
 				</div>			
-			</div>	
-			
-			<div id="add" style="display: none;">
-				<div class="row">
-					<div class="large-12 columns">
-						<div class="add-btn" onclick="location.href='uploadRecord.php'"><?php echo $views['header']['OPTADD1'] ?></div>
-						<div class="add-btn" onclick="location.href='uploadEvent.php'"><?php echo $views['header']['OPTADD2'] ?></div>
-						<div class="add-btn" onclick="location.href='uploadAlbum.php'"><?php echo $views['header']['OPTADD3'] ?></div>
-					</div>
-				</div>
+			    </div>
+
+			</div>			
+		    </div>
+		    <!------------------------------------- LOGO --------------------------------------------->
+		    <div class="large-2 columns logo" id="header-box-logo">				
+			<div id="logo">
+			    <img src="resources/images/logo.png">						
+			</div>					
+		    </div>
+		    <!------------------------------------- SWITCH -------------------------------------------->
+		    <div class="small-5 columns no-display " id='header-box-switch'>
+			<div class="header">
+			    <div class="switch round" onclick="getSwich()">
+				<input id="z" name="switch-z" type="radio" checked>
+				<label for="z">Profilo</label>
+
+				<input id="z1" name="switch-z" type="radio">
+				<label for="z1">Social</label>
+
+				<span></span>
+			    </div>
 			</div>
-			
-			<script>
-				var addState = 0;
-				$('#add-main-btn').click(function() {
-					 $('#add').slideToggle();
-					 if (addState == 0) {
-						 $('._add').css('background-image', 'url(resources/images/icon/close.png)');
-						 addState = 1;
-					} else {
-						 $('._add').css('background-image', 'url(resources/images/icon/add.png)');
-						 addState = 0;
-					}
-				});
-			</script>
-		
+		    </div>	
+		    <div class="large-5 small-6 columns" id="header-box-search">					
+			<div class="row">
+			    <div class="large-10 small-10 columns">
+				<div class="row">
+				    <div class="large-8 small-12 columns " style="padding: 0px;">
+					<div class="header inline">
+					    <!---------------------------- SEARCH ------------------------------------>						
+					    <form class="inline" action="">
+						<span><input id='header-btn-search' name='header-btn-search' type="search" class="search" placeholder="<?php echo $views['header']['SEARCH'] ?>"></span>
+						<!-- <span><input type="search" class="search-small show-for-small" placeholder="Cerca "></span> -->
+					    </form>
+					</div>	
+				    </div>
+				    <div class="large-4 columns hide-for-small " id="header-btn-notify">
+					<div class="header inline" style="float: right;">
+					    <a class="ico-label _flag inline" onclick="headerShow()" ><span class="round alert label iconNotification <?php echo $css_not ?>"><?php echo $totNotification ?></span></a>
+					    <a class="ico-label _setting inline"></a>
+					</div>
+				    </div>
+				</div>
+			    </div>
+			    <div class="large-2 small-2 columns">
+				<a id="add-main-btn" class="add inline _add inline"></a>
+			    </div>
+			</div>						
+		    </div>			
+		</div>	
+
+		<div id="add" style="display: none;">
+		    <div class="row">
+			<div class="large-12 columns">
+			    <div class="add-btn" onclick="location.href = 'uploadRecord.php'"><?php echo $views['header']['OPTADD1'] ?></div>
+			    <div class="add-btn" onclick="location.href = 'uploadEvent.php'"><?php echo $views['header']['OPTADD2'] ?></div>
+			    <div class="add-btn" onclick="location.href = 'uploadAlbum.php'"><?php echo $views['header']['OPTADD3'] ?></div>
+			</div>
+		    </div>
 		</div>
-		
-		
+
+		<script>
+		var addState = 0;
+		$('#add-main-btn').click(function() {
+		    $('#add').slideToggle();
+		    if (addState == 0) {
+			$('._add').css('background-image', 'url(resources/images/icon/close.png)');
+			addState = 1;
+		    } else {
+			$('._add').css('background-image', 'url(resources/images/icon/add.png)');
+			addState = 0;
+		    }
+		});
+		</script>
+
+	    </div>
+
+
 	</div>
-</div>
+    </div>
 
 </header>
