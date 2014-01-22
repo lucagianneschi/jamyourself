72026bc1ef928b6302f16bcb0c53899bf57cd625
Daniele Caldelli <daniele.caldelli@gmail.com>
2014-01-22 17:05:07 +0100

    Aggiunto il love nello stream

--- views/content/general/script.php
+++ views/content/general/script.php
@@ -112,10 +112,7 @@ switch (basename($_SERVER['PHP_SELF'])) {
         ?>
         <script type="text/javascript" src="resources/javascripts/customs/stream.js"></script>
         <script type="text/javascript" src="resources/javascripts/customs/post.js"></script>
-        <!-- script type="text/javascript" src="resources/javascripts/customs/profile.js"></script -->
-        <!-- script type="text/javascript" src="resources/javascripts/customs/profileCallBox.js"></script -->
-        <!-- script type="text/javascript" src="resources/javascripts/customs/profileComment.js"></script -->
-        <!-- script type="text/javascript" src="resources/javascripts/customs/love.js"></script -->
+        <script type="text/javascript" src="resources/javascripts/customs/love.js"></script>
         <?php
         break;
     case "event.php":
