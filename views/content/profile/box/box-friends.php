ec8283116a093a5278dacfc1c5cf5b440207c093
marifree <marialaurafresu@gmail.com>
2014-01-23 17:51:21 +0100

    profile: modifiche box friends

--- views/content/profile/box/box-friends.php
+++ views/content/profile/box/box-friends.php
@@ -23,118 +23,59 @@ if (is_null($friendsBox->error)) {
     ?>
     <div class="row" id="profile-friends">
         <div  class="large-12 columns">
-    	<h3><?php echo $views['friends']['TITLE']; ?> <a data-reveal-id="viewFriendRelation"><span class="orange">[<?php echo $friendshipCounter; ?>]</span></a> </h3>
-    	<div class="row  ">
-    	    <div  class="large-12 columns ">
-    		<div class="box">					
-			<?php
-			if ($friendshipCounter > 0) {
-			    $totalView = $friendshipCounter > 4 ? 4 : $friendshipCounter;
-			    ?>
-			    <div class="row">
-				<?php
-				$i = 1;
-				foreach ($friends as $key => $value) {
-				    switch ($value->getType()) {
-					case 'JAMMER':
-					    $defaultThum = DEFTHUMBJAMMER;
-					    break;
-					case 'VENUE':
-					    $defaultThum = DEFTHUMBVENUE;
-					    break;
-					case 'SPOTTER':
-					    $defaultThum = DEFTHUMBSPOTTER;
-					    break;
-				    }
-				    ?>
-	    			<div  class="small-6 columns">
-	    			    <div class="box-membre" onclick="location.href = 'profile.php?user=<?php echo $value->getObjectId(); ?>'">
-	    				<div class="row " id="collaborator_<?php echo $value->getObjectId(); ?>">
-	    				    <div  class="small-3 columns ">
-	    					<div class="icon-header">
-	    					    <img src="../media/<?php echo $value->getProfileThumbnail(); ?>" onerror="this.src='<?php echo $defaultThum; ?>'">
-	    					</div>
-	    				    </div>
-	    				    <div  class="small-9 columns ">
-	    					<div class="text grey-light breakOffTest"><strong><?php echo $value->getUsername(); ?></strong></div>
-	    				    </div>		
-	    				</div>	
-	    			    </div>
-	    			</div>
-				    <?php
-				    if ($i % 2 == 0) {
-					?>
-				    </div>
-				    <div class="row">
-					<?php
-				    }
-				    if ($i == $totalView)
-					break;
-				    $i++;
-				}
-				?>							
-			    </div>
-			    <!------------------ elenco jammer ------------------------->
-			    <div id="viewFriendRelation" class="reveal-modal">
-				<div class="row">
-				    <?php
-				    $i = 1;
-				    foreach ($friends as $key => $value) {
-					switch ($value->getType()) {
-					    case 'JAMMER':
-						$defaultThum = DEFTHUMBJAMMER;
-						break;
-					    case 'VENUE':
-						$defaultThum = DEFTHUMBVENUE;
-						break;
-					    case 'SPOTTER':
-						$defaultThum = DEFTHUMBSPOTTER;
-						break;
-					}
-					?>
-
-	    			    <div  class="small-6 columns">
-	    				<div class="box-membre" onclick="location.href = 'profile.php?user=<?php echo $value->getObjectId(); ?>'">
-	    				    <div class="row " id="collaborator_<?php echo $value->getObjectId(); ?>">
-	    					<div  class="small-3 columns ">
-	    					    <div class="icon-header">
-	    						<img src="../media/<?php echo $value->getProfileThumbnail(); ?>" onerror="this.src='<?php echo $defaultThum; ?>'">
-	    					    </div>
-	    					</div>
-	    					<div  class="small-9 columns ">
-	    					    <div class="text grey-light breakOffTest"><strong><?php echo $value->getUsername(); ?></strong></div>
-	    					</div>		
-	    				    </div>	
-	    				</div>
-	    			    </div>
-					<?php
-					if ($i % 2 == 0) {
-					    ?>
-					</div>
-					<div class="row">
-					    <?php
-					}
-
-					$i++;
-				    }
-				    ?>
-				</div>
-			    </div>
-			    <!------------------ fine elenco jammer ------------------------->
-			    <?php
-			} else {
-			    ?>	
-			    <div class="row  ">
-				<div  class="large-12 columns ">
-				    <p class="grey"><?php echo $views['friends']['NODATA']; ?></p>
-				</div>
-			    </div>
-			    <?php
-			}
-			?>
-    		</div>	
-    	    </div>
-    	</div>
+    		<h3 style="cursor: pointer" onclick="loadBoxActivity('friendship',21,0,<?php echo $friendshipCounter; ?>)"><?php echo $views['friends']['TITLE']; ?> <span class="orange">[<?php echo $friendshipCounter; ?>]</span> </h3>
+			<div class="row">
+	    	    <div  class="large-12 columns ">
+	    			<div class="box">					
+						<?php 
+						if ($friendshipCounter > 0) { 
+							$i = 0;
+							foreach ($friends as $key => $value) {
+							    switch ($value->getType()) {
+								case 'JAMMER':
+								    $defaultThum = DEFTHUMBJAMMER;
+								    break;
+								case 'VENUE':
+								    $defaultThum = DEFTHUMBVENUE;
+								    break;
+								case 'SPOTTER':
+								    $defaultThum = DEFTHUMBSPOTTER;
+								    break;
+							    }
+								$pathPicture = USERS_DIR . $value->getObjectId(). '/images/profilepicturethumb/';
+								if ($i % 2 == 0) { ?> <div class="row">  <?php } ?>
+					    		
+				    			<div  class="small-6 columns">
+				    				<a href="profile.php?user=<?php echo $value->getObjectId(); ?>">
+					    			    <div class="box-membre">
+						    				<div class="row " id="collaborator_<?php echo $value->getObjectId(); ?>">
+						    				    <div  class="small-3 columns ">
+							    					<div class="icon-header">
+							    					    <img src="<?php echo $pathPicture.$value->getProfileThumbnail(); ?>" onerror="this.src='<?php echo $defaultThum; ?>'">
+							    					</div>
+						    				    </div>
+						    				    <div  class="small-9 columns ">
+						    						<div class="text grey-light breakOffTest"><strong><?php echo $value->getUsername(); ?></strong></div>
+						    				    </div>		
+						    				</div>	
+					    			    </div>
+				    			    </a>
+				    			</div>
+					   		 	<?php 
+					   		 	if (($i+1) % 2 == 0 || count($jammersFollowings) == ($i+1)) {  ?>  </div>  <?php }  
+			    			 	$i++;
+							} 
+						} else {
+					    ?>	
+						    <div class="row  ">
+								<div  class="large-12 columns ">
+								    <p class="grey"><?php echo $views['friends']['NODATA']; ?></p>
+								</div>
+						    </div>
+					    <?php } ?>
+	    			</div>	
+    	    	</div>
+    		</div>
         </div>
     </div>
     <?php
