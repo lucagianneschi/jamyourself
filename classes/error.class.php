866e24c44587c00de5c03ce43922977c0f08207f
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-01-31 21:52:36 +0100

    #26 aggiornamento dei commenti standard

--- classes/error.class.php
+++ classes/error.class.php
@@ -2,18 +2,19 @@
 
 /* ! \par		Info Generali:
  *  \author		Daniele Caldelli
- *  \version	1.0
+ *  \version		1.0
  *  \date		2013
- *  \copyright	Jamyourself.com 2013
+ *  \copyright		Jamyourself.com 2013
  *  \par		Info Classe:
  *  \brief		Error
- *  \details	Classe Error per la gestione degli errori
+ *  \details		Classe Error per la gestione degli errori
  *  \par		Commenti:
  *  \warning
  *  \bug
  *  \todo
  * 
- *  <a href="http://www.socialmusicdiscovering.com/dokuwiki/doku.php?id=documentazione:error:faq">API</a>
+ *  <a href="https://github.com/lucagianneschi/jamyourself/wiki/Definizione-Classe:-Error">Definizione Classe</a>
+ *  <a href="https://github.com/lucagianneschi/jamyourself/wiki/API:-Error">API</a>
  */
 
 class Error {
