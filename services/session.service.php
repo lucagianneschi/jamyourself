12874a7ed441862005ba7a08ac655824f80ab1a6
Daniele Caldelli <daniele.caldelli@gmail.com>
2014-02-01 18:41:29 +0100

    servizio per il controllo dell'utente in sessione

---
+++ services/session.service.php
@@ -0,0 +1,29 @@
+<?php
+
+/* ! \par		Info Generali:
+ * \author		Daniele Caldelli
+ * \version		1.0
+ * \date		2013
+ * \copyright	Jamyourself.com 2013
+ * \par			Info Classe:
+ * \brief		classe per la gestione delle operazioni iniziali
+ * \details		
+ * \par			
+ * \warning
+ * \bug
+ * \todo
+ *
+ */
+if (!defined('ROOT_DIR'))
+    define('ROOT_DIR', '../');
+
+require_once ROOT_DIR . 'config.php';
+require_once CLASSES_DIR . 'userParse.class.php';
+
+if (session_id() == '') session_start();
+
+if (!isset($_SESSION['currentUser'])) {
+    header('Location: ' . ROOT_DIR . 'index.php');
+}
+
+?>
