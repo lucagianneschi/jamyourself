75269bde67bbebf0add63f18b34da2241117f049
Daniele Caldelli <daniele.caldelli@gmail.com>
2014-02-01 18:40:56 +0100

    servizio per il controllo preliminare del caricamento delle pagine della modalità mantainance

---
+++ services/mantainance.service.php
@@ -0,0 +1,26 @@
+<?php
+
+/* ! \par		Info Generali:
+ * \author		Daniele Caldelli
+ * \version		1.0
+ * \date		2013
+ * \copyright	Jamyourself.com 2013
+ * \par			Info Classe:
+ * \brief		classe per la gestione delle operazioni iniziali
+ * \details		
+ * \par			
+ * \warning
+ * \bug
+ * \todo
+ *
+ */
+if (!defined('ROOT_DIR'))
+    define('ROOT_DIR', '../');
+
+require_once ROOT_DIR . 'config.php';
+
+if (MANTAINANCE_MODE) {
+    header('Location: ' . VIEWS_DIR . 'mantainance.php');
+}
+
+?>
