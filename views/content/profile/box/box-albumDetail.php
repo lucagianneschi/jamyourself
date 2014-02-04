67a0005f8c28ee40776c398208b71a29bcbd2a32
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-02-04 22:37:07 +0100

    implementazione FilemanagerService

--- views/content/profile/box/box-albumDetail.php
+++ views/content/profile/box/box-albumDetail.php
@@ -15,6 +15,7 @@ require_once SERVICES_DIR . 'debug.service.php';
 require_once SERVICES_DIR . 'lang.service.php';
 require_once SERVICES_DIR . 'geocoder.service.php';
 require_once LANGUAGES_DIR . 'views/' . getLanguage() . '.views.lang.php';
+require_once SERVICES_DIR . 'fileManager.service.php';
 require_once VIEWS_DIR . 'utilities/share.php';
 require_once BOXES_DIR . 'album.box.php';
 require_once CLASSES_DIR . 'userParse.class.php';
@@ -39,34 +40,32 @@ if (($limit + $skip) < $countImage) {
     $css_other = '';
     $other = $countImage - ($limit + $skip);
 }
-
-$pathImage = USERS_DIR . $userId . '/images/photos/'.$objectId.'/';
+$fileManagerService = new FileManagerService();
 ?>
-
 <ul class="small-block-grid-3 small-block-grid-2 " >	
     <?php
     foreach ($albumDetail->imageArray as $key => $value) {
 	$thumbImage = $value->getThumbnail();
 	?>
         <!------------------------------ THUMBNAIL ---------------------------------->
-        <li><a class="photo-colorbox-group" href="#<?php echo $value->getObjectId(); ?>"><img class="photo" src="<?php echo $pathImage.$value->getThumbnail() ?>" onerror="this.src='<?php echo DEFIMAGE; ?>'"></a></li>
+        <li><a class="photo-colorbox-group" href="#<?php echo $value->getObjectId(); ?>"><img class="photo" src="<?php echo $fileManagerService->getPhotoPath($objectId, $value->getThumbnail()); ?>" onerror="this.src='<?php echo DEFIMAGE; ?>'"></a></li>
 
-<?php } ?>
+    <?php } ?>
 </ul>
 
 <div class="row">
     <div class="small-12 columns">
-		<a class="text orange otherObject <?php echo $css_other; ?>" onclick="loadBoxAlbumDetail('<?php echo $objectId ?>',<?php echo $countImage ?>, 30,<?php echo $limit + $skip ?>)" style="padding-bottom: 15px;float: right;">Other <span><?php echo $other; ?></span> photos</a>	
+	<a class="text orange otherObject <?php echo $css_other; ?>" onclick="loadBoxAlbumDetail('<?php echo $objectId ?>',<?php echo $countImage ?>, 30,<?php echo $limit + $skip ?>)" style="padding-bottom: 15px;float: right;"><?php echo $views['other']; ?><span><?php echo $other; ?></span><?php echo $views['PHOTOS']; ?></a>	
     </div>
 </div>
 <div class='spinnerDetail'></div>
-<?php if(count($albumDetail->imageArray) == 0){ ?>
-	<div class="row  ">
-		<div  class="large-12 columns ">
-		    <p class="grey"><?php echo $views['album']['NODATA']; ?></p>
-		</div>
+<?php if (count($albumDetail->imageArray) == 0) { ?>
+    <div class="row  ">
+        <div  class="large-12 columns ">
+    	<p class="grey"><?php echo $views['album']['NODATA']; ?></p>
+        </div>
     </div>
-	
+
 <?php } ?>
 <!----------------------------------- lightbox ------------------------------------------>
 <div class="row no-display box" id="profile-Image">
@@ -82,78 +81,80 @@ $pathImage = USERS_DIR . $userId . '/images/photos/'.$objectId.'/';
 		$text_love = $views['LOVE'];
 	    }
 	    ?>				 	
-    	<div id="<?php echo $value->getObjectId(); ?>" class="lightbox-photo <?php echo $value->getFilePath(); ?>">
+    	<div id="<?php echo $value->getObjectId(); ?>" class="lightbox-photo <?php echo $fileManagerService->getPhotoPath($objectId, $value->getFilePath()); ?>">
     	    <div class="row " style="max-width: none;">
-	    		<div class="large-12 columns lightbox-photo-box">
-	    		    <div class="album-photo-box" onclick="nextLightBox()"><img class="album-photo"  src="<?php echo $pathImage.$value->getFilePath(); ?>" onerror="this.src='<?php echo DEFIMAGE; ?>'"/></div>
-	    		    <div class="row">
-	    				<div  class="large-12 columns" style="padding-top: 15px;padding-bottom: 15px"><div class="line"></div></div>
-	    		    </div>
-	    		    <div class="row" style="margin-bottom: 10px">
-		    			<div  class="small-6 columns">
-		    			    <a class="note grey " onclick="love(this, 'Image', '<?php echo $value->getObjectId(); ?>', '<?php echo $objectIdUser; ?>')"><?php echo $text_love; ?></a>
-		    			    <a class="note grey" onclick="loadBoxOpinion('<?php echo $value->getObjectId(); ?>', '<?php echo $value->getFromUser()->getObjectId(); ?>', 'Image', '#<?php echo $value->getObjectId(); ?> .box-opinion', 10, 0)"><?php echo $views['COMM']; ?></a>
-		    			    <a class="note grey" onclick="share(this, '<?php echo $value->getObjectId(); ?>', 'profile-Image')"><?php echo $views['SHARE']; ?></a>
-		    			</div>
-		    			<div  class="small-6 columns propriety">
-		    			    <a class="icon-propriety <?php echo $css_love ?>"><?php echo $value->getLoveCounter(); ?></a>
-		    			    <a class="icon-propriety _comment"><?php echo $value->getCommentCounter(); ?></a>
-		    			    <a class="icon-propriety _share"><?php echo $value->getShareCounter(); ?></a>	
-		    			</div>
-	    		    </div>
-	    		    <div class="row">
-		    			<div  class="small-5 columns">
-		    			    <div class="sottotitle white"><?php echo $album_title; ?></div>
-							<?php if ($value->getDescription() != "") { ?>
-						    <div class="text grey"><?php echo $value->getDescription(); ?></div>		
-							<?php }
-							if ($value->getLocation() instanceof parseGeoPoint) {
-							    $location = $value->getLocation();
-							    $lat = $location->lat;
-							    $lng = $location->long;
-							    $geocode = new GeocoderService();
-			
-							    $addressCode = $geocode->getAddress($lat, $lng);
-							    if (count($addressCode) > 0) {
-								$address = $addressCode['locality'] . " - " . $addressCode['country'];
-							    }
-							?>
-							<div class="text grey"><?php echo $address; ?></div>			
-							<?php }
-							$tag = "";
-							if (is_array($value->getTags())) {
-							    foreach ($value->getTags() as $key => $value) {
-								$tag = $tag + ' ' + $value;
-							} ?>
-						    <div class="text grey"><?php echo $tag; ?></div>
-						<?php }
-						?>
-		    			</div>
-		    			<div  class="small-7 columns">
-		    			    <!---------------------------------------- COMMENT ------------------------------------------------->
-		    			    <div class="box-opinion no-display" ></div>
-		    			    <!---------------------------------------- SHARE ---------------------------------------------------->
-		    			</div>
-						<?php
-						//	$paramsImage = getShareParameters('Image', '', $value->getFilePath());
-						?>		    			    
-		    		</div>
-	    			<!-- AddThis Button BEGIN -->
-    			    <div class="addthis_toolbox">
-	    				<div class="hover_menu">
-	    				    <div class="addthis_toolbox addthis_default_style"
-	    					 addThis:url="http://www.socialmusicdiscovering.com/views/share.php?classType=Image&objectId=&imgPath=<?php echo $value->getFilePath(); ?>"
-	    					 addThis:title="<?php //echo $paramsImage['title'];  ?>"
-	    					 onclick="addShare('<?php echo $objectIdUser; ?>', 'Image', '<?php echo $value->getObjectId(); ?>')">
-	    					<a class="addthis_button_twitter"></a>
-	    					<a class="addthis_button_facebook"></a>
-	    					<a class="addthis_button_google_plusone_share"></a>
-	    				    </div>	        
-	    				</div>
-    			    </div>
-	    			    <!-- AddThis Button END -->	
-	    		 </div>			
-	    	</div>
+    		<div class="large-12 columns lightbox-photo-box">
+    		    <div class="album-photo-box" onclick="nextLightBox()"><img class="album-photo"  src="<?php echo $pathImage . $value->getFilePath(); ?>" onerror="this.src='<?php echo DEFIMAGE; ?>'"/></div>
+    		    <div class="row">
+    			<div  class="large-12 columns" style="padding-top: 15px;padding-bottom: 15px"><div class="line"></div></div>
+    		    </div>
+    		    <div class="row" style="margin-bottom: 10px">
+    			<div  class="small-6 columns">
+    			    <a class="note grey " onclick="love(this, 'Image', '<?php echo $value->getObjectId(); ?>', '<?php echo $objectIdUser; ?>')"><?php echo $text_love; ?></a>
+    			    <a class="note grey" onclick="loadBoxOpinion('<?php echo $value->getObjectId(); ?>', '<?php echo $value->getFromUser()->getObjectId(); ?>', 'Image', '#<?php echo $value->getObjectId(); ?> .box-opinion', 10, 0)"><?php echo $views['COMM']; ?></a>
+    			    <a class="note grey" onclick="share(this, '<?php echo $value->getObjectId(); ?>', 'profile-Image')"><?php echo $views['SHARE']; ?></a>
+    			</div>
+    			<div  class="small-6 columns propriety">
+    			    <a class="icon-propriety <?php echo $css_love ?>"><?php echo $value->getLoveCounter(); ?></a>
+    			    <a class="icon-propriety _comment"><?php echo $value->getCommentCounter(); ?></a>
+    			    <a class="icon-propriety _share"><?php echo $value->getShareCounter(); ?></a>	
+    			</div>
+    		    </div>
+    		    <div class="row">
+    			<div  class="small-5 columns">
+    			    <div class="sottotitle white"><?php echo $album_title; ?></div>
+				<?php if ($value->getDescription() != "") { ?>
+				    <div class="text grey"><?php echo $value->getDescription(); ?></div>		
+				    <?php
+				}
+				if ($value->getLocation() instanceof parseGeoPoint) {
+				    $location = $value->getLocation();
+				    $lat = $location->lat;
+				    $lng = $location->long;
+				    $geocode = new GeocoderService();
+				    $addressCode = $geocode->getAddress($lat, $lng);
+				    if (count($addressCode) > 0) {
+					$address = $addressCode['locality'] . " - " . $addressCode['country'];
+				    }
+				    ?>
+				    <div class="text grey"><?php echo $address; ?></div>			
+				    <?php
+				}
+				$tag = "";
+				if (is_array($value->getTags())) {
+				    foreach ($value->getTags() as $key => $value) {
+					$tag = $tag + ' ' + $value;
+				    }
+				    ?>
+				    <div class="text grey"><?php echo $tag; ?></div>
+				<?php }
+				?>
+    			</div>
+    			<div  class="small-7 columns">
+    			    <!---------------------------------------- COMMENT ------------------------------------------------->
+    			    <div class="box-opinion no-display" ></div>
+    			    <!---------------------------------------- SHARE ---------------------------------------------------->
+    			</div>
+			    <?php
+			    //	$paramsImage = getShareParameters('Image', '', $value->getFilePath());
+			    ?>		    			    
+    		    </div>
+    		    <!-- AddThis Button BEGIN -->
+    		    <div class="addthis_toolbox">
+    			<div class="hover_menu">
+    			    <div class="addthis_toolbox addthis_default_style"
+    				 addThis:url="http://www.socialmusicdiscovering.com/views/share.php?classType=Image&objectId=&imgPath=<?php echo $value->getFilePath(); ?>"
+    				 addThis:title="<?php //echo $paramsImage['title'];     ?>"
+    				 onclick="addShare('<?php echo $objectIdUser; ?>', 'Image', '<?php echo $value->getObjectId(); ?>')">
+    				<a class="addthis_button_twitter"></a>
+    				<a class="addthis_button_facebook"></a>
+    				<a class="addthis_button_google_plusone_share"></a>
+    			    </div>	        
+    			</div>
+    		    </div>
+    		    <!-- AddThis Button END -->	
+    		</div>			
+    	    </div>
 
     	</div>
 
