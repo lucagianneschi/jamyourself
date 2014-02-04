f0315f0f5f2826a75a226d654046182fa551ccf2
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-02-04 21:21:05 +0100

    implementazione FilemanagerService - Termine implementazione su Pagina EVENT

--- views/content/event/box/box-opinion.php
+++ views/content/event/box/box-opinion.php
@@ -6,6 +6,7 @@ require_once ROOT_DIR . 'config.php';
 require_once SERVICES_DIR . 'lang.service.php';
 require_once LANGUAGES_DIR . 'views/' . getLanguage() . '.views.lang.php';
 require_once BOXES_DIR . 'comment.box.php';
+require_once SERVICES_DIR . 'fileManager.service.php';
 
 $objectId = $_POST['objectId'];
 $toUser = $_POST['toUser'];
@@ -55,7 +56,10 @@ if ($countComment > 0) {
                             <div  class="small-1 columns ">
                                 <div class="icon-header">
                                     <!-- THUMB USER-->
-                                    <?php $thumbPath = USERS_DIR . $value->getFromUser()->getObjectId() . DIRECTORY_SEPARATOR . "images" . DIRECTORY_SEPARATOR . "profilepicturethumb" . DIRECTORY_SEPARATOR . $value->getFromUser()->getProfileThumbnail(); ?>
+				    <?php
+				    $fileManagerService = new FileManagerService();
+				    $thumbPath = $fileManagerService->getEventPhotoPath($value->getObjectId(), $value->getProfileThumbnail());
+				    ?>
                                     <img src="<?php echo $thumbPath; ?>" onerror="this.src='<?php echo $defaultThum ?>'" alt ="<?php echo $value->getFromUser()->getUsername(); ?>">
                                 </div>
                             </div>
