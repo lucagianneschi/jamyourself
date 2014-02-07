62e5d8bbf72dbd4615b650b9d04216528e313129
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-02-07 16:59:56 +0100

    aggiunti commenti standard

--- config.php
+++ config.php
@@ -185,77 +185,223 @@ define('DEFVIDEOTHUMB', DEF_IMAGES_DIR . 'defaultVideoThumb.jpg');
  */
 define('BADGE_DIR', ROOT_DIR . 'views/resources/images/badge/');
 
+/** \def 'BADGE0', BADGE_DIR . 'badgeDefault.png'
+ *  white badge
+ */
 define('BADGE0', BADGE_DIR . 'badgeDefault.png');
+
+/** \def 'BADGE1', BADGE_DIR . 'badgeOldSchool.png'
+ *  OLD SCHOOL badge
+ */
 define('BADGE1', BADGE_DIR . 'badgeOldSchool.png');
+
+/** \def 'BADGE2', BADGE_DIR . 'badgeWelcome.png'
+ *  WELCOME badge
+ */
 define('BADGE2', BADGE_DIR . 'badgeWelcome.png');
+
+/** \def 'BADGE3', BADGE_DIR . 'badgePub.png'
+ *  PUB badge
+ */
 define('BADGE3', BADGE_DIR . 'badgePub.png');
+
+/** \def 'BADGE4', BADGE_DIR . 'badgeNightLife.png'
+ *  NIGHTLIFE badge
+ */
 define('BADGE4', BADGE_DIR . 'badgeNightLife.png');
+
+/** \def 'BADGE5', BADGE_DIR . 'badgeLive.png'
+ *  LIVE badge
+ */
 define('BADGE5', BADGE_DIR . 'badgeLive.png');
+
+/** \def 'BADGE6', BADGE_DIR . 'badgeRock.png'
+ *  ROCK badge
+ */
 define('BADGE6', BADGE_DIR . 'badgeRock.png');
+
+/** \def 'BADGE7', BADGE_DIR . 'badgeJamSession.png'
+ *  JAMSESSION badge
+ */
 define('BADGE7', BADGE_DIR . 'badgeJamSession.png');
+
+/** \def 'BADGE8', BADGE_DIR . 'badgeJammedIn.png'
+ *  JAMMEDIN badge
+ */
 define('BADGE8', BADGE_DIR . 'badgeJammedIn.png');
+
+/** \def 'BADGE9', BADGE_DIR . 'badgeHappyHour.png'
+ *  HAPPYHOUR badge
+ */
 define('BADGE9', BADGE_DIR . 'badgeHappyHour.png');
+
+/** \def 'BADGE10', BADGE_DIR . 'badgeProducer.png'
+ *  PRODUCER badge
+ */
 define('BADGE10', BADGE_DIR . 'badgeProducer.png');
+
+/** \def 'BADGE11', BADGE_DIR . 'badgeDj.png'
+ *  DJ badge
+ */
 define('BADGE11', BADGE_DIR . 'badgeDj.png');
+
+/** \def 'BADGE12', BADGE_DIR . 'badgeDinner.png'
+ *  DINER badge
+ */
 define('BADGE12', BADGE_DIR . 'badgeDinner.png');
+
+/** \def 'BADGE13', BADGE_DIR . 'badgeContest.png'
+ *  CONTEST badge
+ */
 define('BADGE13', BADGE_DIR . 'badgeContest.png');
+
+/** \def 'BADGE14', BADGE_DIR . 'badgeDance.png'
+ *  DANCE badge
+ */
 define('BADGE14', BADGE_DIR . 'badgeDance.png');
+
+/** \def 'BADGE15', BADGE_DIR . 'badgeElectro.png'
+ *  ELECTRO badge
+ */
 define('BADGE15', BADGE_DIR . 'badgeElectro.png');
+
+/** \def  'BADGE16', BADGE_DIR . 'badgePop.png'
+ *  POP badge
+ */
 define('BADGE16', BADGE_DIR . 'badgePop.png');
+
+/** \def  'BADGE17', BADGE_DIR . 'badgeMetal.png'
+ *  METAL badge
+ */
 define('BADGE17', BADGE_DIR . 'badgeMetal.png');
+
+/** \def  'BADGE18', BADGE_DIR . 'badgeJazz.png'
+ *  JAZZ badge
+ */
 define('BADGE18', BADGE_DIR . 'badgeJazz.png');
+
+/** \def  'BADGE19', BADGE_DIR . 'badgeInDemand.png'
+ *  INDEMAND badge
+ */
 define('BADGE19', BADGE_DIR . 'badgeInDemand.png');
+
+/** \def  'BADGE20', BADGE_DIR . 'badgeTeamUp.png'
+ *  TEAMUP badge
+ */
 define('BADGE20', BADGE_DIR . 'badgeTeamUp.png');
+
+/** \def  'BADGE21', BADGE_DIR . 'badgePhotographer.png'
+ *  PHOTOGRAPHER badge
+ */
 define('BADGE21', BADGE_DIR . 'badgePhotographer.png');
+
+/** \def  'BADGE22', BADGE_DIR . 'badgePr.png'
+ *  PR badge
+ */
 define('BADGE22', BADGE_DIR . 'badgePr.png');
+
+/** \def  'BADGE23', BADGE_DIR . 'badgeJournalist.png'
+ *  JOURNALIST badge
+ */
 define('BADGE23', BADGE_DIR . 'badgeJournalist.png');
 
-##################
-#  QUERY LIMITS  #
-##################
+/** \def  'MAX', 1000
+ *  MAX Query limit 
+ */
 define('MAX', 1000);
+
+/** \def  'MIN', 1000
+ *  MIN Query limit 
+ */
 define('MIN', 1);
+
+/** \def  'DEFAULTQUERY', 1000
+ *  DEAFULT Query limit 
+ */
 define('DEFAULTQUERY', 100);
+
+/** \def  'PLAYLISTLIMIT', 20
+ *  Playlist limit for standard users 
+ */
 define('PLAYLISTLIMIT', 20);
 
-//STRINGHE TITOLI PAGINA
+/** \def  'HOME_TITLE', 'Jamyourself: Meritocratic Social Music Discovering'
+ *  Home page title
+ */
 define('HOME_TITLE', 'Jamyourself: Meritocratic Social Music Discovering');
 
-//STRINGHE DI ERRORE
+/** \def  'LOCATIONNOTFOUND', 'Location Not Found'
+ *  Error String for Location
+ */
 define('LOCATIONNOTFOUND', 'Location Not Found');
-define('ONLYIFLOGGEDIN', 'Just for logged Users');
-##################
-#  CROP SIZES    #
-##################
 
+/** \def  'ONLYIFLOGGEDIN', 'Just for logged Users'
+ *  Error String for unlogged users
+ */
+define('ONLYIFLOGGEDIN', 'Just for logged Users');
+
+/** \def  'PROFILE_IMG_SIZE', 300
+ *  Image size width
+ */
 define('PROFILE_IMG_SIZE', 300);
+
+/** \def  'THUMBNAIL_IMG_SIZE', 150
+ *  Thumbnail size width
+ */
 define('THUMBNAIL_IMG_SIZE', 150);
 
-##################
-#  FILES SIZES   #
-##################
-
+/** \def  'MAX_IMG_UPLOAD_FILE_SIZE', 6291456
+ *  Max Image size 
+ */
 define('MAX_IMG_UPLOAD_FILE_SIZE', 6291456); //6 Mb
+
+/** \def  'MAX_MP3_UPLOAD_FILE_SIZE', 12582912
+ *  Max MP3 size 
+ */
 define('MAX_MP3_UPLOAD_FILE_SIZE', 12582912); //12 Mb
-#########################
-# SOCIAL NETWORK  & BLOG#
-#########################
+
+/** \def  'FACEBOOK', 'https://www.facebook.com/Jamyourselfcom'
+ * Facebook Address
+ */
 define('FACEBOOK', 'https://www.facebook.com/Jamyourselfcom');
+
+/** \def  'TWITTER', 'https://twitter.com/Jamyourself'
+ * TWITTER Address
+ */
 define('TWITTER', 'https://twitter.com/Jamyourself');
+
+/** \def  'BLOG', 'http://blog.jamyourself.com/'
+ * BLOG Address
+ */
 define('BLOG', 'http://blog.jamyourself.com/');
 
-################
-# DEFAULT NAMES#
-################
+/** \def  'DEF_PLAY', 'Playslist'
+ * Deafult Name for playlist
+ */
 define('DEF_PLAY', 'Playslist');
+
+/** \def  'DEF_REC', 'Default Record'
+ * Deafult Name for record
+ */
 define('DEF_REC', 'Default Record');
+
+/** \def  'DEF_ALBUM', 'Default Album'
+ * Deafult Name for album
+ */
 define('DEF_ALBUM', 'Default Album');
+
+/** \def  'SUB_SBJ', 'SUBSCRIBE NUOVO UTENTE'
+ * Deafult Subscribe string
+ */
 define('SUB_SBJ', 'SUBSCRIBE NUOVO UTENTE');
+
+/** \def  'SUB_ADD', 'subscribe@jamyourself.com'
+ * Deafult Subscribe address
+ */
 define('SUB_ADD', 'subscribe@jamyourself.com');
 
-##################
-#  LOG LEVEL     #
-##################
-
+/** \def  'DEBUG', true
+ * LOG LEVEL
+ */
 define('DEBUG', true);
 ?>
