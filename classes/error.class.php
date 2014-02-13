93366d038aec821d90eab65f11dcf7b658db7366
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-02-13 21:27:05 +0100

    adattata classe Error alla nuova struttura #71

--- classes/error.class.php
+++ classes/error.class.php
@@ -2,7 +2,7 @@
 
 /* ! \par		Info Generali:
  *  \author		Daniele Caldelli
- *  \version		1.0
+ *  \version		0.3
  *  \date		2013
  *  \copyright		Jamyourself.com 2013
  *  \par		Info Classe:
@@ -27,28 +27,27 @@ class Error {
     private $errorFunctionParameter;
     private $createdAt;
     private $updatedAt;
-    private $ACL;
 
     /**
-     * \fn		string getObjectId()
+     * \fn	int getObjectId()
      * \brief	Return the objectId value
-     * \return	string
+     * \return	int
      */
     public function getObjectId() {
 	return $this->objectId;
     }
 
     /**
-     * \fn		string getErrorClass()
+     * \fn	int getErrorClass()
      * \brief	Return the class which raise the Error
-     * \return	string
+     * \return	int
      */
     public function getErrorClass() {
 	return $this->errorClass;
     }
 
     /**
-     * \fn		number getErrorCode()
+     * \fn	number getErrorCode()
      * \brief	Return the Error code
      * \return	number
      */
@@ -57,16 +56,16 @@ class Error {
     }
 
     /**
-     * \fn		string getErrorFunction()
+     * \fn	int getErrorFunction()
      * \brief	Return the function which raise the Error
-     * \return	string
+     * \return	int
      */
     public function getErrorFunction() {
 	return $this->errorFunction;
     }
 
     /**
-     * \fn		array getErrorFunctionParameter()
+     * \fn	array getErrorFunctionParameter()
      * \brief	Returns an array containing the parameters of the function
      * \return	array
      */
@@ -75,9 +74,9 @@ class Error {
     }
 
     /**
-     * \fn		string getErrorMessage()
+     * \fn	int getErrorMessage()
      * \brief	Return the Error message
-     * \return	string
+     * \return	int
      */
     public function getErrorMessage() {
 	return $this->errorMessage;
@@ -93,7 +92,7 @@ class Error {
     }
 
     /**
-     * \fn		DateTime getUpdatedAt()
+     * \fn	DateTime getUpdatedAt()
      * \brief	Return the Error modification date
      * \return	DateTime
      */
@@ -102,36 +101,27 @@ class Error {
     }
 
     /**
-     * \fn		parseACL getACL()
-     * \brief	Return the parseACL object representing the Error ACL 
-     * \return	parseACL
-     */
-    public function getACL() {
-	return $this->ACL;
-    }
-
-    /**
-     * \fn		void setObjectId($objectId)
+     * \fn	void setObjectId($objectId)
      * \brief	Sets the objectId value
-     * \param	string
+     * \param	int
      */
     public function setObjectId($objectId) {
 	$this->objectId = $objectId;
     }
 
     /**
-     * \fn		void setErrorClass($errorClass)
+     * \fn	void setErrorClass($errorClass)
      * \brief	Sets the class which raise the Error
-     * \param	string
+     * \param	int
      */
     public function setErrorClass($errorClass) {
 	$this->errorClass = $errorClass;
     }
 
     /**
-     * \fn		void setErrorCode($errorCode)
+     * \fn	void setErrorCode($errorCode)
      * \brief	Sets the Error code
-     * \param	string
+     * \param	int
      */
     public function setErrorCode($errorCode) {
 	$this->errorCode = $errorCode;
@@ -140,14 +130,14 @@ class Error {
     /**
      * \fn		void setErrorFunction($errorFunction)
      * \brief	Sets the function which raise the Error
-     * \param	string
+     * \param	int
      */
     public function setErrorFunction($errorFunction) {
 	$this->errorFunction = $errorFunction;
     }
 
     /**
-     * \fn		void setErrorFunctionParameter($errorFunctionParameter)
+     * \fn	void setErrorFunctionParameter($errorFunctionParameter)
      * \brief	Sets an array containing the parameters of the function
      * \param	array
      */
@@ -156,16 +146,16 @@ class Error {
     }
 
     /**
-     * \fn		void setErrorMessage($errorMessage)
+     * \fn	void setErrorMessage($errorMessage)
      * \brief	Sets the Error message
-     * \param	string
+     * \param	int
      */
     public function setErrorMessage($errorMessage) {
 	$this->errorMessage = $errorMessage;
     }
 
     /**
-     * \fn		void setCreatedAt($createdAt)
+     * \fn	void setCreatedAt($createdAt)
      * \brief	Sets the Error creation date
      * \param	DateTime
      */
@@ -174,7 +164,7 @@ class Error {
     }
 
     /**
-     * \fn		void setUpdatedAt($updatedAt)
+     * \fn	void setUpdatedAt($updatedAt)
      * \brief	Sets the Error modification date
      * \param	DateTime
      */
@@ -183,61 +173,37 @@ class Error {
     }
 
     /**
-     * \fn		void setACL($ACL)
-     * \brief	Sets the parseACL object representing the Error ACL
-     * \param	parseACL
-     */
-    public function setACL($ACL) {
-	$this->ACL = $ACL;
-    }
-
-    /**
-     * \fn		string __toString()
-     * \brief	Return a printable string representing the Error object
-     * \return	string
+     * \fn	string __toString()
+     * \brief	Return a printable int representing the Error object
+     * \return	int
      */
     public function __toString() {
-	$string = '';
-	$string .= '[objectId] => ' . $this->getObjectId() . '<br />';
-	$string .= '[errorClass] => ' . $this->getErrorClass() . '<br />';
-	$string .= '[errorCode] => ' . $this->getErrorCode() . '<br />';
-	$string .= '[errorFunction] => ' . $this->getErrorFunction() . '<br />';
-	$string .= '[errorFunctionParameter] => <br />';
+	$int = '';
+	$int .= '[objectId] => ' . $this->getObjectId() . '<br />';
+	$int .= '[errorClass] => ' . $this->getErrorClass() . '<br />';
+	$int .= '[errorCode] => ' . $this->getErrorCode() . '<br />';
+	$int .= '[errorFunction] => ' . $this->getErrorFunction() . '<br />';
+	$int .= '[errorFunctionParameter] => <br />';
 	if ($this->getErrorFunctionParameter() && count($this->getErrorFunctionParameter()) > 0) {
 	    foreach ($this->getErrorFunctionParameter() as $key => $value) {
-		$string .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-		$string .= '[param] => ' . $key . ' -> ' . $value . '<br />';
+		$int .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+		$int .= '[param] => ' . $key . ' -> ' . $value . '<br />';
 	    }
 	}
 	else
-	    $string .= 'NULL <br/>';
-	$string .= '[errorMessage] => ' . $this->getErrorMessage() . '<br />';
+	    $int .= 'NULL <br/>';
+	$int .= '[errorMessage] => ' . $this->getErrorMessage() . '<br />';
 	if ($this->getCreatedAt() != null) {
-	    $string .= '[createdAt] => ' . $this->getCreatedAt()->format('d-m-Y H:i:s') . '<br />';
+	    $int .= '[createdAt] => ' . $this->getCreatedAt()->format('d-m-Y H:i:s') . '<br />';
 	} else {
-	    $string .= '[createdAt] => NULL<br />';
+	    $int .= '[createdAt] => NULL<br />';
 	}
 	if ($this->getUpdatedAt() != null) {
-	    $string .= '[updatedAt] => ' . $this->getUpdatedAt()->format('d-m-Y H:i:s') . '<br />';
+	    $int .= '[updatedAt] => ' . $this->getUpdatedAt()->format('d-m-Y H:i:s') . '<br />';
 	} else {
-	    $string .= '[updatedAt] => NULL<br />';
+	    $int .= '[updatedAt] => NULL<br />';
 	}
-	if ($this->getACL() != null) {
-	    foreach ($this->getACL()->acl as $key => $acl) {
-		$string .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-		$string .= '[ACL] => ' . $key . '<br />';
-		foreach ($acl as $access => $value) {
-		    $string .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-		    $string .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-		    $string .= '[access] => ' . $access . ' -> ' . $value . '<br />';
-		}
-	    }
-	} else {
-	    $string .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-	    $string .= '[ACL] => NULL<br />';
-	}
-
-	return $string;
+	return $int;
     }
 
 }
