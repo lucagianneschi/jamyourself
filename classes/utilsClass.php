c0b4d302c1e106c79b8b3f97b86c64120f9fae8b
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-02-13 23:42:04 +0100

    eliminate funzioni legate al vecchio DB #71

--- classes/utilsClass.php
+++ classes/utilsClass.php
@@ -23,10 +23,10 @@ require_once ROOT_DIR . 'config.php';
 /**
  * \fn		number executionTime($start, $end)
  * \brief	The function returns the difference between $end and $start parameter in microseconds
- * \param	$start		represent the microsecond time of the begin of the operation
- * \param	$end		represent the microsecond time of the end of the operation
- * \return	number		the number representing the difference in microsecond
- * \return	Error		if the parameters are null
+ * \param	$start	represent the microsecond time of the begin of the operation
+ * \param	$end	represent the microsecond time of the end of the operation
+ * \return	number	the number representing the difference in microsecond
+ * \return	Error	if the parameters are null
  */
 function executionTime($start, $end) {
     if (is_null($start) || is_null($end))
@@ -48,23 +48,6 @@ function executionTime($start, $end) {
 }
 
 /**
- * \fn		parse_decode_array($array)
- * \brief	The function returns a array read from Parse that can be interpreted by the user
- * \param	$array 	represent the array from Parse to decode
- * \return	array		the decoded array
- */
-function parse_decode_array($array) {
-    $decodedArray = array();
-    if (!empty($array) && !is_null($array) && count($array) > 0) {
-	foreach ($array as $string) {
-	    $decodedString = parse_decode_string($string);
-	    array_push($decodedArray, $decodedString);
-	}
-    }
-    return $decodedArray;
-}
-
-/**
  * \fn		string parse_decode_string($string)
  * \brief	The function returns a string read from Parse that can be interpreted by the user
  * \param	$string 	represent the string from Parse to decode
@@ -74,23 +57,6 @@ function parse_decode_string($string) {
     $string = html_entity_decode($string, ENT_QUOTES, 'UTF-8');
     $decodedString = preg_replace('/\<br(\s*)?\/?\>/i', "\n", $string);
     return $decodedString;
-}
-
-/**
- * \fn		parse_encode_array($array)
- * \brief	The function returns an array that can be saved to Parse
- * \param	$array 	represent the array to be saved
- * \return	array		the array encoded for Parse
- */
-function parse_encode_array($array) {
-    $encodedArray = array();
-    if (!empty($array) && !is_null($array) && count($array) > 0) {
-	foreach ($array as $string) {
-	    $encodedString = parse_encode_string($string);
-	    array_push($encodedArray, $encodedString);
-	}
-    }
-    return $encodedArray;
 }
 
 /**
