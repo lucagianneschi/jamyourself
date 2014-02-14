3daba4c864ee8f7fbda720791e411620d2356f0e
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-02-14 23:42:44 +0100

    servizio di connessione e disconnessione DB #74

---
+++ services/connection.service.php
@@ -0,0 +1,64 @@
+<?php
+
+/* ! \par		Info Generali:
+ *  \author		Luca Gianneschi
+ *  \version		0.3
+ *  \date		2013
+ *  \copyright		Jamyourself.com 2013
+ *  \par		Info Classe:
+ *  \brief		Servizio commessione DB
+ *  \details		Servizio commessione DB
+ *  \par		Commenti:
+ *  \warning
+ *  \bug
+ *  \todo
+ *
+ */
+
+class ConnectionService {
+
+    private $host = "localhost";
+    private $user = "username";
+    private $password = "password";
+    private $database = "database";
+    private $active = false;
+
+    /**
+     * \fn	connect()
+     * \brief	connet to the database
+     * \return	true or errors
+     */
+    public function connect() {
+	if (!$this->active) {
+	    mysqli_connect($this->host, $this->user, $this->password, $this->database);
+	    if (mysqli_connect_errno()) {
+		echo "Errore:" . mysqli_connect_error();
+		exit();
+	    } else {
+		$this->active = true;
+		return true;
+	    }
+	} else {
+	    $this->active = true;
+	    return true;
+	}
+    }
+
+    /**
+     * \fn	disconnect()
+     * \brief	disconnet from the database
+     * \return	true
+     */
+    public function disconnect() {
+	if ($this->attiva) {
+	    if (mysqli_close()) {
+		$this->attiva = false;
+		return true;
+	    } else {
+		return false;
+	    }
+	}
+    }
+
+}
+?>
