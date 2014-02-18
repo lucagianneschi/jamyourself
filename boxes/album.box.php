2c05e24d5aaa4104ab5ef81809ca2c0f0056c79d
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-02-18 19:39:07 +0100

    avanzamento códice album.box funzione init() #72

--- boxes/album.box.php
+++ boxes/album.box.php
@@ -38,38 +38,55 @@ class AlbumBox {
      * \todo    inserire orderby
      */
     public function init($id, $limit = 3, $skip = 0, $upload = false) {
-        if ($upload == true) {
-            require_once SERVICES_DIR . 'utils.service.php';
-            $currentUserId = sessionChecker();
-            if (is_null($currentUserId)) {
-            $this->error = ONLYIFLOGGEDIN;
-            return;
-            }
-        }
-        $connectionService = new ConnectionService();
-        $connectionService->connect();
-        if (!$connectionService->active) {
-            $this->error = $connectionService->error;
-            return;
-        } else {
-            $sql = "SELECT <tutti i campi>                      FROM album a, user_album ua                     WHERE ua.id_user = " . $id . "                       AND ua.id_album = a.id                     LIMIT " . $skip . ", " . $limit;
-            $results = mysqli_query($connectionService->connection, $sql);
-            while($row = mysqli_fetch_array($results, MYSQLI_ASSOC)) $rows[] = $row;
-            $albums = array();
-            foreach ($rows as $row) {
-                $album = new Album();
-                $album->setId($row['id']);
-                $album->setActive($row['active']);
-                //...
-                $albums[$row['id']] = $album;
-            }
-            $connectionService->disconnect();
-            if (!$results) {
-                return;
-            } else {
-                $this->albumArray = $albums;
-            }
-        }
+	if ($upload == true) {
+	    require_once SERVICES_DIR . 'utils.service.php';
+	    $currentUserId = sessionChecker();
+	    if (is_null($currentUserId)) {
+		$this->error = ONLYIFLOGGEDIN;
+		return;
+	    }
+	}
+	$connectionService = new ConnectionService();
+	$connectionService->connect();
+	if (!$connectionService->active) {
+	    $this->error = $connectionService->error;
+	    return;
+	} else {
+	    $sql = "SELECT <tutti i campi>
+                      FROM album a, user_album ua
+                     WHERE ua.id_user = " . $id . "
+                       AND ua.id_album = a.id
+                     LIMIT " . $skip . ", " . $limit;
+	    $results = mysqli_query($connectionService->connection, $sql);
+	    while ($row = mysqli_fetch_array($results, MYSQLI_ASSOC))
+		$rows[] = $row;
+	    $albums = array();
+	    foreach ($rows as $row) {
+		$album = new Album();
+		$album->setId($row['id']);
+		$album->setActive($row['active']);
+		$album->setCommentcounter($row['commentcounter']);
+		$album->setCounter($row['counter']);
+		$album->setCover($row['cover']);
+		$album->setDescription($row['description']);
+		$album->setFromuser($row['fromuser']);
+		$album->setImagecounter($row['imagecounter']);
+		$album->setLatitude($row['latitude']);
+		$album->setLongitude($row['longitude']);
+		$album->setLovecounter($row['lovecounter']);
+		$album->setSharecounter($row['sharecounter']);
+		$album->setThumbnail($row['thumbnail']);
+		$album->setTitle($row['title']);
+		$album->setUpdatedat($row['updatedat']);
+		$albums[$row['id']] = $album;
+	    }
+	    $connectionService->disconnect();
+	    if (!$results) {
+		return;
+	    } else {
+		$this->albumArray = $albums;
+	    }
+	}
     }
 
     /**
@@ -97,4 +114,5 @@ class AlbumBox {
     }
 
 }
+
 ?>
