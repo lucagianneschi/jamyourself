a72d5e766199c272421aaa01356de20487decd3b
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-02-19 18:31:29 +0100

    fix #76 eliminata localizzazione restController

--- controllers/restController.php
+++ controllers/restController.php
@@ -3,9 +3,6 @@
 if (!defined('ROOT_DIR'))
     define('ROOT_DIR', '../');
 
-require_once SERVICES_DIR . 'lang.service.php';
-require_once LANGUAGES_DIR . 'controllers/' . getLanguage() . '.controllers.lang.php';
-
 class REST {
 
     public $_allow = array();
@@ -31,49 +28,48 @@ class REST {
     }
 
     private function get_status_message() {
-	global $rest_strings;
 	$status = array(
-	    100 => $rest_strings['100'],
-	    101 => $rest_strings['101'],
-	    200 => $rest_strings['200'],
-	    201 => $rest_strings['201'],
-	    202 => $rest_strings['202'],
-	    203 => $rest_strings['203'],
-	    204 => $rest_strings['204'],
-	    205 => $rest_strings['205'],
-	    206 => $rest_strings['206'],
-	    300 => $rest_strings['300'],
-	    301 => $rest_strings['301'],
-	    302 => $rest_strings['302'],
-	    303 => $rest_strings['303'],
-	    304 => $rest_strings['304'],
-	    305 => $rest_strings['305'],
-	    306 => $rest_strings['306'],
-	    307 => $rest_strings['307'],
-	    400 => $rest_strings['400'],
-	    401 => $rest_strings['401'],
-	    402 => $rest_strings['402'],
-	    403 => $rest_strings['403'],
-	    404 => $rest_strings['404'],
-	    405 => $rest_strings['405'],
-	    406 => $rest_strings['406'],
-	    407 => $rest_strings['407'],
-	    408 => $rest_strings['408'],
-	    409 => $rest_strings['409'],
-	    410 => $rest_strings['410'],
-	    411 => $rest_strings['411'],
-	    412 => $rest_strings['412'],
-	    413 => $rest_strings['413'],
-	    414 => $rest_strings['414'],
-	    415 => $rest_strings['415'],
-	    416 => $rest_strings['416'],
-	    417 => $rest_strings['417'],
-	    500 => $rest_strings['500'],
-	    501 => $rest_strings['501'],
-	    502 => $rest_strings['502'],
-	    503 => $rest_strings['503'],
-	    504 => $rest_strings['504'],
-	    505 => $rest_strings['505']);
+	    100 => 'Continue',
+	    101 => 'Switching Protocols',
+	    200 => 'OK',
+	    201 => 'Created',
+	    202 => 'Accepted',
+	    203 => 'Non-Authoritative Information',
+	    204 => 'No Content',
+	    205 => 'Reset Content',
+	    206 => 'Partial Content',
+	    300 => 'Multiple Choices',
+	    301 => 'Moved Permanently',
+	    302 => 'Found',
+	    303 => 'See Other',
+	    304 => 'Not Modified',
+	    305 => 'Use Proxy',
+	    306 => '(Unused)',
+	    307 => 'Temporary Redirect',
+	    400 => 'Bad Request',
+	    401 => 'Unauthorized',
+	    402 => 'Payment Required',
+	    403 => 'Forbidden',
+	    404 => 'Not Found',
+	    405 => 'Method Not Allowed',
+	    406 => 'Not Acceptable',
+	    407 => 'Proxy Authentication Required',
+	    408 => 'Request Timeout',
+	    409 => 'Conflict',
+	    410 => 'Gone',
+	    411 => 'Length Required',
+	    412 => 'Precondition Failed',
+	    413 => 'Request Entity Too Large',
+	    414 => 'Request-URI Too Long',
+	    415 => 'Unsupported Media Type',
+	    416 => 'Requested Range Not Satisfiable',
+	    417 => 'Expectation Failed',
+	    500 => 'Internal Server Error',
+	    501 => 'Not Implemented',
+	    502 => 'Bad Gateway',
+	    503 => 'Service Unavailable',
+	    504 => 'Gateway Timeout',
+	    505 => 'HTTP Version Not Supported');
 	return ($status[$this->_code]) ? $status[$this->_code] : $status[500];
     }
 
