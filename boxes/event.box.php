d93c721ef2b5a09cabc7cf9181992f50d70fcc1c
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-02-20 23:28:04 +0100

    avanzamento codice #72 per event.box.php : implementazione funzione select da db.service.php

--- boxes/event.box.php
+++ boxes/event.box.php
@@ -19,7 +19,6 @@ if (!defined('ROOT_DIR'))
     define('ROOT_DIR', '../');
 
 require_once ROOT_DIR . 'config.php';
-require_once SERVICES_DIR . 'connection.service.php';
 require_once SERVICES_DIR . 'db.service.php';
 
 /**
@@ -46,7 +45,7 @@ class EventBox {
 		return;
 	    }
 	}
-	$events = selectEvents($id, null, array('createdat' => 'DESC'), $limit, $skip);
+	$events = selectEvents(null, array('fromuser' => $id), array('createdat' => 'DESC'), $limit, $skip);
 	if ($events instanceof Error) {
 	    $this->error = $events->getErrorMessage();
 	}
@@ -59,91 +58,11 @@ class EventBox {
      * \param	$id for event
      */
     public function initForMediaPage($id) {
-	$connectionService = new ConnectionService();
-	$connectionService->connect();
-	if (!$connectionService->active) {
-	    $this->error = $connectionService->error;
-	    return;
-	} else {
-	    $sql = "SELECT e.id id_e,
-                           e.active,
-                           e.address,
-                           e.attendeecounter,
-                           e.cancelledcounter,
-                           e.city,
-                           e.commentcounter,
-                           e.counter,
-                           e.cover,
-                           e.description,
-                           e.eventdate,
-                           e.fromuser,
-                           e.genre,
-                           e.invitedcounter,
-                           e.latitude,
-                           e.longitude,
-                           e.locationname,
-                           e.lovecounter,
-                           e.reviewcounter,
-                           e.refusedcounter,
-                           e.sharecounter,
-                           e.thumbnail thumbnail_e,
-                           e.title,
-                           e.createdat,
-                           e.updatedat,
-                           u.id id_u,
-                           u.username,
-                           u.thumbnail thumbnail_u,
-                           u.type
-                      FROM event e, user u
-                     WHERE e.id = " . $id . "
-                       AND e.fromuser = u.id";
-	    $results = mysqli_query($connectionService->connection, $sql);
-	    while ($row = mysqli_fetch_array($results, MYSQLI_ASSOC))
-		$rows[] = $row;
-	    $events = array();
-	    foreach ($rows as $row) {
-		require_once 'event.class.php';
-		require_once 'user.class.php';
-		$event = new Event();
-		$event->setId($row['id_e']);
-		$event->setActive($row['active']);
-		$event->setAddress($row['address']);
-		$event->setAttendeecounter($row['attendeecounter']);
-		$event->setCancelledcounter($row['cancelledcounter']);
-		$event->setCity($row['city']);
-		$event->setCommentcounter($row['commentcounter']);
-		$event->setCounter($row['counter']);
-		$event->setCover($row['cover']);
-		$event->setCreatedat($row['createdat']);
-		$event->setDescription($row['description']);
-		$event->setEventdate($row['eventdate']);
-		$fromuser = new User($row_user['type']);
-		$fromuser->setId($row_user['id_u']);
-		$fromuser->setThumbnail($row_user['thumbnail_u']);
-		$fromuser->setUsername($row_user['username']);
-		$event->setFromuser($fromuser);
-		$event->setGenre($row['genre']);
-		$event->setInvitedCounter($row['invitedCounter']);
-		$event->setLatitude($row['latitude']);
-		$event->setLocationname($row['locationname']);
-		$event->setLongitude($row['longitude']);
-		$event->setLovecounter($row['lovecounter']);
-		$event->setRefusedcounter($row['refusedcounter']);
-		$event->setReviewcounter($row['reviewcounter']);
-		$event->setSharecounter($row['sharecounter']);
-		$event->setTag($row['tag']);
-		$event->setThumbnail($row['thumbnail_e']);
-		$event->setTitle($row['title']);
-		$event->setUpdatedat($row['updatedat']);
-		$events[$row['id_e']] = $event;
-	    }
-	    $connectionService->disconnect();
-	    if (!$results) {
-		return;
-	    } else {
-		$this->eventArray = $results;
-	    }
+	$events = selectEvents($id);
+	if ($events instanceof Error) {
+	    $this->error = $events->getErrorMessage();
 	}
+	$this->eventArray = $events;
     }
 
     /**
