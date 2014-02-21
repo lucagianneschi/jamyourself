5a03fadef597d78dbdd94d40f35d9041af59d5bf
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-02-21 01:21:39 +0100

    eliminata import non richiesta #72

--- boxes/userInfo.box.php
+++ boxes/userInfo.box.php
@@ -19,7 +19,6 @@ if (!defined('ROOT_DIR'))
     define('ROOT_DIR', '../');
 
 require_once ROOT_DIR . 'config.php';
-require_once SERVICES_DIR . 'connection.service.php';
 
 /**
  * \brief	UserInfoBox class 
