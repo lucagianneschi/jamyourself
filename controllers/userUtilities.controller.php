109c19a594af700c5b9bb26b6ba282433512eb3e
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-03-02 17:54:41 +0100

    pulizia pre nuova versione

--- controllers/userUtilities.controller.php
+++ controllers/userUtilities.controller.php
@@ -20,7 +20,7 @@ if (!defined('ROOT_DIR'))
 
 
 require_once ROOT_DIR . 'config.php';
- 
+
 require_once SERVICES_DIR . 'lang.service.php';
 require_once LANGUAGES_DIR . 'controllers/' . getLanguage() . '.controllers.lang.php';
 require_once CONTROLLERS_DIR . 'restController.php';
@@ -30,33 +30,6 @@ require_once CONTROLLERS_DIR . 'restController.php';
  * \details	controller di utilities riferite alla classe utente
  */
 class UserUtilitiesController extends REST {
-
-    /**
-     * \fn      createActivity($type, $fromuser)
-     * \brief   private function to create ad hoc activity
-     * \param   $type, $fromuser
-     */
-    private function createActivity($type, $fromuser) {
-	require_once CLASSES_DIR . 'activity.class.php';
-	$activity = new Activity();
-	$activity->setActive(true);
-	$activity->setAlbum(null);
-	$activity->setComment(null);
-	$activity->setCounter(0);
-	$activity->setEvent(null);
-	$activity->setFromuser($fromuser);
-	$activity->setImage(null);
-	$activity->setPlaylist(null);
-	$activity->setQuestion(null);
-	$activity->setRecord(null);
-	$activity->setRead(true);
-	$activity->setSong(null);
-	$activity->setStatus('A');
-	$activity->setTouser(null);
-	$activity->setType($type);
-	$activity->setVideo(null);
-	return $activity;
-    }
 
     /**
      * \fn		passwordReset()
@@ -72,55 +45,11 @@ class UserUtilitiesController extends REST {
 		$this->response(array('status' => $controllers['NOEMAILFORRESETPASS']), 403);
 	    }
 	    $email = $this->request['email'];
-	    require_once CLASSES_DIR . 'userParse.class.php';
-	    $userP = new UserParse();
-	    $user = $userP->passwordReset($email);
-	    if ($user instanceof Error) {
-		$this->response(array('status' => $controllers['USERNOTFOUNDFORPASSRESET']), 503);
-	    }
-	    $activity = $this->createActivity('PASSWORDRESETREQUEST', $user->getId());
-	    require_once CLASSES_DIR . 'activityParse.class.php';
-	    $activityParse = new ActivityParse();
-	    $res = $activityParse->saveActivity($activity);
-	    if ($res instanceof Error) {
-		$this->response(array('status' => $controllers['NOACSAVE']), 503);
-	    }
-	    $this->response(array($controllers['OKPASSWORDRESETREQUEST']), 200);
-	} catch (Exception $e) {
-	    $this->response(array('status' => $e->getMessage()), 503);
-	}
-    }
 
-    /**
-     * \fn	public function updateSetting()
-     * \brief   effettua l'update dell'array dei settings
-     * \todo    
-     */
-    public function updateSetting() {
-	try {
-	    global $controllers;
-	    if ($this->get_request_method() != "POST") {
-		$this->response(array('status' => $controllers['NOPOSTREQUEST']), 405);
-	    } elseif (!isset($_SESSION['currentUser'])) {
-		$this->response(array('status' => $controllers['USERNOSES']), 403);
-	    } elseif (!isset($_SESSION['setting'])) {
-		$this->response(array('status' => $controllers['NOSETTING']), 403);
-	    }
-	    $currentUser = $this->request['currentUser'];
-	    $settings = $this->request['setting'];
-	    require_once CLASSES_DIR . 'userParse.class.php';
-	    $userP = new UserParse();
-	    $res = $userP->updateField($currentUser->getId(), 'settings', array($settings));
-	    if ($res instanceof Error) {
-		$this->response(array('status' => $controllers['NOSETTINGUPDATE']), 503);
-	    }
-	    $activity = $this->createActivity("USERSETTINGSUPDATED", $currentUser->getId());
-	    $activityParse = new ActivityParse();
-	    $resAct = $activityParse->saveActivity($activity);
-	    if ($resAct instanceof Error) {
-		$this->response(array('status' => $controllers['NOACSAVE']), 503); //ROLLBACK??
-	    }
-	    $this->response(array('SETTINGUPDATED'), 200);
+
+
+
+	    $this->response(array($controllers['OKPASSWORDRESETREQUEST']), 200);
 	} catch (Exception $e) {
 	    $this->response(array('status' => $e->getMessage()), 503);
 	}
