0ec077a1bccd3f83d829ee04b1603fb596147c85
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-03-04 20:57:07 +0100

    formattazzione

--- services/connection.service.php
+++ services/connection.service.php
@@ -24,62 +24,62 @@ require_once SERVICES_DIR . 'log.service.php';
 class ConnectionService {
 
     /**
-     * \fn		curl($query, $params)
+     * \fn	curl($query, $params)
      * \brief	connet to the database
      * \return	true or errors
      */
     public function curl($query, $params) {
-		$c = curl_init();
-		curl_setopt($c, CURLOPT_URL, 'http://' . NEO4J_HOST . ':' . NEO4J_PORT . '/db/data/cypher');
-		curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($c, CURLOPT_HTTPHEADER, array(
-			'Accept: application/json; stream=true',
-			'Content-type: application/json',
-			'X-Stream: true'
-		));
-		curl_setopt($c, CURLOPT_CUSTOMREQUEST, 'POST');
-		curl_setopt($c, CURLOPT_POST, false);
-		curl_setopt($c, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-		curl_setopt($c, CURLOPT_USERPWD, NEO4J_USER . ':' . NEO4J_PSW);
-		$dataString = json_encode(array('query' => $query, 'params' => $params));
-		curl_setopt($c, CURLOPT_POSTFIELDS, $dataString);
-		$response = curl_exec($c);
-		if ($response === false) {
-			jam_log($_SERVER['SCRIPT_NAME'], __LINE__, 'Unable to connect to ' . NEO4J_HOST);
-			return false;
-		} else {
-			$data = json_decode($response, true);
-			$this->data = $data;
-		}
+	$c = curl_init();
+	curl_setopt($c, CURLOPT_URL, 'http://' . NEO4J_HOST . ':' . NEO4J_PORT . '/db/data/cypher');
+	curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($c, CURLOPT_HTTPHEADER, array(
+	    'Accept: application/json; stream=true',
+	    'Content-type: application/json',
+	    'X-Stream: true'
+	));
+	curl_setopt($c, CURLOPT_CUSTOMREQUEST, 'POST');
+	curl_setopt($c, CURLOPT_POST, false);
+	curl_setopt($c, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+	curl_setopt($c, CURLOPT_USERPWD, NEO4J_USER . ':' . NEO4J_PSW);
+	$dataString = json_encode(array('query' => $query, 'params' => $params));
+	curl_setopt($c, CURLOPT_POSTFIELDS, $dataString);
+	$response = curl_exec($c);
+	if ($response === false) {
+	    jam_log($_SERVER['SCRIPT_NAME'], __LINE__, 'Unable to connect to ' . NEO4J_HOST);
+	    return false;
+	} else {
+	    $data = json_decode($response, true);
+	    $this->data = $data;
+	}
     }
 
     /**
-     * \fn		connect()
+     * \fn	connect()
      * \brief	connect to the database
      * \return	connection or false
      */
     public function connect() {
-		try {
-			$connection = mysqli_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PSW, MYSQL_DB);
-			if (mysqli_connect_errno($connection)) {
-				jam_log(__FILE__, __LINE__, 'Unable to connect to ' . MYSQL_HOST);
-				return false;
-			} else {
-				return $connection;
-			}
-		} catch (Exception $e) {
-			jam_log(__FILE__, __LINE__, 'Unable to connect to ' . MYSQL_HOST);
-			return false;
-		}
+	try {
+	    $connection = mysqli_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PSW, MYSQL_DB);
+	    if (mysqli_connect_errno($connection)) {
+		jam_log(__FILE__, __LINE__, 'Unable to connect to ' . MYSQL_HOST);
+		return false;
+	    } else {
+		return $connection;
+	    }
+	} catch (Exception $e) {
+	    jam_log(__FILE__, __LINE__, 'Unable to connect to ' . MYSQL_HOST);
+	    return false;
+	}
     }
 
     /**
-     * \fn		disconnect($connection)
+     * \fn	disconnect($connection)
      * \brief	disconnet from the database
      * \return	void
      */
     public function disconnect($connection) {
-		@mysqli_close($connection);
+	@mysqli_close($connection);
     }
 
 }
