0ff666839c71b4ca8618bba90bf5b2b6042bc2c9
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-03-04 20:59:18 +0100

    formattazione

--- services/log.service.php
+++ services/log.service.php
@@ -2,7 +2,7 @@
 
 /* ! \par		Info Generali:
  *  \author		Daniele Caldelli
- *  \version            1.0
+ *  \version            0.3
  *  \date		2013
  *  \copyright          Jamyourself.com 2013
  *  \par		Info :
@@ -19,24 +19,34 @@ if (!defined('ROOT_DIR'))
     define('ROOT_DIR', '../');
 require_once ROOT_DIR . 'config.php';
 
-function jam_log($where, $lineError, $error) {
-	if (LOG) {
-		if(!is_dir(LOG_DIR)) {
-			mkdir(LOG_DIR);
-		}
-		$fp = fopen(LOG_DIR . date('Ymd') . '.txt', 'a+');
-		fwrite($fp, '[' . date('Y-m-d H:i:s') . ' | ' . substr($where, strrpos($where, '\\') + 1) . ' (' . $lineError . ')] => ' . $error . "\n");
-		fclose($fp);
+/**
+ * \fn		jamLog($where, $lineError, $error)
+ * \brief	log servive
+ * \return	connection or false
+ */
+function jamLog($where, $lineError, $error) {
+    if (LOG) {
+	if (!is_dir(LOG_DIR)) {
+	    mkdir(LOG_DIR);
 	}
+	$fp = fopen(LOG_DIR . date('Ymd') . '.txt', 'a+');
+	fwrite($fp, '[' . date('Y-m-d H:i:s') . ' | ' . substr($where, strrpos($where, '\\') + 1) . ' (' . $lineError . ')] => ' . $error . "\n");
+	fclose($fp);
+    }
 }
 
-function jam_debug($file, $msg) {
-	if(!is_dir(DEBUG_DIR)) {
-		mkdir(DEBUG_DIR);
-	}
-	$fp = fopen(DEBUG_DIR . $file, 'a+');
-	fwrite($fp, '[' . date('Y-m-d H:i:s') . '] ' . $msg . "\n");
-	fclose($fp);
+/**
+ * \fn		jamDebug($file, $msg)
+ * \brief	debug service
+ * \return	connection or false
+ */
+function jamDebug($file, $msg) {
+    if (!is_dir(DEBUG_DIR)) {
+	mkdir(DEBUG_DIR);
+    }
+    $fp = fopen(DEBUG_DIR . $file, 'a+');
+    fwrite($fp, '[' . date('Y-m-d H:i:s') . '] ' . $msg . "\n");
+    fclose($fp);
 }
 
 ?>
