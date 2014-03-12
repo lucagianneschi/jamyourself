c1aa333455f1a8d77bcf913fa56e5cb8c4efb083
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-03-12 16:36:14 +0100

    avanzamento #98

--- controllers/subscribe.controller.php
+++ controllers/subscribe.controller.php
@@ -1,19 +1,5 @@
 <?php
 
-/* ! \par		Info Generali:
- * @author		Stefano Muscas
- * @version		1.0
- * @since		2013
- * @copyright		Jamyourself.com 2013
- * \par			Info Classe:
- * \brief		controller per subscribe, home, solo per fare BETA
- * \details		invia mail ad indirizzo predefinito per tenere elenco di subscriber
- * \par			Commenti:
- * @warning
- * @bug
- * @todo		Fare APi su Wiki
- *
- */
 if (!defined('ROOT_DIR'))
     define('ROOT_DIR', '../');
 
@@ -24,14 +10,21 @@ require_once LANGUAGES_DIR . 'controllers/' . getLanguage() . '.controllers.lang
 require_once SERVICES_DIR . 'utils.service.php';
 
 /**
- * \brief	SubscribeController class 
- * \details	controller di richiesta di sottoscrizione dalla home
+ * SubscribeController class
+ * effettua sottoscrizione per nuova versione
+ * 
+ * @author Stefano Muscas
+ * @version		0.2
+ * @since		2014-03-12
+ * @copyright		Jamyourself.com 2013	
+ * @warning
+ * @bug
+ * @todo                
  */
 class SubscribeController extends REST {
 
     /**
-     * \brief	subscribe() 
-     * \details	invia mail ad indirizzo predefinito
+     * Invia mail ad indirizzo predefinito
      */
     public function subscribe() {
 	global $controllers;
@@ -50,8 +43,7 @@ class SubscribeController extends REST {
     }
 
     /**
-     * \brief	private function checkEmail($email)
-     * \details	verifica che l'indirizzo inserito sia un indirizzo valido
+     * Verifica coerenza della mail
      */
     private function checkEmail($email) {
 	if (strlen($email) > 50)
