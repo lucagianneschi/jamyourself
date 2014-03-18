a3b67f767306833f425e76ea4d4a3abb3efef3c0
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-03-18 10:28:40 -0400

    aggiornamento #97

--- controllers/playlist.controller.php
+++ controllers/playlist.controller.php
@@ -1,19 +1,5 @@
 <?php
 
-/* ! \par		Info Generali:
- * @author		Luca Gianneschi
- * @version		0.3
- * @since		2013
- * @copyright           Jamyourself.com 2013
- * \par			Info Classe:
- * \brief		controller di gestione della playlist
- * \details		gestisce l'inserimento e la cancellazione di una song dalla tracklist di una playlist
- * \par			Commenti:
- * @warning
- * @bug
- * @todo		 fare API su Wiki, controllare codice removeSong()
- *
- */
 if (!defined('ROOT_DIR'))
     define('ROOT_DIR', '../');
 
@@ -21,18 +7,26 @@ require_once ROOT_DIR . 'config.php';
 require_once SERVICES_DIR . 'lang.service.php';
 require_once LANGUAGES_DIR . 'controllers/' . getLanguage() . '.controllers.lang.php';
 require_once CONTROLLERS_DIR . 'restController.php';
+require_once SERVICES_DIR . 'connection.service.php';
 
 /**
- * \brief	PlaylistController class 
- * \details	controller di gestione playlist
+ * PlaylistController class 
+ * gestisce l'inserimento e la cancellazione di una song dalla tracklist di una playlist
+ * 
+ * @author		Luca Gianneschi
+ * @version		0.2
+ * @since		2014-03-12
+ * @copyright		Jamyourself.com 2013	
+ * @warning
+ * @bug
+ * @todo                
  */
 class PlaylistController extends REST {
 
     public $config;
 
     /**
-     * \fn		construct()
-     * \brief   load config file for the controller
+     * load config file for the controller
      */
     function __construct() {
 	parent::__construct();
@@ -40,33 +34,37 @@ class PlaylistController extends REST {
     }
 
     /**
-     * \fn      addSong()
-     * \brief   add song to playlist
-     * @todo    utilizzare il servizio insert.service per scrivere sul DB
+     * add song to playlist
+     * @todo    prevedere rollback e testare
      */
     public function addSong() {
 	try {
 	    global $controllers;
 	    if ($this->get_request_method() != "POST") {
 		$this->response(array('status' => $controllers['NOPOSTREQUEST']), 405);
-	    } elseif (!isset($_SESSION['currentUser'])) {
+	    } elseif (!isset($_SESSION['id'])) {
 		$this->response(array('status' => $controllers['USERNOSES']), 403);
 	    } elseif (!isset($this->request['songId'])) {
 		$this->response(array('status' => $controllers['NOSONGID']), 403);
-	    } elseif (!isset($_SESSION['playlist'])) {
-		//creare la playlist
+	    } elseif (!isset($this->request['playlistId'])) {
+		$this->response(array('status' => $controllers['NOPLAYLISTID']), 403);
 	    }
-	    $playlistId = $_SESSION['playlist']['id'];
+	    $playlistId = $this->request['playlistId'];
 	    $songId = $this->request['songId'];
-	    $currentUser = $_SESSION['currentUser'];
-	    require_once CLASSES_DIR . 'playlist.class.php';
-	    $playlist = selectPlaylists($playlistId);
-	    if ($playlist instanceof Error) {
-		$this->response(array('status' => $controllers['NOPLAYLIST']), 503);
-	    } elseif (in_array($songId, $playlist->getSongsArray())) {
-		$this->response(array('status' => $controllers['SONGALREADYINTRACKLIST']), 503);
+	    $currentUser = $_SESSION['id'];
+	    $connectionService = new ConnectionService();
+	    $connection = $connectionService->connect();
+	    if ($connection === false) {
+		$this->response(array('status' => $controllers['CONNECTION ERROR']), 403);
 	    }
-
+	    $song = insertSongInPlayslist($connection, $songId, $playlistId);
+	    if (!$song) {
+		$this->response(array('status' => $controllers['POSTERROR']), 503);
+	    }
+	    $relation = createRelation($connection, 'user', $currentUser, 'song', $songId, 'ADDTOPLAYLIST');
+	    if (!$relation) {
+		$this->response(array('status' => $controllers['NODEERROR']), 503);
+	    }
 	    $this->response(array($controllers['SONGADDEDTOPLAYLIST']), 200);
 	} catch (Exception $e) {
 	    $this->response(array('status' => $e->getMessage()), 503);
@@ -74,8 +72,7 @@ class PlaylistController extends REST {
     }
 
     /**
-     * \fn	removeSong()
-     * \brief   remove song to playlist 
+     * remove song to playlist 
      * @todo   
      */
     public function removeSong() {
