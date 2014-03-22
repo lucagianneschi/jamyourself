567c888aa366a1f61482c5fca4f49a40c0f223fd
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-03-21 21:32:58 -0400

    avanzamento codice #97

--- controllers/player.controller.php
+++ controllers/player.controller.php
@@ -1,19 +1,5 @@
 <?php
 
-/* ! \par		Info Generali:
- * \author		Luca Gianneschi
- * \version		1.0
- * \date		2013
- * \copyright           Jamyourself.com 2013
- * \par			Info Classe:
- * \brief		controller di gestione del player
- * \details		gestisce la creazione dell'activity legata al player
- * \par			Commenti:
- * \warning
- * \bug
- * \todo		 completare ed implementare
- *
- */
 if (!defined('ROOT_DIR'))
     define('ROOT_DIR', '../');
 
@@ -21,32 +7,48 @@ require_once ROOT_DIR . 'config.php';
 require_once SERVICES_DIR . 'lang.service.php';
 require_once LANGUAGES_DIR . 'controllers/' . getLanguage() . '.controllers.lang.php';
 require_once CONTROLLERS_DIR . 'restController.php';
- 
+require_once SERVICES_DIR . 'insert.service.php';
+/**
+ * PlayerController class
+ * controller di inserimento commenti
+ * 
+ * @author		Luca Gianneschi
+ * @version		0.2
+ * @since		2013
+ * @copyright		Jamyourself.com 2013	
+ * @warning
+ * @bug
+ * @todo                
+ */
 
 /**
- * \brief	PlayerController class 
- * \details	controller del player musicale
+ * controller del player musicale
  */
 class PlayerController extends REST {
 
     /**
-     * \fn      play()
-     * \brief   add song to playlist
-     * \todo    
+     * add song to playlist  
      */
     public function play() {
 	try {
 	    global $controllers;
 	    if ($this->get_request_method() != "POST") {
 		$this->response(array('status' => $controllers['NOPOSTREQUEST']), 405);
-	    } elseif (!isset($this->request['currentUser'])) {
+	    } elseif (!isset($_SESSION['id'])) {
+		$this->response(array('status' => $controllers['USERNOSES']), 403);
+	    } elseif (!isset($this->request['id'])) {
 		$this->response(array('status' => $controllers['USERNOSES']), 403);
 	    } elseif (!isset($this->request['songId'])) {
 		$this->response(array('status' => $controllers['NOSONGID']), 403);
 	    }
-	    
-	    
-	    
+	    $songId = $this->request['id'];
+	    $connectionService = new ConnectionService();
+	    $connection = $connectionService->connect();
+	    $relation = createRelation($connection, 'user', $_SESSION['id'], 'song', $songId, 'PLAY');
+	    if (!$relation) {
+		$this->response(array('status' => $controllers['NODEERROR']), 503);
+	    }
+	    $connectionService->disconnect($connection);
 	    $this->response(array($controllers['SONGPLAYED']), 200);
 	} catch (Exception $e) {
 	    $this->response(array('status' => $e->getMessage()), 503);
