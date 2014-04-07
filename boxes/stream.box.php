8036b323c65ed1d6420f2ef68956b4bbd93d302c
Daniele Caldelli <daniele.caldelli@gmail.com>
2014-04-07 17:58:54 +0200

    Modificata la query affinchè sia ritornato anche il flag che indica se lo user ha fatto LOVE

--- boxes/stream.box.php
+++ boxes/stream.box.php
@@ -40,37 +40,64 @@ class StreamBox {
      * @todo
      */
     public function init($limit = DEFAULTQUERY) {
-	$currentUser = $_SESSION['id'];
+    $startTimer = microtime();
+    $currentUser = $_SESSION['id'];
 	$connectionService = new ConnectionService();
 	$query = "
-           MATCH (n1:user)-[r1]->(n2:user)-[r2]->(n3)
-           WHERE n1.id = {currentUser}
-             AND TYPE(r2) IN ['ADD', 'CREATE', 'POST']
-             AND TYPE(r1) IN ['COLLABORATION']
-          RETURN LABELS(n3), n3.id
-        ORDER BY r2.createdat DESC
-           LIMIT " . $limit;
+                 MATCH (n1:user)-[r1]->(n2:user)-[r2]->(n3)
+                 WHERE n1.id = {currentUser}
+                   AND TYPE(r2) IN ['ADD', 'CREATE', 'POST']
+                   AND TYPE(r1) IN ['COLLABORATION']
+        OPTIONAL MATCH (n1)-[r3:LOVE]->(n3)
+                RETURN LABELS(n3), n3.id, CASE WHEN r3 IS NULL THEN 0 ELSE 1 END
+              ORDER BY r2.createdat DESC
+                 LIMIT " . $limit;
 	$params = array(
-	    'currentUser' => $currentUser
+	    'currentUser' => (int)$currentUser
 	);
-	$res = $connectionService->curl($query, $params);
-	if ($res === false) {
-	    $this->error = 'Unable to execute query';
+    $res = $connectionService->curl($query, $params);
+    if ($res === false) {
+	    $endTimer = microtime();
+        jamLog(__FILE__, __LINE__, ' [Execution time: ' . executionTime($startTimer, $endTimer) . '] Unable to execute query => ' . $query);
+        $this->error = 'Unable to execute query';
 	} else {
-	    $couples = array();
+        $couples = array();
 	    $connection = $connectionService->connect();
 	    foreach ($res['data'] as $couple) {
-		$class = $couple[0][0];
-		$id = $couple[1];
-		switch ($class) {
-		    case 'comment':
-			$comments = selectComments($connection, $id);
-			$object = $comments[$id];
-			break;
-		}
-		$couples[] = array('class' => $class, 'id' => $id, 'object' => $object);
+            $class = $couple[0][0];
+            $id = $couple[1];
+            switch ($class) {
+                case 'album':
+                    $albums = selectAlbums($connection, $id);
+                    $object = $albums[$id];
+                    break;
+                case 'comment':
+                    $comments = selectComments($connection, $id);
+                    $object = $comments[$id];
+                    break;
+                case 'image':
+                    $images = selectImages($connection, $id);
+                    $object = $images[$id];
+                    break;
+                case 'record':
+                    $records = selectRecords($connection, $id);
+                    $object = $records[$id];
+                    break;
+                case 'song':
+                    $songs = selectSongs($connection, $id);
+                    $object = $songs[$id];
+                    break;
+                case 'video':
+                    $videos = selectVideos($connection, $id);
+                    $object = $videos[$id];
+                    break;
+            }
+            $love = $couple[2];
+            $couples[] = array('class' => $class, 'id' => $id, 'love' => $love, 'object' => $object);
 	    }
-	    $this->activitiesArray = $couples;
+	    $endTimer = microtime();
+        jamLog(__FILE__, __LINE__, ' [Execution time: ' . executionTime($startTimer, $endTimer) . '] ' . count($couples) . ' rows returned');
+        $this->activitiesArray = $couples;
 	}
     }
 
