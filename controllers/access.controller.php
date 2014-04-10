d94c2f8554d12ca644e19708d1edeff11996f090
Daniele Caldelli <daniele.caldelli@gmail.com>
2014-04-10 15:33:05 +0200

    Corretto il reindirizzamento del logout da stream a index (senza apertura automatica del form login)

--- controllers/access.controller.php
+++ controllers/access.controller.php
@@ -9,6 +9,7 @@ require_once LANGUAGES_DIR . 'controllers/' . getLanguage() . '.controllers.lang
 require_once CONTROLLERS_DIR . 'restController.php';
 require_once CLASSES_DIR . 'user.class.php';
 require_once SERVICES_DIR . 'connection.service.php';
+require_once SERVICES_DIR . 'utils.service.php';
 
 /**
  * AccessController class
@@ -30,43 +31,43 @@ class AccessController extends REST {
      * @todo    testare e attivare la password criptata, riga 50
      */
     public function login() {
-	try {
-	    global $controllers;
-	    if ($_SESSION['id'] && $_SESSION['username']) {
-		#TODO
-		//			$this -> response(array('status' => $controllers['ALREADYLOGGEDIND']), 405);
-	    }
-	    if ($this->get_request_method() != "POST") {
-		$this->response(array('status' => $controllers['NOPOSTREQUEST']), 405);
-	    }
-	    if (!isset($this->request['usernameOrEmail']) || !isset($this->request['password'])) {
-		$this->response(array('status' => $controllers['NO CREDENTIALS']), 405);
-	    }
-	    $connectionService = new ConnectionService();
-	    $connection = $connectionService->connect();
-	    if ($connection != false) {
-		$name = mysqli_real_escape_string($connection, stripslashes($this->request['usernameOrEmail']));
-		$password = mysqli_real_escape_string($connection, stripslashes($this->request['password']));
-		//$encriptedPassword = passwordEncryption($password);
-		$user = $this->checkEmailOrUsername($connection, $password, $name);
-		if (!$user) {
-		    $this->response(array('status' => 'INVALID CREDENTIALS'), 503);
-		}
-		$_SESSION['id'] = $user->getId();
-		$_SESSION['username'] = $user->getUsername();
-		$_SESSION['type'] = $user->getType();
-		$_SESSION['settings'] = $user->getSettings();
-		$_SESSION['levelvalue'] = $user->getLevelvalue();
-		$_SESSION['premium'] = $user->getPremium();
-		$_SESSION['premiumexpirationdate'] = $user->getPremiumexpirationdate();
-		$connectionService->disconnect($connection);
-		$this->response(array('status' => $controllers['OKLOGIN']), 200);
-	    }
-	    else
-		$this->response(array('status' => 'ERROR CONNECT'), 503);
-	} catch (Exception $e) {
-	    $this->response(array('status' => $e->getMessage()), 503);
-	}
+        try {
+            global $controllers;
+            if ($_SESSION['id'] && $_SESSION['username']) {
+                #TODO
+                //$this -> response(array('status' => $controllers['ALREADYLOGGEDIND']), 405);
+            }
+            if ($this->get_request_method() != "POST") {
+                $this->response(array('status' => $controllers['NOPOSTREQUEST']), 405);
+            }
+            if (!isset($this->request['usernameOrEmail']) || !isset($this->request['password'])) {
+                $this->response(array('status' => $controllers['NO CREDENTIALS']), 405);
+            }
+            $connectionService = new ConnectionService();
+            $connection = $connectionService->connect();
+            if ($connection != false) {
+                $name = mysqli_real_escape_string($connection, stripslashes($this->request['usernameOrEmail']));
+                $password = mysqli_real_escape_string($connection, stripslashes($this->request['password']));
+                //$encriptedPassword = passwordEncryption($password);
+                $user = $this->checkEmailOrUsername($connection, $password, $name);
+                if (!$user) {
+                    $this->response(array('status' => 'INVALID CREDENTIALS'), 503);
+                }
+                $_SESSION['id'] = $user->getId();
+                $_SESSION['username'] = $user->getUsername();
+                $_SESSION['type'] = $user->getType();
+                $_SESSION['settings'] = $user->getSettings();
+                $_SESSION['levelvalue'] = $user->getLevelvalue();
+                $_SESSION['premium'] = $user->getPremium();
+                $_SESSION['premiumexpirationdate'] = $user->getPremiumexpirationdate();
+                $connectionService->disconnect($connection);
+                $this->response(array('status' => $controllers['OKLOGIN']), 200);
+            } else {
+                $this->response(array('status' => 'ERROR CONNECT'), 503);
+            }
+        } catch (Exception $e) {
+            $this->response(array('status' => $e->getMessage()), 503);
+        }
     }
 
     /**
@@ -74,19 +75,28 @@ class AccessController extends REST {
      * @todo    testare
      */
     public function logout() {
-	try {
-	    global $controllers;
-	    if ($this->get_request_method() != "POST") {
-		$this->response(array('status' => $controllers['NOPOSTREQUEST']), 405);
-	    } elseif (!isset($_SESSION['id']) || !isset($_SESSION['username'])) {
-		$this->response(array('status' => $controllers['USERNOSES']), 403);
-	    }
-	    session_unset();
-	    session_destroy();
-	    $this->response(array('status' => $controllers['OKLOGOUT']), 200);
-	} catch (Exception $e) {
-	    $this->response(array('status' => $e->getMessage()), 503);
-	}
+        $startTimer = microtime();
+        try {
+            global $controllers;
+            if ($this->get_request_method() != "POST") {
+                $endTimer = microtime();
+                jamLog(__FILE__, __LINE__, '[Execution time: ' . executionTime($startTimer, $endTimer) . '] Error during User logout "No POST action"');
+                $this->response(array('status' => $controllers['NOPOSTREQUEST']), 405);
+            } elseif (!isset($_SESSION['id']) || !isset($_SESSION['username'])) {
+                $endTimer = microtime();
+                jamLog(__FILE__, __LINE__, '[Execution time: ' . executionTime($startTimer, $endTimer) . '] Error during User logout "No User in session"');
+                $this->response(array('status' => $controllers['USERNOSES']), 403);
+            }
+            session_unset();
+            session_destroy();
+            $endTimer = microtime();
+            jamLog(__FILE__, __LINE__, '[Execution time: ' . executionTime($startTimer, $endTimer) . '] User logout');
+            $this->response(array('status' => $controllers['OKLOGOUT']), 200);
+        } catch (Exception $e) {
+            $endTimer = microtime();
+            jamLog(__FILE__, __LINE__, '[Execution time: ' . executionTime($startTimer, $endTimer) . '] Error during User logout');
+            $this->response(array('status' => $e->getMessage()), 503);
+        }
     }
 
     /**
