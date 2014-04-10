1d575b43d038401236c4560f3ffbf1dd338cb3d2
Daniele Caldelli <daniele.caldelli@gmail.com>
2014-04-10 17:29:12 +0200

    Modificata una info

--- views/resources/javascripts/customs/love.js
+++ views/resources/javascripts/customs/love.js
@@ -1,4 +1,4 @@
-function love(_this, classType, id, objectIdUser) {
+function love(_this, classType, id, idUser) {
     var json_love = {};
     typeOpt = $(_this).text();
     switch (typeOpt) {
@@ -11,36 +11,36 @@ function love(_this, classType, id, objectIdUser) {
     }
     json_love.classType = classType;
     json_love.id = id;
-    json_love.objectIdUser = objectIdUser;
+    json_love.idUser = idUser;
 
     $.ajax({
-	type: "POST",
-	url: "../../../controllers/request/loveRequest.php",
-	data: json_love
+        type: "POST",
+        url: "../../../controllers/request/loveRequest.php",
+        data: json_love
     })
-	    .done(function(response, status, xhr) {
-	number_love = $.parseJSON(xhr.responseText).status;
-	if (typeOpt == 'Love') {
-	    parent = $(_this).parent().parent();
-	    objectLove = $(parent).find("a._unlove");
-	    $(objectLove).toggleClass('orange grey');
-	    $(objectLove).text(number_love);
-	    $(objectLove).toggleClass('_love _unlove');
-	    $(_this).text('Unlove');
-	} else {
-	    parent = $(_this).parent().parent();
-	    objectLove = $(parent).find("a._love");
-	    $(objectLove).toggleClass('grey orange');
-	    $(objectLove).text(number_love);
-	    $(objectLove).toggleClass('_unlove _love');
-	    $(_this).text('Love');
-	}
-	code = xhr.status;
-	console.log("Code: " + code + " | Message: " + number_love);
-    })
-	    .fail(function(xhr) {
-	message = $.parseJSON(xhr.responseText).status;
-	code = xhr.status;
-	console.log("Code: " + code + " | Message: " + message);
+    .done(function(response, status, xhr) {
+        number_love = $.parseJSON(xhr.responseText).status;
+        if (typeOpt == 'Love') {
+            parent = $(_this).parent().parent();
+            objectLove = $(parent).find("a._unlove");
+            $(objectLove).toggleClass('orange grey');
+            $(objectLove).text(number_love);
+            $(objectLove).toggleClass('_love _unlove');
+            $(_this).text('Unlove');
+        } else {
+            parent = $(_this).parent().parent();
+            objectLove = $(parent).find("a._love");
+            $(objectLove).toggleClass('grey orange');
+            $(objectLove).text(number_love);
+            $(objectLove).toggleClass('_unlove _love');
+            $(_this).text('Love');
+        }
+        code = xhr.status;
+        console.log("Code: " + code + " | Message: " + number_love);
+        })
+    .fail(function(xhr) {
+        message = $.parseJSON(xhr.responseText).status;
+        code = xhr.status;
+        console.log("Code: " + code + " | Message: " + message);
     });
 }
