ec7dfbfaaf5bf2051237345efbcb77b1f7980851
Stefano <stefanom@jamyourself.com>
2014-04-23 15:21:56 +0200

    rimosso check dell'id per la pagina di registrazione

--- services/session.service.php
+++ services/session.service.php
@@ -19,7 +19,7 @@ require_once ROOT_DIR . 'config.php';
 if (session_id() == '')
     session_start();
 
-if (!isset($_SESSION['id']) && basename($_SERVER['PHP_SELF']) != 'index.php') {
+if (!isset($_SESSION['id']) && basename($_SERVER['PHP_SELF']) != 'index.php' && basename($_SERVER['PHP_SELF']) != 'signup.php') {
     header('Location: ' . ROOT_DIR . 'index.php?login');
 } elseif (isset($_SESSION['id']) && basename($_SERVER['PHP_SELF']) == 'index.php') {
     header('Location: ' . VIEWS_DIR . 'stream.php');
